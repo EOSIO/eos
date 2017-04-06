7584ca2f927cb4179b8b02e9ca90884b661750dc
Nathan Hourt <nat.hourt@gmail.com>
2017-04-06 17:49:27 -0500

    Remove cruft, rename BitShares -> Eos

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -107,7 +107,7 @@ namespace detail {
 
       void reset_p2p_node(const fc::path& data_dir)
       { try {
-         _p2p_network = std::make_shared<net::node>("BitShares Reference Implementation");
+         _p2p_network = std::make_shared<net::node>("Eos Reference Implementation");
 
          _p2p_network->load_configuration(data_dir / "p2p");
          _p2p_network->set_node_delegate(this);
