0175c3087c2f1422bb786c14d057c6dad6a20c21
Nathan Hourt <nat.hourt@gmail.com>
2017-04-26 11:16:46 -0500

    Add a template for plugins
    This is just an empty template plugin which will serve as a starting
    point for new plugins.
    
    Also add a helper script, make_new_plugin.sh, which copies the template
    plugin to create your new plugin and automatically renames all of the
    files/directories/classes/binaries/whatever. Invoke like so:
    ./make_new_plugin.sh new_plugin
    Afterwards, the new_plugin folder will be ready to go.

---
+++ plugins/template_plugin/template_plugin.cpp
@@ -0,0 +1,31 @@
+#include <eos/template_plugin/template_plugin.hpp>
+
+namespace eos {
+   class template_plugin_impl {
+      public:
+   };
+
+   template_plugin::template_plugin():my(new template_plugin_impl()){}
+   template_plugin::~template_plugin(){}
+
+   void template_plugin::set_program_options(options_description&, options_description& cfg) {
+      cfg.add_options()
+            ("option-name", bpo::value<string>()->default_value("default value"),
+             "Option Description")
+            ;
+   }
+
+   void template_plugin::plugin_initialize(const variables_map& options) {
+      if(options.count("option-name")) {
+         // Handle the option
+      }
+   }
+
+   void template_plugin::plugin_startup() {
+      // Make the magic happen
+   }
+
+   void template_plugin::plugin_shutdown() {
+      // OK, that's enough magic
+   }
+}
