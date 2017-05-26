6c2e2dcbdebdddfb305dbd8abf240a97a8b6360e
Nathan Hourt <nat.hourt@gmail.com>
2017-05-26 10:11:34 -0500

    Whitespace tweaks to template_plugin

--- plugins/template_plugin/template_plugin.cpp
+++ plugins/template_plugin/template_plugin.cpp
@@ -1,31 +1,33 @@
 #include <eos/template_plugin/template_plugin.hpp>
 
 namespace eos {
-   class template_plugin_impl {
-      public:
-   };
 
-   template_plugin::template_plugin():my(new template_plugin_impl()){}
-   template_plugin::~template_plugin(){}
+class template_plugin_impl {
+   public:
+};
 
-   void template_plugin::set_program_options(options_description&, options_description& cfg) {
-      cfg.add_options()
-            ("option-name", bpo::value<string>()->default_value("default value"),
-             "Option Description")
-            ;
+template_plugin::template_plugin():my(new template_plugin_impl()){}
+template_plugin::~template_plugin(){}
+
+void template_plugin::set_program_options(options_description&, options_description& cfg) {
+   cfg.add_options()
+         ("option-name", bpo::value<string>()->default_value("default value"),
+          "Option Description")
+         ;
+}
+
+void template_plugin::plugin_initialize(const variables_map& options) {
+   if(options.count("option-name")) {
+      // Handle the option
    }
+}
 
-   void template_plugin::plugin_initialize(const variables_map& options) {
-      if(options.count("option-name")) {
-         // Handle the option
-      }
-   }
+void template_plugin::plugin_startup() {
+   // Make the magic happen
+}
 
-   void template_plugin::plugin_startup() {
-      // Make the magic happen
-   }
+void template_plugin::plugin_shutdown() {
+   // OK, that's enough magic
+}
 
-   void template_plugin::plugin_shutdown() {
-      // OK, that's enough magic
-   }
 }
