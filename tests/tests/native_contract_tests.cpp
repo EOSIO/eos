528fedf818da3494c9f0c9fd91f7a81b49672ca1
Nathan Hourt <nat.hourt@gmail.com>
2017-06-28 11:40:09 -0500

    Reorganize tests

---
+++ tests/tests/native_contract_tests.cpp
@@ -0,0 +1,392 @@
+#include <boost/test/unit_test.hpp>
+
+#include <eos/chain/chain_controller.hpp>
+#include <eos/chain/exceptions.hpp>
+#include <eos/chain/account_object.hpp>
+#include <eos/chain/key_value_object.hpp>
+
+#include <eos/native_contract/producer_objects.hpp>
+
+#include <eos/utilities/tempdir.hpp>
+
+#include <fc/crypto/digest.hpp>
+
+#include <boost/test/unit_test.hpp>
+#include <boost/range/algorithm/find.hpp>
+#include <boost/range/algorithm/find_if.hpp>
+#include <boost/range/algorithm/permutation.hpp>
+
+#include "../common/database_fixture.hpp"
+
+using namespace eos;
+using namespace chain;
+
+BOOST_AUTO_TEST_SUITE(system_contract_tests)
+
+//Simple test of account creation
+BOOST_FIXTURE_TEST_CASE(create_account, testing_fixture)
+{ try {
+      Make_Database(db);
+      db.produce_blocks(10);
+
+      BOOST_CHECK_EQUAL(db.get_liquid_balance("init1"), Asset(100000));
+
+      Make_Account(db, joe, init1, Asset(1000));
+
+      { // test in the pending state
+         BOOST_CHECK_EQUAL(db.get_liquid_balance("joe"), Asset(1000));
+         BOOST_CHECK_EQUAL(db.get_liquid_balance("init1"), Asset(100000 - 1000));
+
+         const auto& joe_owner_authority = db_db.get<permission_object, by_owner>(boost::make_tuple("joe", "owner"));
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.threshold, 1);
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.accounts.size(), 0);
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.keys.size(), 1);
+         BOOST_CHECK_EQUAL(string(joe_owner_authority.auth.keys[0].key), string(joe_public_key));
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.keys[0].weight, 1);
+
+         const auto& joe_active_authority = db_db.get<permission_object, by_owner>(boost::make_tuple("joe", "active"));
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.threshold, 1);
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.accounts.size(), 0);
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.keys.size(), 1);
+         BOOST_CHECK_EQUAL(string(joe_active_authority.auth.keys[0].key), string(joe_public_key));
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.keys[0].weight, 1);
+      }
+
+      db.produce_blocks(1); /// verify changes survived creating a new block
+      {
+         BOOST_CHECK_EQUAL(db.get_liquid_balance("joe"), Asset(1000));
+         BOOST_CHECK_EQUAL(db.get_liquid_balance("init1"), Asset(100000 - 1000));
+
+         const auto& joe_owner_authority = db_db.get<permission_object, by_owner>(boost::make_tuple("joe", "owner"));
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.threshold, 1);
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.accounts.size(), 0);
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.keys.size(), 1);
+         BOOST_CHECK_EQUAL(string(joe_owner_authority.auth.keys[0].key), string(joe_public_key));
+         BOOST_CHECK_EQUAL(joe_owner_authority.auth.keys[0].weight, 1);
+
+         const auto& joe_active_authority = db_db.get<permission_object, by_owner>(boost::make_tuple("joe", "active"));
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.threshold, 1);
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.accounts.size(), 0);
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.keys.size(), 1);
+         BOOST_CHECK_EQUAL(string(joe_active_authority.auth.keys[0].key), string(joe_public_key));
+         BOOST_CHECK_EQUAL(joe_active_authority.auth.keys[0].weight, 1);
+      }
+} FC_LOG_AND_RETHROW() }
+
+// Simple test to verify a simple transfer transaction works
+BOOST_FIXTURE_TEST_CASE(transfer, testing_fixture)
+{ try {
+      Make_Database(db)
+
+      BOOST_CHECK_EQUAL(db.head_block_num(), 0);
+      db.produce_blocks(10);
+      BOOST_CHECK_EQUAL(db.head_block_num(), 10);
+
+      SignedTransaction trx;
+      BOOST_REQUIRE_THROW(db.push_transaction(trx), transaction_exception); // no messages
+      trx.messages.resize(1);
+      trx.set_reference_block(db.head_block_id());
+      trx.expiration = db.head_block_time() + 100;
+      trx.messages[0].sender = "init1";
+      trx.messages[0].recipient = config::EosContractName;
+
+      types::Transfer trans = { "init1", "init2", Asset(100), "transfer 100" };
+
+      UInt64 value(5);
+      auto packed = fc::raw::pack(value);
+      auto unpacked = fc::raw::unpack<UInt64>(packed);
+      BOOST_CHECK_EQUAL( value, unpacked );
+      trx.messages[0].type = "Transfer";
+      trx.setMessage(0, "Transfer", trans);
+
+      auto unpack_trans = trx.messageAs<types::Transfer>(0);
+
+      BOOST_REQUIRE_THROW(db.push_transaction(trx), message_validate_exception); // "fail to notify receiver, init2"
+      trx.messages[0].notify = {"init2"};
+      trx.setMessage(0, "Transfer", trans);
+      db.push_transaction(trx);
+
+      BOOST_CHECK_EQUAL(db.get_liquid_balance("init1"), Asset(100000 - 100));
+      BOOST_CHECK_EQUAL(db.get_liquid_balance("init2"), Asset(100000 + 100));
+      db.produce_blocks(1);
+
+      BOOST_REQUIRE_THROW(db.push_transaction(trx), transaction_exception); // not unique
+
+      Transfer_Asset(db, init2, init1, Asset(100));
+      BOOST_CHECK_EQUAL(db.get_liquid_balance("init1"), Asset(100000));
+      BOOST_CHECK_EQUAL(db.get_liquid_balance("init2"), Asset(100000));
+} FC_LOG_AND_RETHROW() }
+
+// Simple test of creating/updating a new block producer
+BOOST_FIXTURE_TEST_CASE(producer_creation, testing_fixture)
+{ try {
+      Make_Database(db)
+      db.produce_blocks();
+      BOOST_CHECK_EQUAL(db.head_block_num(), 1);
+
+      Make_Account(db, producer);
+      Make_Producer(db, producer, producer_public_key);
+
+      while (db.head_block_num() < 3) {
+         auto& producer = db.get_producer("producer");
+         BOOST_CHECK_EQUAL(db.get_producer(producer.owner).owner, "producer");
+         BOOST_CHECK_EQUAL(producer.signing_key, producer_public_key);
+         BOOST_CHECK_EQUAL(producer.last_aslot, 0);
+         BOOST_CHECK_EQUAL(producer.total_missed, 0);
+         BOOST_CHECK_EQUAL(producer.last_confirmed_block_num, 0);
+         db.produce_blocks();
+      }
+
+      Make_Key(signing);
+      Update_Producer(db, "producer", signing_public_key);
+      auto& producer = db.get_producer("producer");
+      BOOST_CHECK_EQUAL(producer.signing_key, signing_public_key);
+} FC_LOG_AND_RETHROW() }
+
+// Test producer votes on blockchain parameters in full blockchain context
+BOOST_FIXTURE_TEST_CASE(producer_voting_parameters, testing_fixture)
+{ try {
+      Make_Database(db)
+      db.produce_blocks(21);
+
+      vector<BlockchainConfiguration> votes{
+         {1024  , 512   , 4096  , Asset(5000   ).amount, Asset(4000   ).amount, Asset(100  ).amount, 512   },
+         {10000 , 100   , 4096  , Asset(3333   ).amount, Asset(27109  ).amount, Asset(10   ).amount, 100   },
+         {2048  , 1500  , 1000  , Asset(5432   ).amount, Asset(2000   ).amount, Asset(50   ).amount, 1500  },
+         {100   , 25    , 1024  , Asset(90000  ).amount, Asset(0      ).amount, Asset(433  ).amount, 25    },
+         {1024  , 1000  , 100   , Asset(10     ).amount, Asset(50     ).amount, Asset(200  ).amount, 1000  },
+         {420   , 400   , 2710  , Asset(27599  ).amount, Asset(1177   ).amount, Asset(27720).amount, 400   },
+         {271   , 200   , 66629 , Asset(2666   ).amount, Asset(99991  ).amount, Asset(277  ).amount, 200   },
+         {1057  , 1000  , 2770  , Asset(972    ).amount, Asset(302716 ).amount, Asset(578  ).amount, 1000  },
+         {9926  , 27    , 990   , Asset(99999  ).amount, Asset(39651  ).amount, Asset(4402 ).amount, 27    },
+         {1005  , 1000  , 1917  , Asset(937111 ).amount, Asset(2734   ).amount, Asset(1    ).amount, 1000  },
+         {80    , 70    , 5726  , Asset(63920  ).amount, Asset(231561 ).amount, Asset(27100).amount, 70    },
+         {471617, 333333, 100   , Asset(2666   ).amount, Asset(2650   ).amount, Asset(2772 ).amount, 333333},
+         {2222  , 1000  , 100   , Asset(33619  ).amount, Asset(1046   ).amount, Asset(10577).amount, 1000  },
+         {8     , 7     , 100   , Asset(5757267).amount, Asset(2257   ).amount, Asset(2888 ).amount, 7     },
+         {2717  , 2000  , 57797 , Asset(3366   ).amount, Asset(205    ).amount, Asset(4472 ).amount, 2000  },
+         {9997  , 5000  , 27700 , Asset(29199  ).amount, Asset(100    ).amount, Asset(221  ).amount, 5000  },
+         {163900, 200   , 882   , Asset(100    ).amount, Asset(5720233).amount, Asset(105  ).amount, 200   },
+         {728   , 80    , 27100 , Asset(28888  ).amount, Asset(6205   ).amount, Asset(5011 ).amount, 80    },
+         {91937 , 44444 , 652589, Asset(87612  ).amount, Asset(123    ).amount, Asset(2044 ).amount, 44444 },
+         {171   , 96    , 123456, Asset(8402   ).amount, Asset(321    ).amount, Asset(816  ).amount, 96    },
+         {17177 , 6767  , 654321, Asset(9926   ).amount, Asset(9264   ).amount, Asset(8196 ).amount, 6767  },
+      };
+      BlockchainConfiguration medians =
+         {1057, 512, 2770, Asset(9926).amount, Asset(2650).amount, Asset(816).amount, 512};
+      // If this fails, the medians variable probably needs to be updated to have the medians of the votes above
+      BOOST_REQUIRE_EQUAL(BlockchainConfiguration::get_median_values(votes), medians);
+
+      for (int i = 0; i < votes.size(); ++i) {
+         auto name = std::string("init") + fc::to_string(i);
+         Update_Producer(db, name, db.get_producer(name).signing_key, votes[i]);
+      }
+
+      BOOST_CHECK_NE(db.get_global_properties().configuration, medians);
+      db.produce_blocks(20);
+      BOOST_CHECK_NE(db.get_global_properties().configuration, medians);
+      db.produce_blocks();
+      BOOST_CHECK_EQUAL(db.get_global_properties().configuration, medians);
+} FC_LOG_AND_RETHROW() }
+
+// Test producer votes on blockchain parameters in full blockchain context, with missed blocks
+BOOST_FIXTURE_TEST_CASE(producer_voting_parameters_2, testing_fixture)
+{ try {
+      Make_Database(db)
+      db.produce_blocks(21);
+
+      vector<BlockchainConfiguration> votes{
+         {1024  , 512   , 4096  , Asset(5000   ).amount, Asset(4000   ).amount, Asset(100  ).amount, 512   },
+         {10000 , 100   , 4096  , Asset(3333   ).amount, Asset(27109  ).amount, Asset(10   ).amount, 100   },
+         {2048  , 1500  , 1000  , Asset(5432   ).amount, Asset(2000   ).amount, Asset(50   ).amount, 1500  },
+         {100   , 25    , 1024  , Asset(90000  ).amount, Asset(0      ).amount, Asset(433  ).amount, 25    },
+         {1024  , 1000  , 100   , Asset(10     ).amount, Asset(50     ).amount, Asset(200  ).amount, 1000  },
+         {420   , 400   , 2710  , Asset(27599  ).amount, Asset(1177   ).amount, Asset(27720).amount, 400   },
+         {271   , 200   , 66629 , Asset(2666   ).amount, Asset(99991  ).amount, Asset(277  ).amount, 200   },
+         {1057  , 1000  , 2770  , Asset(972    ).amount, Asset(302716 ).amount, Asset(578  ).amount, 1000  },
+         {9926  , 27    , 990   , Asset(99999  ).amount, Asset(39651  ).amount, Asset(4402 ).amount, 27    },
+         {1005  , 1000  , 1917  , Asset(937111 ).amount, Asset(2734   ).amount, Asset(1    ).amount, 1000  },
+         {80    , 70    , 5726  , Asset(63920  ).amount, Asset(231561 ).amount, Asset(27100).amount, 70    },
+         {471617, 333333, 100   , Asset(2666   ).amount, Asset(2650   ).amount, Asset(2772 ).amount, 333333},
+         {2222  , 1000  , 100   , Asset(33619  ).amount, Asset(1046   ).amount, Asset(10577).amount, 1000  },
+         {8     , 7     , 100   , Asset(5757267).amount, Asset(2257   ).amount, Asset(2888 ).amount, 7     },
+         {2717  , 2000  , 57797 , Asset(3366   ).amount, Asset(205    ).amount, Asset(4472 ).amount, 2000  },
+         {9997  , 5000  , 27700 , Asset(29199  ).amount, Asset(100    ).amount, Asset(221  ).amount, 5000  },
+         {163900, 200   , 882   , Asset(100    ).amount, Asset(5720233).amount, Asset(105  ).amount, 200   },
+         {728   , 80    , 27100 , Asset(28888  ).amount, Asset(6205   ).amount, Asset(5011 ).amount, 80    },
+         {91937 , 44444 , 652589, Asset(87612  ).amount, Asset(123    ).amount, Asset(2044 ).amount, 44444 },
+         {171   , 96    , 123456, Asset(8402   ).amount, Asset(321    ).amount, Asset(816  ).amount, 96    },
+         {17177 , 6767  , 654321, Asset(9926   ).amount, Asset(9264   ).amount, Asset(8196 ).amount, 6767  },
+      };
+      BlockchainConfiguration medians =
+         {1057, 512, 2770, Asset(9926).amount, Asset(2650).amount, Asset(816).amount, 512};
+      // If this fails, the medians variable probably needs to be updated to have the medians of the votes above
+      BOOST_REQUIRE_EQUAL(BlockchainConfiguration::get_median_values(votes), medians);
+
+      for (int i = 0; i < votes.size(); ++i) {
+         auto name = std::string("init") + fc::to_string(i);
+         Update_Producer(db, name, db.get_producer(name).signing_key, votes[i]);
+      }
+
+      BOOST_CHECK_NE(db.get_global_properties().configuration, medians);
+      db.produce_blocks(2);
+      db.produce_blocks(18, 5);
+      BOOST_CHECK_NE(db.get_global_properties().configuration, medians);
+      db.produce_blocks();
+      BOOST_CHECK_EQUAL(db.get_global_properties().configuration, medians);
+} FC_LOG_AND_RETHROW() }
+
+// Test that if I create a producer and vote for him, he gets in on the next round (but not before)
+BOOST_FIXTURE_TEST_CASE(producer_voting_1, testing_fixture) {
+   try {
+      Make_Database(db)
+      db.produce_blocks();
+
+      Make_Account(db, joe);
+      Make_Account(db, bob);
+      Stake_Asset(db, bob, Asset(100).amount);
+      Make_Producer(db, joe);
+      Approve_Producer(db, bob, joe, true);
+      // Produce blocks up to, but not including, the last block in the round
+      db.produce_blocks(config::BlocksPerRound - db.head_block_num() - 1);
+
+      {
+         BOOST_CHECK_EQUAL(db.get_approved_producers("bob").count("joe"), 1);
+         BOOST_CHECK_EQUAL(db.get_staked_balance("bob"), Asset(100));
+         const auto& joeVotes = db_db.get<ProducerVotesObject, byOwnerName>("joe");
+         BOOST_CHECK_EQUAL(joeVotes.getVotes(), db.get_staked_balance("bob"));
+      }
+
+      // OK, let's go to the next round
+      db.produce_blocks();
+
+      const auto& gpo = db.get_global_properties();
+      BOOST_REQUIRE(boost::find(gpo.active_producers, "joe") != gpo.active_producers.end());
+
+      Approve_Producer(db, bob, joe, false);
+      db.produce_blocks();
+
+      {
+         BOOST_CHECK_EQUAL(db.get_approved_producers("bob").count("joe"), 0);
+         const auto& joeVotes = db_db.get<ProducerVotesObject, byOwnerName>("joe");
+         BOOST_CHECK_EQUAL(joeVotes.getVotes(), 0);
+      }
+   } FC_LOG_AND_RETHROW()
+}
+
+// Same as producer_voting_1, except we first cast the vote for the producer, _then_ get a stake
+BOOST_FIXTURE_TEST_CASE(producer_voting_2, testing_fixture) {
+   try {
+      Make_Database(db)
+      db.produce_blocks();
+
+      Make_Account(db, joe);
+      Make_Account(db, bob);
+      Make_Producer(db, joe);
+      Approve_Producer(db, bob, joe, true);
+      db.produce_blocks();
+
+      {
+         BOOST_CHECK_EQUAL(db.get_approved_producers("bob").count("joe"), 1);
+         BOOST_CHECK_EQUAL(db.get_staked_balance("bob"), Asset(0));
+         const auto& joeVotes = db_db.get<ProducerVotesObject, byOwnerName>("joe");
+         BOOST_CHECK_EQUAL(joeVotes.getVotes(), db.get_staked_balance("bob"));
+      }
+
+      Stake_Asset(db, bob, Asset(100).amount);
+      // Produce blocks up to, but not including, the last block in the round
+      db.produce_blocks(config::BlocksPerRound - db.head_block_num() - 1);
+
+      {
+         BOOST_CHECK_EQUAL(db.get_approved_producers("bob").count("joe"), 1);
+         BOOST_CHECK_EQUAL(db.get_staked_balance("bob"), Asset(100));
+         const auto& joeVotes = db_db.get<ProducerVotesObject, byOwnerName>("joe");
+         BOOST_CHECK_EQUAL(joeVotes.getVotes(), db.get_staked_balance("bob"));
+      }
+
+      // OK, let's go to the next round
+      db.produce_blocks();
+
+      const auto& gpo = db.get_global_properties();
+      BOOST_REQUIRE(boost::find(gpo.active_producers, "joe") != gpo.active_producers.end());
+
+      Approve_Producer(db, bob, joe, false);
+      db.produce_blocks();
+
+      {
+         BOOST_CHECK_EQUAL(db.get_approved_producers("bob").count("joe"), 0);
+         const auto& joeVotes = db_db.get<ProducerVotesObject, byOwnerName>("joe");
+         BOOST_CHECK_EQUAL(joeVotes.getVotes(), 0);
+      }
+   } FC_LOG_AND_RETHROW()
+}
+
+// Test voting for producers by proxy
+BOOST_FIXTURE_TEST_CASE(producer_proxy_voting, testing_fixture) {
+   try {
+      using Action = std::function<void(testing_database&)>;
+      Action approve = [](auto& db) {
+         Approve_Producer(db, proxy, producer, true);
+      };
+      Action setproxy = [](auto& db) {
+         Set_Proxy(db, stakeholder, proxy);
+      };
+      Action allowproxy = [](auto& db) {
+         Allow_Proxy(db, proxy, true);
+      };
+      Action stake = [](auto& db) {
+         Stake_Asset(db, stakeholder, Asset(100).amount);
+      };
+
+      auto run = [this](std::vector<Action> actions) {
+         Make_Database(db)
+               db.produce_blocks();
+
+         Make_Account(db, stakeholder);
+         Make_Account(db, proxy);
+         Make_Account(db, producer);
+         Make_Producer(db, producer);
+
+         for (auto& action : actions) action(db);
+
+         // Produce blocks up to, but not including, the last block in the round
+         db.produce_blocks(config::BlocksPerRound - db.head_block_num() - 1);
+
+         {
+            BOOST_CHECK_EQUAL(db.get_approved_producers("proxy").count("producer"), 1);
+            BOOST_CHECK_EQUAL(db.get_staked_balance("stakeholder"), Asset(100));
+            const auto& producerVotes = db_db.get<ProducerVotesObject, byOwnerName>("producer");
+            BOOST_CHECK_EQUAL(producerVotes.getVotes(), db.get_staked_balance("stakeholder"));
+         }
+
+         // OK, let's go to the next round
+         db.produce_blocks();
+
+         const auto& gpo = db.get_global_properties();
+         BOOST_REQUIRE(boost::find(gpo.active_producers, "producer") != gpo.active_producers.end());
+
+         Approve_Producer(db, proxy, producer, false);
+         db.produce_blocks();
+
+         {
+            BOOST_CHECK_EQUAL(db.get_approved_producers("proxy").count("producer"), 0);
+            const auto& producerVotes = db_db.get<ProducerVotesObject, byOwnerName>("producer");
+            BOOST_CHECK_EQUAL(producerVotes.getVotes(), 0);
+         }
+      };
+
+      // Eh, I'm not going to try all legal permutations here... just several of them
+      run({approve, allowproxy, setproxy, stake});
+      run({allowproxy, approve, setproxy, stake});
+      run({allowproxy, setproxy, approve, stake});
+      run({allowproxy, setproxy, stake, approve});
+      run({stake, allowproxy, setproxy, approve});
+      run({stake, approve, allowproxy, setproxy});
+      run({approve, stake, allowproxy, setproxy});
+      run({approve, allowproxy, stake, setproxy});
+      // Make sure it throws if I try to proxy to an account before the account accepts proxying
+      BOOST_CHECK_THROW(run({setproxy, allowproxy, approve, stake}), chain::message_precondition_exception);
+   } FC_LOG_AND_RETHROW()
+}
+
+BOOST_AUTO_TEST_SUITE_END()
