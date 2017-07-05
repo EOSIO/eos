7b348c0168a5717c9a8fe2df737d18be358bb3a0
Phil Mesnier <mesnierp@ociweb.com>
2017-07-05 15:39:54 -0500

    Adding more p2p functionality, including enhancing the synchronization of new joiners, and the processing of peer addresses.

--- plugins/net_plugin/include/eos/net_plugin/net_plugin.hpp
+++ plugins/net_plugin/include/eos/net_plugin/net_plugin.hpp
@@ -18,7 +18,8 @@ namespace eos {
         void plugin_startup();
         void plugin_shutdown();
 
-     void broadcast_block(const chain::signed_block &sb);
+        void broadcast_block(const chain::signed_block &sb);
+        void broadcast_transaction (const chain::SignedTransaction &txn);
 
       private:
         std::unique_ptr<class net_plugin_impl> my;
