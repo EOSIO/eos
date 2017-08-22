9b29b091c3d23eb30c62f1fc96d4feb241ac7d38
Kevin Heifner <heifnerk@objectcomputing.com>
2017-08-22 12:20:23 -0500

    Moved wallet to eos-walletd

--- programs/eos-walletd/main.cpp
+++ programs/eos-walletd/main.cpp
@@ -1,12 +1,8 @@
 #include <appbase/application.hpp>
 
-#include <eos/producer_plugin/producer_plugin.hpp>
-#include <eos/chain_plugin/chain_plugin.hpp>
 #include <eos/http_plugin/http_plugin.hpp>
-#include <eos/chain_api_plugin/chain_api_plugin.hpp>
-#include <eos/net_plugin/net_plugin.hpp>
-#include <eos/account_history_plugin/account_history_plugin.hpp>
-#include <eos/account_history_api_plugin/account_history_api_plugin.hpp>
+#include "eos/wallet_plugin/wallet_plugin.hpp"
+#include "eos/wallet_api_plugin/wallet_api_plugin.hpp"
 
 #include <fc/log/logger_config.hpp>
 #include <fc/exception/exception.hpp>
@@ -19,14 +15,10 @@ using namespace eos;
 int main(int argc, char** argv)
 {
    try {
-      app().register_plugin<net_plugin>();
-      app().register_plugin<chain_api_plugin>();
       app().register_plugin<http_plugin>();
-      app().register_plugin<producer_plugin>();
-      app().register_plugin<chain_plugin>();
-      app().register_plugin<account_history_plugin>();
-      app().register_plugin<account_history_api_plugin>();
-      if(!app().initialize<chain_plugin, http_plugin, net_plugin>(argc, argv))
+      app().register_plugin<wallet_plugin>();
+      app().register_plugin<wallet_api_plugin>();
+      if(!app().initialize<wallet_plugin, wallet_api_plugin, http_plugin>(argc, argv))
          return -1;
       app().startup();
       app().exec();
