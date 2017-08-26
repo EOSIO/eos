6283188f41aa5c418447f30c4b0a3cbe03329121
Brian Johnson <johnsonb@objectcomputing.com>
2017-08-26 05:39:15 -0500

    Splitting out tracking account transactions into its own index.

--- plugins/account_history_plugin/include/eos/account_history_plugin/transaction_history_object.hpp
+++ plugins/account_history_plugin/include/eos/account_history_plugin/transaction_history_object.hpp
@@ -2,20 +2,7 @@
 
 #include <chainbase/chainbase.hpp>
 
-namespace std {
-
-   template<>
-   struct hash<eos::chain::AccountName>
-   {
-    size_t operator()( const eos::chain::AccountName& name )const
-    {
-        return (uint64_t)name;
-    }
-   };
-}
-
 namespace eos {
-using chain::AccountName;
 using chain::block_id_type;
 using chain::transaction_id_type;
 using namespace boost::multi_index;
@@ -26,18 +13,15 @@ class transaction_history_object : public chainbase::object<chain::transaction_h
    id_type             id;
    block_id_type       block_id;
    transaction_id_type transaction_id;
-   AccountName         account_name;
 };
 
 struct by_id;
 struct by_trx_id;
-struct by_account_name;
 using transaction_history_multi_index = chainbase::shared_multi_index_container<
    transaction_history_object,
    indexed_by<
       ordered_unique<tag<by_id>, BOOST_MULTI_INDEX_MEMBER(transaction_history_object, transaction_history_object::id_type, id)>,
-      hashed_non_unique<tag<by_trx_id>, BOOST_MULTI_INDEX_MEMBER(transaction_history_object, transaction_id_type, transaction_id), std::hash<transaction_id_type>>,
-      hashed_non_unique<tag<by_account_name>, BOOST_MULTI_INDEX_MEMBER(transaction_history_object, AccountName, account_name), std::hash<AccountName>>
+      hashed_unique<tag<by_trx_id>, BOOST_MULTI_INDEX_MEMBER(transaction_history_object, transaction_id_type, transaction_id), std::hash<transaction_id_type>>
    >
 >;
 
@@ -47,5 +31,5 @@ typedef chainbase::generic_index<transaction_history_multi_index> transaction_hi
 
 CHAINBASE_SET_INDEX_TYPE( eos::transaction_history_object, eos::transaction_history_multi_index )
 
-FC_REFLECT( eos::transaction_history_object, (block_id)(transaction_id)(account_name) )
+FC_REFLECT( eos::transaction_history_object, (block_id)(transaction_id) )
 
