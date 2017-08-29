3ba5b73710788fdbf26619429a3c8d595a618780
matthew <zathrose@gmail.com>
2017-08-29 10:41:39 -0500

    Not keeping this for now

--- libraries/utilities/include/generic.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <string.h>
-#include <stdarg.h>
-
-extern char *va(char *buff, int size, const char *format, ... );
-#define V(s...) va(tb, sizeof(tb), ## s)
-
