b381f7fd7247a131a4d6c172682dcf0ac3d394d6
Sandwich <hi.na3s@gmail.com>
2017-08-30 17:38:18 +0200

    remove snapshot files

--- programs/snapshot/lib/vendor/HackTimerWorker.min.js
+++
@@ -1,1 +0,0 @@
-var f={},p=postMessage,r='hasOwnProperty';onmessage=function(e){var d=e.data,i=d.i,t=d[r]('t')?d.t:0;switch(d.n){case'a':f[i]=setInterval(function(){p(i)},t);break;case'b':if(f[r](i)){clearInterval(f[i]);delete f[i]}break;case'c':f[i]=setTimeout(function(){p(i);if(f[r](i))delete f[i]},t);break;case'd':if(f[r](i)){clearTimeout(f[i]);delete f[i]}break}}
