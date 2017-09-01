cd1cc3b984d0f82ea2dd08706268cfdc3fa6deb8
toonsevrin <twan123@live.be>
2017-09-01 17:42:48 +0200

    Added docs

--- Docker/eosc.sh
+++ Docker/eosc.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# Usage:
+# Go into cmd loop: sudo sh eosc.sh
+# Run single cmd:  sudo sh eosc.sh <eosc paramers>
+
 PREFIX="docker exec docker_eos_1 eosc"
 if [ -z $1 ] ; then
   while :
