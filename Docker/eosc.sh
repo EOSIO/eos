96cf7f4806207a68a2207a41da38d66e47381d16
toonsevrin <twan123@live.be>
2017-09-01 17:39:50 +0200

    Added argument support

--- Docker/eosc.sh
+++ Docker/eosc.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-while :
-do
-read  -p "eosc " cmd
-docker exec docker_eos_1 eosc $cmd
-done
+PREFIX="docker exec docker_eos_1 eosc"
+if [ -z $1 ] ; then
+  while :
+  do
+  read  -p "eosc " cmd
+    $PREFIX $cmd
+  done
+else
+  $PREFIX $@
+fi
