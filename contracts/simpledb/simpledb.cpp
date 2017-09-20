20b6de4c5dd529835b9d121bb5d3b6a4dc7ede55
Matias Romeo <matias.romeo@gmail.com>
2017-09-20 03:21:18 -0300

    Add complex type example to simpledb

--- contracts/simpledb/simpledb.cpp
+++ contracts/simpledb/simpledb.cpp
@@ -14,12 +14,11 @@ extern "C" {
       str[str_len] = x;
    }
 
-   //void printhex( char* data, uint32_t datalen );
-
    void apply( uint64_t code, uint64_t action ) {
       if( code == N(simpledb) ) {
-         if( action == N(insertalpha) ) {
-            
+         if( action == N(insertkv1) ) {
+            // eosc push message simpledb insertkv1 '{"key":"a", "value":"aa"}' -S simpledb
+            // eosc get table simpledb simpledb keyvalue1
             char tmp[512];
             auto len = readMessage(tmp, 512);
 
@@ -28,17 +27,35 @@ extern "C" {
             char *ptr = tmp; 
             
             uint32_t key_len = (uint32_t)*ptr; ptr+=1;
-            char* key        = ptr; ptr+=key_len;
+            char* key = ptr; ptr+=key_len;
 
             hack_printstr(key, key_len);
 
-            char value_len   = (uint32_t)*ptr; ptr+=1;
-            char* value      = ptr; ptr+=value_len;
+            char *str = ptr;
+            hack_printstr(str+1, *str);
 
-            hack_printstr(value, value_len);
+            uint32_t err = store_str( N(simpledb), N(keyvalue1), key, key_len, str, ((uint32_t)*str)+1);
+         } else if( action == N(insertkv2) ) {
+            // eosc push message simpledb insertkv2 '{"key":"a", "value":{"name":"aaa", "age":10}}' -S simpledb
+            // eosc get table simpledb simpledb keyvalue2
+            char tmp[512];
+            auto len = readMessage(tmp, 512);
 
-            store_str( N(simpledb), N(record1alpha), key, key_len, value, value_len );
-         } else  if( action == N(insert1) ) {
+            printhex(tmp, len);
+
+            char *ptr = tmp; 
+            
+            uint32_t key_len = (uint32_t)*ptr; ptr+=1;
+            char* key = ptr; ptr+=key_len;
+
+            hack_printstr(key, key_len);
+
+            char* complex_data = ptr;
+            uint32_t complex_data_len = len-(key_len+1);
+
+            uint32_t err = store_str( N(simpledb), N(keyvalue2), key, key_len, complex_data, complex_data_len );
+
+         } else if( action == N(insert1) ) {
             record1 tmp;
             readMessage(&tmp, sizeof(tmp));
             store_i64( N(simpledb), N(record1), &tmp, sizeof(tmp) );
