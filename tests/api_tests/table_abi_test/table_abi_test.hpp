866ad638ddab7c4cc74b9543dd513337485fbdcb
Brian Johnson <johnsonb@objectcomputing.com>
2017-10-17 11:01:05 -0500

    Added tests to verify table reads and writes enforce consistent table keytypes and cleanup of previous tests.

---
+++ tests/api_tests/table_abi_test/table_abi_test.hpp
@@ -0,0 +1,23 @@
+#include <eoslib/eos.hpp>
+#include <eoslib/db.hpp>
+
+struct PACKED(table1) {
+   uint64_t  key;
+   uint128_t value1;
+   int64_t   value2;
+};
+
+struct PACKED(table2) {
+   uint128_t key1;
+   uint128_t key2;
+   uint64_t  value1;
+   int64_t   value2;
+};
+
+struct PACKED(table3) {
+   uint64_t  key1;
+   int64_t   key2;
+   uint64_t  key3;
+   uint128_t value1;
+   int64_t   value2;
+};
