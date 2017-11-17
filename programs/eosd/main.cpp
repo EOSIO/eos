17bada2f4b5f08fd5cd01fbdb491a2680fd81341
Jonathan Giszczak <giszczakj@objectcomputing.com>
2017-11-17 11:22:52 -0600

    Change to static_cast from C-style.

--- programs/eosd/main.cpp
+++ programs/eosd/main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char** argv)
 {
    try {
       app().set_version(eosio::eosd::config::version);
-      ilog("eosd version ${ver}", ("ver", eosio::eosd::config::itoh((uint32_t)app().version_int())));
+      ilog("eosd version ${ver}", ("ver", eosio::eosd::config::itoh(static_cast<uint32_t>(app().version_int()))));
       app().register_plugin<net_plugin>();
       app().register_plugin<chain_api_plugin>();
       app().register_plugin<producer_plugin>();
