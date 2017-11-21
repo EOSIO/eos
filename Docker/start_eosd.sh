74c4490761dd94cdd1a2708ffe0187e451b97cda
Huang-Ming Huang <huangming.huang@gmail.com>
2017-11-21 08:25:00 -0600

    Fix path problem

--- Docker/start_eosd.sh
+++ Docker/start_eosd.sh
@@ -4,19 +4,19 @@ cd /opt/eos/bin
 if [ -f '/opt/eos/bin/data-dir/config.ini' ]; then
     echo
   else
-    cp /etc/eos/config.ini /opt/eos/bin/data-dir
+    cp /config.ini /opt/eos/bin/data-dir
 fi
 
 if [ -f '/opt/eos/bin/data-dir/genesis.json' ]; then
     echo
   else
-    cp /etc/eos/genesis.json /opt/eos/bin/data-dir
+    cp /genesis.json /opt/eos/bin/data-dir
 fi
 
 if [ -d '/opt/eos/bin/data-dir/contracts' ]; then
     echo
   else
-    cp -r /etc/eos/contracts /opt/eos/bin/data-dir
+    cp -r /contracts /opt/eos/bin/data-dir
 fi
 
 exec /opt/eos/bin/eosd $@
