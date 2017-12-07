a802b611efb633320c156d7ef7588cb3dc347378
Kevin Heifner <heifnerk@objectcomputing.com>
2017-12-06 18:34:20 -0600

    Add shortcut out of calling print_help_text #876
    (cherry picked from commit a52199210dbfecd67d65ff729a2fe7efccc89c80)

--- programs/eosioc/main.cpp
+++ programs/eosioc/main.cpp
@@ -1140,7 +1140,7 @@ int main( int argc, char** argv ) {
          }
       } else {
          // attempt to extract the error code if one is present
-         if (!print_help_text(e) || verbose_errors) {
+         if (verbose_errors || !print_help_text(e)) {
             elog("Failed with error: ${e}", ("e", verbose_errors ? e.to_detail_string() : e.to_string()));
          }
       }
