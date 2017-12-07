3b45822ed045ee2242fe6e79bf847563007f2157
Kevin Heifner <heifnerk@objectcomputing.com>
2017-12-06 18:39:25 -0600

    Prevent localize from throwing exceptions #876

--- programs/eosc/localize.hpp
+++ programs/eosc/localize.hpp
@@ -15,6 +15,13 @@ namespace eosio { namespace client { namespace localize {
    #define localized(str, ...) localized_with_variant((str), fc::mutable_variant_object() __VA_ARGS__ )
 
    inline auto localized_with_variant( const char* raw_fmt, const fc::variant_object& args) {
-      return fc::format_string(::gettext(raw_fmt), args);
+      if (raw_fmt != nullptr) {
+         try {
+            return fc::format_string(::gettext(raw_fmt), args);
+         } catch (...) {
+         }
+         return std::string(raw_fmt);
+      }
+      return std::string();
    }
 }}}
