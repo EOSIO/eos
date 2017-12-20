4bc5b4e8a19af48b43dc662538969b63b0255980
Kevin Heifner <heifner_k@ociweb.com>
2017-12-19 20:40:04 -0600

    Merge pull request #979 from EOSIO/p2p-stat-271-gh916-2
    P2p stat 271 gh916 2
    
    (reapplied on noon branch)

--- plugins/net_plugin/include/eos/net_plugin/protocol.hpp
+++ plugins/net_plugin/include/eos/net_plugin/protocol.hpp
@@ -44,7 +44,8 @@ namespace eosio {
     bad_transaction, ///< the peer sent a transaction that failed verification
     validation, ///< the peer sent a block that failed validation
     benign_other, ///< reasons such as a timeout. not fatal but warrant resetting
-    fatal_other ///< a catch-all for errors we don't have discriminated
+    fatal_other, ///< a catch-all for errors we don't have discriminated
+    authentication ///< peer failed authenicatio
   };
 
   constexpr auto reason_str( go_away_reason rsn ) {
@@ -58,6 +59,7 @@ namespace eosio {
     case unlinkable : return "unlinkable block received";
     case bad_transaction : return "bad transaction";
     case validation : return "invalid block";
+    case authentication : return "authentication failure";
     case fatal_other : return "some other failure";
     case benign_other : return "some other non-fatal condition";
     default : return "some crazy reason";
@@ -101,6 +103,7 @@ namespace eosio {
 
   template<typename T>
   struct select_ids {
+    select_ids () : mode(none),pending(0),ids() {}
     id_list_modes  mode;
     uint32_t       pending;
     vector<T>      ids;
@@ -111,11 +114,13 @@ namespace eosio {
   using ordered_blk_ids = select_ids<block_id_type>;
 
   struct notice_message {
+    notice_message () : known_trx(), known_blocks() {}
     ordered_txn_ids known_trx;
     ordered_blk_ids known_blocks;
   };
 
   struct request_message {
+    request_message () : req_trx(), req_blocks() {}
     ordered_txn_ids req_trx;
     ordered_blk_ids req_blocks;
   };
