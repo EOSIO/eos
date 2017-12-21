01fb3474d13fcdb8b872a2ff0e3866a69e1fa197
Bart Wyatt <bart.wyatt@gmail.com>
2017-12-21 10:22:53 -0500

    added test cases for recovering when your multisig owner partner is no longer trusted

--- libraries/testing/include/eosio/testing/tester.hpp
+++ libraries/testing/include/eosio/testing/tester.hpp
@@ -21,18 +21,18 @@ namespace eosio { namespace testing {
          void              produce_blocks( uint32_t n = 1 );
 
          transaction_trace push_transaction( signed_transaction& trx );
-         void              set_tapos( signed_transaction& trx );
+         void              set_tapos( signed_transaction& trx ) const;
 
-         void              create_accounts( vector<account_name> names, asset init_bal ) { 
-            for( auto n : names ) create_account(n, init_bal ); 
+         void              create_accounts( vector<account_name> names, asset init_bal, bool multisig = false ) {
+            for( auto n : names ) create_account(n, init_bal, N(inita), multisig );
          }
 
 
          void set_authority( account_name account, permission_name perm, authority auth,
                                      permission_name parent = config::owner_name );
 
-         void              create_account( account_name name, asset initial_balance = asset(), account_name creator = N(inita) );
-         void              create_account( account_name name, string balance = "0.0000 EOS", account_name creator = N(inita));
+         void              create_account( account_name name, asset initial_balance = asset(), account_name creator = N(inita), bool multisig = false );
+         void              create_account( account_name name, string balance = "0.0000 EOS", account_name creator = N(inita), bool multisig = false );
 
          transaction_trace transfer( account_name from, account_name to, asset amount, string memo = "" );
          transaction_trace transfer( account_name from, account_name to, string amount, string memo = "" );
@@ -42,8 +42,8 @@ namespace eosio { namespace testing {
             return control->get_database().get<ObjectType,IndexBy>( forward<Args>(args)... );
          }
 
-         public_key_type   get_public_key( name keyname, string role = "owner" );
-         private_key_type  get_private_key( name keyname, string role = "owner" );
+         public_key_type   get_public_key( name keyname, string role = "owner" ) const;
+         private_key_type  get_private_key( name keyname, string role = "owner" ) const;
 
          void              set_code( account_name name, const char* wast );
          void              set_abi( account_name name, const char* abi_json );
