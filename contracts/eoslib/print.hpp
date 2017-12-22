184e57da48942fe8e62d3076fa5358c593595bed
Retriever <xphuangsg@gmail.com>
2017-12-22 13:10:30 +0800

    Update print.hpp

--- contracts/eoslib/print.hpp
+++ contracts/eoslib/print.hpp
@@ -120,7 +120,7 @@ namespace eosio {
      *
      *  Example:
      *  @code
-     *  char *s = "Hello World!";
+     *  const char *s = "Hello World!";
      *  uint64_t unsigned_64_bit_int = 1e+18;
      *  uint128_t unsigned_128_bit_int (87654323456);
      *  uint64_t string_as_unsigned_64_bit = N(abcde);
