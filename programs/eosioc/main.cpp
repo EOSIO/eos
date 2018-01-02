a5f57cf991ea181ea4e2b4d4970d2facce6fa6b1
Kevin Heifner <heifnerk@objectcomputing.com>
2018-01-02 15:11:17 -0600

    Fix indent

--- programs/eosioc/main.cpp
+++ programs/eosioc/main.cpp
@@ -1040,7 +1040,7 @@ int main( int argc, char** argv ) {
                auto result = call( push_txns_func, batch );
                //std::cout << fc::json::to_pretty_string(result) << std::endl;
                batch.resize(0);
-	       info = get_info();
+	            info = get_info();
             }
          }
          if( !loop ) break;
