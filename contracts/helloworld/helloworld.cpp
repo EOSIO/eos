4e5620a1ee8c1763e910b473151223645bb2bd64
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-15 12:06:59 +0100

    [contract|helloworld] refactoring

--- contracts/helloworld/helloworld.cpp
+++ contracts/helloworld/helloworld.cpp
@@ -3,16 +3,26 @@
 #include <eoslib/print.hpp>
 
 extern "C" {
+
 void init()  {
-    eosio::print("intializing Hello World contract\n");
+    eosio::print("(helloworld contract) intializing\n");
 }
 
-void apply( uint64_t code, uint64_t action ) {
-    eosio::print( "Hello World: ", eosio::name(code), "->", eosio::name(action), "\n" );
+void apply(uint64_t code, uint64_t action) {
+    eosio::print("(helloworld contract) received ", eosio::name(code), "->", eosio::name(action), "\n");
 
-    if (code == N(helloworld))
-        if (action == N(helloworld))
-            eosio::print("Hello World!");
-}
+    if (code != N(helloworld))
+        return;
+
+    switch(action)
+    {
+    case N(sayhello):
+        eosio::print("Hello World!\n");
+        break;
+
+    default:
+        break;
+    }
 }
 
+} // extern
--- contracts/helloworld/helloworld.h
+++ contracts/helloworld/helloworld.h
@@ -1,3 +1,8 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+
 #ifndef HELLOWORLD_H
 #define HELLOWORLD_H
 
@@ -5,27 +10,6 @@
 
 namespace helloworld {
 
-struct transfer {
-   /**
-   * account to transfer from
-   */
-   uint64_t       from;
-   /**
-   * account to transfer to
-   */
-   uint64_t       to;
-   /**
-   *  quantity to transfer
-   */
-   uint64_t    quantity;
-};
-
 } // namespace
-
-//namespace tester {
-//   struct Message {
-//     char message = 0;
-//   };
-//}
 
 #endif // HELLOWORLD_H
