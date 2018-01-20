8ec95eac1101ef4de901c8b1cdfc0d4bf1818c9c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-20 18:02:16 +0100

    [libeosioclient] functions.hpp created

---
+++ programs/eosioc/libeosioclient/functions.hpp
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+
+const std::string chain_func_base = "/v1/chain";
+const std::string get_info_func = chain_func_base + "/get_info";
+
+#endif // FUNCTIONS_H
