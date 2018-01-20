ce95693635d4648484ee3a40d0858be0f8b5019c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-20 18:05:27 +0100

    [libeosioclient] fundtions into namespace eosio::client

--- programs/eosioc/libeosioclient/functions.hpp
+++ programs/eosioc/libeosioclient/functions.hpp
@@ -3,7 +3,13 @@
 
 #include <string>
 
+namespace eosio {
+namespace client {
+
 const std::string chain_func_base = "/v1/chain";
 const std::string get_info_func = chain_func_base + "/get_info";
+
+} // namespace client
+} // namespace eosio
 
 #endif // FUNCTIONS_H
