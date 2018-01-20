90d3bb86271131d0e53fd081a64de70b76d28ea4
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-20 17:50:30 +0100

    [libeosioclient] added get_info()

--- programs/eosioc/libeosioclient/eosioclient.cpp
+++ programs/eosioc/libeosioclient/eosioclient.cpp
@@ -1,6 +1,6 @@
 #include "eosioclient.hpp"
 
-Eosioclient::Eosioclient()
+eosio::chain_apis::read_only::get_info_results Eosioclient::get_info()
 {
-
+    return call(host, port, get_info_func ).as<eosio::chain_apis::read_only::get_info_results>();
 }
--- programs/eosioc/libeosioclient/eosioclient.hpp
+++ programs/eosioc/libeosioclient/eosioclient.hpp
@@ -1,10 +1,34 @@
 #ifndef EOSIOCLIENT_HPP
 #define EOSIOCLIENT_HPP
 
+#include "eosio/chain_plugin/chain_plugin.hpp"
+
+namespace {
+const std::string chain_func_base = "/v1/chain";
+const std::string get_info_func = chain_func_base + "/get_info";
+}
+
 class Eosioclient
 {
 public:
-    Eosioclient();
+    eosio::chain_apis::read_only::get_info_results get_info();
+
+private:
+    fc::variant call( const std::string& server, uint16_t port,
+                      const std::string& path,
+                      const fc::variant& postdata = fc::variant() );
+
+    template<typename T>
+    fc::variant call( const std::string& server, uint16_t port,
+                      const std::string& path,
+                      const T& v ) { return call( server, port, path, fc::variant(v) ); }
+
+    template<typename T>
+    fc::variant call( const std::string& path,
+                      const T& v ) { return call( host, port, path, fc::variant(v) ); }
+
+    std::string host = "localhost";
+    uint32_t port = 8888;
 };
 
 #endif // EOSIOCLIENT_HPP
