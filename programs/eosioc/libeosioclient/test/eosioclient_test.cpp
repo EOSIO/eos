f905e3cc47bb0dd6b04e668a169294b6412f2307
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-21 11:19:19 +0100

    [libeosioclient] add commented test .. eosiod needed

--- programs/eosioc/libeosioclient/test/eosioclient_test.cpp
+++ programs/eosioc/libeosioclient/test/eosioclient_test.cpp
@@ -1,5 +1,8 @@
 #include <boost/test/unit_test.hpp>
 
+#include <fc/io/json.hpp>
+#include <string>
+
 #include "eosioclient.hpp"
 
 BOOST_AUTO_TEST_SUITE(eosioclient_test)
@@ -8,6 +11,12 @@ BOOST_AUTO_TEST_CASE(instance)
 {
     BOOST_CHECK_NO_THROW(eosio::client::Eosioclient());
 }
+
+//BOOST_AUTO_TEST_CASE(get_info)
+//{
+//    eosio::client::Eosioclient client;
+//    BOOST_CHECK_NO_THROW(client.get_info());
+//}
 
 BOOST_AUTO_TEST_SUITE_END()
 
