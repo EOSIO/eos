28172cfc4421d7ee359acc8379a83c17e2497016
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-21 06:32:58 +0100

    [libeosioclient] setting host:port

--- programs/eosioc/libeosioclient/eosioclient.hpp
+++ programs/eosioc/libeosioclient/eosioclient.hpp
@@ -12,6 +12,9 @@ class Eosioclient
 public:
     eosio::chain_apis::read_only::get_info_results get_info();
 
+    std::string host = "localhost"; /// @todo make private
+    uint32_t port = 8888; /// @todo make private
+
 private:
     fc::variant call( const std::string& server, uint16_t port,
                       const std::string& path,
@@ -25,9 +28,6 @@ private:
     template<typename T>
     fc::variant call( const std::string& path,
                       const T& v ) { return call( host, port, path, fc::variant(v) ); }
-
-    std::string host = "localhost";
-    uint32_t port = 8888;
 };
 
 } // namespace client
