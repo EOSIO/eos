e2c03cfea0c27cd92f1f202369e889ae706e5165
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-21 11:35:50 +0100

    [libeosioclient] Eosioclient::get_code

--- programs/eosioc/libeosioclient/functions.hpp
+++ programs/eosioc/libeosioclient/functions.hpp
@@ -8,6 +8,7 @@ namespace client {
 
 const std::string chain_func_base = "/v1/chain";
 const std::string get_info_func = chain_func_base + "/get_info";
+const std::string get_code_func = chain_func_base + "/get_code";
 
 } // namespace client
 } // namespace eosio
