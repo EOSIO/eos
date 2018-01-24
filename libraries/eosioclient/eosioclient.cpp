5063331db8d770b4199c35d8bd9afb064a0a4e98
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-24 10:22:11 +0100

    [eosioclient] class Eosioclient created

---
+++ libraries/eosioclient/eosioclient.cpp
@@ -0,0 +1,12 @@
+#include "eosioclient.hpp"
+
+namespace eosio {
+namespace client {
+
+Eosioclient::Eosioclient()
+{
+
+}
+
+} // namespace client
+} // namespace eosio
---
+++ libraries/eosioclient/eosioclient.hpp
@@ -0,0 +1,17 @@
+#ifndef EOSIOCLIENT_H
+#define EOSIOCLIENT_H
+
+namespace eosio {
+namespace client {
+
+class Eosioclient
+{
+public:
+    Eosioclient();
+};
+
+
+} // namespace client
+} // namespace eosio
+
+#endif // EOSIOCLIENT_H
