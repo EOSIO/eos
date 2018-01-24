59bb7aea1e3038d1b6d53b88cf44e871a4d6909a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-24 22:27:34 +0100

    [test] removed unuseful tests

--- libraries/eosioclient/test/wallet_test.cpp
+++ libraries/eosioclient/test/wallet_test.cpp
@@ -12,44 +12,6 @@ BOOST_AUTO_TEST_CASE(instance)
     BOOST_CHECK_NO_THROW(eosio::client::Wallet());
 }
 
-BOOST_AUTO_TEST_CASE(default_host)
-{
-    eosio::client::Wallet wallet;
-    BOOST_CHECK_EQUAL("localhost", wallet.host());
-}
-
-BOOST_AUTO_TEST_CASE(default_port)
-{
-    eosio::client::Wallet wallet;
-    BOOST_CHECK_EQUAL(8888, wallet.port());
-}
-
-BOOST_AUTO_TEST_CASE(set_host)
-{
-    eosio::client::Wallet wallet;
-    std::string host("192.168.0.1");
-    wallet.set_host(host);
-    BOOST_CHECK_EQUAL(host, wallet.host());
-}
-
-BOOST_AUTO_TEST_CASE(set_port)
-{
-    eosio::client::Wallet wallet;
-    uint32_t port(1111);
-    wallet.set_port(port);
-    BOOST_CHECK_EQUAL(port, wallet.port());
-}
-
-BOOST_AUTO_TEST_CASE(host)
-{
-    eosio::client::Wallet wallet;
-    wallet.set_port(8888);
-    wallet.set_host("localhost");
-    wallet.host();
-    //BOOST_CHECK_EQUAL(port, wallet.port());
-}
-
-
 BOOST_AUTO_TEST_SUITE_END()
 
 
