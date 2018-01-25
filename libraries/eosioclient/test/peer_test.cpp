79736aefaff4ef21af709de647704c74e3e66270
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-25 10:43:22 +0100

    [eosioclient] added tests for defaults and host, port set

--- libraries/eosioclient/test/peer_test.cpp
+++ libraries/eosioclient/test/peer_test.cpp
@@ -2,11 +2,36 @@
 
 #include "peer.hpp"
 
+using namespace eosio::client;
+
 BOOST_AUTO_TEST_SUITE(peer_test)
 
 BOOST_AUTO_TEST_CASE(instance)
 {
     BOOST_CHECK_NO_THROW(eosio::client::Peer());
+}
+
+BOOST_AUTO_TEST_CASE(defaults)
+{
+    Peer peer;
+    BOOST_CHECK_EQUAL("localhost", peer.host());
+    BOOST_CHECK_EQUAL(8888, peer.port());
+}
+
+BOOST_AUTO_TEST_CASE(set_host)
+{
+    Peer peer;
+    std::string host("192.168.1.1");
+    peer.set_host(host);
+    BOOST_CHECK_EQUAL(host, peer.host());
+}
+
+BOOST_AUTO_TEST_CASE(set_port)
+{
+    Peer peer;
+    int port(1111);
+    peer.set_port(port);
+    BOOST_CHECK_EQUAL(port, peer.port());
 }
 
 //BOOST_AUTO_TEST_CASE(get_block)
--- libraries/eosioclient/test/wallet_test.cpp
+++ libraries/eosioclient/test/wallet_test.cpp
@@ -1,15 +1,37 @@
 #include <boost/test/unit_test.hpp>
 
-#include <fc/io/json.hpp>
-#include <string>
-
 #include "wallet.hpp"
+
+using namespace eosio::client;
 
 BOOST_AUTO_TEST_SUITE(wallet_test)
 
 BOOST_AUTO_TEST_CASE(instance)
 {
     BOOST_CHECK_NO_THROW(eosio::client::Wallet());
+}
+
+BOOST_AUTO_TEST_CASE(defaults)
+{
+    Wallet wallet;
+    BOOST_CHECK_EQUAL("localhost", wallet.host());
+    BOOST_CHECK_EQUAL(8888, wallet.port());
+}
+
+BOOST_AUTO_TEST_CASE(set_host)
+{
+    Wallet wallet;
+    std::string host("192.168.1.1");
+    wallet.set_host(host);
+    BOOST_CHECK_EQUAL(host, wallet.host());
+}
+
+BOOST_AUTO_TEST_CASE(set_port)
+{
+    Wallet wallet;
+    int port(1111);
+    wallet.set_port(port);
+    BOOST_CHECK_EQUAL(port, wallet.port());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
