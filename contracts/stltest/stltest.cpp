db497b181e1b4d71a3fd86ce88236e05b143e44e
Anton Perkov <anton.perkov@blockone.io>
2018-01-24 22:17:52 -0500

    stltest includes all headers from libc

--- contracts/stltest/stltest.cpp
+++ contracts/stltest/stltest.cpp
@@ -4,6 +4,53 @@
  */
 #include <stltest/stltest.hpp> /// defines transfer struct (abi)
 
+// include entire libc
+#include <alloca.h>
+#include <ar.h>
+#include <assert.h>
+#include <byteswap.h>
+#include <cpio.h>
+#include <crypt.h>
+#include <ctype.h>
+#include <elf.h>
+#include <endian.h>
+#include <errno.h>
+#include <features.h>
+#include <fnmatch.h>
+#include <inttypes.h>
+#include <iso646.h>
+#include <limits.h>
+#include <locale.h>
+#include <malloc.h>
+#include <math.h>
+#include <memory.h>
+#include <monetary.h>
+#include <mqueue.h>
+#include <regex.h>
+#include <search.h>
+#include <stdalign.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdc-predef.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdnoreturn.h>
+#include <string.h>
+#include <strings.h>
+#include <stropts.h>
+#include <sysexits.h>
+#include <tar.h>
+#include <uchar.h>
+#include <unistd.h>
+#include <values.h>
+#include <wchar.h>
+#include <wctype.h>
+#include <wordexp.h>
+
+//
+
 #include <array>
 #include <vector>
 #include <stack>
@@ -38,23 +85,26 @@ namespace stltest {
         struct message {
             account_name from;
             account_name to;
-            string msg;
+           //string msg;
 
             static uint64_t get_account() { return current_receiver(); }
             static uint64_t get_name()  { return N(message); }
 
             template<typename DataStream>
             friend DataStream& operator << ( DataStream& ds, const message& m ){
-                return ds << m.from << m.to << m.msg;
+               return ds << m.from << m.to;// << m.msg;
             }
             template<typename DataStream>
             friend DataStream& operator >> ( DataStream& ds, message& m ){
-                return ds >> m.from >> m.to >> m.msg;
+               return ds >> m.from >> m.to;// >> m.msg;
             }
         };
 
         static void on(const message& msg) {
            print("STL test start.\n");
+           void* ptr = malloc(10);
+           free(ptr);
+
            std::array<uint32_t, 10> arr;
            arr.fill(3);
            arr[0] = 0;
@@ -69,17 +119,17 @@ namespace stltest {
            std::queue<unsigned int> q;
            q.push(0);
 
-           std::deque<float> dq;
+           std::deque<long> dq;
            dq.push_front(0.0f);
 
            std::list<uint32_t> l;
            l.push_back(0);
 
-           //std::string s;
-           //s.append(1, 'a');
+           std::string s = "abcdef";
+           s.append(1, 'g');
 
-           std::map<int, double> m;
-           m.emplace(0, 1.0);
+           std::map<int, long> m;
+           m.emplace(0, 1);
            auto mit = m.lower_bound(2);
 
            std::set<long> st;
@@ -93,7 +143,7 @@ namespace stltest {
            //hs.insert(0);
 
            sort(dq.begin(), dq.end());
-           auto lit = find_if(l.begin(), l.end(), [](float f) { return f < 0; });
+           auto lit = find_if(l.begin(), l.end(), [](uint32_t f) { return f < 10; });
 
            print("STL test done.\n");
         }
