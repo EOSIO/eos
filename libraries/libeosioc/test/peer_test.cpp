ef061f387e3247c884f68aef530d14271c0fb0dd
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-01-29 10:04:00 +0100

    [libeosioc] commented code removed

--- libraries/libeosioc/test/peer_test.cpp
+++ libraries/libeosioc/test/peer_test.cpp
@@ -33,14 +33,4 @@ BOOST_AUTO_TEST_CASE(set_port)
     BOOST_CHECK_EQUAL(port, peer.port());
 }
 
-//BOOST_AUTO_TEST_CASE(get_block)
-//{
-//    eosio::client::Peer peer;
-//    peer.set_port(8888);
-//    peer.set_host("localhost");
-//    auto result0 = peer.get_block("5");
-//    auto result1 = peer.get_block("5");
-//    BOOST_CHECK_EQUAL(fc::json::to_string(result0), fc::json::to_string(result1));
-//}
-
 BOOST_AUTO_TEST_SUITE_END()
