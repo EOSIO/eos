a531309df2cfab1faf49f9c533aa54cab8da71cf
Anton Perkov <anton.perkov@blockone.io>
2018-02-06 14:39:24 -0500

    floating point to/from string functions uncommented

--- contracts/musl/include/stdlib.h
+++ contracts/musl/include/stdlib.h
@@ -21,11 +21,11 @@ extern "C" {
 int atoi (const char *);
 long atol (const char *);
 long long atoll (const char *);
-//double atof (const char *);
+double atof (const char *);
 
-//float strtof (const char *__restrict, char **__restrict);
-//double strtod (const char *__restrict, char **__restrict);
-//long double strtold (const char *__restrict, char **__restrict);
+float strtof (const char *__restrict, char **__restrict);
+double strtod (const char *__restrict, char **__restrict);
+long double strtold (const char *__restrict, char **__restrict);
 
 long strtol (const char *__restrict, char **__restrict, int);
 unsigned long strtoul (const char *__restrict, char **__restrict, int);
