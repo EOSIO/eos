da1550c46e6f4244ba0af10aa631351bef6bda2b
Bart Wyatt <bart.wyatt@block.one>
2018-02-06 13:45:03 -0500

    add unequal size case to equality operator

--- libraries/chain/include/eosio/chain/producer_schedule.hpp
+++ libraries/chain/include/eosio/chain/producer_schedule.hpp
@@ -56,6 +56,7 @@ namespace eosio { namespace chain {
    inline bool operator == ( const producer_schedule_type& a, const producer_schedule_type& b ) 
    {
       if( a.version != b.version ) return false;
+      if ( a.producers.size() != b.producers.size() ) return false;
       for( uint32_t i = 0; i < a.producers.size(); ++i )
          if( a.producers[i] != b.producers[i] ) return false;
       return true;
