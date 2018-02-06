2b048d3a79edb32debcba5b2012b3ceb5ceeaf21
Anton Perkov <anton.perkov@blockone.io>
2018-02-05 19:30:00 -0500

    fixes to make STL working

--- contracts/musl/src/stdio/__stdio_close.c
+++ contracts/musl/src/stdio/__stdio_close.c
@@ -9,5 +9,6 @@ weak_alias(dummy, __aio_close);
 
 int __stdio_close(FILE *f)
 {
+   return 0;
    //return syscall(SYS_close, __aio_close(f->fd));
 }
--- contracts/musl/src/stdio/__stdio_write.c
+++ contracts/musl/src/stdio/__stdio_write.c
@@ -1,8 +1,12 @@
 #include "stdio_impl.h"
 //include <sys/uio.h>
+#include <eoslib/print.h>
 
 size_t __stdio_write(FILE *f, const unsigned char *buf, size_t len)
 {
+   prints_l(f->wbase, f->wpos-f->wbase);
+   prints_l((void *)buf, len);
+   return f->wpos-f->wbase + len;
    /*
 	struct iovec iovs[2] = {
 		{ .iov_base = f->wbase, .iov_len = f->wpos-f->wbase },
