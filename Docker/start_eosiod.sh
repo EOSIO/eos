5113d33df9257299fc41c2bb62e5be4b6d1d2b17
Eric Iles <eric.iles@block.one>
2018-02-07 14:48:53 -0500

    Fixes Docker to work with latest code.

--- Docker/start_eosiod.sh
+++ Docker/start_eosiod.sh
@@ -1,21 +1,15 @@
 #!/bin/sh
 cd /opt/eosio/bin
 
-if [ -f '/opt/eosio/bin/data-dir/config.ini' ]; then
-    echo
-  else
+if [ ! -f '/opt/eosio/bin/data-dir/config.ini' ]; then
     cp /config.ini /opt/eosio/bin/data-dir
 fi
 
-if [ -f '/opt/eosio/bin/data-dir/genesis.json' ]; then
-    echo
-  else
-    cp /genesis.json /opt/eosio/bin/data-dir
+if [ ! -f '/opt/eosio/bin/genesis.json' ]; then
+    cp /genesis.json /opt/eosio/bin
 fi
 
-if [ -d '/opt/eosio/bin/data-dir/contracts' ]; then
-    echo
-  else
+if [ ! -d '/opt/eosio/bin/data-dir/contracts' ]; then
     cp -r /contracts /opt/eosio/bin/data-dir
 fi
 
