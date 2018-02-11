2c35a651987299a94d60a6d41a81618c62226ee4
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2018-02-11 14:21:51 -0500

    Support proper spec compliant call_indirect wast syntax
    The wasm spec has a different definition for the call_indirect syntax in textual representation then what we currently support. Add code to support the spec’s syntax as newer binaryen (s2wasm) seems to use the spec’s syntax.
    
    Be aware, this might still not be TECHNICALLY to spec — I think the spec may allow for defining the function prototype right at the call_indirect site. We only support usage of a type name or type index at the call_indirect site.

--- tests/wasm_tests/test_wasts.hpp
+++ tests/wasm_tests/test_wasts.hpp
@@ -174,7 +174,7 @@ static const char memory_table_import[] = R"=====(
 
 static const char table_checker_wast[] = R"=====(
 (module
- (import "env" "assert" (func $assert (param i32 i32)))
+ (import "env" "eos_assert" (func $assert (param i32 i32)))
  (import "env" "printi" (func $printi (param i64)))
  (type $SIG$vj (func (param i64)))
  (table 1024 anyfunc)
@@ -214,16 +214,58 @@ static const char table_checker_wast[] = R"=====(
 )
 )=====";
 
+static const char table_checker_proper_syntax_wast[] = R"=====(
+(module
+ (import "env" "eos_assert" (func $assert (param i32 i32)))
+ (import "env" "printi" (func $printi (param i64)))
+ (type $SIG$vj (func (param i64)))
+ (table 1024 anyfunc)
+ (memory $0 1)
+ (export "apply" (func $apply))
+ (func $apply (param $0 i64) (param $1 i64)
+   (call_indirect (type $SIG$vj)
+     (i64.shr_u
+       (get_local $1)
+       (i64.const 32)
+     )
+     (i32.wrap/i64
+       (get_local $1)
+     )
+   )
+ )
+ (func $apple (type $SIG$vj) (param $0 i64)
+   (call $assert
+     (i64.eq
+       (get_local $0)
+       (i64.const 555)
+     )
+     (i32.const 0)
+   )
+ )
+ (func $bannna (type $SIG$vj) (param $0 i64)
+   (call $assert
+     (i64.eq
+       (get_local $0)
+       (i64.const 7777)
+     )
+     (i32.const 0)
+   )
+ )
+ (elem (i32.const 0) $apple)
+ (elem (i32.const 1022) $apple $bannna)
+)
+)=====";
+
 static const char table_checker_small_wast[] = R"=====(
 (module
- (import "env" "assert" (func $assert (param i32 i32)))
+ (import "env" "eos_assert" (func $assert (param i32 i32)))
  (import "env" "printi" (func $printi (param i64)))
  (type $SIG$vj (func (param i64)))
  (table 128 anyfunc)
  (memory $0 1)
  (export "apply" (func $apply))
  (func $apply (param $0 i64) (param $1 i64)
-   (call_indirect $SIG$vj
+   (call_indirect (type $SIG$vj)
      (i64.shr_u
        (get_local $1)
        (i64.const 32)
