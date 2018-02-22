aa25d383cf7526fdeacd2663be1fb4ab1aacd17a
Kevin Heifner <heifnerk@objectcomputing.com>
2018-02-22 09:10:19 -0600

    Add get_context_free_data

--- contracts/eosiolib/transaction.h
+++ contracts/eosiolib/transaction.h
@@ -91,5 +91,14 @@ extern "C" {
     */
    time expiration();
 
+   /**
+    * Retrieve the signed_transaction.context_free_data[index].
+    * @param index - the index of the context_free_data entry to retrieve
+    * @param buff - output buff of the context_feee_data entry
+    * @param size - amount of context_free_data[index] to retrieve
+    * @return signed_transaction.context_free_data[index].size() or -1 if index not valid
+    */
+   int get_context_free_data( uint32_t index, char* buff, size_t size );
+
    ///@ } transactioncapi
 }
