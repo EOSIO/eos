bd6529fd446ecb72900d523f49ed93be2d3a9447
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-02-23 09:29:41 +0100

    [cmake] FindBinaryen search for include dir and libraries

--- CMakeModules/FindBinaryen.cmake
+++ CMakeModules/FindBinaryen.cmake
@@ -6,9 +6,13 @@
 #  BINARYEN_DEFINITIONS - Compiler switches required for using BINARYEN
 
 find_program(BINARYEN_BIN s2wasm HINTS ${BINARYEN_ROOT}/bin)
+find_path(BINARYEN_INCLUDE_DIRS binaryen-c.h HINTS ${BINARYEN_ROOT}/include)
+find_library(BINARYEN_LIBRARIES binaryen HINTS ${BINARYEN_ROOT}/lib)
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set BINARYEN_FOUND to TRUE
 # if all listed variables are TRUE
 
-find_package_handle_standard_args(BINARYEN DEFAULT_MSG BINARYEN_BIN)
+find_package_handle_standard_args(BINARYEN DEFAULT_MSG BINARYEN_BIN BINARYEN_INCLUDE_DIRS BINARYEN_LIBRARIES )
+
+mark_as_advanced(BINARYEN_BIN BINARYEN_INCLUDE_DIRS BINARYEN_LIBRARIES)
