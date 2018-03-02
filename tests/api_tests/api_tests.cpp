964356b8e7cf5dc572cfcc38e93e603a7f1f2bd7
arhag <arhag@users.noreply.github.com>
2018-03-02 13:47:09 -0500

    fix send_action_large in api_tests/transaction_tests

--- tests/api_tests/api_tests.cpp
+++ tests/api_tests/api_tests.cpp
@@ -471,9 +471,11 @@ BOOST_FIXTURE_TEST_CASE(transaction_tests, tester) { try {
 
    // test send_action_empty
    CALL_TEST_FUNCTION(*this, "test_transaction", "send_action_empty", {});
+
+	// test send_action_large
    BOOST_CHECK_EXCEPTION(CALL_TEST_FUNCTION(*this, "test_transaction", "send_action_large", {}), fc::assert_exception,
          [](const fc::assert_exception& e) {
-            return expect_assert_message(e, "false: abort() called");
+				return expect_assert_message(e, "data_len < config::default_max_inline_action_size: inline action too big");
          }
       );
 
