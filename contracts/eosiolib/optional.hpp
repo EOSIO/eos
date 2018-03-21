06533740c8a6199581ebd16d7014a6234ab39ef3
arhag <arhag@users.noreply.github.com>
2018-03-21 12:19:41 -0400

    improvements to optional

--- contracts/eosiolib/optional.hpp
+++ contracts/eosiolib/optional.hpp
@@ -8,43 +8,44 @@ namespace eosio {
    class optional {
       public:
          typedef T value_type;
-         
+         typedef typename std::aligned_storage<sizeof(T), alignof(T)>::type storage_type;
+
          optional():_valid(false){}
          ~optional(){ reset(); }
-         
+
          optional( optional& o )
-            :_valid(false) 
+            :_valid(false)
          {
             if( o._valid ) new (ptr()) T( *o );
             _valid = o._valid;
          }
-         
+
          optional( const optional& o )
-            :_valid(false) 
+            :_valid(false)
          {
             if( o._valid ) new (ptr()) T( *o );
             _valid = o._valid;
          }
-         
+
          optional( optional&& o )
-            :_valid(false) 
+            :_valid(false)
          {
             if( o._valid ) new (ptr()) T( std::move(*o) );
             _valid = o._valid;
             o.reset();
          }
-         
+
          template<typename U>
          optional( const optional<U>& o )
-            :_valid(false) 
+            :_valid(false)
          {
             if( o._valid ) new (ptr()) T( *o );
             _valid = o._valid;
          }
-         
+
          template<typename U>
          optional( optional<U>& o )
-            :_valid(false) 
+            :_valid(false)
          {
             if( o._valid )
                {
@@ -52,46 +53,46 @@ namespace eosio {
                }
             _valid = o._valid;
          }
-         
+
          template<typename U>
          optional( optional<U>&& o )
-            :_valid(false) 
+            :_valid(false)
          {
             if( o._valid ) new (ptr()) T( std::move(*o) );
             _valid = o._valid;
             o.reset();
          }
-         
+
          template<typename U>
          optional( U&& u )
-            :_valid(true) 
+            :_valid(true)
          {
             new ((char*)ptr()) T( std::forward<U>(u) );
          }
-         
+
          template<typename U>
-         optional& operator=( U&& u ) 
+         optional& operator=( U&& u )
          {
             reset();
             new (ptr()) T( std::forward<U>(u) );
             _valid = true;
             return *this;
          }
-         
+
          template<typename ...Args>
          void emplace(Args&& ... args) {
             if (_valid) {
                reset();
             }
-            
+
             new ((char*)ptr()) T( std::forward<Args>(args)... );
             _valid = true;
          }
-        
+
          template<typename U>
          optional& operator=( optional<U>& o ) {
             if (this != &o) {
-               if( _valid && o._valid ) { 
+               if( _valid && o._valid ) {
                   ref() = *o;
                } else if( !_valid && o._valid ) {
                   new (ptr()) T( *o );
@@ -106,7 +107,7 @@ namespace eosio {
          template<typename U>
          optional& operator=( const optional<U>& o ) {
             if (this != &o) {
-               if( _valid && o._valid ) { 
+               if( _valid && o._valid ) {
                   ref() = *o;
                } else if( !_valid && o._valid ) {
                   new (ptr()) T( *o );
@@ -120,7 +121,7 @@ namespace eosio {
 
          optional& operator=( optional& o ) {
             if (this != &o) {
-               if( _valid && o._valid ) { 
+               if( _valid && o._valid ) {
                   ref() = *o;
                } else if( !_valid && o._valid ) {
                   new (ptr()) T( *o );
@@ -131,10 +132,10 @@ namespace eosio {
             }
             return *this;
          }
-         
+
          optional& operator=( const optional& o ) {
             if (this != &o) {
-               if( _valid && o._valid ) { 
+               if( _valid && o._valid ) {
                   ref() = *o;
                } else if( !_valid && o._valid ) {
                   new (ptr()) T( *o );
@@ -145,13 +146,13 @@ namespace eosio {
             }
             return *this;
          }
-         
+
          template<typename U>
-         optional& operator=( optional<U>&& o ) 
+         optional& operator=( optional<U>&& o )
          {
-            if (this != &o) 
+            if (this != &o)
                {
-                  if( _valid && o._valid ) 
+                  if( _valid && o._valid )
                      {
                         ref() = std::move(*o);
                         o.reset();
@@ -163,12 +164,12 @@ namespace eosio {
                }
             return *this;
          }
-         
-         optional& operator=( optional&& o ) 
+
+         optional& operator=( optional&& o )
          {
-            if (this != &o) 
+            if (this != &o)
                {
-                  if( _valid && o._valid ) 
+                  if( _valid && o._valid )
                      {
                         ref() = std::move(*o);
                         o.reset();
@@ -180,43 +181,42 @@ namespace eosio {
                }
             return *this;
          }
-        
+
          bool valid()const     { return _valid;  }
          bool operator!()const { return !_valid; }
-         
-         // this operation is not safe and can result in unintential 
-         // casts and comparisons, use valid() or !! 
+
+         // this operation is not safe and can result in unintential
+         // casts and comparisons, use valid() or !!
          explicit operator bool()const  { return _valid;  }
-         
-         T&       operator*()      { assert(_valid); return ref(); }
-         const T& operator*()const { assert(_valid); return ref(); }
-         
-         T*       operator->()      
-         { 
-            assert(_valid);
-            return ptr(); 
+
+         T&       operator*()      { eosio_assert(_valid, "dereference of empty optional"); return ref(); }
+         const T& operator*()const { eosio_assert(_valid, "dereference of empty optional"); return ref(); }
+
+         T*       operator->()
+         {
+            eosio_assert(_valid, "dereference of empty optional");
+            return ptr();
          }
-         const T* operator->()const 
-         { 
-            assert(_valid);
-            return ptr(); 
+         const T* operator->()const
+         {
+            eosio_assert(_valid, "dereference of empty optional");
+            return ptr();
          }
-         
+
          optional& operator=(std::nullptr_t)
          {
             reset();
             return *this;
          }
-         
-         void reset()    
-         { 
-            if( _valid ) 
-               {
+
+         void reset()
+         {
+            if( _valid ) {
                   ref().~T(); // cal destructor
-               }
+            }
             _valid = false;
          }
-         
+
          friend bool operator < ( const optional a, optional b )
          {
             if( a.valid() && b.valid() ) return *a < *b;
@@ -227,7 +227,7 @@ namespace eosio {
             if( a.valid() && b.valid() ) return *a == *b;
             return a.valid() == b.valid();
          }
-         
+
          template<typename Stream>
          friend inline eosio::datastream<Stream>& operator>> (eosio::datastream<Stream>& ds, optional& op)
          {
@@ -239,7 +239,7 @@ namespace eosio {
             }
             return ds;
          }
-         
+
          template<typename Stream>
          friend inline eosio::datastream<Stream>& operator<< (eosio::datastream<Stream>& ds, const optional& op)
          {
@@ -248,19 +248,19 @@ namespace eosio {
             if (valid) ds << *op;
             return ds;
          }
-         
+
       private:
          template<typename U> friend class optional;
          T&       ref()      { return *ptr(); }
          const T& ref()const { return *ptr(); }
-         T*       ptr()      { void* v = &_value[0]; return static_cast<T*>(v); }
-         const T* ptr()const { const void* v = &_value[0]; return static_cast<const T*>(v); }
-         
-         // force alignment... to 8 byte boundaries 
-         uint64_t _value[((sizeof(T)+7)/8)];
-         bool     _valid;
+         T*       ptr()      { return reinterpret_cast<T*>(&_value); }
+         const T* ptr()const { return reinterpret_cast<const T*>(&_value); }
+
+         bool         _valid;
+         storage_type _value;
+
    };
-   
+
    template<typename T>
    bool operator == ( const optional<T>& left, const optional<T>& right ) {
       return (!left == !right) || (!!left && *left == *right);
@@ -277,6 +277,5 @@ namespace eosio {
    bool operator != ( const optional<T>& left, const U& u ) {
       return !left || *left != u;
    }
-   
-} // namespace eosio
 
+} // namespace eosio
