8ab07fd62b48c09dd1f3866b898b62929dceabad
Andrianto Lie <andrianto.lie@ii5.com>
2018-03-29 15:12:11 -0400

    Add unit test for testing schedule version

--- tests/chain_tests/producer_schedule_tests.cpp
+++ tests/chain_tests/producer_schedule_tests.cpp
@@ -1,5 +1,6 @@
 #include <boost/test/unit_test.hpp>
 #include <eosio/testing/tester.hpp>
+#include <boost/range/algorithm.hpp>
 
 using namespace eosio::testing;
 using namespace eosio::chain;
@@ -23,13 +24,15 @@ BOOST_AUTO_TEST_SUITE(producer_schedule_tests)
    };
 
    // Calculate the block num of the next round first block
+   // The new producer schedule will become effective when it's in the block of the next round first block
+   // However, it won't be applied until the effective block num is deemed irreversible
    uint64_t calc_block_num_of_next_round_first_block(const chain_controller& control){
-      auto eff_block_num = control.get_dynamic_global_properties().head_block_number + 1;
+      auto res = control.get_dynamic_global_properties().head_block_number + 1;
       const auto blocks_per_round = control.get_global_properties().active_producers.producers.size() * config::producer_repetitions;
-      while((eff_block_num % blocks_per_round) != 0) {
-         eff_block_num++;
+      while((res % blocks_per_round) != 0) {
+         res++;
       }
-      return eff_block_num;
+      return res;
    };
 
    BOOST_FIXTURE_TEST_CASE( verify_producer_schedule, tester ) try {
@@ -43,9 +46,11 @@ BOOST_AUTO_TEST_SUITE(producer_schedule_tests)
             // Determine expected producer
             const auto& expected_producer = get_expected_producer(current_schedule, current_absolute_slot + 1);
 
+            // The new schedule will only be applied once the effective block num is deemed irreversible
+            const bool is_new_schedule_applied = control->last_irreversible_block_num() > eff_new_prod_schd_block_num;
+
             // Ensure that we have the correct schedule at the right time
-            // The new schedule will only be used once the effective block num is deemed irreversible
-            if (control->last_irreversible_block_num() > eff_new_prod_schd_block_num) {
+            if (is_new_schedule_applied) {
                BOOST_TEST(is_schedule_equal(new_prod_schd, current_schedule));
             } else {
                BOOST_TEST(!is_schedule_equal(new_prod_schd, current_schedule));
@@ -102,5 +107,57 @@ BOOST_AUTO_TEST_SUITE(producer_schedule_tests)
 
    } FC_LOG_AND_RETHROW()
 
+
+   BOOST_FIXTURE_TEST_CASE( verify_header_schedule_version, tester ) try {
+
+      // Utility function to ensure that producer schedule version in the header is correct
+      const auto& confirm_header_schd_ver_correctness = [&](const uint64_t expected_version, const uint64_t eff_new_prod_schd_block_num)  {
+         const uint32_t check_duration = 1000; // number of blocks
+         for (uint32_t i = 0; i < check_duration; ++i) {
+            // The new schedule will only be applied once the effective block num is deemed irreversible
+            const bool is_new_schedule_applied = control->last_irreversible_block_num() > eff_new_prod_schd_block_num;
+
+            // Produce block
+            produce_block();
+
+            // Ensure that the head block header is updated at the right time
+            const auto current_schd_ver = control->head_block_header().schedule_version;
+            if (is_new_schedule_applied) {
+               BOOST_TEST(current_schd_ver == expected_version);
+            } else {
+               BOOST_TEST(current_schd_ver != expected_version);
+            }
+         }
+      };
+
+      // Create producer accounts
+      vector<account_name> producers = {
+              "inita", "initb", "initc", "initd", "inite", "initf", "initg",
+              "inith", "initi", "initj", "initk", "initl", "initm", "initn",
+              "inito", "initp", "initq", "initr", "inits", "initt", "initu"
+      };
+      create_accounts(producers);
+
+      // Send set prods action and confirm schedule correctness
+      set_producers(producers, 1);
+      const auto& eff_first_prod_schd_block_num = calc_block_num_of_next_round_first_block(*control);
+      // Confirm the version is correct
+      confirm_header_schd_ver_correctness(1, eff_first_prod_schd_block_num);
+
+      // Shuffle the producers and set the new one with smaller version
+      boost::range::random_shuffle(producers);
+      set_producers(producers, 0);
+      const auto& eff_second_prod_schd_block_num = calc_block_num_of_next_round_first_block(*control);
+      // Even though we set it with smaller version number, the version should be incremented instead
+      confirm_header_schd_ver_correctness(2, eff_second_prod_schd_block_num);
+
+      // Shuffle the producers and set the new one with much larger version number
+      boost::range::random_shuffle(producers);
+      set_producers(producers, 1000);
+      const auto& eff_third_prod_schd_block_num = calc_block_num_of_next_round_first_block(*control);
+      // Even though we set it with much large version number, the version should be incremented instead
+      confirm_header_schd_ver_correctness(3, eff_third_prod_schd_block_num);
+
+   } FC_LOG_AND_RETHROW()
 
 BOOST_AUTO_TEST_SUITE_END()
