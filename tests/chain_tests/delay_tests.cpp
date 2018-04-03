96d220ec731794bba196b2d3ecfcfdff91e0921a
Bucky Kittinger <larrykittinger@gmail.com>
2018-04-03 15:16:16 -0400

    Moved to wrong line

--- tests/chain_tests/delay_tests.cpp
+++ tests/chain_tests/delay_tests.cpp
@@ -1501,8 +1501,9 @@ BOOST_AUTO_TEST_CASE( canceldelay_test ) { try {
 
    BOOST_REQUIRE_EQUAL(transaction_receipt::executed, trace.status);
    BOOST_REQUIRE_EQUAL(1, trace.deferred_transaction_requests.size());
-   BOOST_REQUIRE_EQUAL(std::string(uint128(sender_id_to_cancel)), std::string(uint128(sender_id_canceled)));
+
    const auto sender_id_canceled = trace.deferred_transaction_requests[0].get<deferred_reference>().sender_id;
+   BOOST_REQUIRE_EQUAL(std::string(uint128(sender_id_to_cancel)), std::string(uint128(sender_id_canceled)));
 
    chain.produce_blocks();
 
