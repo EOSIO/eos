2b1f16e3426c858a46afd9e9c651e320b3ef9182
Paul Calabrese <calabrep@objectcomputing.com>
2018-04-04 16:09:56 -0500

    Fix problems with large json strings in cleos commands

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -398,7 +398,7 @@ chain::action create_unlinkauth(const name& account, const name& code, const nam
 
 fc::variant json_from_file_or_string(const string& file_or_str, fc::json::parse_type ptype = fc::json::legacy_parser)
 {
-   if ( is_regular_file(file_or_str) ) {
+   if ( !boost::istarts_with(file_or_str, "{") && is_regular_file(file_or_str) ) {
       return fc::json::from_file(file_or_str, ptype);
    } else {
       return fc::json::from_string(file_or_str, ptype);
