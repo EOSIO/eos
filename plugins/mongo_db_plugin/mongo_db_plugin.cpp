cb76adc9e9396ad7321b5f142b550b448d588db6
Bart Wyatt <bart.wyatt@block.one>
2018-04-05 09:41:48 -0400

    I should have built before pushing

--- plugins/mongo_db_plugin/mongo_db_plugin.cpp
+++ plugins/mongo_db_plugin/mongo_db_plugin.cpp
@@ -405,7 +405,7 @@ void mongo_db_plugin_impl::_process_block(const block_trace& bt, const signed_bl
                      kvp("transaction_id", trans_id_str),
                      kvp("receiver", act.receiver.to_string()),
                      kvp("action", b_oid{msg_oid}),
-                     kvp("console", act.console);
+                     kvp("console", act.console));
       act_doc.append(kvp("data_access", [&act](bsoncxx::builder::basic::sub_array subarr) {
          for (const auto& data : act.data_access) {
             subarr.append([&data](bsoncxx::builder::basic::sub_document subdoc) {
