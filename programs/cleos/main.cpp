272eba442b7883beaef4d30bf2099436985c65a6
Bucky Kittinger <larrykittinger@gmail.com>
2018-04-05 09:59:56 -0400

    Oops, forgot the not

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -838,7 +838,7 @@ int main( int argc, char** argv ) {
       
       
       fc::read_file_contents(wastPath, wast);
-      FC_ASSERT( wast.empty(), "no wast file found ${f}", ("f", wastPath) );
+      FC_ASSERT( !wast.empty(), "no wast file found ${f}", ("f", wastPath) );
       vector<uint8_t> wasm;
       const string binary_wasm_header("\x00\x61\x73\x6d", 4);
       if(wast.compare(0, 4, binary_wasm_header) == 0) {
