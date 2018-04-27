0ddfc40df4b70d6de9bb8b4c81f11a43562c6f81
arhag <arhag@users.noreply.github.com>
2018-04-27 12:12:02 -0400

    initialize new producers permission structure

--- libraries/chain/controller.cpp
+++ libraries/chain/controller.cpp
@@ -290,6 +290,22 @@ struct controller_impl {
 
       create_native_account( config::nobody_account_name, empty_authority, empty_authority );
       create_native_account( config::producers_account_name, empty_authority, active_producers_authority );
+      const auto& active_permission       = authorization.get_permission({config::producers_account_name, config::active_name});
+      const auto& majority_permission     = authorization.create_permission( config::producers_account_name,
+                                                                             config::majority_producers_permission_name,
+                                                                             active_permission.id,
+                                                                             active_producers_authority,
+                                                                             conf.genesis.initial_timestamp );
+      const auto& minority_permission     = authorization.create_permission( config::producers_account_name,
+                                                                             config::minority_producers_permission_name,
+                                                                             majority_permission.id,
+                                                                             active_producers_authority,
+                                                                             conf.genesis.initial_timestamp );
+      const auto& any_producer_permission = authorization.create_permission( config::producers_account_name,
+                                                                             config::any_producer_permission_name,
+                                                                             minority_permission.id,
+                                                                             active_producers_authority,
+                                                                             conf.genesis.initial_timestamp );
    }
 
    void set_pending_tapos() {
