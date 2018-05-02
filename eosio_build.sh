64e733799151018e97e084654dd36a892ad11596
Bill Hamilton <hamiltonmay@ca.rr.com>
2018-05-02 18:37:56 +0000

    Centos: modified eosio_build_centos.sh to run with updated eosio_build.sh variables

--- eosio_build.sh
+++ eosio_build.sh
@@ -199,6 +199,9 @@
 	. "$FILE"
 
 	printf "\\n\\n>>>>>>>> ALL dependencies sucessfully found or installed . Installing EOS.IO\\n\\n"
+	printf ">>>>>>>> CMAKE_BUILD_TYPE=%s\\n" "${CMAKE_BUILD_TYPE}"
+	printf ">>>>>>>> ENABLE_COVERAGE_TESTING=%s\\n" "${ENABLE_COVERAGE_TESTING}"
+	printf ">>>>>>>> DOXYGEN=%s\\n\\n" "${DOXYGEN}"
 
 	if [ ! -d "${BUILD_DIR}" ]; then
 		if ! mkdir -p "${BUILD_DIR}"
