5b7c5fb7529b6107e2322b825599a7971011819f
Khaled Al-Hassanieh <khaled.alhassanieh@gmail.com>
2018-05-03 13:33:06 -0400

    Moved checks location in transfer

--- contracts/eosio.token/eosio.token.cpp
+++ contracts/eosio.token/eosio.token.cpp
@@ -75,11 +75,11 @@ void token::transfer( account_name from,
     require_recipient( from );
     require_recipient( to );
 
-    if ( quantity.symbol.precision() != st.supply.symbol.precision() )
-       quantity.adjust_precision( st.supply.symbol );
-
     eosio_assert( quantity.is_valid(), "invalid quantity" );
     eosio_assert( quantity.amount > 0, "must transfer positive quantity" );
+
+    if ( quantity.symbol.precision() != st.supply.symbol.precision() )
+       quantity.adjust_precision( st.supply.symbol );
 
     sub_balance( from, quantity, st );
     add_balance( to, quantity, st, from );
