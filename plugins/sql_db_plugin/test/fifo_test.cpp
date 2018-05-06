345cf0f88424d840d1bffdaec6ac946df5e341e8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-05-06 08:52:01 +0200

    [sql_db_plugin] tests added

--- plugins/sql_db_plugin/test/fifo_test.cpp
+++ plugins/sql_db_plugin/test/fifo_test.cpp
@@ -6,19 +6,34 @@ using namespace eosio;
 
 BOOST_AUTO_TEST_SUITE(fifo_test)
 
-BOOST_AUTO_TEST_CASE(pop_from_empty_fifo)
+BOOST_AUTO_TEST_CASE(pop_empty_fifo_not_blocking)
 {
     fifo<int> f(fifo<int>::behavior::not_blocking);
-    f.pop_all();
+    auto v = f.pop_all();
+    BOOST_TEST(v.size() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(change_to_not_blocking)
+{
+    fifo<int> f(fifo<int>::behavior::blocking);
+    f.push(1);
+    f.push(2);
+    f.push(3);
+    auto v = f.pop_all();
+    BOOST_TEST(v.size() == 3);
+    f.set_behavior(fifo<int>::behavior::not_blocking);
+    v = f.pop_all();
+    BOOST_TEST(v.size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(pushing_2_int_pop_2_int)
 {
-//    fifo<int> f;
-//    f.push(1);
-//    f.push(2);
-//    BOOST_TEST(1 == f.pop());
-//    BOOST_TEST(2 == f.pop());
+    fifo<int> f(fifo<int>::behavior::not_blocking);
+    f.push(1);
+    f.push(2);
+    auto v = f.pop_all();
+    BOOST_TEST(1 == v.at(0));
+    BOOST_TEST(2 == v.at(1));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
