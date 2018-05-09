3a6b9936c93dd0ae8fa8f9c17452031e628ed33a
Andrianto Lie <andrianto.lie@ii5.com>
2018-05-09 18:05:56 +0800

    Add docs for some smart contract types

--- contracts/eosiolib/public_key.hpp
+++ contracts/eosiolib/public_key.hpp
@@ -3,10 +3,36 @@
 #include <eosiolib/serialize.hpp>
 
 namespace eosio {
+
+   /**
+   *  @defgroup publickeytype Public Key Type
+   *  @ingroup types
+   *  @brief Specifies Public Key Type
+   *
+   *  @{
+   */
+   
+   /**
+    * EOSIO Public Key
+    * @brief EOSIO Public Key
+    */
    struct public_key {
+      /**
+       * Type of the public key, could be either K1 or R1
+       * @brief Type of the public key
+       */
       unsigned_int        type;
+
+      /**
+       * Bytes of the public key
+       * 
+       * @brief Bytes of the public key
+       */
       std::array<char,33> data;
 
       EOSLIB_SERIALIZE( public_key, (type)(data) )
    };
+   
 }
+
+/// @} publickeytype
