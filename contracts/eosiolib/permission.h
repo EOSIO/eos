242f43a2a6899222cf378ac5a34604e1578a77e0
arhag <arhag@users.noreply.github.com>
2018-05-09 18:14:47 -0400

    add tests for get_permission_last_used and get_account_creation_time #2794

--- contracts/eosiolib/permission.h
+++ contracts/eosiolib/permission.h
@@ -28,7 +28,7 @@ extern "C" {
     *  @brief Checks if a permission is authorized by a provided delay and a provided set of keys and permissions
     *
     *  @param account    - the account owner of the permission
-    *  @param permission - the permission name to check for authorization
+    *  @param permission - the name of the permission to check for authorization
     *  @param pubkeys_data - pointer to the start of the serialized vector of provided public keys
     *  @param pubkeys_size  - size (in bytes) of serialized vector of provided public keys (can be 0 if no public keys are to be provided)
     *  @param perms_data - pointer to the start of the serialized vector of provided permissions (empty permission name acts as wildcard)
@@ -44,4 +44,25 @@ extern "C" {
                                    const char* perms_data,   uint32_t perms_size,
                                    uint64_t delay_us
                                  );
+
+   /**
+    *  @brief Returns the last used time of a permission
+    *
+    *  @param account    - the account owner of the permission
+    *  @param permission - the name of the permission
+    *
+    *  @return the last used time (in microseconds since Unix epoch) of the permission
+    */
+   int64_t get_permission_last_used( account_name account, permission_name permission );
+
+
+   /**
+    *  @brief Returns the creation time of an account
+    *
+    *  @param account    - the account
+    *
+    *  @return the creation time (in microseconds since Unix epoch) of the account
+    */
+   int64_t get_account_creation_time( account_name account );
+
 }
