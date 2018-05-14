4cbccaa7437c14f70b6e0f98a5d6ce0861b98e64
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-05-14 22:10:07 +0200

    [sql_db_plugin] accounts table created

--- plugins/sql_db_plugin/db/accounts_table.cpp
+++ plugins/sql_db_plugin/db/accounts_table.cpp
@@ -1,5 +1,7 @@
 #include "accounts_table.h"
 
+#include <fc/log/logger.hpp>
+
 namespace eosio {
 
 accounts_table::accounts_table(std::shared_ptr<soci::session> session):
@@ -13,14 +15,21 @@ void accounts_table::drop()
     try {
         *m_session << "drop table accounts";
     }
-    catch(...){
-
+    catch(std::exception& e){
+        wlog(e.what());
     }
 }
 
 void accounts_table::create()
 {
-    *m_session << "create table accounts(name TEXT)";
+    *m_session << "create table accounts("
+                  "name TEXT,"
+                  "eos_balance REAL,"
+                  "staked_balance REAL,"
+                  "unstaking_balance REAL,"
+                  "abi TEXT,"
+                  "created_at DATETIME,"
+                  "updated_at DATETIME)";
 }
 
 } // namespace
