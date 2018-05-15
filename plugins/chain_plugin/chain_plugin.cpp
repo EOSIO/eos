9d383a2f42c6e39c7b43bd16929615d07cec4634
enumivo <admin@enumivo.org>
2018-05-16 07:50:07 +0800

    rename EOS_ASSERT

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -594,7 +594,7 @@ read_only::abi_json_to_bin_result read_only::abi_json_to_bin( const read_only::a
    if( abi_serializer::to_abi(code_account->abi, abi) ) {
       abi_serializer abis( abi );
       auto action_type = abis.get_action_type(params.action);
-      EOS_ASSERT(!action_type.empty(), action_validate_exception, "Unknown action ${action} in contract ${contract}", ("action", params.action)("contract", params.code));
+      ENU_ASSERT(!action_type.empty(), action_validate_exception, "Unknown action ${action} in contract ${contract}", ("action", params.action)("contract", params.code));
       try {
          result.binargs = abis.variant_to_binary(action_type, params.args);
       } ENU_RETHROW_EXCEPTIONS(chain::invalid_action_args_exception,
