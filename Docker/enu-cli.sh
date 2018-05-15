a04bf0d362f29a581a3466d161df6a99b9a55da6
enumivo <admin@enumivo.org>
2018-05-15 14:56:30 +0800

    rename cleos

--- Docker/enu-cli.sh
+++ Docker/enu-cli.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Usage:
-# Go into cmd loop: sudo ./cleos.sh
-# Run single cmd:  sudo ./cleos.sh <cleos paramers>
+# Go into cmd loop: sudo ./enu-cli.sh
+# Run single cmd:  sudo ./enu-cli.sh <enu-cli paramers>
 
-PREFIX="docker exec docker_nodeos_1 cleos"
+PREFIX="docker exec docker_nodeos_1 enu-cli"
 if [ -z $1 ] ; then
   while :
   do
-    read -e -p "cleos " cmd
+    read -e -p "enu-cli " cmd
     history -s "$cmd"
     $PREFIX $cmd
   done
