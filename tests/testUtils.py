fae86592f9a7ddb0a5820d4a5920d7a5d14d6404
Ciju John <johnc@objectcomputing.com>
2018-05-16 11:23:11 -0500

    Remove hard coded launcher nogen parameter.

--- tests/testUtils.py
+++ tests/testUtils.py
@@ -1276,7 +1276,7 @@ class Cluster(object):
         if len(self.nodes) > 0:
             raise RuntimeError("Cluster already running.")
 
-        cmd="%s -p %s -n %s -s %s -d %s -f --nogen" % (
+        cmd="%s -p %s -n %s -s %s -d %s -f" % (
             Utils.EosLauncherPath, pnodes, totalNodes, topo, delay)
         cmdArr=cmd.split()
         if self.staging:
