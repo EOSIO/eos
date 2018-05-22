8954d0ddb59ba0139fb0c0849b069f13cfc0bb25
enumivo <admin@enumivo.org>
2018-05-22 11:12:39 +0800

    update ram

--- unittests/ram_tests.cpp
+++ unittests/ram_tests.cpp
@@ -91,7 +91,7 @@ BOOST_FIXTURE_TEST_CASE(ram_tests, enumivo_system::enumivo_system_tester) { try 
                         ("payer", "testram11111")
                         ("from", 1)
                         ("to", 10)
-                        ("size", 1915));//("size", 1910));
+                        ("size", 1900));//("size", 1910));
    produce_blocks(1);
    auto ram_usage = rlm.get_account_ram_usage(N(testram11111));
 
