00ca0bda60895348f02ca7a8cd754a356a9c2bce
enumivo <admin@enumivo.org>
2018-05-23 22:21:24 +0800

    update cleos

--- Docker/cleos.sh
+++ Docker/cleos.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Usage:
-# Go into cmd loop: sudo ./cleos.sh
-# Run single cmd:  sudo ./cleos.sh <cleos paramers>
+# Go into cmd loop: sudo ./enucli.sh
+# Run single cmd:  sudo ./enucli.sh <enucli paramers>
 
-PREFIX="docker-compose exec nodeosd cleos"
+PREFIX="docker-compose exec enunoded enucli"
 if [ -z $1 ] ; then
   while :
   do
-    read -e -p "cleos " cmd
+    read -e -p "enucli " cmd
     history -s "$cmd"
     $PREFIX $cmd
   done
