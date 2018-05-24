b000a07855258af5f0e6d3b0e1b7a381a4cb2bff
enumivo <admin@enumivo.org>
2018-05-24 10:37:30 +0800

    revert stringer

--- libraries/chain/name.cpp
+++ libraries/chain/name.cpp
@@ -19,12 +19,12 @@ namespace enumivo { namespace chain {
    name::operator string()const {
      static const char* charmap = ".12345abcdefghijklmnopqrstuvwxyz";
 
-      string str(15,'.');
+      string str(13,'.');
 
       uint64_t tmp = value;
-      for( uint32_t i = 0; i <= 14; ++i ) {
+      for( uint32_t i = 0; i <= 12; ++i ) {
          char c = charmap[tmp & (i == 0 ? 0x0f : 0x1f)];
-         str[14-i] = c;
+         str[12-i] = c;
          tmp >>= (i == 0 ? 4 : 5);
       }
 
