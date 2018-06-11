ed89ef7d46a79cf9d49ce6bc183cfd9f5463725e
Don Johnny <don.johnny.cn@gmail.com>
2018-06-11 10:34:38 +0800

    highlight the prompt head

--- Docker/cleos.sh
+++ Docker/cleos.sh
@@ -8,7 +8,7 @@ PREFIX="docker exec docker_nodeos_1 cleos"
 if [ -z $1 ] ; then
   while :
   do
-    read -e -p "cleos " cmd
+    read -e -p "$(echo -e '\033[41;37m cleos >> \033[0m ')" cmd
     history -s "$cmd"
     $PREFIX $cmd
   done
