b43fa98d24000b2445dc6aadebccdf829f599a50
enumivo <admin@enumivo.org>
2018-06-21 11:10:43 +0800

    set value before returning

--- libraries/chain/name.cpp
+++ libraries/chain/name.cpp
@@ -8,8 +8,12 @@ namespace enumivo { namespace chain {
 
    void name::set( const char* str ) {
       const auto len = strnlen(str, 14);
-      if (string(str) == "enumivo.prods")
+
+      if (string(str) == "enumivo.prods"){
+        value = string_to_name(str);
         return; //allow special account
+      }
+
       ENU_ASSERT(len <= 13, name_type_exception, "Name is longer than 13 characters (${name}) ", ("name", string(str)));
       value = string_to_name(str);
       ENU_ASSERT(to_string() == string(str), name_type_exception,
@@ -19,15 +23,15 @@ namespace enumivo { namespace chain {
 
    // keep in sync with name::to_string() in contract definition for name
    name::operator string()const {
-     static const char* charmap = ".12345abcdefghijklmnopqrstuvwxyz";
+      static const char* charmap = ".12345abcdefghijklmnopqrstuvwxyz";
 
       string str(13,'.');
 
       uint64_t tmp = value;
       for( uint32_t i = 0; i <= 12; ++i ) {
-         char c = charmap[tmp & (i == 0 ? 0x0f : 0x1f)];
-         str[12-i] = c;
-         tmp >>= (i == 0 ? 4 : 5);
+          char c = charmap[tmp & (i == 0 ? 0x0f : 0x1f)];
+          str[12-i] = c;
+          tmp >>= (i == 0 ? 4 : 5);
       }
 
       boost::algorithm::trim_right_if( str, []( char c ){ return c == '.'; } );
