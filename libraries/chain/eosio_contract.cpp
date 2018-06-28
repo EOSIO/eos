5c477a17182679a82ad128adcf90d4a3d8208264
Andrianto Lie <andrianto.lie@ii5.com>
2018-06-28 10:21:35 +0800

    Show the linked action inside deleting linked authority error message

--- libraries/chain/eosio_contract.cpp
+++ libraries/chain/eosio_contract.cpp
@@ -282,7 +282,8 @@ void apply_eosio_deleteauth(apply_context& context) {
       const auto& index = db.get_index<permission_link_index, by_permission_name>();
       auto range = index.equal_range(boost::make_tuple(remove.account, remove.permission));
       EOS_ASSERT(range.first == range.second, action_validate_exception,
-                 "Cannot delete a linked authority. Unlink the authority first");
+                 "Cannot delete a linked authority. Unlink the authority first. This authority is linked to ${code}::${type}.", 
+                 ("code", string(range.first->code))("type", string(range.first->message_type)));
    }
 
    const auto& permission = authorization.get_permission({remove.account, remove.permission});
