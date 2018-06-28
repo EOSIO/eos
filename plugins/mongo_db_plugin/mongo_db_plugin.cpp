cf5552e782c679c637e703bcca708d7f517dfe50
Ciju John <johnc@objectcomputing.com>
2018-06-28 12:30:12 -0500

    Remove destructor log statements as logs may not be avilable here under some circumstance.

--- plugins/mongo_db_plugin/mongo_db_plugin.cpp
+++ plugins/mongo_db_plugin/mongo_db_plugin.cpp
@@ -866,15 +866,10 @@ mongo_db_plugin_impl::mongo_db_plugin_impl()
 }
 
 mongo_db_plugin_impl::~mongo_db_plugin_impl() {
-   try {
-      ilog("mongo_db_plugin shutdown in process please be patient this can take a few minutes");
-      done = true;
-      condition.notify_one();
+   done = true;
+   condition.notify_one();
 
-      consume_thread.join();
-   } catch (std::exception& e) {
-      elog("Exception on mongo_db_plugin shutdown of consume thread: ${e}", ("e", e.what()));
-   }
+   consume_thread.join();
 }
 
 void mongo_db_plugin_impl::wipe_database() {
