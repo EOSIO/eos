33575cf523c313a5239a3e8c11d713f580860eb2
Anton Perkov <anton.perkov@block.one>
2018-07-02 18:04:38 -0400

    temporary change reverted #4196

--- libraries/chain/fork_database.cpp
+++ libraries/chain/fork_database.cpp
@@ -251,7 +251,7 @@ namespace eosio { namespace chain {
 
       auto itr = my->index.find( h->id );
       if( itr != my->index.end() ) {
-         irreversible(*itr); //XXX
+         irreversible(*itr);
          my->index.erase(itr);
       }
 
