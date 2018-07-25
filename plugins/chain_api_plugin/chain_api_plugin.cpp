bffb66e1cf52233d8ebe3bd7559f8db9bcb2b360
Andrianto Lie <andrianto.lie@ii5.com>
2018-07-25 10:22:30 +0800

    Add Get/ Set Whitelist Blacklist RPC API

--- plugins/chain_api_plugin/chain_api_plugin.cpp
+++ plugins/chain_api_plugin/chain_api_plugin.cpp
@@ -94,6 +94,8 @@ void chain_api_plugin::plugin_startup() {
       CHAIN_RO_CALL(abi_json_to_bin, 200),
       CHAIN_RO_CALL(abi_bin_to_json, 200),
       CHAIN_RO_CALL(get_required_keys, 200),
+      CHAIN_RO_CALL(get_whitelist_blacklist, 200),
+      CHAIN_RW_CALL(set_whitelist_blacklist, 200),
       CHAIN_RW_CALL_ASYNC(push_block, chain_apis::read_write::push_block_results, 202),
       CHAIN_RW_CALL_ASYNC(push_transaction, chain_apis::read_write::push_transaction_results, 202),
       CHAIN_RW_CALL_ASYNC(push_transactions, chain_apis::read_write::push_transactions_results, 202)
