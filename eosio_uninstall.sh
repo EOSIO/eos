d2d42f910d59a902ef0f998c6106b14b41a9ca76
Bucky Kittinger <larrykittinger@gmail.com>
2018-07-25 15:14:46 -0400

    Update eosio_uninstall.sh

--- eosio_uninstall.sh
+++ eosio_uninstall.sh
@@ -15,7 +15,7 @@ if [ -d "/usr/local/eosio" ]; then
       case $yn in
          [Yy]* )
             if [ "$(id -u)" -ne 0 ]; then
-               printf "\n\tThis requires sudo, please run ./scripts/clean_old_install.sh with sudo\n\n"
+               printf "\n\tThis requires sudo, please run ./eosio_uninstall.sh with sudo\n\n"
                exit -1
             fi
 
