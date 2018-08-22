042a341741be24d8b90781e58100fb270ffb239d
NorseGaud <connarpierce@gmail.com>
2018-08-22 18:49:20 -0400

    -full to delete directories with configs on uninstall

--- eosio_uninstall.sh
+++ eosio_uninstall.sh
@@ -26,9 +26,15 @@ if [ -d "/usr/local/eosio" ]; then
             for binary in ${binaries[@]}; do
                rm ${binary}
             done
+            # Handle cleanup of directories created from installation
+            if [ "$1" == "--full" ]; then
+               if [ -d ~/Library/Application\ Support/eosio ]; then rm -rf ~/Library/Application\ Support/eosio; fi # Mac OS
+               if [ -d ~/.local/share/eosio ]; then rm -rf ~/.local/share/eosio; fi # Linux
+               if [ -d /usr/local/eosio ]; then rm -rf /usr/local/eosio; fi
+            fi
             popd &> /dev/null
             break;;
-         [Nn]* ) 
+         [Nn]* )
             printf "\tAborting uninstall\n\n"
             exit -1;;
       esac
