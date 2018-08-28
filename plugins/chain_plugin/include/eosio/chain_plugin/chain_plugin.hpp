184d24b74879ae516b2fb5bd520ef3fcc3d27f16
Marlon Williams <marlonjwilliams@gmail.com>
2018-08-28 10:09:51 -0700

    Simplify Wallet Tools EOSIO Blockchain Detection

--- plugins/chain_plugin/include/eosio/chain_plugin/chain_plugin.hpp
+++ plugins/chain_plugin/include/eosio/chain_plugin/chain_plugin.hpp
@@ -97,6 +97,8 @@ public:
       //string                  recent_slots;
       //double                  participation_rate = 0;
       optional<string>        server_version_string;
+      string                  core_symbol;
+      uint64_t                precision;
    };
    get_info_results get_info(const get_info_params&) const;
 
@@ -615,7 +617,7 @@ private:
 FC_REFLECT( eosio::chain_apis::permission, (perm_name)(parent)(required_auth) )
 FC_REFLECT(eosio::chain_apis::empty, )
 FC_REFLECT(eosio::chain_apis::read_only::get_info_results,
-(server_version)(chain_id)(head_block_num)(last_irreversible_block_num)(last_irreversible_block_id)(head_block_id)(head_block_time)(head_block_producer)(virtual_block_cpu_limit)(virtual_block_net_limit)(block_cpu_limit)(block_net_limit)(server_version_string) )
+(server_version)(chain_id)(head_block_num)(last_irreversible_block_num)(last_irreversible_block_id)(head_block_id)(head_block_time)(head_block_producer)(virtual_block_cpu_limit)(virtual_block_net_limit)(block_cpu_limit)(block_net_limit)(server_version_string)(core_symbol)(precision) )
 FC_REFLECT(eosio::chain_apis::read_only::get_block_params, (block_num_or_id))
 FC_REFLECT(eosio::chain_apis::read_only::get_block_header_state_params, (block_num_or_id))
 
