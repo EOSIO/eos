013acac499a128a409ea8dd644cdc7520f99543d
Bart Wyatt <bart.wyatt@block.one>
2018-09-13 10:26:28 -0400

    beginnings of unit test for snapshot

---
+++ unittests/snapshot_tests.cpp
@@ -0,0 +1,22 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+
+#include <boost/test/unit_test.hpp>
+#include <eosio/testing/tester.hpp>
+
+using namespace eosio;
+using namespace testing;
+using namespace chain;
+
+BOOST_AUTO_TEST_SUITE(snapshot_tests)
+
+BOOST_AUTO_TEST_CASE(test_multi_index_snapshot)
+{
+   tester main;
+
+
+}
+
+BOOST_AUTO_TEST_SUITE_END()
