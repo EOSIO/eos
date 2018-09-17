5ce441c7388a3ed20c1ff05370e01b1b74cdf92d
Andrew Falaleev <andrew.falaleev@gmail.com>
2018-09-17 12:04:01 +0700

    Base implementation with external API. #2

--- tests/chaindb/main.cpp
+++ tests/chaindb/main.cpp
@@ -1,7 +1,45 @@
-#include <iostream>
+
+
 #include "multi_index.hpp"
+#include <iostream>
+
+
+struct hello_object {
+    primary_key_t id;
+    uint64_t age;
+    std::string name;
+
+    primary_key_t primary_key() const {
+        return id;
+    }
+
+    const std::string& by_name() const {
+        return name;
+    }
+};
+
+FC_REFLECT(hello_object, (id)(age)(name))
+
+using hello_index =
+    chaindb::multi_index<
+        N(hello), chaindb::primary_key_extractor, hello_object,
+        chaindb::indexed_by<
+            N(name), chaindb::const_mem_fun<hello_object, const std::string&, &hello_object::by_name>>>;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    hello_index hidx(N(test), N(test));
+
+    auto idx = hidx.get_index<N(name)>();
+    auto itr = idx.find("monro");
+
+    if (idx.end() == itr) {
+        hidx.emplace(N(test),[&](auto& o){
+            o.id = hidx.available_primary_key();
+            o.name = "monro";
+            o.age = 10;
+        });
+    }
+
+    //std::cout << "Hello, World!" << std::endl;
     return 0;
 }
