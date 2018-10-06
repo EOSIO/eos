4ee421ebee87482df29ae350f87512ade2d70fe2
Jared Rice Sr <jaredricelegal@gmail.com>
2018-10-06 03:04:08 -0500

    Updated misc tests

--- unittests/misc_tests.cpp
+++ unittests/misc_tests.cpp
@@ -593,22 +593,22 @@ BOOST_AUTO_TEST_CASE(transaction_test) { try {
    variant pretty_trx = fc::mutable_variant_object()
       ("actions", fc::variants({
          fc::mutable_variant_object()
-            ("account", "eosio")
+            ("account", "arisen")
             ("name", "reqauth")
             ("authorization", fc::variants({
                fc::mutable_variant_object()
-                  ("actor", "eosio")
+                  ("actor", "arisen")
                   ("permission", "active")
             }))
             ("data", fc::mutable_variant_object()
-               ("from", "eosio")
+               ("from", "arisen")
             )
          })
       )
       // lets also push a context free action, the multi chain test will then also include a context free action
       ("context_free_actions", fc::variants({
          fc::mutable_variant_object()
-            ("account", "eosio")
+            ("account", "arisen")
             ("name", "nonce")
             ("data", fc::raw::pack(std::string("dummy")))
          })
