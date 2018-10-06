18a37e15a8e2c92097c0ada7244bf9806d40dcb0
Jared Rice Sr <jaredricelegal@gmail.com>
2018-10-06 02:41:44 -0500

    Updated eos-tmp to arisen-tmp

--- libraries/utilities/tempdir.cpp
+++ libraries/utilities/tempdir.cpp
@@ -14,7 +14,7 @@ fc::path temp_directory_path()
    const char* eos_tempdir = getenv("EOS_TEMPDIR");
    if( eos_tempdir != nullptr )
       return fc::path( eos_tempdir );
-   return fc::temp_directory_path() / "eos-tmp";
+   return fc::temp_directory_path() / "arisen-tmp";
 }
 
 } } // arisen::utilities
