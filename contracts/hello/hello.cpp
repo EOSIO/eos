acffc31a13c9fd9a04847bf8abfb1fdc6b7ae6fb
LoveJuzi <jike080401@163.com>
2018-10-10 12:58:41 +0800

    删除测试语句

--- contracts/hello/hello.cpp
+++ contracts/hello/hello.cpp
@@ -15,17 +15,14 @@ public:
 
    /// @abi actoin
    void addword( string word, string desc, uint64_t id ) {
-      print( word, "\t", desc, "\t", id );
    }
 
    /// @abi action
    void delword( string word ) {
-      print( word );
    }
 
    /// @abi action
    void modword( string word, uint64_t id ) {
-      print( word, "\t", id );
    }
 
    /// @abi action
