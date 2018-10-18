b6f0bf68c332e5dfed5fc7743b66695c43347a98
Brian Johnson <johnsonb@objectcomputing.com>
2018-10-18 14:41:56 -0500

    Removed unneeded includes. GH #6041

--- unittests/wasm_tests.cpp
+++ unittests/wasm_tests.cpp
@@ -16,9 +16,6 @@
 #include <noop/noop.wast.hpp>
 #include <noop/noop.abi.hpp>
 
-#include <eosio.system/eosio.system.wast.hpp>
-#include <eosio.system/eosio.system.abi.hpp>
-
 #include <fc/io/fstream.hpp>
 
 #include <Runtime/Runtime.h>
