a94702af9c74384eb428c587733f905316f4a1e0
Kevin Heifner <heifnerk@objectcomputing.com>
2018-10-19 13:23:36 -0400

    Simple test for thread_pool

--- unittests/thread_pool_tests.cpp
+++ unittests/thread_pool_tests.cpp
@@ -17,29 +17,20 @@ BOOST_AUTO_TEST_SUITE(thread_pool_tests)
 BOOST_AUTO_TEST_CASE(basic)
 { try {
 
-   fc::thread_pool pool;
-   pool.init(5);
+      fc::thread_pool pool;
+      pool.init( 5 );
 
-   auto f1 = pool.async(//<fc::flat_set<eosio::chain::public_key_type>>(
-         [](){
-      fc::flat_set<eosio::chain::public_key_type> tmp;
-      tmp.emplace();
-      return tmp;
-   });
+      auto f1 = pool.async( []() {
+         fc::flat_set<eosio::chain::public_key_type> tmp;
+         tmp.emplace();
+         return tmp;
+      } );
 
-//      auto f2 = pool.async_t([](){
-//         fc::flat_set<eosio::chain::public_key_type> tmp;
-//         tmp.emplace();
-//         return tmp;
-//      });
+      auto set1 = f1.get();
 
-   auto set1 = f1.get();
-//   auto set2 = f2.get();
+      BOOST_CHECK_EQUAL( 1, set1.size());
 
-   BOOST_CHECK_EQUAL( 1, set1.size() );
-
-   pool.stop();
-
+      pool.stop();
 
 } FC_LOG_AND_RETHROW() }
 
