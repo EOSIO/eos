a4c41435d3bc2d2aa3e6e20e079bd4563599640a
Kevin Heifner <heifnerk@objectcomputing.com>
2018-10-22 16:31:17 -0500

    test boost::asio::thread_pool

--- unittests/thread_pool_tests.cpp
+++ unittests/thread_pool_tests.cpp
@@ -10,9 +10,19 @@
 #include <fc/exception/exception.hpp>
 
 #include <boost/test/unit_test.hpp>
+#include <boost/asio/thread_pool.hpp>
+#include <boost/asio.hpp>
 
 
 BOOST_AUTO_TEST_SUITE(thread_pool_tests)
+
+template<typename F>
+auto async( boost::asio::thread_pool& pool, F&& f ) {
+   auto task = std::make_shared<std::packaged_task<decltype( f() )()>>( std::forward<F>(f) );
+   boost::asio::post( pool, [task]() { (*task)(); } );
+   return task->get_future();
+}
+
 
 BOOST_AUTO_TEST_CASE(basic)
 { try {
@@ -31,6 +41,21 @@ BOOST_AUTO_TEST_CASE(basic)
       BOOST_CHECK_EQUAL( 1, set1.size());
 
       pool.stop();
+
+      boost::asio::thread_pool p(5);
+
+      auto f2 = async( p, []() {
+         fc::flat_set<eosio::chain::public_key_type> tmp;
+         tmp.emplace();
+         return tmp;
+      });
+
+      auto set2 = f2.get();
+
+      BOOST_CHECK_EQUAL( 1, set2.size());
+
+      p.join();
+      p.stop();
 
 } FC_LOG_AND_RETHROW() }
 
