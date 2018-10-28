407b4c63be7239cb0c461cb1d7a0b3f6c6b626c3
Mário Silva <mmcs@outlook.pt>
2018-10-28 01:40:55 +0000

    covert kvo to json

--- plugins/statetrack_plugin/include/eosio/statetrack_plugin/statetrack_plugin.hpp
+++ plugins/statetrack_plugin/include/eosio/statetrack_plugin/statetrack_plugin.hpp
@@ -4,7 +4,9 @@
  */
 #pragma once
 
-#include <eosio/http_plugin/http_plugin.hpp>
+#include <fc/variant.hpp>
+#include <fc/io/json.hpp>
+#include <eosio/chain/contract_table_objects.hpp>
 #include <eosio/chain_plugin/chain_plugin.hpp>
 
 #include <appbase/application.hpp>
@@ -13,24 +15,43 @@ namespace eosio {
 
 using namespace appbase;
 
+enum op_type_enum { 
+   CREATE, 
+   MODIFY, 
+   REMOVE 
+};
+
+struct db_op_row {
+   uint64_t            id;
+   op_type_enum        op_type;
+   account_name        code;
+   chain::scope_name   scope;
+   chain::table_name   table;
+   account_name        payer;
+   fc::variant         value;
+};
+
 class statetrack_plugin : public plugin<statetrack_plugin> {
 public:
-   APPBASE_PLUGIN_REQUIRES((http_plugin) (chain_plugin))
+   APPBASE_PLUGIN_REQUIRES((chain_plugin))
 
-   statetrack_plugin() = default;
+   statetrack_plugin();
    statetrack_plugin(const statetrack_plugin&) = delete;
    statetrack_plugin(statetrack_plugin&&) = delete;
    statetrack_plugin& operator=(const statetrack_plugin&) = delete;
    statetrack_plugin& operator=(statetrack_plugin&&) = delete;
    virtual ~statetrack_plugin() override = default;
 
-   virtual void set_program_options(options_description& cli, options_description& cfg) override {}
-   void plugin_initialize(const variables_map& vm) {}
+   virtual void set_program_options(options_description& cli, options_description& cfg) override;
+   void plugin_initialize(const variables_map& options);
    void plugin_startup();
-   void plugin_shutdown() {}
+   void plugin_shutdown();
 
 private:
-        std::unique_ptr<class statetrack_plugin_impl> my;
+   std::unique_ptr<class statetrack_plugin_impl> my;
 };
 
 }
+
+FC_REFLECT_ENUM( eosio::op_type_enum, (CREATE)(MODIFY)(REMOVE) )
+FC_REFLECT( eosio::db_op_row, (id)(op_type)(code)(scope)(table)(payer)(value) )
--- plugins/statetrack_plugin/statetrack_plugin.cpp
+++ plugins/statetrack_plugin/statetrack_plugin.cpp
@@ -3,9 +3,6 @@
  *  @copyright defined in eos/LICENSE.txt
  */
 
-#include <fc/variant.hpp>
-#include <fc/io/json.hpp>
-#include <eosio/chain/contract_table_objects.hpp>
 #include <eosio/statetrack_plugin/statetrack_plugin.hpp>
 
 namespace eosio {
@@ -14,46 +11,129 @@ static appbase::abstract_plugin& _statetrack_plugin = app().register_plugin<stat
 
 using namespace eosio;
 
+
 typedef typename chainbase::get_index_type<chain::key_value_object>::type kv_index_type;
 
 class statetrack_plugin_impl {
    public:
-     abi_def get_kvo_abi( const chainbase::database& db, const chain::key_value_object& tio );
-     
+     const chain::table_id_object& get_kvo_tio(const chainbase::database& db, const chain::key_value_object& kvo);
+     const chain::account_object& get_tio_co(const chainbase::database& db, const chain::table_id_object& tio);
+     const abi_def& get_co_abi(const chain::account_object& co );
+     fc::variant get_kvo_row(const chainbase::database& db, const chain::table_id_object& tio, const chain::key_value_object& kvo, bool json = true);
+     db_op_row get_db_op_row(const chainbase::database& db, const chain::key_value_object& kvo, op_type_enum op_type, bool json = true);
+
+     static void copy_inline_row(const chain::key_value_object& obj, vector<char>& data) {
+        data.resize( obj.value.size() );
+        memcpy( data.data(), obj.value.data(), obj.value.size() );
+    }
+    private:
+        fc::microseconds abi_serializer_max_time = fc::microseconds(1000 * 1000);
+        bool shorten_abi_errors = true;
 };
 
-abi_def statetrack_plugin_impl::get_kvo_abi( const chainbase::database& db, const chain::key_value_object& kvo ) {
-   const auto &d = db.db();
+// statetrack plugin implementation
 
-   const chain::table_id_object *tio = d.find<chain::table_id_object>(kvo.t_id);
-   EOS_ASSERT(tio != nullptr, chain::account_query_exception, "Fail to retrieve table_id_object for ${t_id}", ("t_id", kvo.t_id) );
-   
-   const account_object *co = d.find<chain::account_object, by_name>(tio->code);
-   EOS_ASSERT(co != nullptr, chain::account_query_exception, "Fail to retrieve account for ${account}", ("account", account) );
-   
-   abi_def abi;
-   abi_serializer::to_abi(co->abi, abi);
-   return abi;
+const chain::table_id_object& statetrack_plugin_impl::get_kvo_tio(const chainbase::database& db, const chain::key_value_object& kvo) {
+    const chain::table_id_object *tio = db.find<chain::table_id_object>(kvo.t_id);
+    EOS_ASSERT(tio != nullptr, chain::contract_table_query_exception, "Fail to retrieve table for ${t_id}", ("t_id", kvo.t_id) );
+    return *tio;
 }
-   
+
+const chain::account_object& statetrack_plugin_impl::get_tio_co(const chainbase::database& db, const chain::table_id_object& tio) {
+    const chain::account_object *co = db.find<chain::account_object, chain::by_name>(tio.code);
+    EOS_ASSERT(co != nullptr, chain::account_query_exception, "Fail to retrieve account for ${code}", ("code", tio.code) );
+    return *co;
+}
+
+const abi_def& statetrack_plugin_impl::get_co_abi( const chain::account_object& co ) {   
+   abi_def* abi = new abi_def();
+   abi_serializer::to_abi(co.abi, *abi);
+   return *abi;
+}
+
+fc::variant statetrack_plugin_impl::get_kvo_row(const chainbase::database& db, const chain::table_id_object& tio, const chain::key_value_object& kvo, bool json) {
+    vector<char> data;
+    copy_inline_row(kvo, data);
+
+    if (json) {
+        const chain::account_object& co = get_tio_co(db, tio);
+        const abi_def& abi = get_co_abi(co);
+
+        abi_serializer abis;
+        abis.set_abi(abi, abi_serializer_max_time);
+
+        return abis.binary_to_variant( abis.get_table_type(tio.table), data, abi_serializer_max_time, shorten_abi_errors );
+    } else {
+        return fc::variant(data);
+    }
+}
+
+db_op_row statetrack_plugin_impl::get_db_op_row(const chainbase::database& db, const chain::key_value_object& kvo, op_type_enum op_type, bool json) {
+    db_op_row row;
+
+    const chain::table_id_object& tio = get_kvo_tio(db, kvo);
+
+    row.id = kvo.primary_key;
+    row.op_type = op_type;
+    row.code = tio.code;
+    row.scope = tio.scope;
+    row.table = tio.table;
+
+    if(op_type == op_type_enum::CREATE || 
+       op_type == op_type_enum::MODIFY) {
+        row.value = get_kvo_row(db, tio, kvo, json);
+    }
+
+    return row;
+}
+
+// Plugin implementation
+
+statetrack_plugin::statetrack_plugin():my(new statetrack_plugin_impl()){}
+
+void statetrack_plugin::set_program_options(options_description&, options_description& cfg) {
+   cfg.add_options()
+         ("option-name", bpo::value<string>()->default_value("default value"),
+          "Option Description")
+         ;
+}
+
+void statetrack_plugin::plugin_initialize(const variables_map& options) {
+   ilog("initializing statetrack plugin");
+   try {
+      if( options.count( "option-name" )) {
+         // Handle the option
+      }
+   }
+   FC_LOG_AND_RETHROW()
+}
+
 void statetrack_plugin::plugin_startup() {
-    const chainbase::database& db = app().get_plugin<chain_plugin>().chain().db();
+    try {
+      const chainbase::database& db = app().get_plugin<chain_plugin>().chain().db();
    
-    auto& kv_index = db.get_index<kv_index_type>();
-   
-    kv_index.applied_emplace = [&](const chain::key_value_object& kvo) {
-         elog("STATETRACK key_value_object emplace: ${kvo}", ("kvo", kvo.id));
-       
-         
-    };
-   
-    kv_index.applied_modify = [&](const chain::key_value_object& kvo) {
-         elog("STATETRACK key_value_object modify: ${kvo}", ("kvo", kvo.id));
-    };
-   
-    kv_index.applied_remove = [&](const chain::key_value_object& kvo) {
-         elog("STATETRACK key_value_object remove: ${kvo}", ("kvo", kvo.id));
-    };
+        auto& kv_index = db.get_index<kv_index_type>();
+    
+        kv_index.applied_emplace = [&](const chain::key_value_object& kvo) {
+            fc::string data = fc::json::to_string(my->get_db_op_row(db, kvo, op_type_enum::CREATE));
+            ilog("STATETRACK key_value_object emplace: ${data}", ("data", data));
+        };
+    
+        kv_index.applied_modify = [&](const chain::key_value_object& kvo) {
+            fc::string data = fc::json::to_string(my->get_db_op_row(db, kvo, op_type_enum::MODIFY));
+            ilog("STATETRACK key_value_object modify: ${data}", ("data", data));
+        };
+    
+        kv_index.applied_remove = [&](const chain::key_value_object& kvo) {
+            fc::string data = fc::json::to_string(my->get_db_op_row(db, kvo, op_type_enum::REMOVE));
+            ilog("STATETRACK key_value_object remove: ${data}", ("data", data));
+        };
+   }
+   FC_LOG_AND_RETHROW()
+}
+
+void statetrack_plugin::plugin_shutdown() {
+   ilog("statetrack plugin shutdown");
 }
 
 }
