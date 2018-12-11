77c1109892edf8170af5ac30779c6385461fd905
shengminghui <shengminghui@9fbank.com.cn>
2018-12-11 13:54:38 +0800

    test committing

--- contracts/hello/hello.cpp
+++ contracts/hello/hello.cpp
@@ -15,6 +15,10 @@ public:
    void hi2( account_name user ) {
       print( "Hello, ", name{ user } );
    }
+
+   /// @abi action
+   void hi3( account_name user ) { print( "Hello, ", name{ user } );
+   }
 };
 
 EOSIO_ABI( hello, ( hi )( hi2 ))
