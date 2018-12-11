13ce34709443dd24dbbbbaa47f32b5bc07d4c027
shengminghui <shengminghui@9fbank.com.cn>
2018-12-11 13:53:36 +0800

    test committing

--- contracts/hello/hello.cpp
+++ contracts/hello/hello.cpp
@@ -1,14 +1,20 @@
 #include <eosiolib/eosio.hpp>
+
 using namespace eosio;
 
 class hello : public eosio::contract {
-  public:
-      using contract::contract;
+public:
+   using contract::contract;
 
-      /// @abi action 
-      void hi( account_name user ) {
-         print( "Hello, ", name{user} );
-      }
+   /// @abi action
+   void hi( account_name user ) {
+      print( "Hello, ", name{ user } );
+   }
+
+   /// @abi action
+   void hi2( account_name user ) {
+      print( "Hello, ", name{ user } );
+   }
 };
 
-EOSIO_ABI( hello, (hi) )
+EOSIO_ABI( hello, ( hi )( hi2 ))
