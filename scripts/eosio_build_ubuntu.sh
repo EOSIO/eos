83d1d9e68ea7bf7ab39de51dfdccded2d80912b4
Nathan Pierce <nathan.pierce@block.one>
2019-01-14 13:51:32 -0500

    mongodb_version fix

--- scripts/eosio_build_ubuntu.sh
+++ scripts/eosio_build_ubuntu.sh
@@ -47,7 +47,7 @@ case "${OS_NAME}" in
 		if [ $OS_MAJ -gt 16 ]; then
 			export MONGODB_VERSION=4.1.1
 		else
-			export MONGODB_VERSION=3.6.3
+			export MONGODB_VERSION=$MONGODB_VERSION
 		fi
 		# We have to re-set this with the new version
 		export MONGODB_ROOT=${OPT_LOCATION}/mongodb-${MONGODB_VERSION}
