f84de85554948999a8662f6fcc137bfb3add9c6c
thaipandada <thaipandada@gmail.com>
2019-01-15 13:49:10 +0800

    fix aragument name error

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -564,7 +564,7 @@ fc::variant regproducer_variant(const account_name& producer, const public_key_t
             ("producer", producer)
             ("producer_key", key)
             ("url", url)
-            ("location", _location)
+            ("location", location)
             ;
 }
 
