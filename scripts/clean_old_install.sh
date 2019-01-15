b962072ffeb666a0b5692b9691c115917a10a64b
Nathan Pierce <nathan.pierce@block.one>
2019-01-15 09:59:44 -0500

    new clean script failure on missing sudo

--- scripts/clean_old_install.sh
+++ scripts/clean_old_install.sh
@@ -1,43 +1,18 @@
 #! /bin/bash
 
-OPT_LOCATION=${HOME}/opt
-
-# ./build check for local cleanup while refactoring build scripts
-if [ -d "./build" ]; then
+if [ -d "/usr/local/include/eosio" ] || [ -d "$HOME/opt/eosio" ] || [[ $1 == "force" ]]; then # use force for running the script directly
    printf "\nEOSIO installation already found...\n"
    printf "Do you wish to remove this install?\n"
    select yn in "Yes" "No"; do
       case $yn in
          [Yy]* )
-            if [ $( uname ) == "Darwin" ]; then
-               brew uninstall mongo-cxx-driver mongo-c-driver llvm@4 libidn2 gettext --force
-            fi
-            rm -rf ./build
-         break;;
-         [Nn]* )
-            printf "Skipping\n\n"
-            exit 0
-      esac
-   done
-fi
-if [ -d "/usr/local/include/eosio" ] || [[ $1 == "force" ]]; then # use force for running the script directly
-   printf "\nEOSIO installation already found...\n"
-   printf "Do you wish to remove this install? (requires sudo)\n"
-   select yn in "Yes" "No"; do
-      case $yn in
-         [Yy]* )
             if [ "$(id -u)" -ne 0 ]; then
-               printf "\nThis requires sudo, please run ./scripts/clean_old_install.sh with sudo.\n"
+               printf "\nCleanup requires sudo... Please manually run ./scripts/clean_old_install.sh with sudo.\n"
                exit -1
             fi
-            exit
+
             pushd /usr/local &> /dev/null
-
             rm -rf wasm
-
-            if [ $( uname ) == "Darwin" ]; then
-               brew uninstall mongo-cxx-driver mongo-c-driver llvm@4 gettext automake libtool gmp wget --force
-            fi
 
             pushd include &> /dev/null
             rm -rf libbson-1.0 libmongoc-1.0 mongocxx bsoncxx appbase chainbase eosio eosio.system eosiolib fc libc++ musl secp256k* 2>/dev/null
