fce09e8a1723898634a713f76e048b2d6832a30e
Andrew Falaleev <andrew.falaleev@gmail.com>
2019-01-23 21:15:31 +0700

    Update to last version of fc library.

--- programs/eosio-blocklog/main.cpp
+++ programs/eosio-blocklog/main.cpp
@@ -103,7 +103,7 @@ void blocklog::read_log() {
                  (pretty_output.get_object());
       fc::variant v(std::move(enhanced_object));
        if (no_pretty_print)
-          fc::json::to_stream(*out, v, fc::json::stringify_large_ints_and_doubles);
+          fc::json::to_stream(*out, v, fc::json::default_generator);
        else
           *out << fc::json::to_pretty_string(v) << "\n";
    };
