207b8be811cf7fdd206fb37223e81632389d3b58
Zach Butler <zach.butler@block.one>
2019-02-07 18:49:54 -0500

    Fixed incorrect ctest command-line option

--- tools/ctestwrapper.sh
+++ tools/ctestwrapper.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 CORES=`getconf _NPROCESSORS_ONLN` # determine number of cores available
-ctest --parallel $CORES --output-on-failure $@ # run ctest, disregard failure code
+ctest --j $CORES --output-on-failure $@ # run ctest, disregard failure code
 exit 0
