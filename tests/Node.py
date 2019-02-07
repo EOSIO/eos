f231942bb99e8020f99cd5b4c594ea0449f6d4dd
Nathan Pierce <nathan.pierce@block.one>
2019-02-07 10:36:05 -0500

    better ps

--- tests/Node.py
+++ tests/Node.py
@@ -1199,7 +1199,7 @@ class Node(object):
             return False
 
         Utils.Print("PID: %s" % self.pid)
-        os.system('ps -a | grep node')
+        os.system('ps auxf')
         if not Utils.waitForBool(myFunc):
             Utils.Print("ERROR: Failed to validate node shutdown.")
             return False
