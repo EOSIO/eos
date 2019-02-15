2b3f2660f09cb7ab3284b5b323df8fdcd23af52b
Brian Johnson <johnsonb@objectcomputing.com>
2019-02-15 14:31:03 -0600

    Ensure that a caught-up nodeos reports its new handshake status to other connected nodeos. GH #6755

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -1584,6 +1584,10 @@ namespace eosio {
                set_state(head_catchup);
             }
          }
+
+         if (state == in_sync) {
+            send_handshakes();
+         }
       }
       else if (state == lib_catchup) {
          if( blk_num == sync_known_lib_num ) {
