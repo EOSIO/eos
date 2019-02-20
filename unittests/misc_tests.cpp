1fde58fe428e7e138fa23967d2de8125bead7a04
Kevin Heifner <heifnerk@objectcomputing.com>
2019-02-20 11:35:01 -0600

    Add test for derived of derived reflector init

--- unittests/misc_tests.cpp
+++ unittests/misc_tests.cpp
@@ -44,7 +44,7 @@ struct derived_reflect : public base_reflect {
    int dv = 0;
    bool derived_reflect_initialized = false;
    int derived_reflect_called = 0;
-private:
+protected:
    friend struct fc::reflector<derived_reflect>;
    friend struct fc::reflector_init_visitor<derived_reflect>;
    void reflector_init() {
@@ -56,8 +56,26 @@ private:
    }
 };
 
+struct final_reflect : public derived_reflect {
+   int fv = 0;
+   bool final_reflect_initialized = false;
+   int final_reflect_called = 0;
+private:
+   friend struct fc::reflector<final_reflect>;
+   friend struct fc::reflector_init_visitor<final_reflect>;
+   void reflector_init() {
+      BOOST_CHECK_EQUAL( bv, 42 ); // should be deserialized before called, set by test
+      BOOST_CHECK_EQUAL( dv, 52 ); // should be deserialized before called, set by test
+      BOOST_CHECK_EQUAL( fv, 62 ); // should be deserialized before called, set by test
+      ++final_reflect_called;
+      derived_reflect::reflector_init();
+      final_reflect_initialized = true;
+   }
+};
+
 FC_REFLECT( base_reflect, (bv) )
 FC_REFLECT_DERIVED( derived_reflect, (base_reflect), (dv) )
+FC_REFLECT_DERIVED( final_reflect, (derived_reflect), (fv) )
 
 namespace eosio
 {
@@ -819,6 +837,10 @@ BOOST_AUTO_TEST_CASE(reflector_init_test) {
       derived_reflect dr;
       dr.bv = 42;
       dr.dv = 52;
+      final_reflect fr;
+      fr.bv = 42;
+      fr.dv = 52;
+      fr.fv = 62;
       BOOST_CHECK_EQUAL( br.base_reflect_initialized, false );
       BOOST_CHECK_EQUAL( dr.derived_reflect_initialized, false );
 
@@ -842,9 +864,9 @@ BOOST_AUTO_TEST_CASE(reflector_init_test) {
          base_reflect br3;
          fc::raw::unpack( ds2, br3 );
          // to/from variant
-         fc::variant pkt_v( br3 );
+         fc::variant v( br3 );
          base_reflect br4;
-         fc::from_variant( pkt_v, br4 );
+         fc::from_variant( v, br4 );
 
          BOOST_CHECK_EQUAL( br2.bv, 42 );
          BOOST_CHECK_EQUAL( br2.base_reflect_initialized, true );
@@ -876,9 +898,9 @@ BOOST_AUTO_TEST_CASE(reflector_init_test) {
          derived_reflect dr3;
          fc::raw::unpack( ds2, dr3 );
          // to/from variant
-         fc::variant pkt_v( dr3 );
+         fc::variant v( dr3 );
          derived_reflect dr4;
-         fc::from_variant( pkt_v, dr4 );
+         fc::from_variant( v, dr4 );
 
          BOOST_CHECK_EQUAL( dr2.bv, 42 );
          BOOST_CHECK_EQUAL( dr2.base_reflect_initialized, true );
@@ -899,6 +921,106 @@ BOOST_AUTO_TEST_CASE(reflector_init_test) {
          BOOST_CHECK_EQUAL( dr4.dv, 52 );
          BOOST_CHECK_EQUAL( dr4.derived_reflect_initialized, true );
          BOOST_CHECK_EQUAL( dr4.derived_reflect_called, 1 );
+
+         base_reflect br5;
+         ds2.seekp( 0 );
+         fc::raw::unpack( ds2, br5 );
+         base_reflect br6;
+         fc::from_variant( v, br6 );
+
+         BOOST_CHECK_EQUAL( br5.bv, 42 );
+         BOOST_CHECK_EQUAL( br5.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( br5.base_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( br6.bv, 42 );
+         BOOST_CHECK_EQUAL( br6.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( br6.base_reflect_called, 1 );
+      }
+      { // final
+         // pack
+         uint32_t pack_size = fc::raw::pack_size( fr );
+         vector<char> buf( pack_size );
+         fc::datastream<char*> ds( buf.data(), pack_size );
+
+         fc::raw::pack( ds, fr );
+         // unpack
+         ds.seekp( 0 );
+         final_reflect fr2;
+         fc::raw::unpack( ds, fr2 );
+         // pack again
+         pack_size = fc::raw::pack_size( fr2 );
+         fc::datastream<char*> ds2( buf.data(), pack_size );
+         fc::raw::pack( ds2, fr2 );
+         // unpack
+         ds2.seekp( 0 );
+         final_reflect fr3;
+         fc::raw::unpack( ds2, fr3 );
+         // to/from variant
+         fc::variant v( fr3 );
+         final_reflect fr4;
+         fc::from_variant( v, fr4 );
+
+         BOOST_CHECK_EQUAL( fr2.bv, 42 );
+         BOOST_CHECK_EQUAL( fr2.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr2.base_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( fr3.bv, 42 );
+         BOOST_CHECK_EQUAL( fr3.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr3.base_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( fr4.bv, 42 );
+         BOOST_CHECK_EQUAL( fr4.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr4.base_reflect_called, 1 );
+
+         BOOST_CHECK_EQUAL( fr2.dv, 52 );
+         BOOST_CHECK_EQUAL( fr2.derived_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr2.derived_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( fr3.dv, 52 );
+         BOOST_CHECK_EQUAL( fr3.derived_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr3.derived_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( fr4.dv, 52 );
+         BOOST_CHECK_EQUAL( fr4.derived_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr4.derived_reflect_called, 1 );
+
+         BOOST_CHECK_EQUAL( fr2.fv, 62 );
+         BOOST_CHECK_EQUAL( fr2.final_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr2.final_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( fr3.fv, 62 );
+         BOOST_CHECK_EQUAL( fr3.final_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr3.final_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( fr4.fv, 62 );
+         BOOST_CHECK_EQUAL( fr4.final_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( fr4.final_reflect_called, 1 );
+
+         base_reflect br5;
+         ds2.seekp( 0 );
+         fc::raw::unpack( ds2, br5 );
+         base_reflect br6;
+         fc::from_variant( v, br6 );
+
+         BOOST_CHECK_EQUAL( br5.bv, 42 );
+         BOOST_CHECK_EQUAL( br5.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( br5.base_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( br6.bv, 42 );
+         BOOST_CHECK_EQUAL( br6.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( br6.base_reflect_called, 1 );
+
+         derived_reflect dr7;
+         ds2.seekp( 0 );
+         fc::raw::unpack( ds2, dr7 );
+         derived_reflect dr8;
+         fc::from_variant( v, dr8 );
+
+         BOOST_CHECK_EQUAL( dr7.bv, 42 );
+         BOOST_CHECK_EQUAL( dr7.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( dr7.base_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( dr8.bv, 42 );
+         BOOST_CHECK_EQUAL( dr8.base_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( dr8.base_reflect_called, 1 );
+
+         BOOST_CHECK_EQUAL( dr7.dv, 52 );
+         BOOST_CHECK_EQUAL( dr7.derived_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( dr7.derived_reflect_called, 1 );
+         BOOST_CHECK_EQUAL( dr8.dv, 52 );
+         BOOST_CHECK_EQUAL( dr8.derived_reflect_initialized, true );
+         BOOST_CHECK_EQUAL( dr8.derived_reflect_called, 1 );
       }
 
    } FC_LOG_AND_RETHROW()
