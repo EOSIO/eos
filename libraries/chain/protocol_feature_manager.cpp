8c915c90c64aa923c129e78f20d4e791f0fe6665
arhag <arhag@users.noreply.github.com>
2019-03-07 11:45:20 -0500

    set description digest for PREACTIVATE_FEATURE

--- libraries/chain/protocol_feature_manager.cpp
+++ libraries/chain/protocol_feature_manager.cpp
@@ -14,9 +14,16 @@ namespace eosio { namespace chain {
    const std::unordered_map<builtin_protocol_feature_t, builtin_protocol_feature_spec, enum_hash<builtin_protocol_feature_t>>
    builtin_protocol_feature_codenames =
       boost::assign::map_list_of<builtin_protocol_feature_t, builtin_protocol_feature_spec>
-         ( builtin_protocol_feature_t::preactivate_feature, {
+         (  builtin_protocol_feature_t::preactivate_feature, {
             "PREACTIVATE_FEATURE",
-            digest_type{},
+            fc::variant("64fe7df32e9b86be2b296b3f81dfd527f84e82b98e363bc97e40bc7a83733310").as<digest_type>(),
+            // SHA256 hash of the raw message below within the comment delimiters (do not modify message below).
+/*
+Builtin protocol feature: PREACTIVATE_FEATURE
+
+Adds privileged intrinsic to enable a contract to pre-activate a protocol feature specified by its digest.
+Pre-activated protocol features must be activated in the next block.
+*/
             {},
             {time_point{}, false, true} // enabled without preactivation and ready to go at any time
          } )
