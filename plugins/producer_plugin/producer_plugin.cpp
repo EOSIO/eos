679852fd1225dd52245eaa4863e4aa70d5cf85bd
Kevin Heifner <heifnerk@objectcomputing.com>
2019-03-08 09:34:05 -0500

    Move creation of future outside post to prevent wait on future right after creation

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -352,10 +352,10 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
          chain::controller& chain = chain_plug->chain();
          const auto max_trx_time_ms = _max_transaction_time_ms.load();
          fc::microseconds max_trx_cpu_usage = max_trx_time_ms < 0 ? fc::microseconds::maximum() : fc::milliseconds( max_trx_time_ms );
+         // use chain thread pool for sig recovery so that future wait below is not in the same thread pool preventing progress
+         transaction_metadata::create_signing_keys_future( trx, chain.get_thread_pool(), chain.get_chain_id(), max_trx_cpu_usage );
          auto& tp = *_thread_pool;
-         boost::asio::post( tp, [self = this, &chain, max_trx_cpu_usage, trx, persist_until_expired, next]() {
-            // use chain thread pool for sig recovery so that future wait below is not in the same thread pool preventing progress
-            transaction_metadata::create_signing_keys_future( trx, chain.get_thread_pool(), chain.get_chain_id(), max_trx_cpu_usage );
+         boost::asio::post( tp, [self = this, trx, persist_until_expired, next]() {
             if( trx->signing_keys_future.valid() )
                trx->signing_keys_future.wait();
             app().post(priority::low, [self, trx, persist_until_expired, next]() {
