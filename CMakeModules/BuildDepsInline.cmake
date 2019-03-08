b53f5e505f682265f9a3fcade3ae5295ec6ec1d4
Krishnaraj Bhat <krrishnarraj@gmail.com>
2019-03-08 07:40:06 +0530

    Remove windows specific options from BuildDepsInline

--- CMakeModules/BuildDepsInline.cmake
+++ CMakeModules/BuildDepsInline.cmake
@@ -11,27 +11,8 @@ if(CMAKE_GENERATOR MATCHES "Unix Makefiles")
   set(CMAKE_PARALLEL_OPT "-j ${NUM_CORES}")
 endif()
 
-if(WIN32)
-  set(GMP_INSTALL "${CMAKE_CURRENT_BINARY_DIR}/gmp_install")
-  set(INTL_INSTALL "${CMAKE_CURRENT_BINARY_DIR}/intl_install")
-  set(MONGO_C_CMAKE_EXTRA_ARGS "-DENABLE_EXTRA_ALIGNMENT=OFF")
-  set(BOOST_CONF "bootstrap.bat")
-  set(BOOST_BUILD "b2.exe")
-
-  # Find OpenSSL
-  find_package(OpenSSL)
-  if(OPENSSL_FOUND)
-    set(OPENSSL_ROOT_DIR "${OPENSSL_INCLUDE_DIR}/..")
-    set(OPENSSL_USE_STATIC_LIBS TRUE)
-  else()
-    message(FATAL_ERROR "OpenSSL not found. Install latest openssl for windows from https://slproweb.com/products/Win32OpenSSL.html . Pick a non-light version for dev files, and install it to 'C:\\OpenSSL' instead of 'C:\\Program Files\\OpenSSL-Win64'")
-  endif()
-else()
-  set(MONGO_C_CMAKE_EXTRA_ARGS "")
-  set(BOOST_CONF "./bootstrap.sh")
-  set(BOOST_BUILD "./b2")
-endif()
-set(BOOST_BUILD "${BOOST_BUILD} variant=release link=static --prefix=${BOOST_ROOT} -j${NUM_CORES}")
+set(BOOST_CONF "./bootstrap.sh")
+set(BOOST_BUILD "./b2 variant=release link=static --prefix=${BOOST_ROOT} -j${NUM_CORES}")
 
 set(CMAKE_LIST_CONTENT "
   cmake_minimum_required(VERSION 3.5)
@@ -62,7 +43,6 @@ set(CMAKE_LIST_CONTENT "
       -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF
       -DENABLE_TESTS=OFF
       -DENABLE_EXAMPLES=OFF
-      ${MONGO_C_CMAKE_EXTRA_ARGS}
   )
 
   ExternalProject_add(  mongo-cxx-driver
@@ -95,28 +75,6 @@ set(CMAKE_LIST_CONTENT "
       -DLLVM_BUILD_EXAMPLES=OFF
       -DLLVM_BUILD_TESTS=OFF
   )
-
-  if(WIN32)
-    ExternalProject_add(  gmp_download
-      PREFIX              gmp
-      SOURCE_DIR          gmp/src/gmp
-      URL                 https://sourceforge.net/projects/mingw/files/MinGW/Base/gmp/gmp-6.1.2/gmp-6.1.2-2-mingw32-dev.tar.xz/download
-      URL_HASH            SHA256=2EE1F5553B3A16295E5BDB13D7325EDC03C69046B51332DE9C2D1C60B1F04F1A
-      CONFIGURE_COMMAND   \"\"
-      BUILD_COMMAND       \"\"
-      INSTALL_COMMAND     \"${CMAKE_COMMAND}\" -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/deps/build/gmp/src/gmp/ ${GMP_INSTALL}
-    )
-
-    ExternalProject_add(  intl_download
-      PREFIX              intl
-      SOURCE_DIR          intl/src/intl
-      URL                 http://gnuwin32.sourceforge.net/downlinks/libintl-lib-zip.php
-      URL_HASH            SHA256=5C0588041ED0188FA78067A8A9F66E0531B51C6198F37794B94699CA71FFABD7
-      CONFIGURE_COMMAND   \"\"
-      BUILD_COMMAND       \"\"
-      INSTALL_COMMAND     \"${CMAKE_COMMAND}\" -E copy_directory ${CMAKE_CURRENT_BINARY_DIR}/deps/build/intl/src/intl/ ${INTL_INSTALL}
-    )
-  endif()
 ")
 
 set(DEPS "${CMAKE_CURRENT_BINARY_DIR}/deps")
@@ -136,12 +94,4 @@ set(ENV{DESTDIR} "${DESTDIR_ORIG}")
 
 set(LLVM_DIR "${WASM_ROOT}/lib/cmake/llvm")
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};${MONGO_DRIVER_ROOT}")
-
-if(WIN32)
-  set(ENV{BOOST_ROOT} "${BOOST_ROOT}")
-  set(GMP_INCLUDE_DIR "${GMP_INSTALL}/include")
-  set(GMP_LIBRARIES "${GMP_INSTALL}/lib/libgmp.a")
-  set(Intl_INCLUDE_DIR "${INTL_INSTALL}/include")
-  set(Intl_LIBRARY "${INTL_INSTALL}/lib/libintl.lib")
-endif()
 
