7889ac132cdd33f63a1e8125536a5ceca169f030
Evgeny Schepachev <schepachev@gmail.com>
2019-03-12 14:26:50 +0700

    Implemented get_table_rows api function #366

--- libraries/chain/include/cyberway/chaindb/controller.hpp
+++ libraries/chain/include/cyberway/chaindb/controller.hpp
@@ -191,6 +191,10 @@ namespace cyberway { namespace chaindb {
 
         find_info lower_bound(const index_request&, const char* key, size_t);
         find_info upper_bound(const index_request&, const char* key, size_t);
+
+        find_info lower_bound(const index_request& request, const fc::variant& orders) const;
+        find_info upper_bound(const index_request& request, const fc::variant& orders) const;
+
         find_info find(const index_request&, primary_key_t, const char* key, size_t);
 
         find_info begin(const index_request&);
