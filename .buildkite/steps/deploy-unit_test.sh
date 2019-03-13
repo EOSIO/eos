7e2caf1a97bd04ac02676071cb5290b09a10e826
Andrew Falaleev <andrew.falaleev@gmail.com>
2019-03-13 23:40:21 +0700

    Enable payloadless unit tests. #353

--- .buildkite/steps/deploy-unit_test.sh
+++ .buildkite/steps/deploy-unit_test.sh
@@ -15,7 +15,7 @@ docker-compose up -d mongo
 
 # Run unit-tests
 sleep 10s
-docker run --rm --network docker_cyberway-net -ti cyberway/cyberway:$IMAGETAG  /bin/bash -c 'export MONGO_URL=mongodb://mongo:27017; /opt/cyberway/bin/unit_test -l message -r detailed -t "!api_tests/*" -t "!abi_tests/*" -t "!bootseq_tests/*" -t "!eosio_system_tests/*" -t "!forked_tests/*" -t "!identity_tests/*" -t "!misc_tests/*" -t "!multi_index_tests/*" -t "!payloadless_tests/*" -t "!producer_schedule_tests/*" -t "!ram_tests/*" -t "!providebw_tests/*"'
+docker run --rm --network docker_cyberway-net -ti cyberway/cyberway:$IMAGETAG  /bin/bash -c 'export MONGO_URL=mongodb://mongo:27017; /opt/cyberway/bin/unit_test -l message -r detailed -t "!api_tests/*" -t "!abi_tests/*" -t "!bootseq_tests/*" -t "!eosio_system_tests/*" -t "!forked_tests/*" -t "!identity_tests/*" -t "!misc_tests/*" -t "!multi_index_tests/*" -t "!producer_schedule_tests/*" -t "!ram_tests/*" -t "!providebw_tests/*"'
 result=$?
 
 docker-compose down
