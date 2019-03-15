b6f9911c3f82b33855a8bedfb948b9c3e9cffa5f
Kevin Heifner <heifnerk@objectcomputing.com>
2019-03-15 18:44:16 -0500

    Add transaction id to log message

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -2653,7 +2653,6 @@ namespace eosio {
    // called from thread_pool threads
    void net_plugin_impl::handle_message(const connection_ptr& c, const packed_transaction_ptr& trx) {
       fc_dlog(logger, "got a packed transaction, cancel wait");
-      peer_ilog(c, "received packed_transaction");
       if( db_read_mode == eosio::db_read_mode::READ_ONLY ) {
          fc_dlog(logger, "got a txn in read-only mode - dropping");
          return;
@@ -2661,6 +2660,7 @@ namespace eosio {
 
       auto ptrx = std::make_shared<transaction_metadata>( trx );
       const auto& tid = ptrx->id;
+      peer_ilog(c, "received packed_transaction ${id}", ("id", tid));
 
       bool have_trx = dispatcher->have_txn( tid );
       connection_wptr weak_ptr = c;
@@ -2669,7 +2669,7 @@ namespace eosio {
          dispatcher->recv_transaction(c, ptrx);
       });
       if( have_trx ) {
-         fc_dlog( logger, "got a duplicate transaction - dropping" );
+         fc_dlog( logger, "got a duplicate transaction - dropping ${id}", ("id", tid) );
          return;
       }
 
