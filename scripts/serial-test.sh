e82392b8b99dd3108430874eac64ac12540b7a53
Zach Butler <zach.butler@block.one>
2019-03-18 18:25:04 -0400

    Copy-pasta error

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -7,12 +7,10 @@ echo "Starting MongoDB..."
 ~/bin/mongod --fork --dbpath ~/data/mongodb -f ~/etc/mongod.conf --logpath "$(pwd)"/mongod.log
 cd /data/job/build
 echo "Running tests..."
-CPU_CORES=$(getconf _NPROCESSORS_ONLN)
-echo "$CPU_CORES cpu cores detected."
 TEST_COUNT=$(ctest -N -L nonparallelizable_tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
 [[ $TEST_COUNT > 0 ]] && echo "$TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
-echo "$ ctest -j $CPU_CORES -L nonparallelizable_tests --output-on-failure -T Test"
-ctest -j $CPU_CORES -L nonparallelizable_tests --output-on-failure -T Test
+echo "$ ctest -L nonparallelizable_tests --output-on-failure -T Test"
+ctest -L nonparallelizable_tests --output-on-failure -T Test
 echo "Uploading xUnit test results XML file..."
 XML_FILENAME="test-results.xml"
 mv $(pwd)/Testing/$(ls $(pwd)/Testing/ | grep '20' | tail -n 1)/Test.xml $XML_FILENAME
