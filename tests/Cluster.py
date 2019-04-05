8ec809873002b9ee9872f06ad54662e29539c98e
arhag <arhag@users.noreply.github.com>
2019-04-04 22:12:08 -0400

    another attempt to prevent the shuffling of FEATURE_DIGESTS on certain platforms #6115

--- tests/Cluster.py
+++ tests/Cluster.py
@@ -928,7 +928,7 @@ class Cluster(object):
 
         if pfSetupPolicy == PFSetupPolicy.FULL:
             allBuiltinProtocolFeatureDigests = biosNode.getAllBuiltinFeatureDigestsToPreactivate()
-            env["FEATURE_DIGESTS"] = " ".join(allBuiltinProtocolFeatureDigests)
+            env["FEATURE_DIGESTS"] = ";".join(allBuiltinProtocolFeatureDigests)
             Utils.Print("Set FEATURE_DIGESTS to: %s" % env["FEATURE_DIGESTS"])
 
         if 0 != subprocess.call(cmd.split(), stdout=Utils.FNull, env=env):
