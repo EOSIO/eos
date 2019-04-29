b4ff7460d751a0fa06731b4575fe7d23cdc35449
Ben Rush <ben.rush@block.one>
2019-04-29 17:15:58 -0400

    Login in precommand

--- scripts/test_build.sh
+++ scripts/test_build.sh
@@ -2,15 +2,7 @@
 
 set -e
 
-export http_proxy=http://proxy.service:3128
-export https_proxy=http://proxy.service:3128
-
-curl -m 3 google.com
-curl -m 3 archive.ubuntu.com
-
 docker build --network=host -t eosio/test-registry ./Docker
-
-docker login registry.devel.b1ops.net --username $DOCKER_REGISTRY_USERNAME --password $DOCKER_REGISTRY_TOKEN
 
 docker tag eosio/test-registry registry.devel.b1ops.net/eosio/test-registry
 docker push registry.devel.b1ops.net/eosio/test-registry
