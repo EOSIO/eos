7f59d58b6ba2b683aa93501931640170e7e376d8
Nathan Pierce <nathan.pierce@block.one>
2019-05-07 08:34:28 -0400

    new test scripts

--- scripts/parallel-test.sh
+++ scripts/parallel-test.sh
@@ -1,35 +1,15 @@
 #!/bin/bash
-set -e # exit on failure of any "simple" command (excludes &&, ||, or | chains)
-# prepare environment
-PATH=$PATH:~/opt/mongodb/bin
-echo "Extracting build directory..."
-[[ -z "${1}" ]] && tar -zxf build.tar.gz || tar -xzf $1
-cd /data/job/build
-# run tests
-echo "Running tests..."
-CPU_CORES=$(getconf _NPROCESSORS_ONLN)
-echo "$CPU_CORES cpu cores detected."
-TEST_COUNT=$(ctest -N -LE _tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
-[[ $TEST_COUNT > 0 ]] && echo "$TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
+set -eo pipefail
+[[ -f build.tar.gz ]] && tar -xzf build.tar.gz
+cd ./build
+echo "[Running tests]"
 set +e # defer ctest error handling to end
-echo "$ ctest -j $CPU_CORES -LE _tests --output-on-failure -T Test"
-ctest -j $CPU_CORES -LE _tests --output-on-failure -T Test
+ctest -j $(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test
 EXIT_STATUS=$?
 [[ "$EXIT_STATUS" == 0 ]] && set -e
-echo "Done running parallelizable tests."
-# upload artifacts
-echo "Uploading artifacts..."
-XML_FILENAME="test-results.xml"
-mv $(pwd)/Testing/$(ls $(pwd)/Testing/ | grep '20' | tail -n 1)/Test.xml $XML_FILENAME
-buildkite-agent artifact upload config.ini
-buildkite-agent artifact upload genesis.json
-cd ..
-cd build
-buildkite-agent artifact upload $XML_FILENAME
-echo "Done uploading artifacts."
+# Prepare tests for artifact upload
+echo "[Uploading artifacts]"
+mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+buildkite-agent artifact upload test-results.xml
 # ctest error handling
-if [[ "$EXIT_STATUS" != 0 ]]; then
-    echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"
-    echo '    ^^^ scroll up for more information ^^^'
-    exit $EXIT_STATUS
-fi
+[[ $EXIT_STATUS != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"; exit $EXIT_STATUS
