5b17c9a016a48ee715fb2cff7499867801ab0123
Nathan Pierce <nathan.pierce@block.one>
2019-05-07 14:13:23 -0400

    fixes for Zach

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 set -eo pipefail
+echo "+++ Extracting build directory"
 [[ -f build.tar.gz ]] && tar -xzf build.tar.gz
-cd ./build
-echo "[Killing old MongoDB]"
+cd build
+echo "+++ Killing old MongoDB"
 $(pgrep mongod | xargs kill -9) || true
-echo "[Starting MongoDB]"
+echo "+++ Starting MongoDB"
 PATH=$PATH:~/opt/mongodb/bin
 mongod --fork --dbpath ~/data/mongodb -f ~/etc/mongod.conf --logpath ./mongod.log
-echo "[Running tests]"
+echo "+++ Running tests"
+# Counting tests available and if they get disabled for some reason, throw a failure
+TEST_COUNT=$(ctest -N -LE _tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
+[[ $TEST_COUNT > 0 ]] && echo " - $TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
 set +e # defer ctest error handling to end
+echo "ctest -L nonparallelizable_tests --output-on-failure -T Test"
 ctest -L nonparallelizable_tests --output-on-failure -T Test
 EXIT_STATUS=$?
 [[ $EXIT_STATUS == 0 ]] && set -e
-echo "[Uploading artifacts]"
+echo "+++ Uploading artifacts"
 mv -f ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
 buildkite-agent artifact upload test-results.xml
 buildkite-agent artifact upload mongod.log
