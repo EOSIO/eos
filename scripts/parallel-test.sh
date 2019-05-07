32b8cecce8a7130a835e51012092a44a3e78b336
Nathan Pierce <nathan.pierce@block.one>
2019-05-07 12:39:40 -0400

    quick fix for mongo log location [skip ci]

--- scripts/parallel-test.sh
+++ scripts/parallel-test.sh
@@ -9,7 +9,7 @@ EXIT_STATUS=$?
 [[ "$EXIT_STATUS" == 0 ]] && set -e
 # Prepare tests for artifact upload
 echo "[Uploading artifacts]"
-mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+mv -f ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
 buildkite-agent artifact upload test-results.xml
 # ctest error handling
 [[ $EXIT_STATUS != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"; exit $EXIT_STATUS
--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -6,16 +6,15 @@ echo "[Killing old MongoDB]"
 $(pgrep mongod | xargs kill -9) || true
 echo "[Starting MongoDB]"
 PATH=$PATH:~/opt/mongodb/bin
-mongod --fork --dbpath ~/data/mongodb -f ~/etc/mongod.conf --logpath ~/var/log/mongodb/mongod.log
+mongod --fork --dbpath ~/data/mongodb -f ~/etc/mongod.conf --logpath ./mongod.log
 echo "[Running tests]"
 set +e # defer ctest error handling to end
 ctest -L nonparallelizable_tests --output-on-failure -T Test
 EXIT_STATUS=$?
 [[ $EXIT_STATUS == 0 ]] && set -e
 echo "[Uploading artifacts]"
-mv ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
+mv -f ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
 buildkite-agent artifact upload test-results.xml
-mv ~/var/log/mongodb/mongod.log mongod.log
 buildkite-agent artifact upload mongod.log
 # Throw proper exit code
 [[ $EXIT_STATUS != 0 ]] && echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"; exit $EXIT_STATUS
