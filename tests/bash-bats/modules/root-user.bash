d967ff456c5a3380112c39e9ea643d2f1e6dc507
Nathan Pierce <nathan.pierce@block.one>
2019-05-07 17:48:33 -0400

    [skip ci] mac test fixes

--- tests/bash-bats/modules/root-user.bash
+++ tests/bash-bats/modules/root-user.bash
@@ -4,9 +4,13 @@ load ../helpers/functions
 @test "${TEST_LABEL} > Testing root user run" {
     run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION -P"
     [[ ! -z $(echo "${output}" | grep "User: $(whoami)") ]] || exit
-    [[ -z $(echo "${output}" | grep "/usr/bin/sudo -E") ]] || exit
+    if [[ $ARCH == "Linux" ]]; then
+        [[ -z $(echo "${output}" | grep "/usr/bin/sudo -E") ]] || exit
+    fi
     export CURRENT_USER=test
     run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION -P"
     [[ ! -z $(echo "${output}" | grep "User: test") ]] || exit
-    [[ ! -z $(echo "${output}" | grep "/usr/bin/sudo -E") ]] || exit
+    if [[ $ARCH == "Linux" ]]; then
+        [[ ! -z $(echo "${output}" | grep "/usr/bin/sudo -E") ]] || exit
+    fi
 }
