df62b581142fdad09fc0f298c6ed03ac52b17d50
Nathan Pierce <nathan.pierce@block.one>
2019-05-11 15:23:22 -0400

    test revert for testing

--- scripts/parallel-test.sh
+++ scripts/parallel-test.sh
@@ -1,26 +1,35 @@
-#!/usr/bin/env bash
-set -ieo pipefail
-echo "+++ Extracting build directory"
+#!/bin/bash
+set -e # exit on failure of any "simple" command (excludes &&, ||, or | chains)
+# prepare environment
+PATH=$PATH:~/opt/mongodb/bin
+echo "Extracting build directory..."
 [[ -f build.tar.gz ]] && tar -xzf build.tar.gz
-ls -l build
-cd ./build
-echo "+++ Running tests"
-# Counting tests available and if they get disabled for some reason, throw a failure
-TEST_COUNT=$(ctest -LE _tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
+cd /data/job/build
+# run tests
+echo "Running tests..."
+CPU_CORES=$(getconf _NPROCESSORS_ONLN)
+echo "$CPU_CORES cpu cores detected."
+TEST_COUNT=$(ctest -N -LE _tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
 [[ $TEST_COUNT > 0 ]] && echo "$TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
 set +e # defer ctest error handling to end
-CORES=$(getconf _NPROCESSORS_ONLN)
-ctest -j $CORES -LE _tests --output-on-failure -T Test
+echo "$ ctest -j $CPU_CORES -LE _tests --output-on-failure -T Test"
+ctest -j $CPU_CORES -LE _tests --output-on-failure -T Test
 EXIT_STATUS=$?
 [[ "$EXIT_STATUS" == 0 ]] && set -e
-# Prepare tests for artifact upload
-echo "+++ Uploading artifacts"
-mv -f ./Testing/$(ls ./Testing/ | grep '20' | tail -n 1)/Test.xml test-results.xml
-buildkite-agent artifact upload test-results.xml
+echo "Done running parallelizable tests."
+# upload artifacts
+echo "Uploading artifacts..."
+XML_FILENAME="test-results.xml"
+mv $(pwd)/Testing/$(ls $(pwd)/Testing/ | grep '20' | tail -n 1)/Test.xml $XML_FILENAME
+buildkite-agent artifact upload config.ini
+buildkite-agent artifact upload genesis.json
+cd ..
+cd build
+buildkite-agent artifact upload $XML_FILENAME
+echo "Done uploading artifacts."
 # ctest error handling
-if [[ $EXIT_STATUS != 0 ]]; then
+if [[ "$EXIT_STATUS" != 0 ]]; then
     echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"
-    buildkite-agent artifact upload config.ini
-    buildkite-agent artifact upload genesis.json
+    echo '    ^^^ scroll up for more information ^^^'
     exit $EXIT_STATUS
 fi
