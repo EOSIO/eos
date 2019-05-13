3192d681972d6c9c73348f32bf79d58b0f9d2067
Zach Butler <zach.butler@block.one>
2019-05-13 14:46:46 -0400

    Moved package builder steps into script

---
+++ scripts/package-builder.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e # exit on failure of any "simple" command (excludes &&, ||, or | chains)
+echo '+++ :evergreen_tree: Configuring Environment'
+[[ "$BUILD_STEP" == '' ]] && BUILD_STEP="$1"
+[[ "$BUILD_STEP" == '' ]] && (echo '+++ :no_entry: ERROR: Build step name must be given as BUILD_STEP or argument 1!' && exit 1)
+if [[ "$(uname)" == 'Darwin' ]]; then
+    echo 'Darwin family detected, building for brew.'
+    ARTIFACT='build/packages/*.rb;build/packages/*.tar.gz'
+    PACKAGE_TYPE='brew'
+else
+    . /etc/os-release
+    if [[ "$ID_LIKE" == 'rhel fedora' ]]; then
+        echo 'Fedora family detected, building for RPM.'
+        ARTIFACT='build/packages/*.rpm'
+        PACKAGE_TYPE='brew'
+        mkdir -p /root/rpmbuild/BUILD
+        mkdir -p /root/rpmbuild/BUILDROOT
+        mkdir -p /root/rpmbuild/RPMS
+        mkdir -p /root/rpmbuild/SOURCES
+        mkdir -p /root/rpmbuild/SPECS
+        mkdir -p /root/rpmbuild/SRPMS
+        yum install -y rpm-build
+    elif [[ "$ID_LIKE" == 'debian' ]]; then
+        echo 'Debian family detected, building for dpkg.'
+        ARTIFACT='build/packages/*.deb'
+        PACKAGE_TYPE='deb'
+    else
+        echo '+++ :no_entry: ERROR: Could not determine which operating system this script is running on!'
+        echo '$ uname'
+        uname
+        echo '$ echo $ID_LIKE'
+        echo $ID_LIKE
+        echo '$ cat /etc/os-release'
+        cat /etc/os-release
+        echo 'Exiting...'
+        exit 1
+    fi
+fi
+cd /data/job/build/packages
+echo "+++ :arrow_down: Downloading Build Directory"
+buildkite-agent artifact download build.tar.gz . --step "$BUILD_STEP"
+echo "+++ :compression: Extracting Build Directory"
+tar -zxf build.tar.gz
+echo "+++ :package: Starting Package Build"
+generate_package.sh "$PACKAGE_TYPE"
+echo '+++ :arrow_up: Uploading Artifacts'
+cd /data/job
+buildkite-agent artifact upload \"$ARTIFACT\"
+echo "+++ :white_check_mark: Done."
