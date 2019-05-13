ab8922d395a8cd706d96df4d1dc92f1b9c58fddf
Nathan Pierce <nathan.pierce@block.one>
2019-05-12 21:06:18 -0400

    testing non-interactive

--- scripts/serial-test.bash
+++ scripts/serial-test.bash
@@ -17,8 +17,8 @@ echo "+++ Running tests"
 TEST_COUNT=$($CTEST_BIN -N -LE _tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
 [[ $TEST_COUNT > 0 ]] && echo "$TEST_COUNT tests found." || (echo "ERROR: No tests registered with ctest! Exiting..." && exit 1)
 set +e # defer ctest error handling to end
-# interactive mode is necessary for our NP tests to function
-execute $CTEST_BIN -L nonparallelizable_tests --output-on-failure -T Test --interactive-debug-mode 0
+# We cannot use interactive mode for ctest nonparallelizable tests
+execute $CTEST_BIN -L nonparallelizable_tests --output-on-failure -T Test --interactive-debug-mode 1
 EXIT_STATUS=$?
 [[ $EXIT_STATUS == 0 ]] && set -e
 echo "+++ Uploading artifacts"
