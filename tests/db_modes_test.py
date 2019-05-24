9d37e0dab4acc55e0b3a9ea75a9068c2ce957e02
Kayan <taokayan13@gmail.com>
2019-05-24 12:14:45 +0800

    find free port automatically

--- tests/db_modes_test.py
+++ tests/db_modes_test.py
@@ -20,35 +20,37 @@ errorExit=Utils.errorExit
 
 nodeosCmd="./programs/nodeos/nodeos -d {} --config-dir {} \
 --chain-state-db-size-mb 8 --chain-state-db-guard-size-mb 0 --reversible-blocks-db-size-mb 1 \
---reversible-blocks-db-guard-size-mb 0 -e -peosio --plugin eosio::chain_api_plugin \
---http-server-address=127.0.0.1:8888"
+--reversible-blocks-db-guard-size-mb 0 -e -peosio --plugin eosio::chain_api_plugin"
 
-getInfoCmd="./programs/cleos/cleos get info"
+cleosPath="./programs/cleos/cleos"
+port=8888
 
 def run_and_kill(extraCmd="", killsig=signal.SIGTERM, preCmd=""):
-    #ensure port 8888 is free
-    print("waiting port 8888 to be freed")
-    tries = 90
+    global port
+    port = 8888
+    print("looking for free ports")
+    tries = 200
     while (tries > 0):
         try:
             sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            server_address = ('127.0.0.1', 8888)
+            server_address = ('127.0.0.1', port)
             sock.bind(server_address)
             sock.close()
             time.sleep(1)
             tries = 0
         except:
             tries = tries - 1
-            time.sleep(1)
+            port = port + 1
     # now start nodeos
-    cmdArr= preCmd + nodeosCmd + extraCmd
-    print("port is free now, start to run nodeosCmd with extra parameter \"%s\"" % (extraCmd))
+    cmdArr= preCmd + nodeosCmd + " --http-server-address=127.0.0.1:" + str(port) + extraCmd
+    print("port %d is free now, start to run nodeosCmd with extra parameter \"%s\"" % (port, extraCmd))
     proc=subprocess.Popen(cmdArr, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-    tries = 20
+    tries = 10
     ok = False
+    getInfoCmd = cleosPath + " -u http://127.0.0.1:" + str(port) + "/ get info"
     while (tries > 0):
         time.sleep(1)
-        reply = Utils.checkOutput(shlex.split(getInfoCmd), ignoreError=True)
+        reply = Utils.checkOutput(getInfoCmd, ignoreError=True)
         if reply.find("server_version") != -1:
             ok = True
             time.sleep(1)
