c9725cf46fff019379a952de2224899cb3108368
johndebord <john.debord@block.one>
2019-06-06 14:42:38 -0400

    Library is now functional

--- CMakeModules/VersionUtils.cmake
+++ CMakeModules/VersionUtils.cmake
@@ -1,15 +1,13 @@
 function(GENERATE_VERSION_METADATA)
-   set(_VERSION_MAJOR_  ${VERSION_MAJOR})
-   set(_VERSION_MINOR_  ${VERSION_MINOR})
-   set(_VERSION_PATCH_  ${VERSION_PATCH})
-   set(_VERSION_SUFFIX_ ${VERSION_SUFFIX})
-   set(_VERSION_HASH_   ${})
-   set(_VERSION_DIRTY_  ${})
+   set(_VERSION_MAJOR_  ${VERSION_MAJOR}  PARENT_SCOPE)
+   set(_VERSION_MINOR_  ${VERSION_MINOR}  PARENT_SCOPE)
+   set(_VERSION_PATCH_  ${VERSION_PATCH}  PARENT_SCOPE)
+   set(_VERSION_SUFFIX_ ${VERSION_SUFFIX} PARENT_SCOPE)
 
    find_package(Git)
    if(EXISTS ${CMAKE_SOURCE_DIR}/.git AND GIT_FOUND)
       execute_process(
-         COMMAND ${GIT_EXECUTABLE} rev-parse --short=8 HEAD
+         COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
          OUTPUT_VARIABLE _VERSION_HASH_
          ERROR_QUIET
          OUTPUT_STRIP_TRAILING_WHITESPACE )
@@ -18,7 +16,19 @@ function(GENERATE_VERSION_METADATA)
          OUTPUT_VARIABLE _VERSION_DIRTY_
          ERROR_QUIET
          OUTPUT_STRIP_TRAILING_WHITESPACE )
-      string(REGEX MATCH "-dirty$" _VERSION_DIRTY_ ${_VERSION_DIRTY_})
-      # configure_file(${CMAKE_CURRENT_SOURCE_DIR}/version.hpp.in ${CMAKE_CURRENT_BINARY_DIR}/version.hpp @ONLY)
+       string(REGEX MATCH "-dirty$" _VERSION_DIRTY_ ${_VERSION_DIRTY_})
+       # if dirty empty -> false
+       # else -> true
+       #
    endif()
+   set(_VERSION_HASH_   ${_VERSION_HASH_}  PARENT_SCOPE)
+   set(_VERSION_DIRTY_  ${_VERSION_DIRTY_} PARENT_SCOPE)
+   message(STATUS "-------begin-------")
+   message(STATUS ${_VERSION_MAJOR_})
+   message(STATUS ${_VERSION_MINOR_})
+   message(STATUS ${_VERSION_PATCH_})
+   message(STATUS ${_VERSION_SUFFIX_})
+   message(STATUS ${_VERSION_HASH_})
+   message(STATUS ${_VERSION_DIRTY_})
+   message(STATUS "-------end-------")
 endfunction()
--- libraries/version/include/eosio/version/version.hpp
+++ libraries/version/include/eosio/version/version.hpp
@@ -9,10 +9,10 @@
 
 namespace eosio { namespace version {
 
-   ///< Grab the version of the client in the form of: `v1.8.0-rc1`
+   ///< Grab the basic version of the client; example: `v1.8.0-rc1`
    const std::string& version_client();
       
-   ///< Grab the version of the client in the form of: `v1.8.0-rc1-7de458254[-dirty]`
+   ///< Grab the full version of the client; example:  `v1.8.0-rc1-7de458254[-dirty]`
    const std::string& version_full();
       
 } }
