fc19d3a5c5bdf691421c26a7e32569536768b993
johndebord <john.debord@block.one>
2019-06-11 15:42:50 -0400

    Polish

--- CMakeModules/VersionUtils.cmake
+++ CMakeModules/VersionUtils.cmake
@@ -1,37 +1,35 @@
 cmake_minimum_required(
   VERSION 3.5 )
 
-function(GENERATE_VERSION_METADATA)
-   find_package(Git)
-   if(EXISTS ${CMAKE_SOURCE_DIR}/.git AND GIT_FOUND)
-      execute_process(
-         COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
-         OUTPUT_VARIABLE _VERSION_HASH_
-         ERROR_QUIET
-         OUTPUT_STRIP_TRAILING_WHITESPACE )
-      execute_process(
-         COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
-         OUTPUT_VARIABLE _VERSION_DIRTY_
-         ERROR_QUIET
-         OUTPUT_STRIP_TRAILING_WHITESPACE )
-      string(REGEX MATCH "-dirty$" _VERSION_DIRTY_ ${_VERSION_DIRTY_})
-      if("${_VERSION_DIRTY_}" STREQUAL "")
-         set(_VERSION_DIRTY_ "false")
-      else()
-         set(_VERSION_DIRTY_ "true")
-      endif()
-      set(_VERSION_MAJOR_  ${VERSION_MAJOR}   PARENT_SCOPE)
-      set(_VERSION_MINOR_  ${VERSION_MINOR}   PARENT_SCOPE)
-      set(_VERSION_PATCH_  ${VERSION_PATCH}   PARENT_SCOPE)
-      set(_VERSION_SUFFIX_ ${VERSION_SUFFIX}  PARENT_SCOPE)
-      set(_VERSION_HASH_   ${_VERSION_HASH_}  PARENT_SCOPE)
-      set(_VERSION_DIRTY_  ${_VERSION_DIRTY_} PARENT_SCOPE)
+find_package(Git)
+if(EXISTS ${CMAKE_SOURCE_DIR}/.git AND GIT_FOUND)
+   execute_process(
+      COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
+      OUTPUT_VARIABLE _VERSION_HASH_
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE )
+   execute_process(
+      COMMAND ${GIT_EXECUTABLE} describe --tags --dirty
+      OUTPUT_VARIABLE _VERSION_DIRTY_
+      ERROR_QUIET
+      OUTPUT_STRIP_TRAILING_WHITESPACE )
+   string(REGEX MATCH "-dirty$" _VERSION_DIRTY_ ${_VERSION_DIRTY_})
+   if("${_VERSION_DIRTY_}" STREQUAL "")
+      set(_VERSION_DIRTY_ "false")
    else()
-      set(_VERSION_MAJOR_  "unknown" PARENT_SCOPE)
-      set(_VERSION_MINOR_  ""        PARENT_SCOPE)
-      set(_VERSION_PATCH_  ""        PARENT_SCOPE)
-      set(_VERSION_SUFFIX_ ""        PARENT_SCOPE)
-      set(_VERSION_HASH_   ""        PARENT_SCOPE)
-      set(_VERSION_DIRTY_  ""        PARENT_SCOPE)
+      set(_VERSION_DIRTY_ "true")
    endif()
-endfunction()
+   set(_VERSION_MAJOR_  ${VERSION_MAJOR})
+   set(_VERSION_MINOR_  ${VERSION_MINOR})
+   set(_VERSION_PATCH_  ${VERSION_PATCH})
+   set(_VERSION_SUFFIX_ ${VERSION_SUFFIX})
+   set(_VERSION_HASH_   ${_VERSION_HASH_})
+   set(_VERSION_DIRTY_  ${_VERSION_DIRTY_})
+else()
+   set(_VERSION_MAJOR_  "unknown")
+   set(_VERSION_MINOR_  "")
+   set(_VERSION_PATCH_  "")
+   set(_VERSION_SUFFIX_ "")
+   set(_VERSION_HASH_   "")
+   set(_VERSION_DIRTY_  "")
+endif()
