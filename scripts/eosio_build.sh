4199c36ac9e73e7191bc7048775ba424002e3809
Nathan Pierce <nathan.pierce@block.one>
2019-06-11 10:35:30 -0400

    -d scripts check

--- scripts/eosio_build.sh
+++ scripts/eosio_build.sh
@@ -108,6 +108,8 @@ if [ $# -ne 0 ]; then
    done
 fi
 
+[[ ! -d scripts ]] && echo "You must run this script from the eos root!" && exit 1
+
 # Load eosio specific helper functions
 . ./scripts/helpers/eosio.sh
 
