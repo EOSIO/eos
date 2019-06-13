bc7379599dcde31c093608b5ada13128497199db
johndebord <john.debord@block.one>
2019-06-13 11:05:59 -0400

    Fully functional

--- CMakeModules/VersionUtils.cmake
+++ CMakeModules/VersionUtils.cmake
@@ -14,27 +14,30 @@ function(GENERATE_VERSION_METADATA)
       OUTPUT_VARIABLE _VERSION_DIRTY_
       ERROR_QUIET
       OUTPUT_STRIP_TRAILING_WHITESPACE )
-    
+
+   # ...
    string(REGEX MATCH "-dirty$" _VERSION_DIRTY_ ${_VERSION_DIRTY_})
-    
+
+   # ...
    if("${_VERSION_DIRTY_}" STREQUAL "")
       set(_VERSION_DIRTY_ "false")
    else()
       set(_VERSION_DIRTY_ "true")
    endif()
-    
+
+   # ...
    set(_VERSION_MAJOR_  ${V_MAJOR})
    set(_VERSION_MINOR_  ${V_MINOR})
    set(_VERSION_PATCH_  ${V_PATCH})
    set(_VERSION_SUFFIX_ ${V_SUFFIX})
    set(_VERSION_HASH_   ${_VERSION_HASH_})
    set(_VERSION_DIRTY_  ${_VERSION_DIRTY_})
-    
+   
    # Modify and substitute the `.cpp.in` file for a `.cpp` in the build directory.
    configure_file(
       ${LIB_CUR_DIR}/src/version_impl.cpp.in
       ${LIB_BIN_DIR}/src/version_impl.cpp
       @ONLY )
 endfunction(GENERATE_VERSION_METADATA)
-
+  
 GENERATE_VERSION_METADATA()
