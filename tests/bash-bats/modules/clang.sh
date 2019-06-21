7b29c9832c8b9a67cc2f37be20536abf359c0ed8
Scott Arnette <scott.a.arnette@gmail.com>
2019-06-21 13:55:39 -0400

    BATS fixes for Ubuntu implementation.

--- tests/bash-bats/modules/clang.sh
+++ tests/bash-bats/modules/clang.sh
@@ -4,14 +4,14 @@ load ../helpers/functions
 @test "${TEST_LABEL} > Testing CLANG" {
 
     if [[ $NAME == "Darwin" ]]; then
-        run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION"
+        run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION -i /NEWPATH"
         ## CLANG already exists (c++/default)
         [[ ! -z $(echo "${output}" | grep "PIN_COMPILER: true") ]] || exit
         [[ ! -z $(echo "${output}" | grep "DCMAKE_CXX_COMPILER='c++'") ]] || exit
         [[ ! -z $(echo "${output}" | grep "DCMAKE_C_COMPILER='cc'") ]] || exit
     elif [[ $NAME == "Ubuntu" ]]; then
         install-package build-essential WETRUN 1>/dev/null # ubuntu 18 build-essential will be high enough, 16 won't and has a version < 7
-        run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION"
+        run bash -c "printf \"y\n%.0s\" {1..100} | ./$SCRIPT_LOCATION -i /NEWPATH"
         ## CLANG already exists (c++/default) (Ubuntu doesn't have clang>7, so we need to make sure it installs Clang 8)
         [[ ! -z $(echo "${output}" | grep "PIN_COMPILER: false") ]] || exit
         if [[ $VERSION_ID == "16.04" ]]; then
