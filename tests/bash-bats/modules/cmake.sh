38488c657f66f3db4a8c9184d3c48383c1a1a988
Nathan Pierce <nathan.pierce@block.one>
2019-06-28 14:25:45 -0400

    support for darwin's cmake being < required (since we don't source install) + bats

--- tests/bash-bats/modules/cmake.sh
+++ tests/bash-bats/modules/cmake.sh
@@ -5,13 +5,25 @@ load ../helpers/functions
     # Testing for if CMAKE already exists
     export CMAKE=${HOME}/cmake
     touch $CMAKE
-    run bash -c " ./$SCRIPT_LOCATION -y -P"
-    [[ ! -z $(echo "${output}" | grep "Executing: bash -c ${HOME}/cmake") ]] || exit
     export CMAKE_CURRENT_VERSION=3.7.1
     run bash -c " ./$SCRIPT_LOCATION -y -P"
-    [[ ! -z $(echo "${output}" | grep "We will be installing $CMAKE_VERSION") ]] || exit
+    if [[ $ARCH == "Darwin" ]]; then
+        [[ ! -z $(echo "${output}" | grep "($CMAKE_REQUIRED_VERSION). Cannot proceed") ]] || exit # Test the required version
+    else
+        [[ ! -z $(echo "${output}" | grep "We will be installing $CMAKE_VERSION") ]] || exit # Test the required version
+        [[ ! -z $(echo "${output}" | grep "Executing: bash -c ${HOME}/cmake") ]] || exit
+    fi
     # Testing for if cmake doesn't exist to be sure it's set properly
     export CMAKE=
+    run bash -c "./$SCRIPT_LOCATION -y -P"
+    if [[ $ARCH == "Darwin" ]]; then
+        [[ ! -z $(echo "${output}" | grep "($CMAKE_REQUIRED_VERSION). Cannot proceed") ]] || exit # Test the required version
+    else
+        [[ ! -z $(echo "${output}" | grep "We will be installing $CMAKE_VERSION") ]] || exit # Test the required version
+        [[ ! -z $(echo "${output}" | grep "Executing: bash -c ${BIN_DIR}/cmake") ]] || exit
+        [[ ! -z $(echo "${output}" | grep "CMAKE successfully installed") ]] || exit
+    fi
+    export CMAKE_CURRENT_VERSION=
     run bash -c "./$SCRIPT_LOCATION -y -P"
     if [[ $ARCH == "Darwin" ]]; then
         [[ ! -z $(echo "${output}" | grep "Executing: bash -c /usr/local/bin/cmake -DCMAKE_BUILD") ]] || exit
