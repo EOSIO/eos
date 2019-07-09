e861c845f8a50389b489bfa5f8f80ecbcbad032b
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-09 13:39:18 -0400

    Removed local reference when mounting EOS volume.

--- .cicd/eosio_build_u18.sh
+++ .cicd/eosio_build_u18.sh
@@ -8,6 +8,6 @@ cd eos
 git checkout release/1.8.x
 git submodule update --init --recursive
 
-docker run --rm -v /Users/scott.arnette/Repos/eos/.cicd/eos:/eos eos-dev bash -c "mkdir /eos/build && cd /eos/build && cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true /eos && make -j$(nproc)"
+docker run --rm -v $(pwd):/eos eos-dev bash -c "mkdir /eos/build && cd /eos/build && cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true /eos && make -j$(nproc)"
 
 cd .. && rm -rf eos
