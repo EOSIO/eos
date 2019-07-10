fffa86be6c098500f3ffa9801fd30f3a4ff122e9
Scott Arnette <scott.a.arnette@gmail.com>
2019-07-10 15:54:11 -0400

    Created dockerfile and script for amazonlinux2.

---
+++ .cicd/eosio_build_a2.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -eu
+
+rm -rf ./eos
+docker build -t eos-dev-a2 -f amazonlinux-2.dockerfile .
+git clone https://github.com/EOSIO/eos
+cd eos
+git checkout release/1.8.x
+git submodule update --init --recursive
+
+docker run --rm -v $(pwd):/eos eos-dev-a2 bash -c "mkdir /eos/build && cd /eos/build && cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true /eos && make -j$(nproc)"
+
+cd .. && rm -rf eos
