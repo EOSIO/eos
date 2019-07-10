c13ced16900805b22c09552355594999e4c3f03f
Justin Brown <justin.brown@block.one>
2019-07-10 18:29:15 -0400

    adds docker pull

---
+++ .cicd/before_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "os: $TRAVIS_OS_NAME"
+echo "docker image: $DOCKER_IMAGE"
+
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+  #todo: native build
+else
+  echo "building in docker image $DOCKER_IMAGE"
+  docker pull $DOCKER_IMAGE
+  #todo: mount source volume 
+  docker run -itd --name builder $DOCKER_IMAGE
+  docker exec builder 'echo "ps aux"'
+fi
+
+docker pull $DOCKER_IMAGE
--- .cicd/install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "$TRAVIS_OS_NAME"
-echo "$DOCKER_IMAGE"
