4eed44973c9e69fa20747e8da90391c15ae2b83a
Nathan Pierce <nathan.pierce@block.one>
2019-07-11 12:46:15 -0400

    cleanup

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -21,6 +21,7 @@ else # linux
     [[ $IMAGE_TAG == ubuntu-16.04 ]] || [[ $IMAGE_TAG == amazonlinux-2 ]] \
         && CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang'" # Explicitly set to clang else it will try and use gcc that's installed by default
     ( [[ $IMAGE_TAG == amazonlinux-2 ]] || [[ $IMAGE_TAG == centos-7 ]] ) && EXPORTS="export PATH=/usr/lib64/ccache:$PATH &&" || EXPORTS="export PATH=/usr/lib/ccache:$PATH &&" # ccache needs to come first in the list (devtoolset-8 overrides that if we include this in the Dockerfile)
-
+    
+    # DOCKER
     docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-$IMAGE_TAG bash -c "$PRE_COMMANDS ccache -s && mkdir /eos/build && cd /eos/build && $EXPORTS cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true $CMAKE_EXTRAS /eos && make -j $(getconf _NPROCESSORS_ONLN)"
 fi
