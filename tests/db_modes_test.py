bec2147753e8fb333738e876921f85605a10b474
Kevin Heifner <heifnerk@objectcomputing.com>
2019-07-11 15:12:08 -0400

    Allow more time for nodoes to start for slow test machines

--- tests/db_modes_test.py
+++ tests/db_modes_test.py
@@ -45,7 +45,7 @@ def run_and_kill(extraCmd="", killsig=signal.SIGTERM, preCmd=""):
     cmdArr= preCmd + nodeosCmd + " --http-server-address=127.0.0.1:" + str(port) + extraCmd
     print("port %d is free now, start to run nodeosCmd with extra parameter \"%s\"" % (port, extraCmd))
     proc=subprocess.Popen(cmdArr, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-    tries = 10
+    tries = 30
     ok = False
     getInfoCmd = cleosPath + " -u http://127.0.0.1:" + str(port) + "/ get info"
     while (tries > 0):
