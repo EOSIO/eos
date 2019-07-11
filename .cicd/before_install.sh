a3b49d6e8d50ac2e28410b1d5b63d2cd6762ae8d
Justin Brown <justin.brown@block.one>
2019-07-11 08:34:48 -0400

    outputs os versions

--- .cicd/before_install.sh
+++ .cicd/before_install.sh
@@ -5,11 +5,12 @@ echo "docker image: $DOCKER_IMAGE"
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   #todo: native build
-  echo "building for mac"
+  echo "building for mac:"
+  sw_vers
 else
   echo "building in docker image $DOCKER_IMAGE"
   docker pull $DOCKER_IMAGE
   #todo: mount source volume 
   docker run -itd --name builder $DOCKER_IMAGE
-  docker exec builder 'echo "ps aux"'
+  docker exec builder bash -c "cat /etc/os-release"
 fi
