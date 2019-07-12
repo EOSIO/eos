94de3c8bf9396f0dd88cf25184c5adfa65fb1d12
Nathan Pierce <nathan.pierce@block.one>
2019-07-12 14:22:58 -0400

    added tests for mac

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -10,6 +10,7 @@ if [[ "$(uname)" == Darwin ]]; then
     cd build
     cmake ..
     make -j $CPU_CORES
+    ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test
 else # linux
     echo 'Detected Linux, building in Docker.'
     echo "$ docker pull eosio/producer:ci-$IMAGE_TAG"
@@ -23,7 +24,9 @@ else # linux
     ## Ubuntu 16 is pinned only
     [[ $IMAGE_TAG == ubuntu-16.04 ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE='/tmp/pinned_toolchain.cmake'"
     ( [[ $IMAGE_TAG == amazonlinux-2 ]] || [[ $IMAGE_TAG == centos-7 ]] ) && EXPORTS="export PATH=/usr/lib64/ccache:$PATH &&" || EXPORTS="export PATH=/usr/lib/ccache:$PATH &&" # ccache needs to come first in the list (devtoolset-8 overrides that if we include this in the Dockerfile)
-    TEST_COMMAND="&& ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test"
     # DOCKER
-    docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-$IMAGE_TAG bash -c "echo \$CXX && echo \$CC && $PRE_COMMANDS ccache -s && mkdir /eos/build && cd /eos/build && $EXPORTS cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true $CMAKE_EXTRAS /eos && make -j $(getconf _NPROCESSORS_ONLN) $TEST_COMMAND"
+    docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-$IMAGE_TAG bash -c " \
+        $PRE_COMMANDS ccache -s && \
+        mkdir /eos/build && cd /eos/build && $EXPORTS cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true $CMAKE_EXTRAS /eos && make -j $(getconf _NPROCESSORS_ONLN) 
+        ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test"
 fi
