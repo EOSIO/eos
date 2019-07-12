545f63b66ae87cc621159f173f7f0849bd380ae8
Kevin Heifner <heifnerk@objectcomputing.com>
2019-07-12 13:28:53 -0400

    Test without shutdown

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -956,7 +956,7 @@ namespace eosio {
       self->socket_open = false;
       boost::system::error_code ec;
       if( self->socket->is_open() ) {
-         self->socket->shutdown( tcp::socket::shutdown_both, ec );
+//         self->socket->shutdown( tcp::socket::shutdown_both, ec );
          self->socket->close();
       }
       self->socket.reset( new tcp::socket( my_impl->thread_pool->get_executor() ) );
