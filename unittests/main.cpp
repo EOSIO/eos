e723387234c984e87a9f6963dc1e75236a6194f5
Olexiy Buyanskyy <olexiyb@gmail.com>
2019-07-12 14:12:04 +0300

    enable debug mode by default.

--- unittests/main.cpp
+++ unittests/main.cpp
@@ -20,21 +20,7 @@ void translate_fc_exception(const fc::exception &e) {
 }
 
 boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[]) {
-   // Turn off blockchain logging if no --verbose parameter is not added
-   // To have verbose enabled, call "tests/chain_test -- --verbose"
-   bool is_verbose = false;
-   std::string verbose_arg = "--verbose";
-   for (int i = 0; i < argc; i++) {
-      if (verbose_arg == argv[i]) {
-         is_verbose = true;
-         break;
-      }
-   }
-   if(is_verbose) {
-      fc::logger::get(DEFAULT_LOGGER).set_log_level(fc::log_level::debug);
-   } else {
-      fc::logger::get(DEFAULT_LOGGER).set_log_level(fc::log_level::off);
-   }
+   fc::logger::get(DEFAULT_LOGGER).set_log_level(fc::log_level::debug);
 
    // Register fc::exception translator
    boost::unit_test::unit_test_monitor.register_exception_translator<fc::exception>(&translate_fc_exception);
