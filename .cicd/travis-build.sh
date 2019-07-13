e83fb7cc704f441fd3eb0d1629b0b59faa164f44
Justin Brown <justin.brown@block.one>
2019-07-13 14:39:48 -0400

    removes verbose logging

--- .cicd/travis-build.sh
+++ .cicd/travis-build.sh
@@ -24,7 +24,7 @@ else # linux
     [[ $IMAGE_TAG == ubuntu-16.04 ]] && EXPORTS="export CCACHE_EXTRAFILES=\"/eos/unittests/wasm_tests.cpp:/eos/unittests/incbin.h:/eos/plugins/producer_api_plugin.cpp\" &&" && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE='/tmp/pinned_toolchain.cmake' -DCMAKE_CXX_COMPILER_LAUNCHER=ccache" # pinned only
     [[ $IMAGE_TAG == amazonlinux-2 || $IMAGE_TAG == centos-7 ]] && EXPORTS="export PATH=/usr/lib64/ccache:$PATH &&" || EXPORTS="$EXPORTS export PATH=/usr/lib/ccache:$PATH &&" # ccache needs to come first in the list (devtoolset-8 overrides that if we include this in the Dockerfile)
     # DOCKER
-    docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_LOGFILE=/dev/stdout -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-$IMAGE_TAG bash -c " \
+    docker run --rm -v $(pwd):/eos -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache eosio/producer:ci-$IMAGE_TAG bash -c " \
         $PRE_COMMANDS ccache -s && \
         mkdir /eos/build && cd /eos/build && $EXPORTS cmake -DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DOPENSSL_ROOT_DIR='/usr/include/openssl' -DBUILD_MONGO_DB_PLUGIN=true $CMAKE_EXTRAS /eos && make -j $(getconf _NPROCESSORS_ONLN) 
         ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test"
