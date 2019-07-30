bd6ac36188dded505de6ee2eb143241979f54dc9
Nathan Pierce <nathan.pierce@block.one>
2019-07-30 11:41:36 -0400

    removed submodule regression check for travis/mac

--- .cicd/travis/travis-build.sh
+++ .cicd/travis/travis-build.sh
@@ -13,7 +13,6 @@ if [[ "$(uname)" == Darwin ]]; then
     if ${ENABLE_PARALLEL_TESTS:-true}; then fold-execute ctest -j$(getconf _NPROCESSORS_ONLN) -LE _tests --output-on-failure -T Test; fi
     if ${ENABLE_SERIAL_TESTS:-true}; then mkdir -p ./mongodb && fold-execute mongod --dbpath ./mongodb --fork --logpath mongod.log && fold-execute ctest -L nonparallelizable_tests --output-on-failure -T Test; fi
     if ${ENABLE_LR_TESTS:-false}; then fold-execute ctest -L long_running_tests --output-on-failure -T Test; fi
-    if ${ENABLE_SUBMODULE_REGRESSION_TEST:-true}; then cd $ROOT_DIR && fold-execute ./.cicd/submodule-regression-checker.sh; fi
 else # linux
     DOCKER_RUN_EXTRAS="-e ENABLE_PACKAGE_BUILDER=false" # Travis doesn't need to test or push packages
     execute eval docker run --rm -v $(pwd):/workdir -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e CCACHE_DIR=/opt/.ccache -e TRAVIS $DOCKER_RUN_EXTRAS $FULL_TAG
