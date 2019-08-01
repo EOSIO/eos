41b2a06494d6d96eb5c3c12072579c46fc6dc51d
Kevin Heifner <heifnerk@objectcomputing.com>
2019-08-01 17:12:46 -0500

    Use future instead of shared_future and remove next method which is not as performant

--- libraries/chain/include/eosio/chain/transaction_metadata.hpp
+++ libraries/chain/include/eosio/chain/transaction_metadata.hpp
@@ -16,8 +16,7 @@ namespace eosio { namespace chain {
 
 class transaction_metadata;
 using transaction_metadata_ptr = std::shared_ptr<transaction_metadata>;
-using recover_keys_next = std::function<void(std::tuple<transaction_metadata_ptr, std::exception_ptr>)>;
-using recover_keys_future = std::shared_future<transaction_metadata_ptr>;
+using recover_keys_future = std::future<transaction_metadata_ptr>;
 
 /**
  *  This data structure should store context-free cached data about a transaction such as
@@ -37,12 +36,6 @@ class transaction_metadata {
    private:
       struct private_type{};
 
-      transaction_metadata() = delete;
-      transaction_metadata(const transaction_metadata&) = delete;
-      transaction_metadata(transaction_metadata&&) = delete;
-      transaction_metadata operator=(transaction_metadata&) = delete;
-      transaction_metadata operator=(transaction_metadata&&) = delete;
-
       static void check_variable_sig_size(const packed_transaction_ptr& trx, uint32_t max) {
          for(const signature_type& sig : trx->get_signed_transaction().signatures)
             EOS_ASSERT(sig.variable_size() <= max, sig_variable_size_limit_exception,
@@ -50,6 +43,21 @@ class transaction_metadata {
       }
 
    public:
+      // creation of tranaction_metadata should restricted to start_recover_keys and create_no_recover_keys below, public for make_shared
+      explicit transaction_metadata( const private_type& pt, const packed_transaction_ptr& ptrx,
+                                     fc::microseconds sig_cpu_usage, flat_set<public_key_type> recovered_pub_keys )
+         : _packed_trx( ptrx )
+         , _sig_cpu_usage( sig_cpu_usage )
+         , _recovered_pub_keys( std::move( recovered_pub_keys ) ) {
+      }
+
+      transaction_metadata() = delete;
+      transaction_metadata(const transaction_metadata&) = delete;
+      transaction_metadata(transaction_metadata&&) = delete;
+      transaction_metadata operator=(transaction_metadata&) = delete;
+      transaction_metadata operator=(transaction_metadata&&) = delete;
+
+
       const packed_transaction_ptr& packed_trx()const { return _packed_trx; }
       const transaction_id_type& id()const { return _packed_trx->id(); }
       fc::microseconds signature_cpu_usage()const { return _sig_cpu_usage; }
@@ -59,20 +67,12 @@ class transaction_metadata {
       static recover_keys_future
       start_recover_keys( const packed_transaction_ptr& trx, boost::asio::io_context& thread_pool,
                           const chain_id_type& chain_id, fc::microseconds time_limit,
-                          uint32_t max_variable_sig_size = UINT32_MAX,
-                          recover_keys_next = recover_keys_next() );
+                          uint32_t max_variable_sig_size = UINT32_MAX );
 
       static transaction_metadata_ptr
       create_no_recover_keys( const packed_transaction& trx ) {
-         return std::make_shared<transaction_metadata>(
-               private_type(), std::make_shared<packed_transaction>( trx ), fc::microseconds(), flat_set<public_key_type>() );
-      }
-
-      explicit transaction_metadata( const private_type& pt, const packed_transaction_ptr& ptrx,
-            fc::microseconds sig_cpu_usage, flat_set<public_key_type> recovered_pub_keys )
-         : _packed_trx( ptrx )
-         , _sig_cpu_usage( sig_cpu_usage )
-         , _recovered_pub_keys( std::move( recovered_pub_keys ) ) {
+         return std::make_shared<transaction_metadata>( private_type(),
+               std::make_shared<packed_transaction>( trx ), fc::microseconds(), flat_set<public_key_type>() );
       }
 
 };
--- libraries/chain/transaction_metadata.cpp
+++ libraries/chain/transaction_metadata.cpp
@@ -8,25 +8,16 @@ recover_keys_future transaction_metadata::start_recover_keys( const packed_trans
                                                               boost::asio::io_context& thread_pool,
                                                               const chain_id_type& chain_id,
                                                               fc::microseconds time_limit,
-                                                              uint32_t max_variable_sig_size,
-                                                              recover_keys_next next )
+                                                              uint32_t max_variable_sig_size )
 {
-   return async_thread_pool( thread_pool,
-      [trx, chain_id, time_limit, max_variable_sig_size, next{std::move( next )}]() {
-         try {
-            fc::time_point deadline = time_limit == fc::microseconds::maximum() ?
-                                      fc::time_point::maximum() : fc::time_point::now() + time_limit;
-            check_variable_sig_size( trx, max_variable_sig_size );
-            const signed_transaction& trn = trx->get_signed_transaction();
-            flat_set<public_key_type> recovered_pub_keys;
-            fc::microseconds cpu_usage = trn.get_signature_keys( chain_id, deadline, recovered_pub_keys );
-            auto mtrx = std::make_shared<transaction_metadata>( private_type(), trx, cpu_usage, std::move( recovered_pub_keys ) );
-            if( next ) next( std::make_tuple( mtrx, nullptr ) );
-            return mtrx;
-         } catch( ... ) {
-            if( next ) next( std::make_tuple( nullptr, std::current_exception() ) );
-            throw;
-         }
+   return async_thread_pool( thread_pool, [trx, chain_id, time_limit, max_variable_sig_size]() {
+         fc::time_point deadline = time_limit == fc::microseconds::maximum() ?
+                                   fc::time_point::maximum() : fc::time_point::now() + time_limit;
+         check_variable_sig_size( trx, max_variable_sig_size );
+         const signed_transaction& trn = trx->get_signed_transaction();
+         flat_set<public_key_type> recovered_pub_keys;
+         fc::microseconds cpu_usage = trn.get_signature_keys( chain_id, deadline, recovered_pub_keys );
+         return std::make_shared<transaction_metadata>( private_type(), trx, cpu_usage, std::move( recovered_pub_keys ) );
       }
    );
 }
