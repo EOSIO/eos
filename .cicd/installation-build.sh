0fbed3644d62bcedc20a03ddce4dedbb5f64794f
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-06 14:17:03 -0400

    Debugging.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -5,10 +5,11 @@ set -eo pipefail
 # TODO: Add if statement to gate this only running on release/ , develop , and master $BUILDKITE_BRANCH.
 cd ..
 determine_eos_version
-execute docker run --name eos-binaries-$BUILDKITE_COMMIT -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
-
 export EOS_BINARIES_TAG="eosio/producer:eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH"
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+
+docker run --name eos-binaries-$BUILDKITE_COMMIT -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
+
 docker tag eos-binaries-$BUILDKITE_COMMIT $EOS_BINARIES_TAG
 docker push $EOS_BINARIES_TAG
 docker push $EOS_BINARIES_TAG-$BUILDKITE_COMMIT
