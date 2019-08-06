038598da49584abc651b88a6b33323e465231cae
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-06 11:05:50 -0400

    Debugging.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -2,4 +2,5 @@
 set -eo pipefail
 . ./.helpers
 
-execute docker run -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true $FULL_TAG bash -c "pwd && ls -la /workdir"
+cd ..
+execute docker run -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true $FULL_TAG
