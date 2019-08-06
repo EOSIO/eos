2b7ad3ac0b555debba4e312a7b8cd377d14f7c1d
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-06 12:55:46 -0400

    Updated hash. Changed tagging.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -7,11 +7,9 @@ cd ..
 determine_eos_version
 execute docker run --name eos-binaries -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
 
-export BINARIES_TAG_HASH="eosio/eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH:$BUILDKITE_COMMIT"
-export BINARIES_TAG_LATEST="eosio/eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH:latest"
+export EOS_BINARIES_TAG="eosio/eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH"
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
-docker tag eos-binaries $BINARIES_TAG_HASH
-docker tag eos-binaries $BINARIES_TAG_LATEST
-docker push $BINARIES_TAG_HASH
-docker push $BINARIES_TAG_LATEST
+docker tag eos-binaries $EOS_BINARIES_TAG
+docker push $EOS_BINARIES_TAG
+docker push $EOS_BINARIES_TAG:$BUILDKITE_COMMIT
 
