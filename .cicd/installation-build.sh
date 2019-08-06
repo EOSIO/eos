4d8606ffd5ff4aa533cef970d9328a8bb8ace3ab
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-06 13:30:35 -0400

    Changed tag.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -7,9 +7,9 @@ cd ..
 determine_eos_version
 execute docker run --name eos-binaries -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
 
-export EOS_BINARIES_TAG="eosio/eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH"
+export EOS_BINARIES_TAG="eosio/producer:eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH"
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
 docker tag eos-binaries $EOS_BINARIES_TAG
 docker push $EOS_BINARIES_TAG
-docker push $EOS_BINARIES_TAG:$BUILDKITE_COMMIT
+docker push $EOS_BINARIES_TAG-$BUILDKITE_COMMIT
 
