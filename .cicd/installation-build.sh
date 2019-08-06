444a472170b364fc9d0021f89f58d16f4ef57a97
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-06 11:35:40 -0400

    Added flags to disable tests and uploads.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -3,4 +3,8 @@ set -eo pipefail
 . ./.helpers
 
 cd ..
-execute docker run -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true $FULL_TAG
+execute docker run --name eos-binaries -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
+
+docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
+docker tag eos-binaries eosio/producer:eos-binaries-$HASHED_IMAGE_TAG
+docker push eosio/producer:eos-binaries-$HASHED_IMAGE_TAG
