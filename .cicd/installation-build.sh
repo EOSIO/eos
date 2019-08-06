ef49909f8a5ba0053eaf1171d23b409c98973e12
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-06 12:53:09 -0400

    Added tags and push commands.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -2,9 +2,16 @@
 set -eo pipefail
 . ./.helpers
 
+# TODO: Add if statement to gate this only running on release/ , develop , and master $BUILDKITE_BRANCH.
 cd ..
+determine_eos_version
 execute docker run --name eos-binaries -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
 
+export BINARIES_TAG_HASH="eosio/eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH:$BUILDKITE_COMMIT"
+export BINARIES_TAG_LATEST="eosio/eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH:latest"
 docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
-docker tag eos-binaries eosio/producer:eos-binaries-$HASHED_IMAGE_TAG
-docker push eosio/producer:eos-binaries-$HASHED_IMAGE_TAG
+docker tag eos-binaries $BINARIES_TAG_HASH
+docker tag eos-binaries $BINARIES_TAG_LATEST
+docker push $BINARIES_TAG_HASH
+docker push $BINARIES_TAG_LATEST
+
