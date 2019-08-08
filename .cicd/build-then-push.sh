e8aaf1ea7d3a57868932d314f98f81642e42d5f9
Zach Butler <zach.butler@block.one>
2019-08-08 17:48:37 -0400

    build-then-push.sh now exports build.tar.gz artifact on Buildkite

--- .cicd/build-then-push.sh
+++ .cicd/build-then-push.sh
@@ -6,3 +6,5 @@ execute docker build -f ./.cicd/docker/ubuntu-18.04-build.dockerfile -t eosio/ci
 # push
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 execute docker push eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}')
+# attach build.tar.gz artifact
+[[ "$BUILDKITE" == 'true' ]] && buildkite-agent artifact upload build.tar.gz
