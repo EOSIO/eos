17ee21428e21cbc7b244e1b1a39549373b5c2325
Zach Butler <zach.butler@block.one>
2019-08-08 17:43:54 -0400

    Fixed docker build paths

--- .cicd/build-then-push.sh
+++ .cicd/build-then-push.sh
@@ -2,7 +2,7 @@
 set -e
 . ./.cicd/.helpers
 # build
-execute docker build -f ./.cicd/ubuntu-18.04-build.dockerfile -t eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}') .
+execute docker build -f ./.cicd/docker/ubuntu-18.04-build.dockerfile -t eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}') .
 # push
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 execute docker push eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}')
