6df20917a19fc7fef008594e14088655cefe6f48
Olexiy Buyanskyy <olexiy.buyanskyy@remme.io>
2019-08-08 14:36:22 +0300

    fixed db_modes_test

--- tests/db_modes_test.sh
+++ tests/db_modes_test.sh
@@ -30,7 +30,7 @@ done
 
 EOSIO_STUFF_DIR=$(mktemp -d)
 trap "rm -rf $EOSIO_STUFF_DIR" EXIT
-NODEOS_LAUNCH_PARAMS="./programs/nodeos/nodeos -d $EOSIO_STUFF_DIR --config-dir $EOSIO_STUFF_DIR \
+NODEOS_LAUNCH_PARAMS="./programs/remnode/remnode -d $EOSIO_STUFF_DIR --config-dir $EOSIO_STUFF_DIR \
 --chain-state-db-size-mb 8 --chain-state-db-guard-size-mb 0 --reversible-blocks-db-size-mb 1 \
 --reversible-blocks-db-guard-size-mb 0 -e -peosio"
 
