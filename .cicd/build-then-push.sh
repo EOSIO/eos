f38bb62e91b69f3c05331b8eb23d6cd74f58a617
Zach Butler <zach.butler@block.one>
2019-08-09 10:10:59 -0400

    Allow build-then-push.sh to be run locall and in Travis

--- .cicd/build-then-push.sh
+++ .cicd/build-then-push.sh
@@ -4,7 +4,9 @@ set -e
 # build
 execute docker build -f ./.cicd/docker/ubuntu-18.04-build.dockerfile -t eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}') .
 # push
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-execute docker push eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}')
-# attach build.tar.gz artifact
-[[ "$BUILDKITE" == 'true' ]] && buildkite-agent artifact upload build.tar.gz
+if [[ "$TRAVIS" != 'true' ]]; then
+    [[ "$BUILDKITE" != 'true' ]] && docker login || echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    execute docker push eosio/ci-contracts-builder:base-ubuntu-18.04-$(git log | head -n 1 | awk '{print $2}')
+    # attach build.tar.gz artifact
+    [[ "$BUILDKITE" == 'true' ]] && buildkite-agent artifact upload build.tar.gz
+fi
