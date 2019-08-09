7934b83eb6074510a4410d2be3426295b8d6e605
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-09 16:34:16 -0400

    Removed using EOS version. Rely on branch and git commit.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -4,9 +4,8 @@ set -eo pipefail
 
 if [[ $BUILDKITE_BRANCH =~ ^release/[0-9].[0-9]+.x$ || $BUILDKITE_BRANCH =~ ^master$ || $BUILDKITE_BRANCH =~ ^develop$ || $FORCE_BINARIES_BUILD == true ]]; then
     cd ..
-    determine_eos_version
     export BRANCH=$(echo $BUILDKITE_BRANCH | sed 's/\//\-/')
-    export EOS_BINARIES_TAG="eosio/producer:eos-binaries-$BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH"
+    export EOS_BINARIES_TAG="eosio/producer:eos-binaries-$BRANCH"
     docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
     docker run --name eos-binaries-$BUILDKITE_COMMIT -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
     docker commit eos-binaries-$BUILDKITE_COMMIT $EOS_BINARIES_TAG
