f79fc9de0d096cd3175791d729bf1fa724a97fea
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-12 12:05:47 -0400

    Fixed issue with branch names containing slashes.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -4,13 +4,8 @@ set -eo pipefail
 
 if [[ $BUILDKITE_BRANCH =~ ^release/[0-9].[0-9]+.x$ || $BUILDKITE_BRANCH =~ ^master$ || $BUILDKITE_BRANCH =~ ^develop$ || $FORCE_BINARIES_BUILD == true ]]; then
     cd ..
-<<<<<<< Updated upstream
-    determine_eos_version
-    export EOS_BINARIES_TAG="eosio/producer:eos-binaries-$BUILDKITE_BRANCH-$EOSIO_VERSION_MAJOR.$EOSIO_VERSION_MINOR.$EOSIO_VERSION_PATCH"
-=======
     export BRANCH=$(echo $BUILDKITE_BRANCH | sed 's/\//\-/')
     export EOS_BINARIES_TAG="eosio/producer:eos-binaries-$BRANCH"
->>>>>>> Stashed changes
     docker login -u $DOCKERHUB_USERNAME -p $DOCKERHUB_PASSWORD
     docker run --name eos-binaries-$BUILDKITE_COMMIT -v $(pwd):/workdir -e JOBS -e ENABLE_INSTALL=true -e ENABLE_PARALLEL_TESTS=false -e ENABLE_SERIAL_TESTS=false -e TRAVIS=true $FULL_TAG
     docker commit eos-binaries-$BUILDKITE_COMMIT $EOS_BINARIES_TAG
