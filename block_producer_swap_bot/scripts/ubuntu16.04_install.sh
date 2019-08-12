fa9ec356bad396d7f129b890c5be9cf37ea117f1
Artem Saprykin <asaprykin17@gmail.com>
2019-08-12 18:38:40 +0300

    ubuntu16 script added

---
+++ block_producer_swap_bot/scripts/ubuntu16.04_install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+sudo apt-get upgrade
+apt-get update \
+&& apt-get install -y software-properties-common curl \
+&& add-apt-repository ppa:deadsnakes/ppa \
+&& apt-get update \
+&& apt-get install -y python3.6 python3.6-venv
+
+sudo apt-get install python3.6-dev
+
+sudo apt-get install virtualenv
+
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+
+virtualenv -p python3.6 swap_env
+source ./swap_env/bin/activate
+pip3 install -r requirements.txt
