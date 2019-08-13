b38961368df8e2f08d8502a13f91f0fdc771e2b7
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-13 10:16:00 -0400

    Removed function to determine EOS version.

--- .cicd/helpers/general.sh
+++ .cicd/helpers/general.sh
@@ -3,10 +3,3 @@ export BUILD_DIR=$ROOT_DIR/build
 export CICD_DIR=$ROOT_DIR/.cicd
 export HELPERS_DIR=$CICD_DIR/helpers
 export JOBS=${JOBS:-"$(getconf _NPROCESSORS_ONLN)"}
-
-function determine_eos_version() {
-    export EOSIO_VERSION_MAJOR=$(cat ./CMakeLists.txt | grep -E "^[[:blank:]]*set[[:blank:]]*\([[:blank:]]*VERSION_MAJOR" | tail -1 | sed 's/.*VERSION_MAJOR //g' | sed 's/ //g' | sed 's/"//g' | cut -d\) -f1)
-    export EOSIO_VERSION_MINOR=$(cat ./CMakeLists.txt | grep -E "^[[:blank:]]*set[[:blank:]]*\([[:blank:]]*VERSION_MINOR" | tail -1 | sed 's/.*VERSION_MINOR //g' | sed 's/ //g' | sed 's/"//g' | cut -d\) -f1)
-    export EOSIO_VERSION_PATCH=$(cat ./CMakeLists.txt | grep -E "^[[:blank:]]*set[[:blank:]]*\([[:blank:]]*VERSION_PATCH" | tail -1 | sed 's/.*VERSION_PATCH //g' | sed 's/ //g' | sed 's/"//g' | cut -d\) -f1)
-    export EOSIO_VERSION_SUFFIX=$(cat ./CMakeLists.txt | grep -E "^[[:blank:]]*set[[:blank:]]*\([[:blank:]]*VERSION_SUFFIX" | tail -1 | sed 's/.*VERSION_SUFFIX //g' | sed 's/ //g' | sed 's/"//g' | cut -d\) -f1)
-}
