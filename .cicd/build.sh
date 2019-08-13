591c57978a819eb54092a3b4ba6337211ed2a238
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-13 15:22:09 -0400

    Add build type to cmake.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -60,6 +60,6 @@ else # Linux
     fi
 
     echo "docker run $ARGS $evars eosio/producer:eos-ubuntu-18.04-7f7907879240c857f2a7952115157003f8281f32 bash -c \"$COMMANDS\""
-    docker run $ARGS $evars eosio/producer:eos-ubuntu-18.04-7f7907879240c857f2a7952115157003f8281f32 bash -c \\\"$COMMANDS\\\"
+    eval docker run $ARGS $evars eosio/producer:eos-ubuntu-18.04-7f7907879240c857f2a7952115157003f8281f32 bash -c \"$COMMANDS\"
 
 fi
