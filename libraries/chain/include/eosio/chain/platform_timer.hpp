357e30c76554ce899f2c7ee3da208ce03d5b9555
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2019-08-20 09:28:48 -0400

    callback support for the checktime timer

--- libraries/chain/include/eosio/chain/platform_timer.hpp
+++ libraries/chain/include/eosio/chain/platform_timer.hpp
@@ -2,6 +2,8 @@
 #include <fc/time.hpp>
 #include <fc/fwd.hpp>
 
+#include <eosio/chain/exceptions.hpp>
+
 #include <signal.h>
 
 namespace eosio { namespace chain {
@@ -13,12 +15,29 @@ struct platform_timer {
    void start(fc::time_point tp);
    void stop();
 
+   /* Sets a callback for when timer expires. Be aware this could might fire from a signal handling context and/or
+      on any particular thread. Only a single callback can be registered at once; trying to register more will
+      result in an exception. Setting to nullptr is allowed as a "no callback" hint */
+   void set_expiry_callback(void(*func)(void*), void* user) {
+      EOS_ASSERT(!(func && _expiration_callback), misc_exception, "Setting a platform_timer callback when one already exists");
+      _expiration_callback = func;
+      _expiration_callback_data = user;
+   }
+
    volatile sig_atomic_t expired = 1;
 
 private:
    struct impl;
    constexpr static size_t fwd_size = 8;
    fc::fwd<impl,fwd_size> my;
+
+   void call_expiration_callback() {
+      if(_expiration_callback)
+         _expiration_callback(_expiration_callback_data);
+   }
+
+   void(*_expiration_callback)(void*) = nullptr;
+   void* _expiration_callback_data;
 };
 
 }}
