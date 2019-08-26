dd7ab6bf0f046f7ac1c54cee420033e8ff619836
Nathan Pierce <nathan.pierce@block.one>
2019-08-26 11:43:46 -0400

    quick fixes

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -26,7 +26,7 @@ else # Linux
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_BUILD_TYPE='Release'"
     elif [[ $IMAGE_TAG == 'ubuntu-16.04' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
-        CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/.cicd/helpers/clang.make -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+        CMAKE_EXTRAS="$CMAKE_EXTRA -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
     elif [[ $IMAGE_TAG == 'centos-7.6' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'amazon_linux-2' ]]; then
