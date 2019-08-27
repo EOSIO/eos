cbb69a21deb0f0801cc4f10d6820e3ab7cabec3e
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-27 17:24:34 -0400

    Fix conditional cmake flags and PRE_COMMANDS.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -22,9 +22,15 @@ else # Linux
     PRE_COMMANDS="cd $MOUNTED_DIR/build"
     # PRE_COMMANDS: Executed pre-cmake
     # CMAKE_EXTRAS: Executed within and right before the cmake path (cmake CMAKE_EXTRAS ..)
-    if [[ ! $IMAGE_TAG =~ 'unpinned' ]]; then
-        CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/.cicd/helpers/clang.make -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
-    elif [[ $IMAGE_TAG =~ 'ubuntu' ]]; then
+    [[ ! $IMAGE_TAG =~ 'unpinned' ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/.cicd/helpers/clang.make -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+
+    if [[ $IMAGE_TAG == 'amazon_linux-2' ]]; then
+        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
+    elif [[ $IMAGE_TAG == 'centos-7.6' ]]; then
+        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
+    elif [[ $IMAGE_TAG == 'ubuntu-16.04' ]]; then
+        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
+    elif [[ $IMAGE_TAG == 'ubuntu-18.04' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'amazon_linux-2-unpinned' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
@@ -32,10 +38,11 @@ else # Linux
     elif [[ $IMAGE_TAG == 'centos-7.6-unpinned' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'ubuntu-18.04-unpinned' ]]; then
-        CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DCMAKE_BUILD_TYPE='Release'"
+        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
+        CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang'"
     fi
 
-    BUILD_COMMANDS="cmake $CMAKE_EXTRAS -DBUILD_MONGO_DB_PLUGIN=true .. && make -j$JOBS"
+    BUILD_COMMANDS="cmake $CMAKE_EXTRAS -DBUILD_MONGO_DB_PLUGIN=true -DCMAKE_BUILD_TYPE='Release' .. && make -j$JOBS"
 
     # Docker Commands
     if [[ $BUILDKITE == true ]]; then
