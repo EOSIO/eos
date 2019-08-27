d524f1815e64e2ec47195a577763b20f2116e3f3
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-27 17:12:32 -0400

    Fix brew.

--- .cicd/anka/macos-10.14.sh
+++ .cicd/anka/macos-10.14.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 brew update
-brew install boost, cmake, python@2, python, libtool, libusb, graphviz, automake, wget, gmp, llvm@4, llvm@7, pkgconfig, doxygen
+brew install boost@1.70 cmake python@2 python libtool libusb graphviz automake wget gmp llvm@4 pkgconfig doxygen
 git clone https://git.llvm.org/git/llvm.git --branch release_80 --recursive
 cd llvm
 mkdir build
