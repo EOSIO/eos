859148858c9b9310a13c8479b2731b1c0ff8de62
Nathan Pierce <nathan.pierce@block.one>
2019-08-28 16:23:54 -0400

    quick fixes

--- .cicd/platforms/macos-10.14.sh
+++ .cicd/platforms/macos-10.14.sh
@@ -12,7 +12,7 @@ git clone --single-branch --branch release_80 https://git.llvm.org/git/llvm.git 
     cd ../ && git clone --single-branch --branch release_80 https://git.llvm.org/git/compiler-rt.git && cd compiler-rt && git checkout 5bc7979 && \
     mkdir ../../build && cd ../../build && \
     cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX='/usr/local' -DLLVM_BUILD_EXTERNAL_COMPILER_RT=ON -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_ENABLE_LIBCXX=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_INCLUDE_DOCS=OFF -DLLVM_OPTIMIZED_TABLEGEN=ON -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_BUILD_TYPE=Release .. && \
-    make -j $(nproc) && \
+    make -j $(getconf _NPROCESSORS_ONLN) && \
     make install && \
     cd ../.. && \
     rm -rf clang8/
