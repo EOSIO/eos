0ffa210aac470e081d43d5104a6428ea67fa3833
Nathan Pierce <nathan.pierce@block.one>
2019-08-29 11:40:49 -0400

    quick fixes

--- .cicd/platforms/macos-10.14-unpinned.sh
+++ .cicd/platforms/macos-10.14-unpinned.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 brew update
-brew install boost@1.70 cmake python@2 python libtool libusb graphviz automake wget gmp llvm@4 pkgconfig doxygen
+brew install git boost@1.70 cmake python@2 python libtool libusb graphviz automake wget gmp llvm@4 pkgconfig doxygen openssl mongodb
