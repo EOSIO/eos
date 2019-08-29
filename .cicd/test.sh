e4ffaf92b9be69d00e54896a4a59a6a008cc7850
Zach Butler <zach.butler@block.one>
2019-08-29 19:04:47 -0400

    Test Runner: Catch and re-throw errors

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -5,8 +5,15 @@ set -eo pipefail
 # tests
 if [[ $(uname) == 'Darwin' ]]; then # macOS
     ./"$@"
+    EXIT_STATUS=$?
 else # Linux
     . $HELPERS_DIR/docker-hash.sh
     echo "$ docker run --rm --init -v $(pwd):$MOUNTED_DIR $(buildkite-intrinsics) $FULL_TAG bash -c \"$MOUNTED_DIR/$@\""
     eval docker run --rm --init -v $(pwd):$MOUNTED_DIR $(buildkite-intrinsics) $FULL_TAG bash -c "$MOUNTED_DIR/$@"
+    EXIT_STATUS=$?
+fi
+# re-throw
+if [[ "$EXIT_STATUS" != 0 ]]; then
+    echo "Failing due to non-zero exit status from ctest: $EXIT_STATUS"
+    exit $EXIT_STATUS
 fi
