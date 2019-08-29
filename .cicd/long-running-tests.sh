fcaef568db8722d7efc7781698d886e051f5df8a
Zach Butler <zach.butler@block.one>
2019-08-29 18:14:02 -0400

    Don't forget to use general.sh.buildkite-intrinsics() in long-running-test.sh

--- .cicd/long-running-tests.sh
+++ .cicd/long-running-tests.sh
@@ -12,12 +12,6 @@ else # Linux
     PRE_COMMANDS="cd $MOUNTED_DIR/build"
     [[ $IMAGE_TAG == 'centos-7.6' ]] && PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
     COMMANDS="$PRE_COMMANDS && $TEST"
-    # Load BUILDKITE Environment Variables for use in docker run
-    if [[ -f $BUILDKITE_ENV_FILE ]]; then
-        evars=""
-        while read -r var; do
-            evars="$evars --env ${var%%=*}"
-        done < "$BUILDKITE_ENV_FILE"
-    fi
-    eval docker run $ARGS $evars $FULL_TAG bash -c \"$COMMANDS\"
+    echo "$ docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\""
+    eval docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\"
 fi
