89abe58bbb74a2d310984b007a9090b2ee514201
Scott Arnette <scott.a.arnette@gmail.com>
2019-08-29 13:38:26 -0400

    Darwin on Travis needs UNPINNED=true.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -9,7 +9,7 @@ CMAKE_EXTRAS="-DBUILD_MONGO_DB_PLUGIN=true"
 if [[ $(uname) == 'Darwin' ]]; then
 
     # You can't use chained commands in execute
-    [[ $TRAVIS == true ]] && export UNPINNED=true && ccache -s && ./$CICD_DIR/platforms/macos-10.14.sh
+    [[ $TRAVIS == true ]] && export UNPINNED=true && export PINNED=false && ccache -s && ./$CICD_DIR/platforms/macos-10.14.sh
     [[ $PINNED == true ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$HELPERS_DIR/clang.make"
     cd $BUILD_DIR
     cmake $CMAKE_EXTRAS ..
