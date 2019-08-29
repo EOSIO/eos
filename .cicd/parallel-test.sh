d16261e8477e6cbefad20506b9258a9ad28335e7
Zach Butler <zach.butler@block.one>
2019-08-29 18:44:19 -0400

    Call test scripts instead of building test commands from scratch

--- .cicd/parallel-test.sh
+++ .cicd/parallel-test.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 . ./.cicd/helpers/general.sh
-TEST="ctest -j$JOBS -LE _tests --output-on-failure -T Test"
-if [[ $(uname) == 'Darwin' ]]; then
-    # You can't use chained commands in execute
-    cd $BUILD_DIR
-    bash -c "$TEST"
+if [[ $(uname) == 'Darwin' ]]; then # macOS
+    ./scripts/parallel-test.sh
 else # Linux
-    ARGS=${ARGS:-"--rm --init -v $(pwd):$MOUNTED_DIR"}
     . $HELPERS_DIR/docker-hash.sh
-    COMMANDS="cd $MOUNTED_DIR/build && $TEST"
-    echo "$ docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\""
-    eval docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\"
+    echo "$ docker run --rm --init -v $(pwd):$MOUNTED_DIR $(buildkite-intrinsics) $FULL_TAG bash -c \"$MOUNTED_DIR/scripts/parallel-test.sh\""
+    eval docker run --rm --init -v $(pwd):$MOUNTED_DIR $(buildkite-intrinsics) $FULL_TAG bash -c "$MOUNTED_DIR/scripts/parallel-test.sh"
 fi
--- .cicd/serial-test.sh
+++ .cicd/serial-test.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 set -eo pipefail
 . ./.cicd/helpers/general.sh
-[[ -z $1 ]] && TEST_NAME="-L nonparallelizable_tests" || TEST_NAME="-R ^$1$"
-TEST="mkdir -p ./mongodb && mongod --dbpath ./mongodb --fork --logpath mongod.log && ctest $TEST_NAME --output-on-failure -T Test"
-if [[ $(uname) == 'Darwin' ]]; then
-    # You can't use chained commands in execute
-    cd $BUILD_DIR
-    bash -c "$TEST"
+if [[ $(uname) == 'Darwin' ]]; then # macOS
+    ./scripts/serial-test.sh
 else # Linux
-    ARGS=${ARGS:-"--rm --init -v $(pwd):$MOUNTED_DIR"}
     . $HELPERS_DIR/docker-hash.sh
-    PRE_COMMANDS="cd $MOUNTED_DIR/build"
-    [[ $IMAGE_TAG == 'centos-7.6' ]] && PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
-    COMMANDS="$PRE_COMMANDS && $TEST"
-    echo "$ docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\""
-    eval docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\"
+    echo "$ docker run --rm --init -v $(pwd):$MOUNTED_DIR $(buildkite-intrinsics) $FULL_TAG bash -c \"$MOUNTED_DIR/scripts/serial-test.sh\""
+    eval docker run --rm --init -v $(pwd):$MOUNTED_DIR $(buildkite-intrinsics) $FULL_TAG bash -c "$MOUNTED_DIR/scripts/serial-test.sh"
 fi
