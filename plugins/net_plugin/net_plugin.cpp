ee1fc9230f35304a75af6c42af210bccbf8429ae
Kevin Heifner <heifnerk@objectcomputing.com>
2019-09-03 16:10:25 -0500

    Deprecate network-version-match option as it is needlessly restrictive

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -2913,7 +2913,7 @@ namespace eosio {
          ( "connection-cleanup-period", bpo::value<int>()->default_value(def_conn_retry_wait), "number of seconds to wait before cleaning up dead connections")
          ( "max-cleanup-time-msec", bpo::value<int>()->default_value(10), "max connection cleanup time per cleanup call in millisec")
          ( "network-version-match", bpo::value<bool>()->default_value(false),
-           "True to require exact match of peer network version.")
+           "DEPRECATED, needless restriction. True to require exact match of peer network version.")
          ( "net-threads", bpo::value<uint16_t>()->default_value(my->thread_pool_size),
            "Number of worker threads in net_plugin thread pool" )
          ( "sync-fetch-span", bpo::value<uint32_t>()->default_value(def_sync_fetch_span), "number of blocks to retrieve in a chunk from any individual peer during synchronization")
