c59032d880a106c23a6bc675b6da6c851c36a5a5
Bart Wyatt <bart.wyatt@block.one>
2019-09-05 13:01:51 -0400

    change behavior of recover_key to better support variable length keys

--- libraries/chain/include/eosio/chain/config.hpp
+++ libraries/chain/include/eosio/chain/config.hpp
@@ -38,6 +38,7 @@ const static name eosio_code_name { N(eosio.code) };
 const static int      block_interval_ms = 500;
 const static int      block_interval_us = block_interval_ms*1000;
 const static uint64_t block_timestamp_epoch = 946684800000ll; // epoch is year 2000.
+const static uint32_t genesis_num_supported_key_types = 2;
 
 /** Percentages are fixed point with a denominator of 10,000 */
 const static int percent_100 = 10000;
--- libraries/chain/wasm_interface.cpp
+++ libraries/chain/wasm_interface.cpp
@@ -803,19 +803,35 @@ class crypto_api : public context_aware_api {
                         array_ptr<char> pub, size_t publen ) {
          fc::crypto::signature s;
          datastream<const char*> ds( sig, siglen );
-         datastream<char*> pubds( pub, publen );
-
          fc::raw::unpack(ds, s);
 
          EOS_ASSERT(s.which() < context.db.get<protocol_state_object>().num_supported_key_types, unactivated_signature_type,
-           "Unactivated signature type used during recover_key");
+                    "Unactivated signature type used during recover_key");
 
          if(context.control.is_producing_block())
             EOS_ASSERT(s.variable_size() <= context.control.configured_subjective_signature_length_limit(),
                        sig_variable_size_limit_exception, "signature variable length component size greater than subjective maximum");
 
-         fc::raw::pack( pubds, fc::crypto::public_key( s, digest, false ) );
-         return pubds.tellp();
+
+         auto recovered = fc::crypto::public_key(s, digest, false);
+
+         // the key types newer than the first 2 may be varible in length
+         if (s.which() > config::genesis_num_supported_key_types ) {
+            EOS_ASSERT(publen >= 33, wasm_execution_error,
+                       "destination buffer must at least be able to hold an ECC public key");
+            auto packed_pubkey = fc::raw::pack(recovered);
+            auto copy_size = std::min<size_t>(publen, packed_pubkey.size());
+            memcpy(pub, packed_pubkey.data(), copy_size);
+            return packed_pubkey.size();
+         } else {
+            // legacy behavior, key types 0 and 1 always pack to 33 bytes.
+            // this will do one less copy for those keys while maintaining the rules of
+            //    [0..33) dest sizes: assert (asserts in fc::raw::pack)
+            //    [33..inf) dest sizes: return packed size (always 33)
+            datastream<char*> out_ds( pub, publen );
+            fc::raw::pack(out_ds, recovered);
+            return out_ds.tellp();
+         }
       }
 
       template<class Encoder> auto encode(char* data, size_t datalen) {
