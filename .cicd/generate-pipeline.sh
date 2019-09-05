656dd26a9426c3709dea60539b8d056fda56c796
Zach Butler <zach.butler@block.one>
2019-09-05 10:13:49 -0400

    Add $SKIP_MAC to "Brew Updater" step

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -360,7 +360,7 @@ cat <<EOF
     agents:
       queue: "automation-basic-builder-fleet"
     timeout: "${TIMEOUT:-5}"
-    skip: ${SKIP_MOJAVE}${SKIP_PACKAGE_BUILDER}
+    skip: ${SKIP_MOJAVE}${SKIP_PACKAGE_BUILDER}${SKIP_MAC}
 
 EOF
 IFS=$oIFS
