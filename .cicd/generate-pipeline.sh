6b4a889bf833cfdaf35db5a519ae01562b418cb1
Justin Brown <justin.brown@block.one>
2019-09-10 10:31:42 -0400

    switches fleet

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -75,7 +75,7 @@ echo $PLATFORMS_JSON_ARRAY | jq -cr ".[]" | while read -r PLATFORM_JSON; do
 EOF
     fi
 done
-BUILDKITE_AGENT_QUEUE="automation-eos-builder-fleet"
+BUILDKITE_AGENT_QUEUE="automation-eks-eos-builder-fleet"
 echo "  - wait"; echo ""
 
 ################
@@ -90,7 +90,6 @@ echo $PLATFORMS_JSON_ARRAY | jq -cr ".[]" | while read -r PLATFORM_JSON; do
     command:
       - "buildkite-agent artifact download build.tar.gz . --step '$(echo "$PLATFORM_JSON" | jq -r .ICON) $(echo "$PLATFORM_JSON" | jq -r .PLATFORM_NAME_FULL) - Build' --build 'af9e43be-a344-497e-8def-0528846f4b47' && tar -xzf build.tar.gz"
       - "./.cicd/test.sh scripts/serial-test.sh $TEST_NAME"
-      - python --version
     env:
       IMAGE_TAG: $(echo "$PLATFORM_JSON" | jq -r .FILE_NAME)
       BUILDKITE_AGENT_ACCESS_TOKEN:
