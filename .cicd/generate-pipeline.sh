3506d39ae0b76339d1e04280af1a54d2306f86e8
Scott Arnette <scott.a.arnette@gmail.com>
2019-09-12 10:36:33 -0400

    Logic prep for only triggering on PRs.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -330,7 +330,7 @@ done
 #####################
 # TRIGGERS ON PR #
 if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && $BUILDKITE_PIPELINE_SLUG != "eosio-lrt" ]]; then
-# if [[ $BUILDKITE_PULL_REQUEST ]]; then
+# if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && $BUILDKITE_PULL_REQUEST ]]; then
     cat <<EOF
   - label: ":pipeline: Trigger LRTs"
     trigger: "eosio-lrt"
