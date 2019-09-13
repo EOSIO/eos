c6b762917341ec82a2f31b0e7e16d1bb92127a33
Nathan Pierce <nathan.pierce@block.one>
2019-09-13 16:16:48 -0400

    anka plugin update and new features for failover of registry

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -108,7 +108,7 @@ EOF
       - "cd eos && ./.cicd/build.sh"
       - "cd eos && tar -pczf build.tar.gz build && buildkite-agent artifact upload build.tar.gz"
     plugins:
-      - chef/anka#v0.5.1:
+      - chef/anka#v0.5.4:
           no-volume: true
           inherit-environment-vars: true
           vm-name: $(echo "$PLATFORM_JSON" | jq -r .ANKA_TEMPLATE_NAME)
@@ -118,6 +118,10 @@ EOF
           always-pull: true
           debug: true
           wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 5
     timeout: ${TIMEOUT:-180}
     agents:
       - "queue=mac-anka-large-node-fleet"
@@ -153,7 +157,7 @@ EOF
       - "cd eos && buildkite-agent artifact download build.tar.gz . --step '$(echo "$PLATFORM_JSON" | jq -r .ICON) $(echo "$PLATFORM_JSON" | jq -r .PLATFORM_NAME_FULL) - Build' && tar -xzf build.tar.gz"
       - "cd eos && ./.cicd/test.sh scripts/parallel-test.sh"
     plugins:
-      - chef/anka#v0.5.1:
+      - chef/anka#v0.5.4:
           no-volume: true
           inherit-environment-vars: true
           vm-name: $(echo "$PLATFORM_JSON" | jq -r .ANKA_TEMPLATE_NAME)
@@ -161,6 +165,10 @@ EOF
           always-pull: true
           debug: true
           wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 5
     timeout: ${TIMEOUT:-20}
     agents:
       - "queue=mac-anka-node-fleet"
@@ -198,7 +206,7 @@ EOF
       - "cd eos && buildkite-agent artifact download build.tar.gz . --step '$(echo "$PLATFORM_JSON" | jq -r .ICON) $(echo "$PLATFORM_JSON" | jq -r .PLATFORM_NAME_FULL) - Build' && tar -xzf build.tar.gz"
       - "cd eos && ./.cicd/test.sh scripts/serial-test.sh $TEST_NAME"
     plugins:
-      - chef/anka#v0.5.1:
+      - chef/anka#v0.5.4:
           no-volume: true
           inherit-environment-vars: true
           vm-name: $(echo "$PLATFORM_JSON" | jq -r .ANKA_TEMPLATE_NAME)
@@ -206,6 +214,10 @@ EOF
           always-pull: true
           debug: true
           wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 5
     timeout: ${TIMEOUT:-20}
     agents:
       - "queue=mac-anka-node-fleet"
@@ -244,7 +256,7 @@ EOF
       - "cd eos && buildkite-agent artifact download build.tar.gz . --step '$(echo "$PLATFORM_JSON" | jq -r .ICON) $(echo "$PLATFORM_JSON" | jq -r .PLATFORM_NAME_FULL) - Build' && tar -xzf build.tar.gz"
       - "cd eos && ./.cicd/test.sh scripts/long-running-test.sh $TEST_NAME"
     plugins:
-      - chef/anka#v0.5.1:
+      - chef/anka#v0.5.4:
           no-volume: true
           inherit-environment-vars: true
           vm-name: $(echo "$PLATFORM_JSON" | jq -r .ANKA_TEMPLATE_NAME)
@@ -252,6 +264,10 @@ EOF
           always-pull: true
           debug: true
           wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 5
     timeout: ${TIMEOUT:-180}
     agents:
       - "queue=mac-anka-node-fleet"
@@ -328,7 +344,7 @@ cat <<EOF
       - "cd eos && buildkite-agent artifact download build.tar.gz . --step ':darwin: macOS 10.13 - Build' && tar -xzf build.tar.gz"
       - "cd eos && ./.cicd/package.sh"
     plugins:
-      - chef/anka#v0.5.1:
+      - chef/anka#v0.5.4:
           no-volume: true
           inherit-environment-vars: true
           vm-name: 10.13.6_6C_14G_40G
@@ -336,6 +352,10 @@ cat <<EOF
           always-pull: true
           debug: true
           wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 5
     agents:
       - "queue=mac-anka-node-fleet"
     timeout: ${TIMEOUT:-10}
@@ -347,7 +367,7 @@ cat <<EOF
       - "cd eos && buildkite-agent artifact download build.tar.gz . --step ':darwin: macOS 10.14 - Build' && tar -xzf build.tar.gz"
       - "cd eos && ./.cicd/package.sh"
     plugins:
-      - chef/anka#v0.5.1:
+      - chef/anka#v0.5.4:
           no-volume: true
           inherit-environment-vars: true
           vm-name: 10.14.4_6C_14G_40G
@@ -355,6 +375,10 @@ cat <<EOF
           always-pull: true
           debug: true
           wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 5
     agents:
       - "queue=mac-anka-node-fleet"
     timeout: ${TIMEOUT:-10}
