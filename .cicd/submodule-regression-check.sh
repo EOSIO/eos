f42b8fa908a0989c3576920c0907512a5070b59a
Nathan Pierce <nathan.pierce@block.one>
2019-09-16 16:57:13 -0400

    quick fixes

--- .cicd/submodule-regression-check.sh
+++ .cicd/submodule-regression-check.sh
@@ -7,12 +7,12 @@ declare -A BASE_MAP
 if ${TRAVIS:-false}; then
     BASE_BRANCH=$TRAVIS_BRANCH
     CURRENT_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH:-$TRAVIS_BRANCH} # We default to TRAVIS_BRANCH if it's not a PR so it passes on non PR runs
-    [[ $TRAVIS_PULL_REQUEST_SLUG != 'EOSIO/eos' ]] && CURRENT_BRANCH=$TRAVIS_COMMIT # Support git log & echo output
+    [[ ! -z $TRAVIS_PULL_REQUEST_SLUG ]] && CURRENT_BRANCH=$TRAVIS_COMMIT # Support git log & echo output
 else
     BASE_BRANCH=${BUILDKITE_PULL_REQUEST_BASE_BRANCH:-$BUILDKITE_BRANCH}
     CURRENT_BRANCH=$BUILDKITE_BRANCH
 fi
-[[ $BASE_BRANCH == $CURRENT_BRANCH ]] && echo 'BASE_BRANCH and CURRENT_BRANCH are the same' exit 0
+[[ $BASE_BRANCH == $CURRENT_BRANCH ]] && echo 'BASE_BRANCH and CURRENT_BRANCH are the same' && exit 0
 
 echo "getting submodule info for $CURRENT_BRANCH"
 while read -r a b; do
@@ -34,14 +34,16 @@ for k in "${!BASE_MAP[@]}"; do
     echo "  timestamp on $BASE_BRANCH: $base_ts"
     if (( $pr_ts < $base_ts)); then
         echo "$k is older on $CURRENT_BRANCH than $BASE_BRANCH; investigating..."
-        if [[ $TRAVIS == true && $TRAVIS_PULL_REQUEST_SLUG != 'EOSIO/eos' ]]; then # IF it's a forked PR, we need to switch back to the PR ref/head so we can git log properly
+        if [[ $TRAVIS == true && ! -z $TRAVIS_PULL_REQUEST_SLUG ]]; then # IF it's a forked PR, we need to switch back to the PR ref/head so we can git log properly
             echo "git fetch origin +refs/pull/$TRAVIS_PULL_REQUEST/merge:"
             git fetch origin +refs/pull/$TRAVIS_PULL_REQUEST/merge: &> /dev/null
             echo "switching back to $TRAVIS_PULL_REQUEST_SLUG:$TRAVIS_PULL_REQUEST_BRANCH ($TRAVIS_COMMIT)"
             echo 'git checkout -qf FETCH_HEAD'
             git checkout -qf FETCH_HEAD &> /dev/null
+        elif [[ $BUILDKITE == true ]]; then
+            echo "switching back to $CURRENT_BRANCH"
+            git checkout -f $CURRENT_BRANCH &> /dev/null
         fi
-        echo "git --no-pager log $CURRENT_BRANCH ^$BASE_BRANCH --pretty=format:\"%H\""
         if [[ ! -z $(for c in $(git --no-pager log $CURRENT_BRANCH ^$BASE_BRANCH --pretty=format:"%H"); do git show --pretty="" --name-only $c; done | grep "^$k$") ]]; then
             echo "ERROR: $k has regressed"
             exit 1
