17a3004101048adb8c906ae9c83935e7c6e4029c
Scott Arnette <scott.a.arnette@gmail.com>
2019-09-19 14:07:10 -0400

    Add echos to cmake and make commands to be executed on Mac.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -15,7 +15,7 @@ if [[ "$(uname)" == 'Darwin' ]]; then
     fi
     [[ ! "$PINNED" == 'false' || "$UNPINNED" == 'true' ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$HELPERS_DIR/clang.make"
     cd $BUILD_DIR
-    echo "cmake $CMAKE_EXTRAS"
+    echo "cmake $CMAKE_EXTRAS .."
     cmake $CMAKE_EXTRAS ..
     echo "make -j$JOBS"
     make -j$JOBS
