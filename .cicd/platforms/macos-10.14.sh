809e3885543431e59c82cd863d4a3ff632fdafc9
Nathan Pierce <nathan.pierce@block.one>
2019-09-24 10:36:23 -0400

    fix for PINNED/UNPINNED

--- .cicd/platforms/macos-10.14.sh
+++ .cicd/platforms/macos-10.14.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 VERSION=1
 brew update
 brew install git cmake python@2 python libtool libusb graphviz automake wget gmp llvm@7 pkgconfig doxygen openssl jq || :
-if [[ ! $PINNED == false || $UNPINNED == true ]]; then
+if ( [[ ! -z $PINNED ]] && [[ ! $PINNED == false ]] ) || ( [[ ! -z $UNPINNED ]] && [[ ! $UNPINNED == true ]] ) || ( [[ -z $PINNED ]] && [[ -z $UNPINNED ]] ); then
     # install clang from source
     git clone --single-branch --branch release_80 https://git.llvm.org/git/llvm.git clang8
     cd clang8
