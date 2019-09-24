15f9c07425d7daa190b3c871c1f52d709a6c8d37
Nathan Pierce <nathan.pierce@block.one>
2019-09-24 11:34:27 -0400

    quick fix

--- .cicd/helpers/general.sh
+++ .cicd/helpers/general.sh
@@ -1,6 +1,7 @@
 export ROOT_DIR=$( dirname "${BASH_SOURCE[0]}" )/../..
 export BUILD_DIR=$ROOT_DIR/build
 export CICD_DIR=$ROOT_DIR/.cicd
+export SCRIPTS_DIR=$ROOT_DIR/scripts
 export HELPERS_DIR=$CICD_DIR/helpers
 export JOBS=${JOBS:-"$(getconf _NPROCESSORS_ONLN)"}
 export MOUNTED_DIR='/workdir'
