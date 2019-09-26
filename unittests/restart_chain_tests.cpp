bf3b19931fe55012715d44b7a44b5bd5e32cebfd
johnsonb@objectcomputing.com <Brian Johnson>
2019-09-26 13:38:23 -0500

    Added test to verify controller will not proceed past database initialization if chain id configured incorrectly.

--- unittests/restart_chain_tests.cpp
+++ unittests/restart_chain_tests.cpp
@@ -57,4 +57,28 @@ BOOST_AUTO_TEST_CASE(test_existing_state_without_block_log)
    }
 }
 
+BOOST_AUTO_TEST_CASE(test_restart_with_different_chain_id)
+{
+   tester chain;
+
+   chain.create_account(N(snapshot));
+   std::vector<signed_block_ptr> blocks;
+   blocks.push_back(chain.produce_block());
+   blocks.push_back(chain.produce_block());
+   blocks.push_back(chain.produce_block());
+
+   tester other;
+   for (auto new_block : blocks) {
+      other.push_block(new_block);
+   }
+   blocks.clear();
+
+   other.close();
+   genesis_state genesis;
+   genesis.initial_timestamp = fc::time_point::from_iso_string("2020-01-01T00:00:00.999");
+   genesis.initial_key = eosio::testing::base_tester::get_public_key( config::system_account_name, "active" );
+   fc::optional<chain_id_type> chain_id = genesis.compute_chain_id();
+   BOOST_REQUIRE_EXCEPTION(other.open(chain_id), block_log_exception, fc_exception_message_starts_with("Controller configured with: "));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
