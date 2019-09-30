d40eea3500c25f5690a4d9abafc2cb9a7a70f42d
Scott Arnette <scott.a.arnette@gmail.com>
2019-09-30 14:33:04 -0400

    Fix conditional image-tags.

--- .conan/conan-build.sh
+++ .conan/conan-build.sh
@@ -6,19 +6,19 @@ if [[ $(uname) == 'Darwin' ]]; then
     export SDKROOT=$(xcrun --show-sdk-path) # Fixes fatal error: 'string.h' file not found : https://github.com/conan-io/cmake-conan/issues/159#issuecomment-519486541
     CMAKE_EXTRAS="-DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=ON -DUSE_CONAN=true"
 else # Linux
-    if [[ $IMAGE_TAG == 'amazon_linux-2-unpinned' ]]; then
+    if [[ $IMAGE_TAG =~ 'amazon_linux-2' ]]; then
         yum update -y
         yum install -y python3 python3-devel clang llvm-devel llvm-static git curl tar gzip automake make
         yum install -y jq procps-ng python python-devel
         CMAKE_EXTRAS="-DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=ON -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DUSE_CONAN=true"
-    elif [[ $IMAGE_TAG == 'centos-7.6-unpinned' ]]; then
+    elif [[ $IMAGE_TAG =~ 'centos-7.6' ]]; then
         yum update -y
         yum install -y epel-release && yum --enablerepo=extras install -y centos-release-scl && yum --enablerepo=extras install -y devtoolset-8
         yum install -y rh-python36 llvm7.0-devel llvm7.0-static git curl automake
         yum install -y jq python python-devel
         source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable
         CMAKE_EXTRAS="-DCMAKE_BUILD_TYPE='Release' -DCORE_SYMBOL_NAME='SYS' -DPKG_CONFIG_USE_CMAKE_PREFIX_PATH=ON -DLLVM_DIR='/usr/lib64/llvm7.0/lib/cmake/llvm' -DUSE_CONAN=true"
-    elif [[ $IMAGE_TAG == 'ubuntu-18.04-unpinned' ]]; then
+    elif [[ $IMAGE_TAG =~ 'ubuntu-18.04' ]]; then
         apt-get update && apt-get upgrade -y
         apt-get install -y python3 python3-dev python3-pip clang llvm-7-dev git curl automake
         apt-get install -y jq python2.7 python2.7-dev
