2f7cdbdf156df2ab1e7513b904e9446743162090
Scott Arnette <scott.a.arnette@gmail.com>
2019-09-30 13:38:34 -0400

    Update logic to ensure pinned flags do not enable for conan builds.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -20,7 +20,7 @@ else # Linux
     PRE_COMMANDS="cd $MOUNTED_DIR/build"
     # PRE_COMMANDS: Executed pre-cmake
     # CMAKE_EXTRAS: Executed within and right before the cmake path (cmake CMAKE_EXTRAS ..)
-    [[ ! $IMAGE_TAG =~ 'unpinned' ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/scripts/pinned_toolchain.cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+    [[ ! $IMAGE_TAG =~ 'unpinned' && ! $IMAGE_TAG =~ 'conan' ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/scripts/pinned_toolchain.cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
     if [[ $IMAGE_TAG == 'amazon_linux-2' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'centos-7.6' ]]; then
@@ -32,15 +32,12 @@ else # Linux
     elif [[ $IMAGE_TAG == 'amazon_linux-2-unpinned' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang'"
-        [[ "$USE_CONAN" == 'true' ]] && FULL_TAG="amazonlinux:2"
     elif [[ $IMAGE_TAG == 'centos-7.6-unpinned' ]]; then
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DLLVM_DIR='/usr/lib64/llvm7.0/lib/cmake/llvm'"
         PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
-        [[ "$USE_CONAN" == 'true' ]] && FULL_TAG="centos:7"
     elif [[ $IMAGE_TAG == 'ubuntu-18.04-unpinned' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DLLVM_DIR='/usr/lib/llvm-7/lib/cmake/llvm'"
-        [[ "$USE_CONAN" == 'true' ]] && FULL_TAG="ubuntu:18.04"
     fi
     BUILD_COMMANDS="cmake $CMAKE_EXTRAS .. && make -j$JOBS"
     # Docker Commands
