b00a0c961c327e3e9b08119cf595410a9167345f
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-01 13:37:18 -0400

    Test with A2.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -476,7 +476,7 @@ cat <<EOF
   - label: ":ubuntu: Ubuntu 18.04 - Contract Builder"
     command: "./.cicd/installation-build.sh"
     env:
-      IMAGE_TAG: "ubuntu-18.04-unpinned"
+      IMAGE_TAG: "amazon_linux-2-unpinned"
       BUILDKITE_AGENT_ACCESS_TOKEN:
     agents:
       queue: "$BUILDKITE_AGENT_QUEUE"
