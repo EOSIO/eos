c82cad37fd6bbdfb1cea6b89291fd626724c3b7e
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-02 17:21:00 -0400

    Attempt to fix double git cloning.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -8,8 +8,8 @@ if [[ $(uname) == 'Darwin' ]]; then
     [[ $TRAVIS == true ]] && export PINNED=false && ccache -s && CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER_LAUNCHER=ccache" && ./$CICD_DIR/platforms/unpinned/macos-10.14-unpinned.sh
     ( [[ ! $PINNED == false || $UNPINNED == true ]] ) && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$SCRIPTS_DIR/pinned_toolchain.cmake"
     if [[ "$USE_CONAN" == 'true' ]]; then
-        sed -n '/## Build Steps/,/make -j/p' $CONAN_DIR/macos-10.14-conan.md | grep -v -e '```' -e '^$' >> $CONAN_DIR/conan-build.sh
-        bash -c "$CONAN_DIR/conan-build.sh && cp -r $ROOT_DIR/eos/build/* $ROOT_DIR/build && cp -r ~/.conan $ROOT_DIR/conan"
+        sed -n '/## Build Steps/,/make -j/p' $CONAN_DIR/macos-10.14-conan.md | grep -v -e '```' -e '^$' -e 'git' -e 'cd eos' >> $CONAN_DIR/conan-build.sh
+        bash -c "$CONAN_DIR/conan-build.sh && cp -r ~/.conan $ROOT_DIR/conan"
     else
         cd $BUILD_DIR
         cmake $CMAKE_EXTRAS ..
@@ -39,7 +39,7 @@ else # Linux
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++-7' -DCMAKE_C_COMPILER='clang-7' -DLLVM_DIR='/usr/lib/llvm-7/lib/cmake/llvm'"
     elif [[ $IMAGE_TAG =~ 'conan' ]]; then
-        sed -n '/## Build Steps/,/make -j/p' $CONAN_DIR/$IMAGE_TAG.md | grep -v -e '```' -e '^$' >> $CONAN_DIR/conan-build.sh
+        sed -n '/## Build Steps/,/make -j/p' $CONAN_DIR/$IMAGE_TAG.md | grep -v -e '```' -e '^$' -e 'git' -e 'cd eos' >> $CONAN_DIR/conan-build.sh
     fi
     BUILD_COMMANDS="cmake $CMAKE_EXTRAS .. && make -j$JOBS"
     # Docker Commands
@@ -55,7 +55,7 @@ else # Linux
     fi
     . $HELPERS_DIR/file-hash.sh $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.dockerfile
     COMMANDS="$PRE_COMMANDS && $COMMANDS"
-    [[ "$USE_CONAN" == 'true' ]] && COMMANDS="$MOUNTED_DIR/.conan/conan-build.sh && cp -r /eos/build/* $MOUNTED_DIR/build && cp -r ~/.conan $MOUNTED_DIR/conan"
+    [[ "$USE_CONAN" == 'true' ]] && COMMANDS="cd $MOUNTED_DIR && $MOUNTED_DIR/.conan/conan-build.sh && cp -r ~/.conan $MOUNTED_DIR/conan"
     echo "$ docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\""
     eval docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\"
 fi
