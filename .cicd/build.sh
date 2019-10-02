8dfc10a14ad850ae4f3204fc1498b426cc3b3647
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-02 14:14:54 -0400

    Switch CMake flags to use clang-7 for U18.

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -38,7 +38,7 @@ else # Linux
         PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'ubuntu-18.04-unpinned' ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
-        CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang' -DLLVM_DIR='/usr/lib/llvm-7/lib/cmake/llvm'"
+        CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++-7' -DCMAKE_C_COMPILER='clang-7' -DLLVM_DIR='/usr/lib/llvm-7/lib/cmake/llvm'"
     elif [[ $IMAGE_TAG == 'amazon_linux-2-conan' ]]; then
         sed -n '/## Build Steps/,/make -j/p' $CONAN_DIR/AMAZON_LINUX-2.md | grep -v '```' | grep -v '\*\*' >> $CONAN_DIR/conan-build.sh
     elif [[ $IMAGE_TAG == 'centos-7.6-conan' ]]; then
