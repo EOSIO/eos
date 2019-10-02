28dca73affb598c0a1ada05d36cbfb4d5d66998b
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-02 14:58:27 -0400

    Adjust names of markdown to support CI.

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -9,7 +9,7 @@ TAG=$(echo $FULL_TAG | cut -d: -f2)
 EXISTS=$(curl -s -H "Authorization: Bearer $(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${ORG_REPO}:pull" | jq --raw-output .token)" "https://registry.hub.docker.com/v2/${ORG_REPO}/manifests/$TAG")
 # build, if neccessary
 if [[ $EXISTS =~ '404 page not found' || $EXISTS =~ 'manifest unknown' || $FORCE_BASE_IMAGE == 'true' ]]; then # if we cannot pull the image, we build and push it first
-    [[ "$USE_CONAN" == 'true' ]] && sed -n '/## Environment/,/## Build/p' $CONAN_DIR/$IMAGE_TAG | grep -v -e '```' -e '\#\#' -e '^$' -e 'export' | sed -e 's/^/RUN /' >> $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.dockerfile
+    [[ "$USE_CONAN" == 'true' ]] && sed -n '/## Environment/,/## Build/p' $CONAN_DIR/$IMAGE_TAG.md | grep -v -e '```' -e '\#\#' -e '^$' -e 'export' | sed -e 's/^/RUN /' >> $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.dockerfile
     docker build -t $FULL_TAG -f $CICD_DIR/platforms/$BUILD_TYPE/${IMAGE_TAG}.dockerfile .
     docker push $FULL_TAG
 else
