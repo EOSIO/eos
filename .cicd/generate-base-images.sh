d96e69b31f750980e0eca927186511ff170dd227
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-02 15:23:41 -0400

    Hash Conan dockerfile after writing it.

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -5,6 +5,7 @@ if [[ $(uname) == 'Darwin' ]]; then # macOS
     [[ "$USE_CONAN" == 'true' ]] && sed -n '/## Environment/,/## Build/p' $CONAN_DIR/$IMAGE_TAG.md | grep -v -e '```' -e '\#\#' -e '^$' >> $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.sh
     . $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.sh
 else # Linux
+    [[ "$USE_CONAN" == 'true' ]] && sed -n '/## Environment/,/## Build/p' $CONAN_DIR/$IMAGE_TAG.md | grep -v -e '```' -e '\#\#' -e '^$' -e 'export' | sed -e 's/^/RUN /' >> $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.dockerfile
     . $HELPERS_DIR/file-hash.sh $CICD_DIR/platforms/$BUILD_TYPE/${IMAGE_TAG}.dockerfile
     # look for Docker image
     echo "+++ :mag_right: Looking for $FULL_TAG"
@@ -13,7 +14,6 @@ else # Linux
     EXISTS=$(curl -s -H "Authorization: Bearer $(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${ORG_REPO}:pull" | jq --raw-output .token)" "https://registry.hub.docker.com/v2/${ORG_REPO}/manifests/$TAG")
     # build, if neccessary
     if [[ $EXISTS =~ '404 page not found' || $EXISTS =~ 'manifest unknown' || $FORCE_BASE_IMAGE == 'true' ]]; then # if we cannot pull the image, we build and push it first
-        [[ "$USE_CONAN" == 'true' ]] && sed -n '/## Environment/,/## Build/p' $CONAN_DIR/$IMAGE_TAG.md | grep -v -e '```' -e '\#\#' -e '^$' -e 'export' | sed -e 's/^/RUN /' >> $CICD_DIR/platforms/$BUILD_TYPE/$IMAGE_TAG.dockerfile
         docker build -t $FULL_TAG -f $CICD_DIR/platforms/$BUILD_TYPE/${IMAGE_TAG}.dockerfile .
         docker push $FULL_TAG
     else
