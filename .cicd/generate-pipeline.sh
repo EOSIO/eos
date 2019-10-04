b2c08bd1b53ba401e02967cbf4a891fc265b6c69
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-04 10:17:55 -0400

    Finished fixing package builder steps on Linux. Incorrect IMAGE_TAG variables were set.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -425,7 +425,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':centos: CentOS 7.6 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "centos-7.6"
+      IMAGE_TAG: "centos-7.6-pinned"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "el7" # OS and PKGTYPE required for lambdas
@@ -440,7 +440,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 16.04 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "ubuntu-16.04"
+      IMAGE_TAG: "ubuntu-16.04-pinned"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
@@ -455,7 +455,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 18.04 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "ubuntu-18.04"
+      IMAGE_TAG: "ubuntu-18.04-pinned"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
