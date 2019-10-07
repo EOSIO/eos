5a8fda1dd9dda0462cb9817a12cddd8d2d37f0da
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-07 13:18:18 -0400

    Revert "removed scripts"
    This reverts commit 41e33e6b3344d853576695fa2f72900630f271ce.

---
+++ scripts/eosio_build_centos.sh
@@ -0,0 +1,99 @@
+echo "OS name: ${NAME}"
+echo "OS Version: ${VERSION_ID}"
+echo "CPU cores: ${CPU_CORES}"
+echo "Physical Memory: ${MEM_GIG}G"
+echo "Disk space total: ${DISK_TOTAL}G"
+echo "Disk space available: ${DISK_AVAIL}G"
+
+( [[ $NAME == "CentOS Linux" ]] && [[ "$(echo ${VERSION} | sed 's/ .*//g')" < 7 ]] ) && echo " - You must be running Centos 7 or higher to install EOSIO." && exit 1
+
+[[ $MEM_GIG -lt 7 ]] && echo "Your system must have 7 or more Gigabytes of physical memory installed." && exit 1
+[[ "${DISK_AVAIL}" -lt "${DISK_MIN}" ]] && echo " - You must have at least ${DISK_MIN}GB of available storage to install EOSIO." && exit 1
+
+echo ""
+
+# Repo necessary for rh-python3 and devtoolset-8
+ensure-scl
+# GCC8 for Centos / Needed for CMAKE install even if we're pinning
+ensure-devtoolset
+if [[ -d /opt/rh/devtoolset-8 ]]; then
+	echo "${COLOR_CYAN}[Enabling Centos devtoolset-8 (so we can use GCC 8)]${COLOR_NC}"
+	execute-always source /opt/rh/devtoolset-8/enable
+	echo " - ${COLOR_GREEN}Centos devtoolset-8 successfully enabled!${COLOR_NC}"
+fi
+# Ensure packages exist
+ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos7_deps"
+export PYTHON3PATH="/opt/rh/rh-python36"
+if $DRYRUN || [ -d $PYTHON3PATH ]; then
+	echo "${COLOR_CYAN}[Enabling python36]${COLOR_NC}"
+	execute source $PYTHON3PATH/enable
+	echo " ${COLOR_GREEN}- Python36 successfully enabled!${COLOR_NC}"
+	echo ""
+fi
+# Handle clang/compiler
+ensure-compiler
+# CMAKE Installation
+ensure-cmake
+# CLANG Installation
+build-clang
+# LLVM Installation
+ensure-llvm
+# BOOST Installation
+ensure-boost
+
+if $INSTALL_MONGO; then
+
+	echo "${COLOR_CYAN}[Ensuring MongoDB installation]${COLOR_NC}"
+	if [[ ! -d $MONGODB_ROOT ]]; then
+		execute bash -c "cd $SRC_DIR && \
+		curl -OL https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-amazon-$MONGODB_VERSION.tgz \
+		&& tar -xzf mongodb-linux-x86_64-amazon-$MONGODB_VERSION.tgz \
+		&& mv $SRC_DIR/mongodb-linux-x86_64-amazon-$MONGODB_VERSION $MONGODB_ROOT \
+		&& touch $MONGODB_LOG_DIR/mongod.log \
+		&& rm -f mongodb-linux-x86_64-amazon-$MONGODB_VERSION.tgz \
+		&& cp -f $REPO_ROOT/scripts/mongod.conf $MONGODB_CONF \
+		&& mkdir -p $MONGODB_DATA_DIR \
+		&& rm -rf $MONGODB_LINK_DIR \
+		&& rm -rf $BIN_DIR/mongod \
+		&& ln -s $MONGODB_ROOT $MONGODB_LINK_DIR \
+		&& ln -s $MONGODB_LINK_DIR/bin/mongod $BIN_DIR/mongod"
+		echo " - MongoDB successfully installed @ ${MONGODB_ROOT}."
+	else
+		echo " - MongoDB found with correct version @ ${MONGODB_ROOT}."
+	fi
+	echo "${COLOR_CYAN}[Ensuring MongoDB C driver installation]${COLOR_NC}"
+	if [[ ! -d $MONGO_C_DRIVER_ROOT ]]; then
+		execute bash -c "cd $SRC_DIR && \
+		curl -LO https://github.com/mongodb/mongo-c-driver/releases/download/$MONGO_C_DRIVER_VERSION/mongo-c-driver-$MONGO_C_DRIVER_VERSION.tar.gz \
+		&& tar -xzf mongo-c-driver-$MONGO_C_DRIVER_VERSION.tar.gz \
+		&& cd mongo-c-driver-$MONGO_C_DRIVER_VERSION \
+		&& mkdir -p cmake-build \
+		&& cd cmake-build \
+		&& $CMAKE -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$EOSIO_INSTALL_DIR -DENABLE_BSON=ON -DENABLE_SSL=OPENSSL -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DENABLE_STATIC=ON -DENABLE_ICU=OFF -DENABLE_SNAPPY=OFF $PINNED_TOOLCHAIN .. \
+		&& make -j${JOBS} \
+		&& make install \
+		&& cd ../.. \
+		&& rm mongo-c-driver-$MONGO_C_DRIVER_VERSION.tar.gz"
+		echo " - MongoDB C driver successfully installed @ ${MONGO_C_DRIVER_ROOT}."
+	else
+		echo " - MongoDB C driver found with correct version @ ${MONGO_C_DRIVER_ROOT}."
+	fi
+	echo "${COLOR_CYAN}[Ensuring MongoDB CXX driver installation]${COLOR_NC}"
+	if [[ ! -d $MONGO_CXX_DRIVER_ROOT ]]; then
+		execute bash -c "cd $SRC_DIR && \
+		curl -L https://github.com/mongodb/mongo-cxx-driver/archive/r$MONGO_CXX_DRIVER_VERSION.tar.gz -o mongo-cxx-driver-r$MONGO_CXX_DRIVER_VERSION.tar.gz \
+		&& tar -xzf mongo-cxx-driver-r${MONGO_CXX_DRIVER_VERSION}.tar.gz \
+		&& cd mongo-cxx-driver-r$MONGO_CXX_DRIVER_VERSION \
+		&& sed -i 's/\"maxAwaitTimeMS\", count/\"maxAwaitTimeMS\", static_cast<int64_t>(count)/' src/mongocxx/options/change_stream.cpp \
+		&& sed -i 's/add_subdirectory(test)//' src/mongocxx/CMakeLists.txt src/bsoncxx/CMakeLists.txt \
+		&& cd build \
+		&& $CMAKE -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$EOSIO_INSTALL_DIR -DCMAKE_PREFIX_PATH=$EOSIO_INSTALL_DIR $PINNED_TOOLCHAIN .. \
+		&& make -j${JOBS} VERBOSE=1 \
+		&& make install \
+		&& cd ../.. \
+		&& rm -f mongo-cxx-driver-r$MONGO_CXX_DRIVER_VERSION.tar.gz"
+		echo " - MongoDB C++ driver successfully installed @ ${MONGO_CXX_DRIVER_ROOT}."
+	else
+		echo " - MongoDB C++ driver found with correct version @ ${MONGO_CXX_DRIVER_ROOT}."
+	fi
+fi
---
+++ scripts/eosio_install.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+set -eo pipefail
+VERSION=2.1
+##########################################################################
+# This is the EOSIO automated install script for Linux and Mac OS.
+# This file was downloaded from https://github.com/EOSIO/eos
+#
+# Copyright (c) 2017, Respective Authors all rights reserved.
+#
+# After June 1, 2018 this software is available under the following terms:
+#
+# The MIT License
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# https://github.com/EOSIO/eos/blob/master/LICENSE.txt
+##########################################################################
+
+# Ensure we're in the repo root and not inside of scripts
+cd $( dirname "${BASH_SOURCE[0]}" )/..
+
+# Load eosio specific helper functions
+. ./scripts/helpers/eosio.sh
+
+[[ ! $NAME == "Ubuntu" ]] && set -i # Ubuntu doesn't support interactive mode since it uses dash
+
+[[ ! -f ${BUILD_DIR}/CMakeCache.txt ]] && printf "${COLOR_RED}Please run ${SCRIPT_DIR}/eosio_build.sh first!${COLOR_NC}" && exit 1
+echo "${COLOR_CYAN}====================================================================================="
+echo "========================== ${COLOR_WHITE}Starting EOSIO Installation${COLOR_CYAN} ==============================${COLOR_NC}"
+execute cd $BUILD_DIR
+execute make install
+execute cd ..
+
+printf "\n${COLOR_RED}      ___           ___           ___                       ___\n"
+printf "     /  /\\         /  /\\         /  /\\        ___          /  /\\ \n"
+printf "    /  /:/_       /  /::\\       /  /:/_      /  /\\        /  /::\\ \n"
+printf "   /  /:/ /\\     /  /:/\\:\\     /  /:/ /\\    /  /:/       /  /:/\\:\\ \n"
+printf "  /  /:/ /:/_   /  /:/  \\:\\   /  /:/ /::\\  /__/::\\      /  /:/  \\:\\ \n"
+printf " /__/:/ /:/ /\\ /__/:/ \\__\\:\\ /__/:/ /:/\\:\\ \\__\\/\\:\\__  /__/:/ \\__\\:\\ \n"
+printf " \\  \\:\\/:/ /:/ \\  \\:\\ /  /:/ \\  \\:\\/:/~/:/    \\  \\:\\/\\ \\  \\:\\ /  /:/ \n"
+printf "  \\  \\::/ /:/   \\  \\:\\  /:/   \\  \\::/ /:/      \\__\\::/  \\  \\:\\  /:/ \n"
+printf "   \\  \\:\\/:/     \\  \\:\\/:/     \\__\\/ /:/       /__/:/    \\  \\:\\/:/ \n"
+printf "    \\  \\::/       \\  \\::/        /__/:/        \\__\\/      \\  \\::/ \n"
+printf "     \\__\\/         \\__\\/         \\__\\/                     \\__\\/ \n\n${COLOR_NC}"
+
+printf "==============================================================================================\\n"
+printf "${COLOR_GREEN}EOSIO has been installed into ${CACHED_INSTALL_PATH}/bin${COLOR_NC}"
+printf "\\n${COLOR_YELLOW}Uninstall with: ${SCRIPT_DIR}/eosio_uninstall.sh${COLOR_NC}\\n"
+printf "==============================================================================================\\n\\n"
+resources
