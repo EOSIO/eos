62fc8cb1780a99b88d99217c9dea88445cac80b2
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-10 14:21:42 -0400

    Test with test step failing.

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -46,6 +46,7 @@ else # run specific serial test
         # run tests
         set +e # defer ctest error handling to end
         echo "$ ctest -R ^$TEST$ --output-on-failure -T Test"
+        exit 1
         ctest -R ^$TEST$ --output-on-failure -T Test
         EXIT_STATUS=$?
         echo "Done running $TEST."
