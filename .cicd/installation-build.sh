7eb6dc58136ef6a361b59eab0bf5f0f0bc0d629f
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-10 14:45:52 -0400

    Revert "Test with contracts and package builder steps failing."
    This reverts commit acd714a16fa68f7030a7f5d7cbc46cd552d14304.

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -6,7 +6,6 @@ if [[ $BUILDKITE_BRANCH =~ ^release/[0-9].[0-9]+.x$ || $BUILDKITE_BRANCH =~ ^mas
     export BRANCH=$(echo $BUILDKITE_BRANCH | sed 's/\//\_/')
     export CONTRACTS_BUILDER_TAG="eosio/ci-contracts-builder:base-ubuntu-18.04"
     export ARGS="--name ci-contracts-builder-$BUILDKITE_COMMIT --init -v $(pwd):$MOUNTED_DIR"
-    exit 1
     $CICD_DIR/build.sh
     docker commit ci-contracts-builder-$BUILDKITE_COMMIT $CONTRACTS_BUILDER_TAG-latest
     docker commit ci-contracts-builder-$BUILDKITE_COMMIT $CONTRACTS_BUILDER_TAG-$BUILDKITE_COMMIT
