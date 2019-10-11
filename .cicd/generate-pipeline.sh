f5093732fc2109486209a52a904731d7562797c4
Scott Arnette <scott.a.arnette@gmail.com>
2019-10-11 10:49:21 -0400

    Add support for packaging Conan binaries.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -426,7 +426,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':centos: CentOS 7.6 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "centos-7.6-pinned"
+      IMAGE_TAG: "centos-7.6-$PLATFORM_TYPE"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "el7" # OS and PKGTYPE required for lambdas
@@ -441,7 +441,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 16.04 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "ubuntu-16.04-pinned"
+      IMAGE_TAG: "ubuntu-16.04-$PLATFORM_TYPE"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
@@ -456,7 +456,7 @@ cat <<EOF
       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 18.04 - Build' --agent-access-token \$\$BUILDKITE_AGENT_ACCESS_TOKEN && tar -xzf build.tar.gz"
       - "./.cicd/package.sh"
     env:
-      IMAGE_TAG: "ubuntu-18.04-pinned"
+      IMAGE_TAG: "ubuntu-18.04-$PLATFORM_TYPE"
       BUILDKITE_AGENT_ACCESS_TOKEN:
       PLATFORM_TYPE: $PLATFORM_TYPE
       OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
