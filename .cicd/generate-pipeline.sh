f1c131a25000198214e6fd76a6ca59716b268471
Nathan Pierce <nathan.pierce@block.one>
2019-10-16 01:50:00 -0400

    test

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -48,9 +48,9 @@ for FILE in $(ls $CICD_DIR/platforms); do
     # Anka Template and Tags
     export ANKA_TAG_BASE='clean::cicd::git-ssh::nas::brew::buildkite-agent'
     if [[ $FILE_NAME =~ 'macos-10.14' ]]; then
-      export ANKA_TEMPLATE_NAME='10.14.4_6C_14G_40G'
+      export ANKA_TEMPLATE_NAME='10.14.6_6C_14G_40G'
     elif [[ $FILE_NAME =~ 'macos-10.15' ]]; then
-      export ANKA_TEMPLATE_NAME='10.15_beta_6C_14G_40G'
+      export ANKA_TEMPLATE_NAME='10.15.0_6C_14G_40G'
     else # Linux
       export ANKA_TAG_BASE=''
       export ANKA_TEMPLATE_NAME=''
