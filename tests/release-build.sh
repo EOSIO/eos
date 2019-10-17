d8b3ff2754b2534e92188ca05bff6c908a685286
olexiybuyanskyy <51361183+olexiybuyanskyy@users.noreply.github.com>
2019-10-17 13:48:00 +0300

    Renamed nodeos utility to remnode to fix the test (#81)

--- tests/release-build.sh
+++ tests/release-build.sh
@@ -3,11 +3,11 @@
 echo ''
 echo '                         ##### Release Build Test #####'
 echo ''
-echo '    The purpose of this test is to ensure that nodeos was built with compiler'
+echo '    The purpose of this test is to ensure that remnode was built with compiler'
 echo 'optimizations enabled. While there is no way to programmatically determine that'
-echo 'given one binary, we do set a debug flag in nodeos when it is built with'
+echo 'given one binary, we do set a debug flag in remnode when it is built with'
 echo 'asserts. This test checks that debug flag. Anyone intending to build and install'
-echo 'nodeos from source should perform a "release build" which excludes asserts and'
+echo 'remnode from source should perform a "release build" which excludes asserts and'
 echo 'debugging symbols, and performs compiler optimizations.'
 echo ''
 # check for xxd
@@ -17,16 +17,16 @@ if ! $(xxd --version 2>/dev/null); then
     echo 'The xxd hex dump tool can be installed as part of the vim-common package on most operating systems.'
     exit 1
 fi
-# find nodeos
+# find remnode
 set
 set -x
 #[[ $(git --version) ]] && cd "$(git rev-parse --show-toplevel)/build" || cd "$(dirname "${BASH_SOURCE[0]}")/.."
-if [[ ! -f programs/nodeos/nodeos ]]; then
-    echo 'ERROR: nodeos binary not found!'
+if [[ ! -f programs/remnode/remnode ]]; then
+    echo 'ERROR: remnode binary not found!'
     echo ''
     echo 'I looked here...'
-    echo "$ ls -la \"$(pwd)/programs/nodeos\""
-    ls -la "$(pwd)/programs/nodeos"
+    echo "$ ls -la \"$(pwd)/programs/remnode\""
+    ls -la "$(pwd)/programs/remnode"
     echo '...which I derived from one of these paths:'
     echo '$ echo "$(git rev-parse --show-toplevel)/build"'
     echo "$(git rev-parse --show-toplevel)/build"
@@ -35,13 +35,13 @@ if [[ ! -f programs/nodeos/nodeos ]]; then
     echo 'Release build test not run.'
     exit 2
 fi
-# run nodeos to generate state files
+# run remnode to generate state files
 mkdir release-build-test
-programs/nodeos/nodeos --config-dir "$(pwd)/release-build-test/config" --data-dir "$(pwd)/release-build-test/data" 1>/dev/null 2>/dev/null &
+programs/remnode/remnode --config-dir "$(pwd)/release-build-test/config" --data-dir "$(pwd)/release-build-test/data" 1>/dev/null 2>/dev/null &
 sleep 10
-kill $! # kill nodeos gracefully, by PID
+kill $! # kill remnode gracefully, by PID
 if [[ ! -f release-build-test/data/state/shared_memory.bin ]]; then
-    echo 'ERROR: nodeos state not found!'
+    echo 'ERROR: remnode state not found!'
     echo ''
     echo 'Looked for shared_memory.bin in the following places:'
     echo "$ ls -la \"$(pwd)/release-build-test/data/state\""
