9330444d50e84d45e3c33c5e31a39abc950dc955
taokayan <taokayan13@gmail.com>
2019-11-19 10:04:19 +0800

    another try

--- scripts/ls-test.sh
+++ scripts/ls-test.sh
@@ -31,9 +31,9 @@ fi
 python3.7 -m pip install requests
 python3.7 -m pip install dataclasses
 
+set +e # defer ctest error handling to end
 echo "ready to execute: ctest -L ls_tests -V -j $JOBS"
 ctest -L ls_tests -V -j $JOBS
-
 EXIT_STATUS=$?
 echo 'Done running launcher service related tests.'
-
+exit $EXIT_STATUS
