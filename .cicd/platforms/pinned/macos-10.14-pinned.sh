25fbeecb0d9e2cf9ff582ff19b85c0099e783a4b
Nathan Pierce <nathan.pierce@block.one>
2019-11-25 10:46:54 -0500

    cicd changes

--- .cicd/platforms/pinned/macos-10.14-pinned.sh
+++ .cicd/platforms/pinned/macos-10.14-pinned.sh
@@ -2,7 +2,17 @@
 set -eo pipefail
 VERSION=1
 brew update
-brew install git cmake python@2 python libtool libusb graphviz automake wget gmp llvm@4 pkgconfig doxygen openssl jq || :
+brew install git cmake python@2 python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl jq || :
+# install llvm 4 from source
+git clone --depth 1 --single-branch --branch release_40 https://github.com/llvm-mirror/llvm.git llvm
+cd llvm
+mkdir build
+cd build
+cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX='/usr/local' -DLLVM_TARGETS_TO_BUILD='host' -DLLVM_BUILD_TOOLS=false -DLLVM_ENABLE_RTTI=1 -DCMAKE_BUILD_TYPE=Release ..
+make -j $(getconf _NPROCESSORS_ONLN)
+make install
+cd ../..
+rm -rf llvm
 # install clang from source
 git clone --single-branch --branch release_80 https://git.llvm.org/git/llvm.git clang8
 cd clang8
