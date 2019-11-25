b1b3f7a2e83f7a10b7e3c70c85d5893758f58f7e
Aaron Fu <aaron.fu@block.one>
2019-11-25 10:39:53 +0800

    Revert "merge with develop version of generate-pipeline"
    This reverts commit 37034ac57694c1ce98a65906fd06738fa0e493b6.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -132,11 +132,9 @@ EOF
           failover-registries:
             - 'registry_1'
             - 'registry_2'
-          pre-execute-sleep: 10
+          pre-execute-sleep: 5
           pre-commands:
-            - "rm -rf mac-anka-fleet; git clone git@github.com:EOSIO/mac-anka-fleet.git && cd mac-anka-fleet && . ./ensure-tag.bash -u 12 -r 25G -a '-n'"
-      - thedyrt/skip-checkout#v0.1.1:
-          cd: ~
+            - "git clone git@github.com:EOSIO/mac-anka-fleet.git && cd mac-anka-fleet && . ./ensure-tag.bash -u 12 -r 25G -a '-n'"
     env:
       REPO: ${BUILDKITE_PULL_REQUEST_REPO:-$BUILDKITE_REPO}
       REPO_COMMIT: $BUILDKITE_COMMIT
