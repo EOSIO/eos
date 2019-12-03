d2c690734a0e283950869f5a209a2169cde2afd8
Nathan Pierce <nathan.pierce@block.one>
2019-12-03 16:32:22 -0500

    quick fix

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -76,8 +76,6 @@ for FILE in $(ls $CICD_DIR/platform-templates/); do
         "ICON": env.ICON
     }]')
 done
-
-exit 
 # set build_source whether triggered or not
 if [[ ! -z ${BUILDKITE_TRIGGERED_FROM_BUILD_ID} ]]; then
     export BUILD_SOURCE="--build \$BUILDKITE_TRIGGERED_FROM_BUILD_ID"
