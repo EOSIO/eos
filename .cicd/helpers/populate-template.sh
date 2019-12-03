804cd7b12b7f566d762707470eeccf26ab491ddd
Nathan Pierce <nathan.pierce@block.one>
2019-12-03 15:01:12 -0500

    quick fix

--- .cicd/helpers/populate-template.sh
+++ .cicd/helpers/populate-template.sh
@@ -2,6 +2,8 @@
 set -eo pipefail
 export POP_FILE_NAME="populated-$(echo ${FILE_NAME:-$IMAGE_TAG})"
 # Collect commands from code block, add RUN before the start of commands, and add it to temporary template
+echo $IMAGE_TAG
+echo $FILE_NAME
 DOC_CODE_BLOCK=$(cat docs/dep-install-${IMAGE_TAG:-$FILE_NAME}.md | sed -n '/```/,/```/p')
 SANITIZED_COMMANDS=$(echo "$DOC_CODE_BLOCK" | grep -v -e '```' -e '\#.*' -e '^$')
 if [[ ! $POP_FILE_NAME =~ 'macos' ]]; then # Linux / Docker
