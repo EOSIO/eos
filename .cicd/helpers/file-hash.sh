e508f0f439614ef66f3629adad9500b3e94ec78e
Nathan Pierce <nathan.pierce@block.one>
2019-12-03 15:13:36 -0500

    removed setting FILE_NAME in the file-hash script

--- .cicd/helpers/file-hash.sh
+++ .cicd/helpers/file-hash.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 [[ -z $1 ]] && echo "Please provide the file to be hashed" && exit 1
-FILE_NAME=$(basename $1 | awk '{split($0,a,/\.(d|s)/); print a[1] }')
 export DETERMINED_HASH=$(sha1sum $1 | awk '{ print $1 }')
-export HASHED_IMAGE_TAG="eos-${FILE_NAME}-${DETERMINED_HASH}"
+export HASHED_IMAGE_TAG="eos-$(basename $1 | awk '{split($0,a,/\.(d|s)/); print a[1] }')-${DETERMINED_HASH}"
 export FULL_TAG="eosio/ci:$HASHED_IMAGE_TAG"
