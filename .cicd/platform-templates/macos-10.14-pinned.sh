dbfad120a1cf11f31703e2cdc43a95d1c64b84d0
Nathan Pierce <nathan.pierce@block.one>
2019-12-05 19:18:06 -0500

    mac path fixes

--- .cicd/platform-templates/macos-10.14-pinned.sh
+++ .cicd/platform-templates/macos-10.14-pinned.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
-EOSIO_LOCATION=/root/eosio
-EOSIO_INSTALL_LOCATION=/root/eosio/install
+EOSIO_LOCATION=/Users/anka/eosio
+EOSIO_INSTALL_LOCATION=/Users/anka/eosio/install
 VERSION=1
 # Commands from the documentation are inserted right below this line
 # Anything below here is exclusive to our CI/CD
