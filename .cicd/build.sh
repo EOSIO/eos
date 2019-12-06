65dd82876c842c8ec8a25e2a21dd858490f964ef
Nathan Pierce <nathan.pierce@block.one>
2019-12-05 22:58:59 -0500

    fixes/testing

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -14,24 +14,23 @@ if [[ "$(uname)" == 'Darwin' ]]; then
 else # Linux
     ARGS=${ARGS:-"--rm --init -v /tmp/$POPULATED_FILE_NAME:/build-script"}
     # PRE_COMMANDS: Executed pre-cmake
-    # CMAKE_EXTRAS: Executed within and right before the cmake path (cmake CMAKE_EXTRAS ..)
-    [[ ! $IMAGE_TAG =~ 'unpinned' ]] && CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_TOOLCHAIN_FILE=$MOUNTED_DIR/scripts/pinned_toolchain.cmake -DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
+    [[ ! $IMAGE_TAG =~ 'unpinned' ]] && CMAKE_EXTRAS="-DCMAKE_CXX_COMPILER_LAUNCHER=ccache"
     if [[ $IMAGE_TAG == 'amazon_linux-2-pinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
+        PRE_COMMANDS="export PATH=/usr/lib64/ccache:\\\$PATH"
     elif [[ "$IMAGE_TAG" == 'centos-7.7-pinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
+        PRE_COMMANDS="export PATH=/usr/lib64/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'ubuntu-16.04-pinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
+        PRE_COMMANDS="export PATH=/usr/lib/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'ubuntu-18.04-pinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
+        PRE_COMMANDS="export PATH=/usr/lib/ccache:\\\$PATH"
     elif [[ $IMAGE_TAG == 'amazon_linux-2-unpinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib64/ccache:\\\$PATH"
+        PRE_COMMANDS="export PATH=/usr/lib64/ccache:\\\$PATH"
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++' -DCMAKE_C_COMPILER='clang'"
     elif [[ "$IMAGE_TAG" == 'centos-7.7-unpinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
+        PRE_COMMANDS="source /opt/rh/devtoolset-8/enable && source /opt/rh/rh-python36/enable && export PATH=/usr/lib64/ccache:\\\$PATH"
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DLLVM_DIR='/opt/rh/llvm-toolset-7.0/root/usr/lib64/cmake/llvm'"
     elif [[ $IMAGE_TAG == 'ubuntu-18.04-unpinned' ]]; then
-        PRE_COMMANDS="$PRE_COMMANDS && export PATH=/usr/lib/ccache:\\\$PATH"
+        PRE_COMMANDS="export PATH=/usr/lib/ccache:\\\$PATH"
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER='clang++-7' -DCMAKE_C_COMPILER='clang-7' -DLLVM_DIR='/usr/lib/llvm-7/lib/cmake/llvm'"
     fi
     BUILD_COMMANDS="BUILDKITE_COMMIT=$BUILDKITE_COMMIT ./build-script"
@@ -41,10 +40,9 @@ else # Linux
         $CICD_DIR/generate-base-images.sh
     elif [[ $TRAVIS == true ]]; then
         ARGS="$ARGS -v /usr/lib/ccache -v $HOME/.ccache:/opt/.ccache -e JOBS -e TRAVIS -e CCACHE_DIR=/opt/.ccache"
-        COMMANDS="ccache -s && $BUILD_COMMANDS"
+        BUILD_COMMANDS="ccache -s && $BUILD_COMMANDS"
     fi
     . $HELPERS_DIR/populate-template-and-hash.sh -h # obtain $FULL_TAG (and don't overwrite existing file)
-    COMMANDS="$PRE_COMMANDS && $COMMANDS"
-    echo "$ docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\""
-    eval docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$COMMANDS\"
+    echo "$ docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$PRE_COMMANDS && $BUILD_COMMANDS\""
+    eval docker run $ARGS $(buildkite-intrinsics) $FULL_TAG bash -c \"$PRE_COMMANDS && $BUILD_COMMANDS\"
 fi
