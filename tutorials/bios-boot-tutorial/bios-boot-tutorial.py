4c7462c14ed47cdd6a51474368f0200a996b25b4
iamveritas <ovidiu.craciun@block.one>
2019-12-10 08:59:39 +0000

    correct comment, clean up not needed sleep lines

--- tutorials/bios-boot-tutorial/bios-boot-tutorial.py
+++ tutorials/bios-boot-tutorial/bios-boot-tutorial.py
@@ -296,7 +296,7 @@ def stepCreateTokens():
     run(args.cleos + 'push action eosio.token issue \'["eosio", "%s", "memo"]\' -p eosio' % intToCurrency(totalAllocation))
     sleep(1)
 def stepSetSystemContract():
-    # Nearly all of the protocol upgrade features introduced in v1.8 first require a special protocol 
+    # All of the protocol upgrade features introduced in v1.8 first require a special protocol 
     # feature (codename PREACTIVATE_FEATURE) to be activated and for an updated version of the system 
     # contract that makes use of the functionality introduced by that feature to be deployed. 
 
@@ -313,34 +313,24 @@ def stepSetSystemContract():
     # activate remaining features
     # GET_SENDER
     retry(args.cleos + 'push action eosio activate \'["f0af56d2c5a48d60a4a5b5c903edfb7db3a736a94ed589d0b797df33ff9d3e1d"]\' -p eosio')
-    sleep(1)
     # FORWARD_SETCODE
     retry(args.cleos + 'push action eosio activate \'["2652f5f96006294109b3dd0bbde63693f55324af452b799ee137a81a905eed25"]\' -p eosio')
-    sleep(1)
     # ONLY_BILL_FIRST_AUTHORIZER
     retry(args.cleos + 'push action eosio activate \'["8ba52fe7a3956c5cd3a656a3174b931d3bb2abb45578befc59f283ecd816a405"]\' -p eosio')
-    sleep(1)
     # RESTRICT_ACTION_TO_SELF
     retry(args.cleos + 'push action eosio activate \'["ad9e3d8f650687709fd68f4b90b41f7d825a365b02c23a636cef88ac2ac00c43"]\' -p eosio')
-    sleep(1)
     # DISALLOW_EMPTY_PRODUCER_SCHEDULE
     retry(args.cleos + 'push action eosio activate \'["68dcaa34c0517d19666e6b33add67351d8c5f69e999ca1e37931bc410a297428"]\' -p eosio')
-    sleep(1)
      # FIX_LINKAUTH_RESTRICTION
     retry(args.cleos + 'push action eosio activate \'["e0fb64b1085cc5538970158d05a009c24e276fb94e1a0bf6a528b48fbc4ff526"]\' -p eosio')
-    sleep(1)
      # REPLACE_DEFERRED
     retry(args.cleos + 'push action eosio activate \'["ef43112c6543b88db2283a2e077278c315ae2c84719a8b25f25cc88565fbea99"]\' -p eosio')
-    sleep(1)
     # NO_DUPLICATE_DEFERRED_ID
     retry(args.cleos + 'push action eosio activate \'["4a90c00d55454dc5b059055ca213579c6ea856967712a56017487886a4d4cc0f"]\' -p eosio')
-    sleep(1)
     # ONLY_LINK_TO_EXISTING_PERMISSION
     retry(args.cleos + 'push action eosio activate \'["1a99a59d87e06e09ec5b028a9cbb7749b4a5ad8819004365d02dc4379a8b7241"]\' -p eosio')
-    sleep(1)
     # RAM_RESTRICTIONS
     retry(args.cleos + 'push action eosio activate \'["4e7bf348da00a945489b2a681749eb56f5de00b900014e137ddae39f48f69d67"]\' -p eosio')
-    sleep(1)
     run(args.cleos + 'push action eosio setpriv' + jsonArg(['eosio.msig', 1]) + '-p eosio@active')
 
 def stepInitSystemContract():
