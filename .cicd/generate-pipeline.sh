5ca8bbea92189461e3b8fd067d27da9369043c75
Nathan Pierce <nathan.pierce@block.one>
2019-12-19 16:01:08 -0500

    quick fix

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -117,7 +117,7 @@ EOF
         cat <<EOF
   - label: "$(echo "$PLATFORM_JSON" | jq -r .ICON) $(echo "$PLATFORM_JSON" | jq -r .PLATFORM_NAME_FULL) - Build"
     command:
-      - "git clone \$BUILDKITE_REPO eosio/eos/eos && cd eosio/eos && $GIT_FETCH git checkout -f \$BUILDKITE_COMMIT && git submodule update --init --recursive"
+      - "git clone \$BUILDKITE_REPO eosio/eos && cd eosio/eos && $GIT_FETCH git checkout -f \$BUILDKITE_COMMIT && git submodule update --init --recursive"
       - "cd eosio/eos && ./.cicd/build.sh"
       - "cd eosio/eos && tar -pczf build.tar.gz build && buildkite-agent artifact upload build.tar.gz"
     plugins:
