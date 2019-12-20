9a5ff833e60f2baf5b33083915e7472f36394b33
Zach Butler <zach.butler@block.one>
2019-12-20 10:01:10 -0500

    Commit messages sometimes have invalid YAML syntax which breaks the pipeline

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -446,7 +446,7 @@ if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && $TRIGGER_JOB == "true" ]]; then
     trigger: "eosio-lrt"
     async: true
     build:
-      message: "${BUILDKITE_MESSAGE}"
+      message: "Triggered by EOSIO build ${BUILDKITE_BUILD_NUMBER}"
       commit: "${BUILDKITE_COMMIT}"
       branch: "${BUILDKITE_BRANCH}"
       env:
@@ -469,7 +469,7 @@ if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && $TRIGGER_JOB = "true" ]]; then
     trigger: "eos-multiversion-tests"
     async: true
     build:
-      message: "${BUILDKITE_MESSAGE}"
+      message: "Triggered by EOSIO build ${BUILDKITE_BUILD_NUMBER}"
       commit: "${BUILDKITE_COMMIT}"
       branch: "${BUILDKITE_BRANCH}"
       env:
@@ -488,7 +488,7 @@ if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && -z "${SKIP_INSTALL}${SKIP_LINUX}$
     trigger: "eosio-sync-from-genesis"
     async: false
     build:
-      message: "${BUILDKITE_MESSAGE}"
+      message: "Triggered by EOSIO build ${BUILDKITE_BUILD_NUMBER}"
       commit: "${BUILDKITE_COMMIT}"
       branch: "${BUILDKITE_BRANCH}"
       env:
