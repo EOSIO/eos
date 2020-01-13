c70ad37321a3041435a0b3446f530da4491bbd55
Aaron Fu <aaron.fu@block.one>
2020-01-13 15:37:02 +0800

    try

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -523,148 +523,148 @@ EOF
 # EOF
 #     fi
 # fi
-# # pipeline tail
-# cat <<EOF
-#   - wait:
-#     continue_on_failure: true
+# pipeline tail
+cat <<EOF
+  - wait:
+    continue_on_failure: true
 
-#   - label: ":bar_chart: Test Metrics"
-#     command:
-#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-#       - "git clone \$BUILDKITE_REPO ."
-#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-#       - "echo '+++ :compression: Extracting Test Metrics Code'"
-#       - "tar -zxf .cicd/metrics/test-metrics.tar.gz"
-#       - "echo '+++ :javascript: Running test-metrics.js'"
-#       - "node --max-old-space-size=32768 test-metrics.js"
-#     plugins:
-#       - thedyrt/skip-checkout#v0.1.1:
-#           cd: ~
-#     agents:
-#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-#     timeout: ${TIMEOUT:-10}
-#     soft_fail: true
+  - label: ":bar_chart: Test Metrics"
+    command:
+      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+      - "git clone \$BUILDKITE_REPO ."
+      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+      - "echo '+++ :compression: Extracting Test Metrics Code'"
+      - "tar -zxf .cicd/metrics/test-metrics.tar.gz"
+      - "echo '+++ :javascript: Running test-metrics.js'"
+      - "node --max-old-space-size=32768 test-metrics.js"
+    plugins:
+      - thedyrt/skip-checkout#v0.1.1:
+          cd: ~
+    agents:
+      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+    timeout: ${TIMEOUT:-10}
+    soft_fail: true
 
-#   - wait
+  - wait
 
-#     # packaging
-#   - label: ":centos: CentOS 7.7 - Package Builder"
-#     command:
-#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-#       - "git clone \$BUILDKITE_REPO ."
-#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-#       - "buildkite-agent artifact download build.tar.gz . --step ':centos: CentOS 7.7 - Build' && tar -xzf build.tar.gz"
-#       - "./.cicd/package.sh"
-#     plugins:
-#       - thedyrt/skip-checkout#v0.1.1:
-#           cd: ~
-#     env:
-#       IMAGE_TAG: "centos-7.7-$PLATFORM_TYPE"
-#       PLATFORM_TYPE: $PLATFORM_TYPE
-#       OS: "el7" # OS and PKGTYPE required for lambdas
-#       PKGTYPE: "rpm"
-#     agents:
-#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-#     timeout: ${TIMEOUT:-10}
-#     skip: ${SKIP_CENTOS_7_7}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
+    # packaging
+  - label: ":centos: CentOS 7.7 - Package Builder"
+    command:
+      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+      - "git clone \$BUILDKITE_REPO ."
+      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+      - "buildkite-agent artifact download build.tar.gz . --step ':centos: CentOS 7.7 - Build' && tar -xzf build.tar.gz"
+      - "./.cicd/package.sh"
+    plugins:
+      - thedyrt/skip-checkout#v0.1.1:
+          cd: ~
+    env:
+      IMAGE_TAG: "centos-7.7-$PLATFORM_TYPE"
+      PLATFORM_TYPE: $PLATFORM_TYPE
+      OS: "el7" # OS and PKGTYPE required for lambdas
+      PKGTYPE: "rpm"
+    agents:
+      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+    timeout: ${TIMEOUT:-10}
+    skip: ${SKIP_CENTOS_7_7}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-#   - label: ":ubuntu: Ubuntu 16.04 - Package Builder"
-#     command:
-#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-#       - "git clone \$BUILDKITE_REPO ."
-#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-#       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 16.04 - Build' && tar -xzf build.tar.gz"
-#       - "./.cicd/package.sh"
-#     plugins:
-#       - thedyrt/skip-checkout#v0.1.1:
-#           cd: ~
-#     env:
-#       IMAGE_TAG: "ubuntu-16.04-$PLATFORM_TYPE"
-#       PLATFORM_TYPE: $PLATFORM_TYPE
-#       OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
-#       PKGTYPE: "deb"
-#     agents:
-#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-#     timeout: ${TIMEOUT:-10}
-#     skip: ${SKIP_UBUNTU_16_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
+  - label: ":ubuntu: Ubuntu 16.04 - Package Builder"
+    command:
+      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+      - "git clone \$BUILDKITE_REPO ."
+      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+      - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 16.04 - Build' && tar -xzf build.tar.gz"
+      - "./.cicd/package.sh"
+    plugins:
+      - thedyrt/skip-checkout#v0.1.1:
+          cd: ~
+    env:
+      IMAGE_TAG: "ubuntu-16.04-$PLATFORM_TYPE"
+      PLATFORM_TYPE: $PLATFORM_TYPE
+      OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
+      PKGTYPE: "deb"
+    agents:
+      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+    timeout: ${TIMEOUT:-10}
+    skip: ${SKIP_UBUNTU_16_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-#   - label: ":ubuntu: Ubuntu 18.04 - Package Builder"
-#     command:
-#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-#       - "git clone \$BUILDKITE_REPO ."
-#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-#       - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 18.04 - Build' && tar -xzf build.tar.gz"
-#       - "./.cicd/package.sh"
-#     plugins:
-#       - thedyrt/skip-checkout#v0.1.1:
-#           cd: ~
-#     env:
-#       IMAGE_TAG: "ubuntu-18.04-$PLATFORM_TYPE"
-#       PLATFORM_TYPE: $PLATFORM_TYPE
-#       OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
-#       PKGTYPE: "deb"
-#     agents:
-#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-#     timeout: ${TIMEOUT:-10}
-#     skip: ${SKIP_UBUNTU_18_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
+  - label: ":ubuntu: Ubuntu 18.04 - Package Builder"
+    command:
+      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+      - "git clone \$BUILDKITE_REPO ."
+      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+      - "buildkite-agent artifact download build.tar.gz . --step ':ubuntu: Ubuntu 18.04 - Build' && tar -xzf build.tar.gz"
+      - "./.cicd/package.sh"
+    plugins:
+      - thedyrt/skip-checkout#v0.1.1:
+          cd: ~
+    env:
+      IMAGE_TAG: "ubuntu-18.04-$PLATFORM_TYPE"
+      PLATFORM_TYPE: $PLATFORM_TYPE
+      OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
+      PKGTYPE: "deb"
+    agents:
+      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+    timeout: ${TIMEOUT:-10}
+    skip: ${SKIP_UBUNTU_18_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-#   - label: ":darwin: macOS 10.14 - Package Builder"
-#     command:
-#       - "git clone \$BUILDKITE_REPO eos && cd eos && $GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-#       - "cd eos && buildkite-agent artifact download build.tar.gz . --step ':darwin: macOS 10.14 - Build' && tar -xzf build.tar.gz"
-#       - "cd eos && ./.cicd/package.sh"
-#     plugins:
-#       - chef/anka#v0.5.4:
-#           no-volume: true
-#           inherit-environment-vars: true
-#           vm-name: 10.14.6_6C_14G_40G
-#           vm-registry-tag: "clean::cicd::git-ssh::nas::brew::buildkite-agent"
-#           always-pull: true
-#           debug: true
-#           wait-network: true
-#           failover-registries:
-#             - 'registry_1'
-#             - 'registry_2'
-#           pre-execute-sleep: 10
-#       - thedyrt/skip-checkout#v0.1.1:
-#           cd: ~
-#     agents:
-#       - "queue=mac-anka-node-fleet"
-#     timeout: ${TIMEOUT:-60}
-#     skip: ${SKIP_MACOS_10_14}${SKIP_PACKAGE_BUILDER}${SKIP_MAC}
+  - label: ":darwin: macOS 10.14 - Package Builder"
+    command:
+      - "git clone \$BUILDKITE_REPO eos && cd eos && $GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+      - "cd eos && buildkite-agent artifact download build.tar.gz . --step ':darwin: macOS 10.14 - Build' && tar -xzf build.tar.gz"
+      - "cd eos && ./.cicd/package.sh"
+    plugins:
+      - chef/anka#v0.5.4:
+          no-volume: true
+          inherit-environment-vars: true
+          vm-name: 10.14.6_6C_14G_40G
+          vm-registry-tag: "clean::cicd::git-ssh::nas::brew::buildkite-agent"
+          always-pull: true
+          debug: true
+          wait-network: true
+          failover-registries:
+            - 'registry_1'
+            - 'registry_2'
+          pre-execute-sleep: 10
+      - thedyrt/skip-checkout#v0.1.1:
+          cd: ~
+    agents:
+      - "queue=mac-anka-node-fleet"
+    timeout: ${TIMEOUT:-60}
+    skip: ${SKIP_MACOS_10_14}${SKIP_PACKAGE_BUILDER}${SKIP_MAC}
 
-#   - label: ":ubuntu: Ubuntu 18.04 - Contract Builder"
-#     command: "./.cicd/installation-build.sh"
-#     env:
-#       IMAGE_TAG: "ubuntu-18.04-unpinned"
-#       PLATFORM_TYPE: "unpinned"
-#     agents:
-#       queue: "$BUILDKITE_BUILD_AGENT_QUEUE"
-#     timeout: ${TIMEOUT:-30}
-#     skip: ${SKIP_CONTRACT_BUILDER}${SKIP_LINUX}
+  - label: ":ubuntu: Ubuntu 18.04 - Contract Builder"
+    command: "./.cicd/installation-build.sh"
+    env:
+      IMAGE_TAG: "ubuntu-18.04-unpinned"
+      PLATFORM_TYPE: "unpinned"
+    agents:
+      queue: "$BUILDKITE_BUILD_AGENT_QUEUE"
+    timeout: ${TIMEOUT:-30}
+    skip: ${SKIP_CONTRACT_BUILDER}${SKIP_LINUX}
 
-#   - wait
+  - wait
 
-#   - label: ":git: Git Submodule Regression Check"
-#     command: "./.cicd/submodule-regression-check.sh"
-#     agents:
-#       queue: "automation-basic-builder-fleet"
-#     timeout: ${TIMEOUT:-5}
+  - label: ":git: Git Submodule Regression Check"
+    command: "./.cicd/submodule-regression-check.sh"
+    agents:
+      queue: "automation-basic-builder-fleet"
+    timeout: ${TIMEOUT:-5}
 
-#   - label: ":beer: Brew Updater"
-#     command:
-#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-#       - "git clone \$BUILDKITE_REPO ."
-#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-#       - "buildkite-agent artifact download eosio.rb . --step ':darwin: macOS 10.14 - Package Builder'"
-#       - "buildkite-agent artifact upload eosio.rb"
-#     plugins:
-#       - thedyrt/skip-checkout#v0.1.1:
-#           cd: ~
-#     agents:
-#       queue: "automation-basic-builder-fleet"
-#     timeout: "${TIMEOUT:-5}"
-#     skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}
+  - label: ":beer: Brew Updater"
+    command:
+      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+      - "git clone \$BUILDKITE_REPO ."
+      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+      - "buildkite-agent artifact download eosio.rb . --step ':darwin: macOS 10.14 - Package Builder'"
+      - "buildkite-agent artifact upload eosio.rb"
+    plugins:
+      - thedyrt/skip-checkout#v0.1.1:
+          cd: ~
+    agents:
+      queue: "automation-basic-builder-fleet"
+    timeout: "${TIMEOUT:-5}"
+    skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}
 
-# EOF
-# IFS=$oIFS
+EOF
+IFS=$oIFS
