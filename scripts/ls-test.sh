4519f0db17e2c3041fc36e7fc781b09037fec0b8
Aaron Fu <aaron.fu@block.one>
2020-01-16 09:50:38 +0800

    output-on-failure

--- scripts/ls-test.sh
+++ scripts/ls-test.sh
@@ -34,11 +34,9 @@ python3 -m pip install requests
 
 set +e # defer ctest error handling to end
 
-#echo "ready to execute: ctest -L ls_tests -V -j $JOBS -T Test"
 #ctest -L ls_tests --output-on-failure -j $JOBS -T Test
-
-echo "ready to execute: ctest -L ls_tests -V -j 32 -T Test"
-# ctest -L ls_tests --output-on-failure -j 32 -T Test
+echo "ready to execute: ctest -L ls_tests --output-on-failure -j 32 -T Test"
+ctest -L ls_tests --output-on-failure -j 32 -T Test
 export PYTHONIOENCODING=UTF-8
 echo "PYTHONIOENCODING is $PYTHONIOENCODING"
 ctest -L ls_tests -V -j 32 -T Test
