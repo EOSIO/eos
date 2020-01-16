902e1980446c14e577f9c2801905239e9aefe110
Chris Gundlach <cgundlach@naughtydawgllc.com>
2020-01-16 12:56:21 -0800

    updated ignores

--- libraries/chain/include/eosio/chain/block_state.hpp
+++ libraries/chain/include/eosio/chain/block_state.hpp
@@ -69,4 +69,5 @@ namespace eosio { namespace chain {
 
 } } /// namespace eosio::chain
 
+// @ignore _pub_keys_recovered _cached_trxs
 FC_REFLECT_DERIVED( eosio::chain::block_state, (eosio::chain::block_header_state), (block)(validated) )
