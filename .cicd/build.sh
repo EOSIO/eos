5c3f0411a93e1fe3cf9a4eb14d087737afd4d299
Nathan Pierce <nathan.pierce@block.one>
2020-01-15 22:29:11 -0500

    quick fix

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -10,7 +10,6 @@ if [[ $BUILDKITE == true ]]; then # Buildkite uses tags with deps already on the
 else
     [[ $ENABLE_INSTALL == true ]] && . ./.cicd/helpers/populate-template-and-hash.sh '<!-- DAC ENV' '<!-- DAC CLONE' '<!-- DAC DEPS' '<!-- DAC BUILD' '<!-- DAC INSTALL' || . ./.cicd/helpers/populate-template-and-hash.sh '<!-- DAC ENV' '<!-- DAC CLONE' '<!-- DAC DEPS' '<!-- DAC BUILD'
     sed -i -e '/git clone https:\/\/github.com\/EOSIO\/eos\.git.*/d' /tmp/$POPULATED_FILE_NAME # We don't need to clone twice
-    sed -i -e 's/export EOS_LOCATION=\$EOSIO_LOCATION\/eos/export EOS_LOCATION=\$(pwd)/g' /tmp/$POPULATED_FILE_NAME
 fi
 if [[ "$(uname)" == 'Darwin' ]]; then
     # You can't use chained commands in execute
