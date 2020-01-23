3f21d9aa07fefd16189eaf32b92a969a31554e5c
Nathan Pierce <nathan.pierce@block.one>
2020-01-23 18:18:05 -0500

    testing

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -474,190 +474,190 @@ if ( [[ ! $PINNED == false ]] ); then
 
 EOF
 fi
-# trigger eosio-lrt post pr
-if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && $TRIGGER_JOB == "true" ]]; then
-    if ( [[ ! $PINNED == false ]] ); then
-        cat <<EOF
-  - label: ":pipeline: Trigger Long Running Tests"
-    trigger: "eosio-lrt"
-    async: true
-    build:
-      message: "Triggered by $BUILDKITE_PIPELINE_SLUG build $BUILDKITE_BUILD_NUMBER"
-      commit: "${BUILDKITE_COMMIT}"
-      branch: "${BUILDKITE_BRANCH}"
-      env:
-        BUILDKITE_PULL_REQUEST: "${BUILDKITE_PULL_REQUEST}"
-        BUILDKITE_PULL_REQUEST_BASE_BRANCH: "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}"
-        BUILDKITE_PULL_REQUEST_REPO: "${BUILDKITE_PULL_REQUEST_REPO}"
-        BUILDKITE_TRIGGERED_FROM_BUILD_URL: "${BUILDKITE_BUILD_URL}"
-        SKIP_BUILD: "true"
-        SKIP_WASM_SPEC_TESTS: "true"
-        PINNED: "${PINNED}"
+# # trigger eosio-lrt post pr
+# if [[ -z $BUILDKITE_TRIGGERED_FROM_BUILD_ID && $TRIGGER_JOB == "true" ]]; then
+#     if ( [[ ! $PINNED == false ]] ); then
+#         cat <<EOF
+#   - label: ":pipeline: Trigger Long Running Tests"
+#     trigger: "eosio-lrt"
+#     async: true
+#     build:
+#       message: "Triggered by $BUILDKITE_PIPELINE_SLUG build $BUILDKITE_BUILD_NUMBER"
+#       commit: "${BUILDKITE_COMMIT}"
+#       branch: "${BUILDKITE_BRANCH}"
+#       env:
+#         BUILDKITE_PULL_REQUEST: "${BUILDKITE_PULL_REQUEST}"
+#         BUILDKITE_PULL_REQUEST_BASE_BRANCH: "${BUILDKITE_PULL_REQUEST_BASE_BRANCH}"
+#         BUILDKITE_PULL_REQUEST_REPO: "${BUILDKITE_PULL_REQUEST_REPO}"
+#         BUILDKITE_TRIGGERED_FROM_BUILD_URL: "${BUILDKITE_BUILD_URL}"
+#         SKIP_BUILD: "true"
+#         SKIP_WASM_SPEC_TESTS: "true"
+#         PINNED: "${PINNED}"
 
-EOF
-    fi
-fi
-# trigger eosio-sync-from-genesis for every build
-if [[ "$BUILDKITE_PIPELINE_SLUG" == 'eosio' && -z "${SKIP_INSTALL}${SKIP_LINUX}${SKIP_DOCKER}${SKIP_SYNC_TESTS}" ]]; then
-    cat <<EOF
-  - label: ":chains: Sync from Genesis Test"
-    trigger: "eosio-sync-from-genesis"
-    async: false
-    build:
-      message: "Triggered by $BUILDKITE_PIPELINE_SLUG build $BUILDKITE_BUILD_NUMBER"
-      commit: "${BUILDKITE_COMMIT}"
-      branch: "${BUILDKITE_BRANCH}"
-      env:
-        BUILDKITE_TRIGGERED_FROM_BUILD_URL: "${BUILDKITE_BUILD_URL}"
-        SKIP_JUNGLE: "${SKIP_JUNGLE}"
-        SKIP_KYLIN: "${SKIP_KYLIN}"
-        SKIP_MAIN: "${SKIP_MAIN}"
-        TIMEOUT: "${TIMEOUT}"
+# EOF
+#     fi
+# fi
+# # trigger eosio-sync-from-genesis for every build
+# if [[ "$BUILDKITE_PIPELINE_SLUG" == 'eosio' && -z "${SKIP_INSTALL}${SKIP_LINUX}${SKIP_DOCKER}${SKIP_SYNC_TESTS}" ]]; then
+#     cat <<EOF
+#   - label: ":chains: Sync from Genesis Test"
+#     trigger: "eosio-sync-from-genesis"
+#     async: false
+#     build:
+#       message: "Triggered by $BUILDKITE_PIPELINE_SLUG build $BUILDKITE_BUILD_NUMBER"
+#       commit: "${BUILDKITE_COMMIT}"
+#       branch: "${BUILDKITE_BRANCH}"
+#       env:
+#         BUILDKITE_TRIGGERED_FROM_BUILD_URL: "${BUILDKITE_BUILD_URL}"
+#         SKIP_JUNGLE: "${SKIP_JUNGLE}"
+#         SKIP_KYLIN: "${SKIP_KYLIN}"
+#         SKIP_MAIN: "${SKIP_MAIN}"
+#         TIMEOUT: "${TIMEOUT}"
 
-EOF
-fi
-# trigger eosio-resume-from-state for every build
-if [[ "$BUILDKITE_PIPELINE_SLUG" == 'eosio' && -z "${SKIP_INSTALL}${SKIP_LINUX}${SKIP_DOCKER}${SKIP_SYNC_TESTS}" ]]; then
-    cat <<EOF
-  - label: ":outbox_tray: Resume from State Test"
-    trigger: "eosio-resume-from-state"
-    async: false
-    build:
-      message: "Triggered by $BUILDKITE_PIPELINE_SLUG build $BUILDKITE_BUILD_NUMBER"
-      commit: "${BUILDKITE_COMMIT}"
-      branch: "${BUILDKITE_BRANCH}"
-      env:
-        BUILDKITE_TRIGGERED_FROM_BUILD_URL: "${BUILDKITE_BUILD_URL}"
-        SKIP_JUNGLE: "${SKIP_JUNGLE}"
-        SKIP_KYLIN: "${SKIP_KYLIN}"
-        SKIP_MAIN: "${SKIP_MAIN}"
-        TIMEOUT: "${TIMEOUT}"
+# EOF
+# fi
+# # trigger eosio-resume-from-state for every build
+# if [[ "$BUILDKITE_PIPELINE_SLUG" == 'eosio' && -z "${SKIP_INSTALL}${SKIP_LINUX}${SKIP_DOCKER}${SKIP_SYNC_TESTS}" ]]; then
+#     cat <<EOF
+#   - label: ":outbox_tray: Resume from State Test"
+#     trigger: "eosio-resume-from-state"
+#     async: false
+#     build:
+#       message: "Triggered by $BUILDKITE_PIPELINE_SLUG build $BUILDKITE_BUILD_NUMBER"
+#       commit: "${BUILDKITE_COMMIT}"
+#       branch: "${BUILDKITE_BRANCH}"
+#       env:
+#         BUILDKITE_TRIGGERED_FROM_BUILD_URL: "${BUILDKITE_BUILD_URL}"
+#         SKIP_JUNGLE: "${SKIP_JUNGLE}"
+#         SKIP_KYLIN: "${SKIP_KYLIN}"
+#         SKIP_MAIN: "${SKIP_MAIN}"
+#         TIMEOUT: "${TIMEOUT}"
 
-EOF
-fi
-# pipeline tail
-cat <<EOF
-  - wait:
-    continue_on_failure: true
+# EOF
+# fi
+# # pipeline tail
+# cat <<EOF
+#   - wait:
+#     continue_on_failure: true
 
-  - label: ":bar_chart: Test Metrics"
-    command:
-      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-      - "git clone \$BUILDKITE_REPO ."
-      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-      - "echo '+++ :compression: Extracting Test Metrics Code'"
-      - "tar -zxf .cicd/metrics/test-metrics.tar.gz"
-      - "echo '+++ :javascript: Running test-metrics.js'"
-      - "node --max-old-space-size=32768 test-metrics.js"
-    plugins:
-      - thedyrt/skip-checkout#v0.1.1:
-          cd: ~
-    agents:
-      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-    timeout: ${TIMEOUT:-10}
-    soft_fail: true
+#   - label: ":bar_chart: Test Metrics"
+#     command:
+#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+#       - "git clone \$BUILDKITE_REPO ."
+#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+#       - "echo '+++ :compression: Extracting Test Metrics Code'"
+#       - "tar -zxf .cicd/metrics/test-metrics.tar.gz"
+#       - "echo '+++ :javascript: Running test-metrics.js'"
+#       - "node --max-old-space-size=32768 test-metrics.js"
+#     plugins:
+#       - thedyrt/skip-checkout#v0.1.1:
+#           cd: ~
+#     agents:
+#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+#     timeout: ${TIMEOUT:-10}
+#     soft_fail: true
 
-  - wait
+#   - wait
 
-    # packaging
-  - label: ":centos: CentOS 7.7 - Package Builder"
-    command:
-      - "./.cicd/package.sh"
-    env:
-      IMAGE_TAG: "centos-7.7-$PLATFORM_TYPE"
-      PLATFORM_FULL_NAME: ":centos: CentOS 7.7"
-      OS: "el7" # OS and PKGTYPE required for lambdas
-      PKGTYPE: "rpm"
-    agents:
-      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-    timeout: ${TIMEOUT:-10}
-    skip: ${SKIP_CENTOS_7_7}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
+#     # packaging
+#   - label: ":centos: CentOS 7.7 - Package Builder"
+#     command:
+#       - "./.cicd/package.sh"
+#     env:
+#       IMAGE_TAG: "centos-7.7-$PLATFORM_TYPE"
+#       PLATFORM_FULL_NAME: ":centos: CentOS 7.7"
+#       OS: "el7" # OS and PKGTYPE required for lambdas
+#       PKGTYPE: "rpm"
+#     agents:
+#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+#     timeout: ${TIMEOUT:-10}
+#     skip: ${SKIP_CENTOS_7_7}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-  - label: ":ubuntu: Ubuntu 16.04 - Package Builder"
-    command:
-      - "./.cicd/package.sh"
-    env:
-      IMAGE_TAG: "ubuntu-16.04-$PLATFORM_TYPE"
-      PLATFORM_FULL_NAME: ":ubuntu: Ubuntu 16.04"
-      OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
-      PKGTYPE: "deb"
-    agents:
-      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-    timeout: ${TIMEOUT:-10}
-    skip: ${SKIP_UBUNTU_16_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
+#   - label: ":ubuntu: Ubuntu 16.04 - Package Builder"
+#     command:
+#       - "./.cicd/package.sh"
+#     env:
+#       IMAGE_TAG: "ubuntu-16.04-$PLATFORM_TYPE"
+#       PLATFORM_FULL_NAME: ":ubuntu: Ubuntu 16.04"
+#       OS: "ubuntu-16.04" # OS and PKGTYPE required for lambdas
+#       PKGTYPE: "deb"
+#     agents:
+#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+#     timeout: ${TIMEOUT:-10}
+#     skip: ${SKIP_UBUNTU_16_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-  - label: ":ubuntu: Ubuntu 18.04 - Package Builder"
-    command:
-      - "./.cicd/package.sh"
-    env:
-      IMAGE_TAG: "ubuntu-18.04-$PLATFORM_TYPE"
-      PLATFORM_FULL_NAME: ":ubuntu: Ubuntu 18.04"
-      OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
-      PKGTYPE: "deb"
-    agents:
-      queue: "$BUILDKITE_TEST_AGENT_QUEUE"
-    timeout: ${TIMEOUT:-10}
-    skip: ${SKIP_UBUNTU_18_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
+#   - label: ":ubuntu: Ubuntu 18.04 - Package Builder"
+#     command:
+#       - "./.cicd/package.sh"
+#     env:
+#       IMAGE_TAG: "ubuntu-18.04-$PLATFORM_TYPE"
+#       PLATFORM_FULL_NAME: ":ubuntu: Ubuntu 18.04"
+#       OS: "ubuntu-18.04" # OS and PKGTYPE required for lambdas
+#       PKGTYPE: "deb"
+#     agents:
+#       queue: "$BUILDKITE_TEST_AGENT_QUEUE"
+#     timeout: ${TIMEOUT:-10}
+#     skip: ${SKIP_UBUNTU_18_04}${SKIP_PACKAGE_BUILDER}${SKIP_LINUX}
 
-  - label: ":darwin: macOS 10.14 - Package Builder"
-    command:
-      - "brew install md5sha1sum && brew link --overwrite md5sha1sum"
-      - "git clone \$BUILDKITE_REPO ~/eosio/eos && cd ~/eosio/eos && $GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-      - "cd ~/eosio/eos && ./.cicd/package.sh"
-    plugins:
-      - EOSIO/anka#v0.5.7:
-          no-volume: true
-          inherit-environment-vars: true
-          vm-name: 10.14.6_6C_14G_40G
-          vm-registry-tag: "clean::cicd::git-ssh::nas::brew::buildkite-agent"
-          always-pull: true
-          debug: true
-          wait-network: true
-          failover-registries:
-            - 'registry_1'
-            - 'registry_2'
-          pre-execute-ping-sleep: "8.8.8.8"
-      - thedyrt/skip-checkout#v0.1.1:
-          cd: ~
-    agents:
-      - "queue=mac-anka-node-fleet"
-    env:
-      IMAGE_TAG: "macos-10.14-$PLATFORM_TYPE"
-      PLATFORM_FULL_NAME: ":darwin: macOS 10.14"
-    timeout: ${TIMEOUT:-10}
-    skip: ${SKIP_MACOS_10_14}${SKIP_PACKAGE_BUILDER}${SKIP_MAC}
+#   - label: ":darwin: macOS 10.14 - Package Builder"
+#     command:
+#       - "brew install md5sha1sum && brew link --overwrite md5sha1sum"
+#       - "git clone \$BUILDKITE_REPO ~/eosio/eos && cd ~/eosio/eos && $GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+#       - "cd ~/eosio/eos && ./.cicd/package.sh"
+#     plugins:
+#       - EOSIO/anka#v0.5.7:
+#           no-volume: true
+#           inherit-environment-vars: true
+#           vm-name: 10.14.6_6C_14G_40G
+#           vm-registry-tag: "clean::cicd::git-ssh::nas::brew::buildkite-agent"
+#           always-pull: true
+#           debug: true
+#           wait-network: true
+#           failover-registries:
+#             - 'registry_1'
+#             - 'registry_2'
+#           pre-execute-ping-sleep: "8.8.8.8"
+#       - thedyrt/skip-checkout#v0.1.1:
+#           cd: ~
+#     agents:
+#       - "queue=mac-anka-node-fleet"
+#     env:
+#       IMAGE_TAG: "macos-10.14-$PLATFORM_TYPE"
+#       PLATFORM_FULL_NAME: ":darwin: macOS 10.14"
+#     timeout: ${TIMEOUT:-10}
+#     skip: ${SKIP_MACOS_10_14}${SKIP_PACKAGE_BUILDER}${SKIP_MAC}
 
-  - label: ":docker: Docker - Label Container with Git Branch and Git Tag"
-    command: .cicd/docker-tag.sh
-    env:
-      IMAGE_TAG: "ubuntu-18.04-unpinned"
-    agents:
-      queue: "$BUILDKITE_BUILD_AGENT_QUEUE"
-    timeout: ${TIMEOUT:-10}
-    skip: ${SKIP_INSTALL}${SKIP_LINUX}${SKIP_DOCKER}${SKIP_PACKAGE_BUILDER}
+#   - label: ":docker: Docker - Label Container with Git Branch and Git Tag"
+#     command: .cicd/docker-tag.sh
+#     env:
+#       IMAGE_TAG: "ubuntu-18.04-unpinned"
+#     agents:
+#       queue: "$BUILDKITE_BUILD_AGENT_QUEUE"
+#     timeout: ${TIMEOUT:-10}
+#     skip: ${SKIP_INSTALL}${SKIP_LINUX}${SKIP_DOCKER}${SKIP_PACKAGE_BUILDER}
 
-  - wait
+#   - wait
 
-  - label: ":git: Git Submodule Regression Check"
-    command: "./.cicd/submodule-regression-check.sh"
-    agents:
-      queue: "automation-basic-builder-fleet"
-    timeout: ${TIMEOUT:-5}
+#   - label: ":git: Git Submodule Regression Check"
+#     command: "./.cicd/submodule-regression-check.sh"
+#     agents:
+#       queue: "automation-basic-builder-fleet"
+#     timeout: ${TIMEOUT:-5}
 
-  - label: ":beer: Brew Updater"
-    command:
-      - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
-      - "git clone \$BUILDKITE_REPO ."
-      - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
-      - "buildkite-agent artifact download eosio.rb . --step ':darwin: macOS 10.14 - Package Builder'"
-      - "buildkite-agent artifact upload eosio.rb"
-    plugins:
-      - thedyrt/skip-checkout#v0.1.1:
-          cd: ~
-    agents:
-      queue: "automation-basic-builder-fleet"
-    timeout: "${TIMEOUT:-5}"
-    skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}
+#   - label: ":beer: Brew Updater"
+#     command:
+#       - "ssh-keyscan -H github.com >> ~/.ssh/known_hosts"
+#       - "git clone \$BUILDKITE_REPO ."
+#       - "$GIT_FETCH git checkout -f \$BUILDKITE_COMMIT"
+#       - "buildkite-agent artifact download eosio.rb . --step ':darwin: macOS 10.14 - Package Builder'"
+#       - "buildkite-agent artifact upload eosio.rb"
+#     plugins:
+#       - thedyrt/skip-checkout#v0.1.1:
+#           cd: ~
+#     agents:
+#       queue: "automation-basic-builder-fleet"
+#     timeout: "${TIMEOUT:-5}"
+#     skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}
 
-EOF
-IFS=$oIFS
+# EOF
+# IFS=$oIFS
