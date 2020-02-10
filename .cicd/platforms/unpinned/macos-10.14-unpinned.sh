8946ca865da5828ad84d19e814e1a99cb111cefb
Scott Arnette <scott.a.arnette@gmail.com>
2020-02-10 09:25:37 -0500

    Must be in existing directory.

--- .cicd/platforms/unpinned/macos-10.14-unpinned.sh
+++ .cicd/platforms/unpinned/macos-10.14-unpinned.sh
@@ -23,7 +23,7 @@ make -j $(getconf _NPROCESSORS_ONLN) VERBOSE=1 && \
 sudo make install && \
 rm -rf ~/mongo-cxx-driver-r3.4.0.tar.gz ~/mongo-cxx-driver-r3.4.0
 # install pip dependencies.
-python3 -m pip install --upgrade pip
+cd ~ && python3 -m pip install --upgrade pip
 python3 -m pip install requests
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
