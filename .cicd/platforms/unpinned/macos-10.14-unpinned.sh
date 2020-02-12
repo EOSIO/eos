b503a0f8ec13d9574e5d107251563d9f63918809
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2020-02-12 10:10:23 -0500

    remove brew's python@2 install
    brew no longer has the package and it's not needed anyways

--- .cicd/platforms/unpinned/macos-10.14-unpinned.sh
+++ .cicd/platforms/unpinned/macos-10.14-unpinned.sh
@@ -2,7 +2,7 @@
 set -eo pipefail
 VERSION=2
 brew update
-brew install git cmake python@2 python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl@1.1 jq boost || :
+brew install git cmake python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl@1.1 jq boost || :
 # install llvm 4 from source
 git clone --depth 1 --single-branch --branch release_40 https://github.com/llvm-mirror/llvm.git llvm
 cd llvm
