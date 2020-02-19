a1d0c2d327be8a3b4f7f9420837d075a5f1c9bab
Kevin Heifner <heifnerk@objectcomputing.com>
2020-02-19 11:13:54 -0600

    Handle exhaustion in start_block of pending trxs

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -432,7 +432,8 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
          });
       }
 
-      void process_incoming_transaction_async(const transaction_metadata_ptr& trx, bool persist_until_expired, next_function<transaction_trace_ptr> next) {
+      bool process_incoming_transaction_async(const transaction_metadata_ptr& trx, bool persist_until_expired, next_function<transaction_trace_ptr> next) {
+         bool exhausted = false;
          chain::controller& chain = chain_plug->chain();
 
          auto send_response = [this, &trx, &chain, &next](const fc::static_variant<fc::exception_ptr, transaction_trace_ptr>& response) {
@@ -473,18 +474,18 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                      std::make_shared<expired_tx_exception>(
                            FC_LOG_MESSAGE( error, "expired transaction ${id}, expiration ${e}, block time ${bt}",
                                            ("id", id)("e", trx->packed_trx()->expiration())( "bt", bt )))));
-               return;
+               return true;
             }
 
             if( chain.is_known_unexpired_transaction( id )) {
                send_response( std::static_pointer_cast<fc::exception>( std::make_shared<tx_duplicate>(
                      FC_LOG_MESSAGE( error, "duplicate transaction ${id}", ("id", id)))) );
-               return;
+               return true;
             }
 
             if( !chain.is_building_block()) {
                _unapplied_transactions.add_incoming( trx, persist_until_expired, next );
-               return;
+               return true;
             }
 
             auto deadline = fc::time_point::now() + fc::milliseconds( _max_transaction_time_ms );
@@ -507,6 +508,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                               ("txid", trx->id()));
                      if( block_is_exhausted() ) {
                         schedule_maybe_produce_block( true );
+                        exhausted = true;
                      }
                   } else {
                      fc_dlog( _trx_trace_log, "[TRX_TRACE] Speculative execution COULD NOT FIT tx: ${txid} RETRYING",
@@ -532,6 +534,8 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
          } catch ( std::bad_alloc& ) {
             chain_plugin::handle_bad_alloc();
          } CATCH_AND_CALL(send_response);
+
+         return !exhausted;
       }
 
 
@@ -638,7 +642,7 @@ void producer_plugin::set_program_options(
           "Percentage of cpu block production time used to produce block. Whole number percentages, e.g. 80 for 80%")
          ("last-block-cpu-effort-percent", bpo::value<uint32_t>()->default_value(config::default_block_cpu_effort_pct / config::percent_1),
           "Percentage of cpu block production time used to produce last block. Whole number percentages, e.g. 80 for 80%")
-         ("max-block-cpu-usage-threshold-us", bpo::value<uint32_t>()->default_value( 1000 ),
+         ("max-block-cpu-usage-threshold-us", bpo::value<uint32_t>()->default_value( 5000 ),
           "Threshold of cpu block production to consider block full; when within threshold of max-block-cpu-usage no additional transactions will be attempted")
          ("max-block-net-usage-threshold-bytes", bpo::value<uint32_t>()->default_value( 1024 ),
           "Threshold of net block production to consider block full; when within threshold of max-block-net-usage no additional transactions will be attempted")
@@ -1746,7 +1750,7 @@ bool producer_plugin_impl::process_scheduled_and_incoming_trxs( const fc::time_p
          ++sch_itr;
          continue; // do not allow schedule and execute in same block
       }
-      if( deadline <= fc::time_point::now() ) {
+      if( exhausted || deadline <= fc::time_point::now() ) {
          exhausted = true;
          break;
       }
@@ -1778,10 +1782,13 @@ bool producer_plugin_impl::process_scheduled_and_incoming_trxs( const fc::time_p
          auto next = itr->next;
          bool persist_until_expired = itr->trx_type == trx_enum_type::incoming_persisted;
          itr = _unapplied_transactions.erase( itr );
-         process_incoming_transaction_async( trx_meta, persist_until_expired, next );
+         if( !process_incoming_transaction_async( trx_meta, persist_until_expired, next ) ) {
+            exhausted = true;
+            break;
+         }
       }
 
-      if (deadline <= fc::time_point::now()) {
+      if (exhausted || deadline <= fc::time_point::now()) {
          exhausted = true;
          break;
       }
@@ -1846,8 +1853,11 @@ bool producer_plugin_impl::process_incoming_trxs( const fc::time_point& deadline
          auto next = itr->next;
          bool persist_until_expired = itr->trx_type == trx_enum_type::incoming_persisted;
          itr = _unapplied_transactions.erase( itr );
-         process_incoming_transaction_async( trx_meta, persist_until_expired, next );
          ++processed;
+         if( !process_incoming_transaction_async( trx_meta, persist_until_expired, next ) ) {
+            exhausted = true;
+            break;
+         }
       }
       fc_dlog( _log, "Processed ${n} pending transactions, ${p} left", ("n", processed)("p", _unapplied_transactions.incoming_size()) );
    }
