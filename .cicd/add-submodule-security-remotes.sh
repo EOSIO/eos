e69c2728475fe73f0a4a4c64d1d4c691c9b2540e
NorseGaud <connarpierce@gmail.com>
2020-02-19 10:20:33 -0500

    name change for submodule remote script + added a MAP/list so we can support -security submodules with different names

--- .cicd/add-submodule-security-remotes.sh
+++ .cicd/add-submodule-security-remotes.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -eo pipefail
+# Add repos to the below MAP if their security repo name differs
+## Format: SUBMODULE_NAME[space]SECURITY_SUBMODULE_NAME
+MAP=$(cat <<-'LIST'
+    eos eosio-security
+LIST
+)
 [[ $BUILDKITE_LABEL == ':pipeline: Pipeline Upload' ]] && exit 0 # Don't do anything if the step is Pipeline Upload
 echo "[Preparing Submodules]"
 git submodule update --init --recursive --force 1>/dev/null || true # We need to get the .git folder in the submodule dir so we can set the remote for security and the || true avoids it failing the script
@@ -13,7 +19,16 @@ if [[ $BUILDKITE_PIPELINE_SLUG =~ '-security' ]] || [[ ${FORCE_SUBMODULE_REMOTE_
         SUBMOD_COMMIT=$(echo $SUBMOD | awk '{print $1}' | sed 's/+//g' | sed 's/-//g')
         SUBMOD_PATH=$(echo $SUBMOD | awk '{print $2}')
         SUBMOD_NAME=$(basename $SUBMOD_PATH)
-        REMOTE_URL="git@github.com:EOSIO/$SUBMOD_NAME-security.git"
+        SEC_SUBMOD_NAME="${SUBMOD_NAME}-security"
+        for ITEM in $MAP; do
+            ITEM_SUBMODULE=$(echo $ITEM | awk '{print $1}')
+            ITEM_SUBMODULE_ACTUAL=$(echo $ITEM | awk '{print $2}')
+            if [[ $ITEM_SUBMODULE == $SUBMOD_NAME ]]; then
+                echo " - Found $ITEM_SUBMODULE, but its security repo is named $ITEM_SUBMODULE_ACTUAL. Using that instead!"
+                SEC_SUBMOD_NAME=$ITEM_SUBMODULE_ACTUAL
+            fi
+        done
+        REMOTE_URL="git@github.com:EOSIO/$SEC_SUBMOD_NAME.git"
         echo "Preparing $SUBMOD_NAME [$CWD/$SUBMOD_PATH] with security remote $REMOTE_URL"
         cd $CWD/$SUBMOD_PATH
         git remote add remote $REMOTE_URL || true
