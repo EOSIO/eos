e6043307ecd621a031ad3d627d4ab1f2d1bd51a9
Allen Han <allenhan2@gmail.com>
2020-02-20 11:10:36 -0500

    fix pr comments and add const

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -445,13 +445,13 @@ auto abi_serializer_resolver = [](const name& account) -> fc::optional<abi_seria
    static unordered_map<account_name, fc::optional<abi_serializer> > abi_cache;
    auto it = abi_cache.find( account );
    if ( it == abi_cache.end() ) {
-       fc::optional<abi_serializer> abis;
-       auto raw_abi_result = call(get_raw_abi_func, fc::mutable_variant_object("account_name", account));
-       auto raw_abi_blob = raw_abi_result["abi"].as_blob().data;
-       if (raw_abi_blob.size() != 0) {
-           auto abi_result = fc::raw::unpack<abi_def>(raw_abi_blob);
-           abis.emplace(abi_result, abi_serializer_max_time);
-       } else {
+      const auto raw_abi_result = call(get_raw_abi_func, fc::mutable_variant_object("account_name", account));
+      const auto raw_abi_blob = raw_abi_result["abi"].as_blob().data;
+
+      fc::optional<abi_serializer> abis;
+      if (raw_abi_blob.size() != 0) {
+         abis.emplace(fc::raw::unpack<abi_def>(raw_abi_blob), abi_serializer_max_time);
+      } else {
          std::cerr << "ABI for contract " << account.to_string() << " not found. Action data will be shown in hex only." << std::endl;
       }
       abi_cache.emplace( account, abis );
@@ -2673,12 +2673,10 @@ int main( int argc, char** argv ) {
    getAbi->add_option("name", accountName, localized("The name of the account whose abi should be retrieved"))->required();
    getAbi->add_option("-f,--file",filename, localized("The name of the file to save the contract .abi to instead of writing to console") );
    getAbi->set_callback([&] {
-      auto raw_abi_result = call(get_raw_abi_func, fc::mutable_variant_object("account_name", accountName));
-      auto raw_abi_blob = raw_abi_result["abi"].as_blob().data;
+      const auto raw_abi_result = call(get_raw_abi_func, fc::mutable_variant_object("account_name", accountName));
+      const auto raw_abi_blob = raw_abi_result["abi"].as_blob().data;
       if (raw_abi_blob.size() != 0) {
-          auto abi_result = fc::raw::unpack<abi_def>(raw_abi_result["abi"].as_blob().data);
-          std::cout << "789\n";
-          auto abi = fc::json::to_pretty_string(abi_result);
+          const auto abi = fc::json::to_pretty_string(fc::raw::unpack<abi_def>(raw_abi_blob));
           if (filename.size()) {
               std::cerr << localized("saving abi to ${filename}", ("filename", filename)) << std::endl;
               std::ofstream abiout(filename.c_str());
@@ -2686,8 +2684,7 @@ int main( int argc, char** argv ) {
           } else {
               std::cout << abi << "\n";
           }
-      } else
-      {
+      } else {
         FC_THROW_EXCEPTION(key_not_found_exception, "Key ${key}", ("key", "abi"));
       }
    });
