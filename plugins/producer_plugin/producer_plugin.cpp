ba05b42f2afb5f1cd0dbb2af818574720bed0f01
Kevin Heifner <heifnerk@objectcomputing.com>
2020-02-21 09:25:50 -0600

    Remove block time from late block log message since we don't know what timestamp a pending block should have

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -339,9 +339,8 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
       bool on_incoming_block(const signed_block_ptr& block, const std::optional<block_id_type>& block_id) {
          auto& chain = chain_plug->chain();
          if ( _pending_block_mode == pending_block_mode::producing ) {
-            fc_wlog( _log, "dropped incoming block #${num} while producing #${pbn} for ${bt}, id: ${id}",
+            fc_wlog( _log, "dropped incoming block #${num} while producing #${pbn}, id: ${id}",
                      ("num", block->block_num())("pbn", chain.head_block_num() + 1)
-                     ("bt", chain.head_block_time() + fc::milliseconds(config::block_interval_ms))
                      ("id", block_id ? (*block_id).str() : "UNKNOWN") );
             return false;
          }
