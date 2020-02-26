615733a726387508b281ef2413fb106b9aa60e35
Kevin Heifner <heifnerk@objectcomputing.com>
2020-02-26 18:44:15 -0500

    Add back in bcast of block during api-read-only because it is a bit strange mode and difficult to document

--- plugins/net_plugin/net_plugin.cpp
+++ plugins/net_plugin/net_plugin.cpp
@@ -1922,7 +1922,7 @@ namespace eosio {
    void dispatch_manager::bcast_block(const block_state_ptr& bs) {
       fc_dlog( logger, "bcast block ${b}", ("b", bs->block_num) );
 
-      if( my_impl->sync_master->syncing_with_peer() || my_impl->db_read_mode == db_read_mode::API_READ_ONLY ) return;
+      if( my_impl->sync_master->syncing_with_peer() ) return;
       
       bool have_connection = false;
       for_each_block_connection( [&have_connection]( auto& cp ) {
