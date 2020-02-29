c172fcfdf6100d7afdf455760b84d32897e1d1d5
Bart Wyatt <bart.wyatt@block.one>
2020-02-29 15:03:50 -0500

    strawman API and test rigging for logfile subsystem

--- plugins/trace_api_plugin/test/include/eosio/trace_api_plugin/test_common.hpp
+++ plugins/trace_api_plugin/test/include/eosio/trace_api_plugin/test_common.hpp
@@ -1,19 +1,109 @@
 #pragma once
 
-namespace eosio::trace_api_plugin::test_common {
+#include <fc/io/json.hpp>
+
+#include <eosio/chain/asset.hpp>
+#include <eosio/chain/name.hpp>
+
+#include <eosio/trace_api_plugin/data_log.hpp>
+#include <eosio/trace_api_plugin/metadata_log.hpp>
+
+namespace eosio::trace_api_plugin {
    /**
     * Utilities that make writing tests easier
     */
 
-   fc::sha256 operator"" _h(const char* input, std::size_t) {
-      return fc::sha256(input);
+   namespace test_common {
+      fc::sha256 operator"" _h(const char* input, std::size_t) {
+         return fc::sha256(input);
+      }
+
+      chain::name operator"" _n(const char* input, std::size_t) {
+         return chain::name(input);
+      }
+
+      chain::asset operator"" _t(const char* input, std::size_t) {
+         return chain::asset::from_string(input);
+      }
    }
 
-   chain::name operator"" _n(const char* input, std::size_t) {
-      return chain::name(input);
+   // TODO: promote these to the main files?
+   // I prefer not to have these operators but they are convenient for BOOST TEST integration
+   //
+
+   bool operator==(const authorization_trace_v0& lhs, const authorization_trace_v0& rhs) {
+      return
+         lhs.account == rhs.account &&
+         lhs.permission == rhs.permission;
    }
 
-   chain::asset operator"" _t(const char* input, std::size_t) {
-      return chain::asset::from_string(input);
+   bool operator==(const action_trace_v0& lhs, const action_trace_v0& rhs) {
+      return
+         lhs.global_sequence == rhs.global_sequence &&
+         lhs.receiver == rhs.receiver &&
+         lhs.account == rhs.account &&
+         lhs.action == rhs.action &&
+         lhs.authorization == rhs.authorization &&
+         lhs.data == rhs.data;
+   }
+
+   bool operator==(const transaction_trace_v0& lhs,  const transaction_trace_v0& rhs) {
+      return
+         lhs.id == rhs.id &&
+         lhs.status == rhs.status &&
+         lhs.actions == rhs.actions;
+   }
+
+   bool operator==(const block_trace_v0 &lhs, const block_trace_v0 &rhs) {
+      return
+         lhs.id == rhs.id &&
+         lhs.number == rhs.number &&
+         lhs.previous_id == rhs.previous_id &&
+         lhs.timestamp == rhs.timestamp &&
+         lhs.producer == rhs.producer &&
+         lhs.transactions == rhs.transactions;
+   }
+
+   std::ostream& operator<<(std::ostream &os, const block_trace_v0 &bt) {
+      os << fc::json::to_string( bt, fc::time_point::maximum() );
+      return os;
+   }
+
+   bool operator==(const block_entry_v0& lhs, const block_entry_v0& rhs) {
+      return
+         lhs.id == rhs.id &&
+         lhs.number == rhs.number &&
+         lhs.offset == rhs.offset;
+   }
+
+   bool operator!=(const block_entry_v0& lhs, const block_entry_v0& rhs) {
+      return !(lhs == rhs);
+   }
+
+   bool operator==(const lib_entry_v0& lhs, const lib_entry_v0& rhs) {
+      return
+         lhs.lib == rhs.lib;
+   }
+
+   bool operator!=(const lib_entry_v0& lhs, const lib_entry_v0& rhs) {
+      return !(lhs == rhs);
+   }
+
+   std::ostream& operator<<(std::ostream& os, const block_entry_v0& be) {
+      os << fc::json::to_string(be, fc::time_point::maximum());
+      return os;
+   }
+
+   std::ostream& operator<<(std::ostream& os, const lib_entry_v0& le) {
+      os << fc::json::to_string(le, fc::time_point::maximum());
+      return os;
+   }
+}
+
+namespace fc {
+   template<typename ...Ts>
+   std::ostream& operator<<(std::ostream &os, const fc::static_variant<Ts...>& v ) {
+      os << fc::json::to_string(v, fc::time_point::maximum());
+      return os;
    }
 }
