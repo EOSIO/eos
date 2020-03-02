8288f5b2ce7da3f981806b4947608a0f202ebc83
Bart Wyatt <bart.wyatt@block.one>
2020-03-02 17:06:51 -0500

    extend tests to support mock corruption of metadata log, add test for corrupt metadata in scan

--- plugins/trace_api_plugin/test/test_responses.cpp
+++ plugins/trace_api_plugin/test/test_responses.cpp
@@ -100,8 +100,14 @@ struct response_test_fixture {
          uint64_t cur = offset;
          for (; cur < fixture.metadata_log.size(); cur++) {
             const auto& entry = fixture.metadata_log.at(cur);
-            if (! fn(entry) ) {
-               break;
+            if (entry.contains<std::exception_ptr>()) {
+               std::rethrow_exception(entry.get<std::exception_ptr>());
+            }
+
+            if (entry.contains<metadata_log_entry>()) {
+               if (!fn(entry)) {
+                  break;
+               }
             }
          }
 
@@ -126,7 +132,7 @@ struct response_test_fixture {
    }
 
    // fixture data and methods
-   std::vector<metadata_log_entry> metadata_log = {};
+   std::vector<fc::static_variant<std::exception_ptr, metadata_log_entry>> metadata_log = {};
    std::map<uint64_t, fc::static_variant<std::exception_ptr, data_log_entry>> data_log = {};
 
    response_impl_type<mock_logfile_provider> response_impl;
@@ -137,7 +143,7 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
    BOOST_FIXTURE_TEST_CASE(basic_empty_block_response, response_test_fixture)
    {
       metadata_log = decltype(metadata_log){
-         block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 }
+         metadata_log_entry{ block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } }
       };
 
       data_log = decltype(data_log) {
@@ -172,7 +178,7 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
    BOOST_FIXTURE_TEST_CASE(basic_block_response, response_test_fixture)
    {
       metadata_log = decltype(metadata_log){
-         block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 }
+         metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } }
       };
 
       data_log = decltype(data_log) {
@@ -236,7 +242,7 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
    BOOST_FIXTURE_TEST_CASE(lib_edge_response, response_test_fixture)
    {
       metadata_log = decltype(metadata_log){
-         block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 }
+         metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } }
       };
 
       data_log = decltype(data_log) {
@@ -277,7 +283,7 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
       BOOST_TEST(to_kv(expected_pending_response) == to_kv(pending_response), boost::test_tools::per_element());
 
       // push an entry to the metadata log that marks that block as LIB
-      metadata_log.emplace_back(lib_entry_v0{ 1 });
+      metadata_log.emplace_back( metadata_log_entry { lib_entry_v0{ 1 } } );
 
       fc::variant irreversible_response = get_block_trace( 1 );
       BOOST_TEST(to_kv(expected_irreversible_response) == to_kv(irreversible_response), boost::test_tools::per_element());
@@ -287,7 +293,7 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
    BOOST_FIXTURE_TEST_CASE(better_block_edge_response, response_test_fixture)
    {
       metadata_log = decltype(metadata_log){
-         block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 }
+         metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } }
       };
 
       data_log = decltype(data_log) {
@@ -341,23 +347,33 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
       };
 
       // push an entry to the metadata log that marks that block as LIB
-      metadata_log.emplace_back(block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000002"_h, 1, updated_block_offset });
+      metadata_log.emplace_back(metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000002"_h, 1, updated_block_offset } });
 
       fc::variant irreversible_response = get_block_trace( 1 );
       BOOST_TEST(to_kv(expected_updated_response) == to_kv(irreversible_response), boost::test_tools::per_element());
    }
 
-   BOOST_FIXTURE_TEST_CASE(corrupt_block_response, response_test_fixture)
+   BOOST_FIXTURE_TEST_CASE(corrupt_block_data, response_test_fixture)
    {
       metadata_log = decltype(metadata_log){
-         block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 }
+         metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } }
       };
 
       data_log = decltype(data_log) {
          {
             0,
-            std::make_exception_ptr(std::runtime_error("DIE"))
+            std::make_exception_ptr(std::runtime_error("BAD DATA"))
          }
+      };
+
+      BOOST_REQUIRE_THROW(get_block_trace( 1 ), bad_data_exception);
+   }
+
+   BOOST_FIXTURE_TEST_CASE(corrupt_block_metadata, response_test_fixture)
+   {
+      metadata_log = decltype(metadata_log){
+         metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } },
+         std::make_exception_ptr(std::runtime_error("BAD METADATA"))
       };
 
       BOOST_REQUIRE_THROW(get_block_trace( 1 ), bad_data_exception);
@@ -366,7 +382,7 @@ BOOST_AUTO_TEST_SUITE(trace_responses)
    BOOST_FIXTURE_TEST_CASE(missing_block_data, response_test_fixture)
    {
       metadata_log = decltype(metadata_log){
-         block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 }
+         metadata_log_entry { block_entry_v0 { "b000000000000000000000000000000000000000000000000000000000000001"_h, 1, 0 } }
       };
 
       fc::variant null_response = get_block_trace( 1 );
