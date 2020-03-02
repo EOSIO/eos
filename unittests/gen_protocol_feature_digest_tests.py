63f71877f36101edc2cff95e419bd556bcf45458
Steven Watanabe <steven.watanabe@block.one>
2020-03-02 10:25:44 -0500

    Should use python 3.

--- unittests/gen_protocol_feature_digest_tests.py
+++ unittests/gen_protocol_feature_digest_tests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import re
 import hashlib
@@ -19,7 +19,7 @@ def main():
         print('BOOST_AUTO_TEST_CASE(protocol_feature_digest_tests) {')
         print('   std::map<std::string, std::string> digests;')
         for match in re.finditer(pattern, contents):
-            print('   digests.emplace("%s", "%s");' % (match.group(2),  hashlib.sha256(match.group(1)).hexdigest()))
+            print('   digests.emplace("%s", "%s");' % (match.group(2),  hashlib.sha256(match.group(1).encode('utf8')).hexdigest()))
         print('   for(const auto& [id, spec] : builtin_protocol_feature_codenames) {')
         print('      BOOST_TEST(digests[spec.codename] == fc::variant(spec.description_digest).as<std::string>());')
         print('   }')
