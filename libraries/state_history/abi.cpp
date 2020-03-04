137b8f56b2a819d9a374a28f6afc1384f181e8c5
Todd Fleming <todd.fleming@block.one>
2020-03-04 16:34:05 -0500

    removed action_receipt_v1

--- libraries/state_history/abi.cpp
+++ libraries/state_history/abi.cpp
@@ -526,7 +526,7 @@ extern const char* const state_history_plugin_abi = R"({
         { "name": "request", "types": ["get_status_request_v0", "get_blocks_request_v0", "get_blocks_ack_request_v0"] },
         { "name": "result", "types": ["get_status_result_v0", "get_blocks_result_v0"] },
 
-        { "name": "action_receipt", "types": ["action_receipt_v0", "action_receipt_v1"] },
+        { "name": "action_receipt", "types": ["action_receipt_v0"] },
         { "name": "action_trace", "types": ["action_trace_v0", "action_trace_v1"] },
         { "name": "partial_transaction", "types": ["partial_transaction_v0"] },
         { "name": "transaction_trace", "types": ["transaction_trace_v0"] },
