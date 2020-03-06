fb5256f1b882c4d9bb9c61d829d8eb170316aa67
Brian Johnson <johnsonb@objectcomputing.com>
2020-03-06 13:11:44 -0600

    Changed find_* methods to return true for finding index and only find_create_* methods to allow file to be created.

--- plugins/trace_api_plugin/store_provider.cpp
+++ plugins/trace_api_plugin/store_provider.cpp
@@ -20,7 +20,7 @@ namespace eosio::trace_api_plugin {
    void store_provider::append(const block_trace_v0& bt) {
       fc::cfile trace;
       fc::cfile index;
-      find_slice_pair(bt.number, true, trace, index);
+      find_or_create_slice_pair(bt.number, true, trace, index);
       const uint64_t offset = append_store(bt, trace);
       auto be = metadata_log_entry { block_entry_v0 { .id = bt.id, .number = bt.number, .offset = offset }};
       append_store(be, index);
@@ -29,15 +29,15 @@ namespace eosio::trace_api_plugin {
    void store_provider::append_lib(uint32_t lib) {
       fc::cfile index;
       const uint32_t slice_number = _slice_provider.slice_number(lib);
-      _slice_provider.find_index_slice(slice_number, true, index);
+      _slice_provider.find_or_create_index_slice(slice_number, true, index);
       auto le = metadata_log_entry { lib_entry_v0 { .lib = lib }};
       append_store(le, index);
    }
 
-   void store_provider::find_slice_pair(uint32_t block_height, bool append, fc::cfile& trace, fc::cfile& index) {
+   void store_provider::find_or_create_slice_pair(uint32_t block_height, bool append, fc::cfile& trace, fc::cfile& index) {
       const uint32_t slice_number = _slice_provider.slice_number(block_height);
-      const bool trace_created = _slice_provider.find_trace_slice(slice_number, append, trace);
-      const bool index_created = _slice_provider.find_index_slice(slice_number, append, index);
+      const bool trace_created = _slice_provider.find_or_create_trace_slice(slice_number, append, trace);
+      const bool index_created = _slice_provider.find_or_create_index_slice(slice_number, append, index);
       if (trace_created != index_created) {
          const std::string trace_status = trace_created ? "new" : "existing";
          const std::string index_status = index_created ? "new" : "existing";
@@ -51,31 +51,50 @@ namespace eosio::trace_api_plugin {
          throw path_does_not_exist("The provided path does not exist: " + _slice_dir.generic_string());
    }
 
-   bool slice_provider::find_index_slice(uint32_t slice_number, bool append, fc::cfile& slice_file) const {
-      const bool new_slice = find_slice(_trace_index_prefix, slice_number, slice_file);
-      if( new_slice ) {
-         index_header h { .version = _current_version };
-         store_provider::append_store(h, slice_file);
-      } else {
-         const auto header = store_provider::extract_store<index_header>(slice_file);
-         if (header.version != _current_version) {
-            throw old_slice_version("Old slice file with version: " + boost::lexical_cast<std::string>(header.version) +
-               " is in directory, only supporting version: " + boost::lexical_cast<std::string>(_current_version));
-         }
-         if( append ) {
-            slice_file.seek_end(0);
-         }
+   bool slice_provider::find_or_create_index_slice(uint32_t slice_number, bool append, fc::cfile& index_file) const {
+      const bool found = find_index_slice(slice_number, append, index_file);
+      if( !found ) {
+         create_new_index_slice_file(index_file);
       }
-      return new_slice;
+      return found;
    }
 
-   bool slice_provider::find_trace_slice(uint32_t slice_number, bool append, fc::cfile& slice_file) const {
-      const bool new_slice = find_slice(_trace_prefix, slice_number, slice_file);
-
-      if( append ) {
-         slice_file.seek_end(0);
+   bool slice_provider::find_index_slice(uint32_t slice_number, bool append, fc::cfile& index_file) const {
+      if( !find_slice(_trace_index_prefix, slice_number, index_file) ) {
+         return false;
       }
-      return new_slice;
+
+      validate_existing_index_slice_file(index_file, append);
+      return true;
+   }
+
+   void slice_provider::create_new_index_slice_file(fc::cfile& index_file) const {
+      index_file.open(fc::cfile::create_or_update_rw_mode);
+      index_header h { .version = _current_version };
+      store_provider::append_store(h, index_file);
+   }
+
+   void slice_provider::validate_existing_index_slice_file(fc::cfile& index_file, bool append) const {
+      const auto header = store_provider::extract_store<index_header>(index_file);
+      if (header.version != _current_version) {
+         throw old_slice_version("Old slice file with version: " + std::to_string(header.version) +
+                                 " is in directory, only supporting version: " + std::to_string(_current_version));
+      }
+      if( append ) {
+         index_file.seek_end(0);
+      }
+   }
+
+   bool slice_provider::find_or_create_trace_slice(uint32_t slice_number, bool append, fc::cfile& trace_file) const {
+      const bool found = find_slice(_trace_prefix, slice_number, trace_file);
+
+      if( !found ) {
+         trace_file.open(fc::cfile::create_or_update_rw_mode);
+      }
+      else if( append ) {
+         trace_file.seek_end(0);
+      }
+      return found;
    }
 
    bool slice_provider::find_slice(const char* slice_prefix, uint32_t slice_number, fc::cfile& slice_file) const {
@@ -92,8 +111,11 @@ namespace eosio::trace_api_plugin {
       }
       const path slice_path = _slice_dir / filename;
       slice_file.set_file_path(slice_path);
-      const bool file_exists = exists(slice_path);
+      if( !exists(slice_path)) {
+         return false;
+      }
+
       slice_file.open(fc::cfile::create_or_update_rw_mode);
-      return !file_exists;
+      return true;
    }
 }
