42cde0f3bbe10dff60b15b0408308fe2f3ebc057
Kevin Heifner <heifnerk@objectcomputing.com>
2020-03-19 18:43:33 -0500

    Remove replay_opts_disabled_by_policy parameter from controller::light_validation_allowed and use conf.force_all_checks directly which also matches existing comment.

--- libraries/chain/include/eosio/chain/controller.hpp
+++ libraries/chain/include/eosio/chain/controller.hpp
@@ -273,11 +273,11 @@ namespace eosio { namespace chain {
 
          int64_t set_proposed_producers( vector<producer_authority> producers );
 
-         bool light_validation_allowed(bool replay_opts_disabled_by_policy) const;
+         bool light_validation_allowed() const;
          bool skip_auth_check()const;
-         bool skip_db_sessions( )const;
-         bool skip_db_sessions( block_status bs )const;
          bool skip_trx_checks()const;
+         bool skip_db_sessions()const;
+         bool skip_db_sessions( block_status bs )const;
          bool is_trusted_producer( const account_name& producer) const;
 
          bool contracts_console()const;
