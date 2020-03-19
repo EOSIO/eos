372c20ac46e8498323e162a07495290f56431acd
Bart Wyatt <bart.wyatt@block.one>
2020-03-19 10:30:22 -0400

    add comments and throw better errors from compressed_file; added tests for prime-sized data that better excercises seeks in the middle of readable chunks
    (cherry picked from commit 704e200defcc3c8a232c05918e0b7d4db42c5b17)

--- plugins/trace_api_plugin/test/test_compressed_file.cpp
+++ plugins/trace_api_plugin/test/test_compressed_file.cpp
@@ -42,43 +42,101 @@ struct temp_file_fixture {
    std::list<bfs::path> paths;
 };
 
+typedef std::tuple<uint64_t, std::array<char, 6733>> test_types;
+
+namespace {
+
+   template<typename T>
+   struct is_std_array {
+      static constexpr bool value = false;
+   };
+
+   template<typename T, size_t S>
+   struct is_std_array<std::array<T, S>> {
+      static constexpr bool value = true;
+   };
+
+   template<typename T>
+   constexpr bool is_std_array_v = is_std_array<T>::value;
+
+   template<typename T>
+   T convert_to(uint64_t value) {
+      if constexpr (is_std_array_v<T>) {
+         T result({0});
+         std::memcpy(result.data(), &value, std::min<size_t>(sizeof(value), result.size()));
+         return result;
+      } else {
+         return T(value);
+      }
+   }
+
+   template<typename T>
+   T make_random() {
+      if constexpr (is_std_array_v<T>) {
+         constexpr size_t input_byte_size = (std::tuple_size_v<T> * sizeof(typename T::value_type));
+         constexpr size_t temp_size = (input_byte_size / sizeof(uint32_t)) + 1;
+
+         std::array<uint32_t, temp_size> temp;
+         std::generate(temp.begin(), temp.end(), []() {
+            return (uint32_t)std::rand();
+         });
+
+         T result;
+         std::memcpy(result.data(), temp.data(), input_byte_size);
+         return result;
+      } else {
+         return (T)std::rand();
+      }
+   }
+}
+
+namespace std {
+   template<typename T, size_t S>
+   std::ostream& operator<<(std::ostream &os, const std::array<T,S> &array) {
+      os << fc::to_hex(reinterpret_cast<const char*>(array.data()), S * sizeof(T));
+      return os;
+   }
+}
+
 BOOST_AUTO_TEST_SUITE(compressed_file_tests)
-BOOST_FIXTURE_TEST_CASE(random_access_test, temp_file_fixture) {
+
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(random_access_test, T, test_types, temp_file_fixture) {
    // generate a large dataset where ever 8 bytes is the offset to that 8 bytes of data
-   auto data = std::vector<uint64_t>(1024);
+   auto data = std::vector<T>(128);
    std::generate(data.begin(), data.end(), [offset=0ULL]() mutable {
       auto result = offset;
-      offset+=sizeof(uint64_t);
-      return result;
+      offset+=sizeof(T);
+      return convert_to<T>(result);
    });
 
-   auto uncompressed_filename = create_temp_file(data.data(), data.size() * sizeof(uint64_t));
+   auto uncompressed_filename = create_temp_file(data.data(), data.size() * sizeof(T));
    auto compressed_filename = create_temp_file(nullptr, 0);
 
    BOOST_TEST(compressed_file::process(uncompressed_filename, compressed_filename, 32));
 
    // test that you can read all of the offsets from the compressed form by opening and seeking to them
-   for( const auto& entry : data ) {
+   for (int i = 0; i < data.size(); i++) {
+      const auto& entry = data.at(i);
       auto compf = compressed_file(compressed_filename);
       compf.open();
-      compf.seek((long)entry);
-      uint64_t value;
-      compf.read(reinterpret_cast<char*>(&value), sizeof(value));
+      T value;
+      compf.seek((long)i * sizeof(T));
+      compf.read(reinterpret_cast<char*>(&value), sizeof(T));
       BOOST_TEST(value == entry);
       compf.close();
    }
 }
 
-BOOST_FIXTURE_TEST_CASE(sequential_access, temp_file_fixture) {
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(sequential_access, T, test_types, temp_file_fixture) {
    // generate a large dataset where ever 8 bytes is the offset to that 8 bytes of data
-   auto data = std::vector<uint64_t>(1024);
+   auto data = std::vector<T>(128);
    std::generate(data.begin(), data.end(), [offset=0ULL]() mutable {
       auto result = offset;
-      offset+=sizeof(uint64_t);
-      return result;
+      offset+=sizeof(T);
+      return convert_to<T>(result);
    });
 
-   auto uncompressed_filename = create_temp_file(data.data(), data.size() * sizeof(uint64_t));
+   auto uncompressed_filename = create_temp_file(data.data(), data.size() * sizeof(T));
    auto compressed_filename = create_temp_file(nullptr, 0);
 
    BOOST_TEST(compressed_file::process(uncompressed_filename, compressed_filename, 32));
@@ -87,33 +145,35 @@ BOOST_FIXTURE_TEST_CASE(sequential_access, temp_file_fixture) {
    auto compf = compressed_file(compressed_filename);
    compf.open();
    for( const auto& entry : data ) {
-      uint64_t value;
+      T value;
       compf.read(reinterpret_cast<char*>(&value), sizeof(value));
       BOOST_TEST(value == entry);
    }
    compf.close();
 }
 
-BOOST_FIXTURE_TEST_CASE(blob_access, temp_file_fixture) {
+BOOST_FIXTURE_TEST_CASE_TEMPLATE(blob_access, T, test_types, temp_file_fixture) {
    // generate a large dataset where ever 8 bytes is the offset to that 8 bytes of data
-   auto data = std::vector<uint64_t>(1024);
+   auto data = std::vector<T>(128);
    std::generate(data.begin(), data.end(), []() {
-      return (uint64_t)std::rand();
+      return make_random<T>();
    });
 
-   auto uncompressed_filename = create_temp_file(data.data(), data.size() * sizeof(uint64_t));
+   auto uncompressed_filename = create_temp_file(data.data(), data.size() * sizeof(T));
    auto compressed_filename = create_temp_file(nullptr, 0);
 
    BOOST_TEST(compressed_file::process(uncompressed_filename, compressed_filename, 32));
 
-   // test that you can read all of the offsets from the compressed form sequentially
-   auto actual_data = std::vector<uint64_t>(1024);
-   auto compf = compressed_file(compressed_filename);
-   compf.open();
-   compf.read(reinterpret_cast<char*>(actual_data.data()), actual_data.size() * sizeof(uint64_t));
-   BOOST_TEST(data == actual_data, boost::test_tools::per_element());
-   compf.close();
+   // test that you can read all of the offsets from the compressed form through the end of the file
+   for (int i = 0; i < data.size(); i++) {
+      auto actual_data = std::vector<T>(128);
+      auto compf = compressed_file(compressed_filename);
+      compf.open();
+      compf.seek(i * sizeof(T));
+      compf.read(reinterpret_cast<char*>(actual_data.data()), (actual_data.size() - i) * sizeof(T));
+      compf.close();
+      BOOST_REQUIRE_EQUAL_COLLECTIONS(data.begin() + i, data.end(), actual_data.begin(), actual_data.end() - i);
+   }
 }
-
 
 BOOST_AUTO_TEST_SUITE_END()
