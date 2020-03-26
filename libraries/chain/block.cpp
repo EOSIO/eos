717282e60cdebb090983f7155255739eca6c2070
Huang-Ming Huang <huangh@objectcomputing.com>
2020-03-26 08:14:48 -0500

    Remove unnecessary assertion

--- libraries/chain/block.cpp
+++ libraries/chain/block.cpp
@@ -122,9 +122,7 @@ namespace eosio { namespace chain {
       auto visitor = overloaded{
           [](const transaction_id_type &id) -> transaction_receipt::trx_type { return id; },
           [](const pruned_transaction &trx) -> transaction_receipt::trx_type {
-             auto& prunable_data = trx.get_prunable_data().prunable_data;
-             EOS_ASSERT(prunable_data.contains<prunable_transaction_data::full_legacy>(), tx_not_full_legacy, "pruned_transaction does not contain the full legacy data");
-             auto& legacy = prunable_data.get<prunable_transaction_data::full_legacy>();
+             const auto& legacy = trx.get_prunable_data().prunable_data.get<prunable_transaction_data::full_legacy>();
              return packed_transaction(trx.get_packed_transaction(), legacy.signatures, legacy.packed_context_free_data, trx.get_compression());
           }};
 
