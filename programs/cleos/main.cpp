1bf5984ebb4fd8ad9ef703d811ebc88ecfe8ad14
deck <deckb@@users.noreply.github.com>
2020-04-24 13:46:19 -0400

    fix help strings

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -1042,8 +1042,8 @@ struct create_account_subcommand {
       );
       createAccount->add_option("creator", creator, localized("The name of the account creating the new account"))->required();
       createAccount->add_option("name", account_name, localized("The name of the new account"))->required();
-      createAccount->add_option("OwnerKey", owner_key_str, localized("The owner public key or permission level for the new account"))->required();
-      createAccount->add_option("ActiveKey", active_key_str, localized("The active public key or permission level for the new account"));
+      createAccount->add_option("OwnerKey", owner_key_str, localized("The owner public key, permission level, or authority for the new account"))->required();
+      createAccount->add_option("ActiveKey", active_key_str, localized("The active public key, permission level, or authority for the new account"));
 
       if (!simple) {
          createAccount->add_option("--stake-net", stake_net,
