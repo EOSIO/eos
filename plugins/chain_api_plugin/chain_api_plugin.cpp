a0fe1698fbe9fa731119b844f2d2fbdea9915793
Kevin Heifner <heifnerk@objectcomputing.com>
2020-04-24 09:10:00 -0500

    Add better handling of param for async calls including send_transaction. Sends 400 on unable to parse input.

--- plugins/chain_api_plugin/chain_api_plugin.cpp
+++ plugins/chain_api_plugin/chain_api_plugin.cpp
@@ -79,18 +79,23 @@ namespace {
    [api_handle](string, string body, url_response_callback cb) mutable { \
       if (body.empty()) body = "{}"; \
       api_handle.validate(); \
-      api_handle.call_name(fc::json::from_string(body).as<api_namespace::call_name ## _params>(),\
-         [cb, body](const fc::static_variant<fc::exception_ptr, call_result>& result){\
-            if (result.contains<fc::exception_ptr>()) {\
-               try {\
-                  result.get<fc::exception_ptr>()->dynamic_rethrow_exception();\
-               } catch (...) {\
-                  http_plugin::handle_exception(#api_name, #call_name, body, cb);\
+      try { \
+         auto params = parse_params<api_namespace::call_name ## _params>(body);\
+         api_handle.call_name( std::move(params),\
+            [cb, body](const fc::static_variant<fc::exception_ptr, call_result>& result){\
+               if (result.contains<fc::exception_ptr>()) {\
+                  try {\
+                     result.get<fc::exception_ptr>()->dynamic_rethrow_exception();\
+                  } catch (...) {\
+                     http_plugin::handle_exception(#api_name, #call_name, body, cb);\
+                  }\
+               } else {\
+                  cb(http_response_code, result.visit(async_result_visitor()));\
                }\
-            } else {\
-               cb(http_response_code, result.visit(async_result_visitor()));\
-            }\
-         });\
+            });\
+      } catch (...) { \
+         http_plugin::handle_exception(#api_name, #call_name, body, cb); \
+      } \
    }\
 }
 
