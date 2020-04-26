0d97166576d0e68cbf8fa31b3badd4ff200e404c
Todd Fleming <todd.fleming@block.one>
2020-04-26 11:58:04 -0400

    rodeos

--- programs/rodeos/rocksdb_plugin.cpp
+++ programs/rodeos/rocksdb_plugin.cpp
@@ -1,5 +1,6 @@
 #include "rocksdb_plugin.hpp"
 
+#include <boost/filesystem.hpp>
 #include <fc/exception/exception.hpp>
 
 namespace b1 {
@@ -23,7 +24,8 @@ rocksdb_plugin::~rocksdb_plugin() {}
 
 void rocksdb_plugin::set_program_options(options_description& cli, options_description& cfg) {
    auto op = cfg.add_options();
-   op("rdb-database", bpo::value<std::string>()->default_value("./rodeos.rocksdb"), "Primary database path");
+   op("rdb-database", bpo::value<bfs::path>()->default_value("rodeos.rocksdb"),
+      "Database path (absolute path or relative to application data dir)");
    op("rdb-threads", bpo::value<uint32_t>(),
       "Increase number of background RocksDB threads. Only used with cloner_plugin. Recommend 8 for full history "
       "on large chains.");
@@ -34,7 +36,11 @@ void rocksdb_plugin::set_program_options(options_description& cli, options_descr
 
 void rocksdb_plugin::plugin_initialize(const variables_map& options) {
    try {
-      my->db_path = options["rdb-database"].as<std::string>();
+      auto db_path = options.at("rdb-database").as<bfs::path>();
+      if (db_path.is_relative())
+         my->db_path = app().data_dir() / db_path;
+      else
+         my->db_path = db_path;
       if (!options["rdb-threads"].empty())
          my->threads = options["rdb-threads"].as<uint32_t>();
       if (!options["rdb-max-files"].empty())
@@ -49,8 +55,12 @@ void rocksdb_plugin::plugin_shutdown() {}
 
 std::shared_ptr<chain_kv::database> rocksdb_plugin::get_db() {
    std::lock_guard<std::mutex> lock(my->mutex);
-   if (!my->database)
+   if (!my->database) {
+      ilog("rodeos database is ${d}", ("d", my->db_path.string()));
+      if (!bfs::exists(my->db_path.parent_path()))
+         bfs::create_directories(my->db_path.parent_path());
       my->database = std::make_shared<chain_kv::database>(my->db_path.c_str(), true, my->threads, my->max_open_files);
+   }
    return my->database;
 }
 
