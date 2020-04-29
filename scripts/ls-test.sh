5b84d7eae91caaf957ca8dbfc648df50cc40c0b7
Brian Johnson <johnsonb@objectcomputing.com>
2020-04-29 16:57:25 -0500

    Don't need to tie up 4 threads, only one is used.

--- scripts/ls-test.sh
+++ scripts/ls-test.sh
@@ -22,8 +22,8 @@ else
 fi
 # prep tests
 echo '{ "initial_timestamp": "2018-06-01T12:00:00.000", "initial_key": "EOS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV", "initial_configuration": { "max_block_net_usage": 1048576, "target_block_net_usage_pct": 1000, "max_transaction_net_usage": 524288, "base_per_transaction_net_usage": 12, "net_usage_leeway": 500, "context_free_discount_net_usage_num": 20, "context_free_discount_net_usage_den": 100, "max_block_cpu_usage": 200000, "target_block_cpu_usage_pct": 1000, "max_transaction_cpu_usage": 150000, "min_transaction_cpu_usage": 100, "max_transaction_lifetime": 3600, "deferred_trx_expiration_window": 600, "max_transaction_delay": 3888000, "max_inline_action_size": 4096, "max_inline_action_depth": 4, "max_authority_depth": 6 } }' > genesis.json
-echo "starting launcher service: ./programs/launcher-service/launcher-service --http-server-address=0.0.0.0:1234 --http-threads=4 --genesis-json=./genesis.json > launcher_service.log 2>&1 &"
-./programs/launcher-service/launcher-service --http-server-address=0.0.0.0:1234 --http-threads=4 --genesis-json=./genesis.json > launcher_service.log 2>&1 &
+echo "starting launcher service: ./programs/launcher-service/launcher-service --http-server-address=0.0.0.0:1234 --genesis-json=./genesis.json > launcher_service.log 2>&1 &"
+./programs/launcher-service/launcher-service --http-server-address=0.0.0.0:1234 --genesis-json=./genesis.json > launcher_service.log 2>&1 &
 sleep 1
 echo "platform ID is $ID"
 echo "IMAGE_TAG is $IMAGE_TAG"
