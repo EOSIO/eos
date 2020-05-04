3ed2147ccbfadf42204fb698d69a03e4935ce81a
Victor Camacho <victor.camacho@block.one>
2020-05-04 00:07:56 -0400

    modify first test to check for some conditions related to permissions

--- unittests/state_history_tests.cpp
+++ unittests/state_history_tests.cpp
@@ -2,6 +2,7 @@
 
 #include <boost/test/unit_test.hpp>
 #include <eosio/testing/tester.hpp>
+#include <eosio/chain/authorization_manager.hpp>
 
 using namespace eosio;
 using namespace testing;
@@ -9,46 +10,65 @@ using boost::signals2::scoped_connection;
 
 BOOST_AUTO_TEST_SUITE(state_history_plugin_tests)
 
-BOOST_AUTO_TEST_CASE(test_deltas) {
+BOOST_AUTO_TEST_CASE(test_deltas_account) {
    tester main;
-
-   auto v = eosio::state_history::create_deltas(main.control->db(), false);
-
-   std::string name="permission";
+   std::string name="account";
    auto find_by_name = [&name](const auto& x) {
       return x.name == name;
    };
 
-   auto it = std::find_if(v.begin(), v.end(), find_by_name);
-   BOOST_REQUIRE(it==v.end());
+   auto v = eosio::state_history::create_deltas(main.control->db(), false);
 
-   name="resource_limits";
-   it = std::find_if(v.begin(), v.end(), find_by_name);
-   BOOST_REQUIRE(it==v.end());
+   auto it_account = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_account!=v.end());
+   BOOST_REQUIRE(it_account->rows.obj.size()==1);
+
+   name="permission";
+   auto it_permission = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_permission==v.end());
 
    main.create_account(N(newacc));
 
    v = eosio::state_history::create_deltas(main.control->db(), false);
 
+   name="account";
+   it_account = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_account!=v.end());
+   BOOST_REQUIRE(it_account->rows.obj.size()==2);
+
    name="permission";
-   it = std::find_if(v.begin(), v.end(), find_by_name);
-   BOOST_REQUIRE(it!=v.end());
+   it_permission = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_permission!=v.end());
+   BOOST_REQUIRE(it_permission->rows.obj.size()==2);
 
-   name="resource_limits";
-   it = std::find_if(v.begin(), v.end(), find_by_name);
-   BOOST_REQUIRE(it!=v.end());
+   auto& authorization = main.control->get_mutable_authorization_manager();
+   const permission_object* ptr = authorization.find_permission({N(newacc), N(active)});
+   BOOST_REQUIRE(ptr!=nullptr);
 
-   main.produce_block();
+   authorization.create_permission(N(newacc), N(mypermission), ptr->id, ptr->auth, fc::time_point::now() );
+
+   permission_level pl2{N(newacc), N(mypermission)};
+   const permission_object* ptr_sub = authorization.find_permission(pl2);
+
+   BOOST_REQUIRE(ptr_sub!=nullptr);
 
    v = eosio::state_history::create_deltas(main.control->db(), false);
+   it_permission = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_permission!=v.end());
+   BOOST_REQUIRE(it_permission->rows.obj.size()==3);
 
-   name="permission";
-   it = std::find_if(v.begin(), v.end(), find_by_name);
-   BOOST_REQUIRE(it==v.end());
+   auto empty_authority = authority(1, {}, {});
+   authorization.modify_permission(*ptr_sub, empty_authority);
 
-   name="resource_limits";
-   it = std::find_if(v.begin(), v.end(), find_by_name);
-   BOOST_REQUIRE(it==v.end());
+   v = eosio::state_history::create_deltas(main.control->db(), false);
+   it_permission = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_permission!=v.end());
+   BOOST_REQUIRE(it_permission->rows.obj.size()==3);
+
+   authorization.remove_permission(*ptr_sub);
+   v = eosio::state_history::create_deltas(main.control->db(), false);
+   it_permission = std::find_if(v.begin(), v.end(), find_by_name);
+   BOOST_REQUIRE(it_permission->rows.obj.size()==2);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
