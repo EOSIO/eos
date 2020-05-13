e6f1dab72738dd176b01bbbb6ca59daee4560575
Alok Garg <alok.garg-ext@block.one>
2020-05-13 19:25:48 -0400

    Change comment

--- tests/ship_test.py
+++ tests/ship_test.py
@@ -208,8 +208,8 @@ finally:
                 # output file should have lots of output, but if user passes in a huge number of requests, these could go on forever
                 printTruncatedFile("%s%d.out" % (shipClientFilePrefix, i), maxLines=20000)
 
-        //if not keepLogs:
-        //   shutil.rmtree(shipTempDir, ignore_errors=True)
+        #if not keepLogs:
+        #   shutil.rmtree(shipTempDir, ignore_errors=True)
     if not testSuccessful and dumpErrorDetails:
         Print(Utils.FileDivider)
         Print("Compare Blocklog")
