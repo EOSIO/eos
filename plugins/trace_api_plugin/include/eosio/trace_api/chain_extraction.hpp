01357893ba21ced8470d195bbfbe7725576b4dda
Kevin Heifner <heifnerk@objectcomputing.com>
2020-05-22 11:34:47 -0500

    Remove unneded {}

--- plugins/trace_api_plugin/include/eosio/trace_api/chain_extraction.hpp
+++ plugins/trace_api_plugin/include/eosio/trace_api/chain_extraction.hpp
@@ -84,7 +84,7 @@ private:
          std::vector<transaction_trace_v1>& traces = bt.transactions_v1;
          traces.reserve( block_state->block->transactions.size() + 1 );
          if( onblock_trace )
-            traces.emplace_back( to_transaction_trace_v1( {*onblock_trace} ));
+            traces.emplace_back( to_transaction_trace_v1( *onblock_trace ));
          for( const auto& r : block_state->block->transactions ) {
             transaction_id_type id;
             if( r.trx.contains<transaction_id_type>()) {
