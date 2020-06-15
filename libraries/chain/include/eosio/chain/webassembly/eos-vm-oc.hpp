4dffae8b1fe88d45255fec0e124b7529704102a2
Steven Watanabe <steven.watanabe@block.one>
2020-06-15 10:34:26 -0400

    Unduplicate type_converter.

--- libraries/chain/include/eosio/chain/webassembly/eos-vm-oc.hpp
+++ libraries/chain/include/eosio/chain/webassembly/eos-vm-oc.hpp
@@ -186,82 +186,27 @@ struct wasm_function_type_provider<Ret(Args...)> {
 
 struct eos_vm_oc_execution_interface {
    inline const auto& operand_from_back(std::size_t index) const { return *(os - index - 1); }
+
+   template <typename T>
+   inline void* validate_pointer(vm::wasm_ptr_t ptr, vm::wasm_size_t len) const {
+      return array_ptr_impl<T>(ptr, len);
+   }
+
+   inline void* validate_null_terminated_pointer(vm::wasm_ptr_t ptr) const {
+      return null_terminated_ptr_impl(ptr);
+   }
+
    eosio::vm::native_value* os;
 };
 
-struct eos_vm_oc_type_converter : public eosio::vm::type_converter<webassembly::interface, eos_vm_oc_execution_interface> {
-   using base_type = eosio::vm::type_converter<webassembly::interface, eos_vm_oc_execution_interface>;
-   using base_type::type_converter;
+struct eos_vm_oc_type_converter : public basic_type_converter<eos_vm_oc_execution_interface> {
+   using base_type = basic_type_converter<eos_vm_oc_execution_interface>;
+   using base_type::basic_type_converter;
    using base_type::to_wasm;
-   using base_type::as_result;
-   using base_type::get_host;
 
-   EOS_VM_FROM_WASM(bool, (uint32_t value)) { return value ? 1 : 0; }
-
-   EOS_VM_FROM_WASM(memcpy_params, (vm::wasm_ptr_t dst, vm::wasm_ptr_t src, vm::wasm_size_t size)) {
-      auto d = array_ptr_impl<char>(dst, size);
-      auto s = array_ptr_impl<char>(src, size);
-      array_ptr_impl<char>(dst, 1);
-      return { d, s, size };
-   }
-
-   EOS_VM_FROM_WASM(memcmp_params, (vm::wasm_ptr_t lhs, vm::wasm_ptr_t rhs, vm::wasm_size_t size)) {
-     auto l = array_ptr_impl<char>(lhs, size);
-     auto r = array_ptr_impl<char>(rhs, size);
-     return { l, r, size };
-   }
-
-   EOS_VM_FROM_WASM(memset_params, (vm::wasm_ptr_t dst, int32_t val, vm::wasm_size_t size)) {
-     auto d = array_ptr_impl<char>(dst, size);
-     array_ptr_impl<char>(dst, 1);
-     return { d, val, size };
-   }
-
-   template <typename T>
-   auto from_wasm(vm::wasm_ptr_t ptr) const
-      -> std::enable_if_t< std::is_pointer_v<T>,
-                           vm::argument_proxy<T> > {
-      void* p = array_ptr_impl<std::remove_pointer_t<T>>(ptr, 1);
-      return {p};
-   }
-
-   template <typename T>
-   auto from_wasm(vm::wasm_ptr_t ptr, vm::wasm_size_t len, vm::tag<T> = {}) const
-      -> std::enable_if_t<vm::is_span_type_v<T>, T> {
-      void* p = array_ptr_impl<typename T::value_type>(ptr, len);
-      return {static_cast<typename T::pointer>(p), len};
-   }
-
-   template <typename T>
-   auto from_wasm(vm::wasm_ptr_t ptr, vm::wasm_size_t len, vm::tag<T> = {}) const
-      -> std::enable_if_t< vm::is_argument_proxy_type_v<T> &&
-                           vm::is_span_type_v<typename T::proxy_type>, T> {
-      void* p = array_ptr_impl<typename T::pointee_type>(ptr, len);
-      return {p, len};
-   }
-
-   template <typename T>
-   auto from_wasm(vm::wasm_ptr_t ptr, vm::tag<T> = {}) const
-      -> std::enable_if_t< vm::is_argument_proxy_type_v<T> &&
-                           std::is_pointer_v<typename T::proxy_type>, T> {
-      if constexpr(T::is_legacy()) {
-         EOS_ASSERT(ptr != 0, wasm_execution_error, "references cannot be created for null pointers");
-      }
-      void* p = array_ptr_impl<typename T::pointee_type>(ptr, 1);
-      return {p};
-   }
-
-   EOS_VM_FROM_WASM(null_terminated_ptr, (vm::wasm_ptr_t ptr)) {
-      auto p = null_terminated_ptr_impl(ptr);
-      return {static_cast<const char*>(p)};
-   }
-   EOS_VM_FROM_WASM(name, (uint64_t e)) { return name{e}; }
-   uint64_t to_wasm(name&& n) { return n.to_uint64_t(); }
    vm::wasm_ptr_t to_wasm(void*&& ptr) {
       return convert_native_to_wasm(static_cast<char*>(ptr));
    }
-   EOS_VM_FROM_WASM(float32_t, (float f)) { return ::to_softfloat32(f); }
-   EOS_VM_FROM_WASM(float64_t, (double f)) { return ::to_softfloat64(f); }
 
    template<typename T>
    inline decltype(auto) as_value(const vm::native_value& val) const {
