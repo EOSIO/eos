96dd7ef6dc4af2b08bca2208f0eda0441a189340
Matt Witherspoon <32485495+spoonincode@users.noreply.github.com>
2020-06-16 10:25:16 -0400

    remove sig_digest support

--- plugins/amqp_witness_plugin/amqp_witness_plugin.cpp
+++ plugins/amqp_witness_plugin/amqp_witness_plugin.cpp
@@ -7,51 +7,17 @@ namespace eosio {
 static appbase::abstract_plugin& _amqp_witness_plugin = app().register_plugin<amqp_witness_plugin>();
 
 struct amqp_witness_plugin_impl {
-   enum class witness_signature_over {
-      action_mroot,
-      sig_digest
-   };
-
    signature_provider_plugin::signature_provider_type signature_provider;
    unsigned staleness_limit = 600;
    std::string amqp_server = "amqp://";
    std::string exchange = "witness_signatures";
    std::optional<std::string> routing_key;
-   witness_signature_over witness_signature_type = witness_signature_over::action_mroot;
    bool delete_previous = false;
 
    std::unique_ptr<reliable_amqp_publisher> rqueue;
-
-   struct amqp_witness_msg {
-      bool is_sig_digest = false;
-      chain::digest_type digest;
-      chain::signature_type sig;
-   };
 };
 
-std::ostream& operator<<(std::ostream& os, const amqp_witness_plugin_impl::witness_signature_over wso) {
-   if(wso == amqp_witness_plugin_impl::witness_signature_over::action_mroot)
-      os << "action_mroot";
-   else
-      os << "sig_digest";
-   return os;
-}
-
-std::istream& operator>>(std::istream& is, amqp_witness_plugin_impl::witness_signature_over& wso) {
-   std::string s;
-   is >> s;
-   if (s == "action_mroot")
-      wso = amqp_witness_plugin_impl::witness_signature_over::action_mroot;
-   else if (s == "sig_digest")
-      wso = amqp_witness_plugin_impl::witness_signature_over::sig_digest;
-   else
-      is.setstate(std::ios_base::failbit);
-   return is;
-}
-
-amqp_witness_plugin::amqp_witness_plugin() : my(new amqp_witness_plugin_impl()) {
-   app().register_config_type<amqp_witness_plugin_impl::witness_signature_over>();
-}
+amqp_witness_plugin::amqp_witness_plugin() : my(new amqp_witness_plugin_impl()) {}
 
 void amqp_witness_plugin::set_program_options(options_description& cli, options_description& cfg) {
    auto default_priv_key = eosio::chain::private_key_type::regenerate<fc::ecc::private_key_shim>(fc::sha256::hash(std::string("witness")));
@@ -72,15 +38,6 @@ void amqp_witness_plugin::set_program_options(options_description& cli, options_
          ("witness-amqp-routing-key", boost::program_options::value<string>()->notifier([this](const string& v) {
                   my->routing_key = v;
                }), "AMQP routing key to set on published messages")
-         ("witness-signature-type", boost::program_options::value<amqp_witness_plugin_impl::witness_signature_over>()
-                                    ->default_value(my->witness_signature_type)->value_name("action_mroot/block_id")
-                                    ->notifier([this](const amqp_witness_plugin_impl::witness_signature_over& v) {
-                  my->witness_signature_type = v;
-               }), "What the witness signature should sign:\n"
-                   "\"action_mroot\": signs the action_mroot of a block\n"
-                   "\"block_id\": signs the block id of a block\n"
-                   "Appropriate value is application specific. e.g. a light validator may need block_id, but simply validating actions in blocks only needs action_mroot."
-               )
          ;
 
    cli.add_options()
@@ -109,13 +66,7 @@ void amqp_witness_plugin::plugin_startup() {
 
       my->rqueue->post_on_io_context([this, bsp]() {
          try {
-            amqp_witness_plugin_impl::amqp_witness_msg msg = {
-               .is_sig_digest = my->witness_signature_type == amqp_witness_plugin_impl::witness_signature_over::sig_digest,
-               .digest = msg.is_sig_digest ? bsp->sig_digest() : bsp->header.action_mroot,
-               .sig = my->signature_provider(msg.digest)
-            };
-
-            my->rqueue->publish_message(msg);
+            my->rqueue->publish_message(std::make_pair(bsp->header.action_mroot, my->signature_provider(bsp->header.action_mroot)));
          }
          FC_LOG_AND_DROP();
       });
@@ -125,5 +76,3 @@ void amqp_witness_plugin::plugin_startup() {
 void amqp_witness_plugin::plugin_shutdown() {}
 
 }
-
-FC_REFLECT(eosio::amqp_witness_plugin_impl::amqp_witness_msg, (is_sig_digest)(digest)(sig));
