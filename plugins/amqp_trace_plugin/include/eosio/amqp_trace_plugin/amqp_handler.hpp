cd28210e3259472a937125c89fae1c0560e1e9e3
Kevin Heifner <heifnerk@objectcomputing.com>
2020-06-18 09:55:06 -0500

    Call declare queue on amqp thread

--- plugins/amqp_trace_plugin/include/eosio/amqp_trace_plugin/amqp_handler.hpp
+++ plugins/amqp_trace_plugin/include/eosio/amqp_trace_plugin/amqp_handler.hpp
@@ -7,34 +7,22 @@
 
 namespace eosio {
 
-class amqp_handler : public AMQP::LibBoostAsioHandler {
-public:
-   explicit amqp_handler( boost::asio::io_service& io_service ) : AMQP::LibBoostAsioHandler( io_service ) {}
-
-   void onError( AMQP::TcpConnection* connection, const char* message ) override {
-      elog( "amqp connection failed: ${m}", ("m", message) );
-   }
-
-   uint16_t onNegotiate( AMQP::TcpConnection* connection, uint16_t interval ) override {
-      return 0; // disable heartbeats
-   }
-};
-
 /// Designed to work with io_service running on a dedicated thread.
 class amqp {
 public:
    using on_error_t = std::function<void(const std::string& err)>;
    amqp( boost::asio::io_service& io_service, const std::string& address, std::string name, on_error_t on_err )
-         : name_( std::move( name ) )
+         : connected_( std::make_unique<std::promise<void>>() )
+         , name_( std::move( name ) )
          , on_error( std::move(on_err) )
    {
       AMQP::Address amqp_address( address );
       ilog( "Connecting to AMQP address ${a} - Queue: ${q}...", ("a", std::string( amqp_address ))( "q", name_ ) );
 
-      handler_ = std::make_unique<amqp_handler>( io_service );
+      handler_ = std::make_unique<amqp_handler>( *this, io_service );
       connection_ = std::make_unique<AMQP::TcpConnection>( handler_.get(), amqp_address );
       channel_ = std::make_unique<AMQP::TcpChannel>( connection_.get() );
-      declare_queue();
+      wait();
    }
 
    void publish( const std::string& exchange, const std::string& correlation_id, const char* data, size_t data_size ) {
@@ -55,27 +43,55 @@ public:
 
 private:
    void declare_queue() {
-      std::promise<void> connected;
       auto& queue = channel_->declareQueue( name_, AMQP::durable );
       queue.onSuccess( [&]( const std::string& name, uint32_t messagecount, uint32_t consumercount ) {
          dlog( "AMQP Connected Successfully!\n Queue ${q} - Messages: ${mc} - Consumers: ${cc}",
                ("q", name)( "mc", messagecount )( "cc", consumercount ) );
-         connected.set_value();
+         connected_->set_value();
       } );
       queue.onError( [&, on_err=on_error]( const char* error_message ) {
          on_err( error_message );
-         connected.set_value();
+         connected_->set_value();
       } );
-      auto r = connected.get_future().wait_for(std::chrono::seconds(10));
-      if( r == std::future_status::timeout ) {
-         on_error("AMQP timeout declaring queue");
+   }
+
+   void wait() {
+      if( connected_ ) {
+         auto r = connected_->get_future().wait_for( std::chrono::seconds( 10 ) );
+         if( r == std::future_status::timeout ) {
+            on_error( "AMQP timeout declaring queue" );
+         }
+         connected_.reset();
       }
    }
+
+   class amqp_handler : public AMQP::LibBoostAsioHandler {
+   public:
+      explicit amqp_handler( amqp& impl, boost::asio::io_service& io_service )
+            : AMQP::LibBoostAsioHandler( io_service )
+            , amqp_(impl)
+      {}
+
+      void onError( AMQP::TcpConnection* connection, const char* message ) override {
+         elog( "amqp connection failed: ${m}", ("m", message) );
+      }
+
+      uint16_t onNegotiate( AMQP::TcpConnection* connection, uint16_t interval ) override {
+         return 0; // disable heartbeats
+      }
+
+      void onReady(AMQP::TcpConnection* c) override {
+         amqp_.declare_queue();
+      }
+
+      amqp& amqp_;
+   };
 
 private:
    std::unique_ptr<amqp_handler> handler_;
    std::unique_ptr<AMQP::TcpConnection> connection_;
    std::unique_ptr<AMQP::TcpChannel> channel_;
+   std::unique_ptr<std::promise<void>> connected_;
    std::string name_;
    on_error_t on_error;
 };
