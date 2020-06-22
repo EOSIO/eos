9b94ef57d58e1ca66bfb587c2e2391f507af8091
Brian Johnson <johnsonb@objectcomputing.com>
2020-06-22 12:59:25 -0500

    Removing the redundant --http-max-response-time-ms parameters.

--- tests/nodeos_multiple_version_protocol_feature_test.py
+++ tests/nodeos_multiple_version_protocol_feature_test.py
@@ -97,9 +97,6 @@ try:
                           extraNodeosArgs=" --plugin eosio::producer_api_plugin ",
                           useBiosBootFile=False,
                           specificExtraNodeosArgs={
-                             0:"--http-max-response-time-ms 990000",
-                             1:"--http-max-response-time-ms 990000",
-                             2:"--http-max-response-time-ms 990000",
                              3:"--last-block-time-offset-us -200000"},
                           onlySetProds=True,
                           pfSetupPolicy=PFSetupPolicy.NONE,
