ce2a8cff62e508fff21618bc45a4d9802e062fd1
Timothy Banks <timothy.banks@block.one>
2020-06-29 15:39:11 -0400

    Cleanup up documentation

--- libraries/fc/include/fc/static_variant.hpp
+++ libraries/fc/include/fc/static_variant.hpp
@@ -1,14 +1,3 @@
-/** This source adapted from https://github.com/kmicklas/variadic-static_variant
- *
- * Copyright (C) 2013 Kenneth Micklas
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- **/
 #pragma once
 #include <stdexcept>
 #include <typeinfo>
@@ -61,7 +50,7 @@ constexpr const T& get(const std::variant<Types...>& v)
 }
 
 // fc::visit mimics the functionality of std::visit with the
-// exception that in the case of the variant not holding a valur, it will raise an fc::assert_exception.
+// exception that in the case of the variant not holding a value, it will raise an fc::assert_exception.
 //
 // Prefer calling fc::visit in cases where you are excepting an fc exception.
 // All other cases, prefer calling std::visit.
@@ -159,6 +148,6 @@ template<typename... T> void from_variant( const fc::variant& v, std::variant<T.
   std::visit( to_static_variant(ar[1]), s );
 }
 
-template<typename... T> struct get_typename { static const char* name()   { return BOOST_CORE_TYPEID(std::variant<T...>).name();   } };
+template<typename... T> struct get_typename { static const char* name() { return BOOST_CORE_TYPEID(std::variant<T...>).name(); } };
 
 }
