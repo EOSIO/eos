735bfa0233b9a36317089ca559ff82d16c83c7f0
Kevin Heifner <heifnerk@objectcomputing.com>
2020-06-30 15:33:48 -0500

    Use reliable_amqp_publisher for publishing messages

--- programs/rodeos/streamer_plugin.cpp
+++ programs/rodeos/streamer_plugin.cpp
@@ -4,11 +4,11 @@
 #include "streams/logger.hpp"
 #include "streams/rabbitmq.hpp"
 #include "streams/stream.hpp"
-
 #include <abieos.hpp>
 #include <eosio/abi.hpp>
 #include <eosio/chain/exceptions.hpp>
 #include <fc/exception/exception.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <memory>
 
 namespace b1 {
@@ -34,7 +34,7 @@ struct streamer_plugin_impl : public streamer_t {
    void publish_to_streams(const stream_wrapper_v0& sw) {
       for (const auto& stream : streams) {
          if (stream->check_route(sw.route)) {
-            stream->publish(sw.data.data(), sw.data.size(), sw.route);
+            stream->publish(sw.data, sw.route);
          }
       }
    }
@@ -46,6 +46,7 @@ struct streamer_plugin_impl : public streamer_t {
    }
 
    std::vector<std::unique_ptr<stream_handler>> streams;
+   bool delete_previous = false;
 };
 
 static abstract_plugin& _streamer_plugin = app().register_plugin<streamer_plugin>();
@@ -62,10 +63,21 @@ void streamer_plugin::set_program_options(options_description& cli, options_desc
       "RabbitMQ Streams to exchanges if any; Format: amqp://USER:PASSWORD@ADDRESS:PORT/EXCHANGE[::EXCHANGE_TYPE][/STREAMING_ROUTE, ...]");
    op("stream-loggers", bpo::value<std::vector<string>>()->composing(),
       "Logger Streams if any; Format: [routing_keys, ...]");
+
+   cli.add_options()
+     ("stream-delete-unsent", bpo::bool_switch(&my->delete_previous),
+      "Delete unsent AMQP stream data retained from previous connections");
 }
 
 void streamer_plugin::plugin_initialize(const variables_map& options) {
    try {
+      const boost::filesystem::path stream_data_path = appbase::app().data_dir() / "stream";
+
+      if( my->delete_previous ) {
+         if( boost::filesystem::exists( stream_data_path ) )
+            boost::filesystem::remove_all( stream_data_path );
+      }
+
       if (options.count("stream-loggers")) {
          auto loggers = options.at("stream-loggers").as<std::vector<std::string>>();
          initialize_loggers(my->streams, loggers);
@@ -73,23 +85,24 @@ void streamer_plugin::plugin_initialize(const variables_map& options) {
 
       if (options.count("stream-rabbits")) {
          auto rabbits = options.at("stream-rabbits").as<std::vector<std::string>>();
-         initialize_rabbits_queue(app().get_io_service(), my->streams, rabbits);
+         initialize_rabbits_queue(app().get_io_service(), my->streams, rabbits, stream_data_path);
       }
 
       if (options.count("stream-rabbits-exchange")) {
          auto rabbits = options.at("stream-rabbits-exchange").as<std::vector<std::string>>();
-         initialize_rabbits_exchange(app().get_io_service(), my->streams, rabbits);
+         initialize_rabbits_exchange(app().get_io_service(), my->streams, rabbits, stream_data_path);
       }
 
       ilog("initialized streams: ${streams}", ("streams", my->streams.size()));
-   }
-   FC_LOG_AND_RETHROW()
+   } FC_LOG_AND_RETHROW()
 }
 
 void streamer_plugin::plugin_startup() {
-   cloner_plugin* cloner = app().find_plugin<cloner_plugin>();
-   EOS_ASSERT( cloner, eosio::chain::plugin_config_exception, "cloner_plugin not found" );
-   cloner->set_streamer(my);
+   try {
+      cloner_plugin* cloner = app().find_plugin<cloner_plugin>();
+      EOS_ASSERT( cloner, eosio::chain::plugin_config_exception, "cloner_plugin not found" );
+      cloner->set_streamer( my );
+   } FC_LOG_AND_RETHROW()
 }
 
 void streamer_plugin::plugin_shutdown() {}
