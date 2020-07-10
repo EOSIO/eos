6ba4fe64c4aff7be052a64b603d18a4314a47fba
Maxim Kirov <maxim.kirov@block.one>
2020-07-10 15:05:18 -0400

    added return values output in cleos

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -473,6 +473,7 @@ void print_action( const fc::variant& at ) {
    auto func = act["name"].as_string();
    auto args = fc::json::to_string( act["data"], fc::time_point::maximum() );
    auto console = at["console"].as_string();
+   auto return_value = at["return_value"].as_string();
 
    /*
    if( code == "eosio" && func == "setcode" )
@@ -482,6 +483,9 @@ void print_action( const fc::variant& at ) {
    */
    if( args.size() > 100 ) args = args.substr(0,100) + "...";
    cout << "#" << std::setw(14) << right << receiver << " <= " << std::setw(28) << std::left << (code +"::" + func) << " " << args << "\n";
+   if ( return_value.size())  {
+      cout << std::setw(28) << std::left << "=>" << "return value: " << return_value << "\n";
+   }
    if( console.size() ) {
       std::stringstream ss(console);
       string line;
@@ -1137,7 +1141,7 @@ struct create_account_subcommand {
 
             if( active_key_str.empty() ) {
                active = owner;
-            } else if ( active_key_str.find('{') != string::npos ) { 
+            } else if ( active_key_str.find('{') != string::npos ) {
                try{
                   active = parse_json_authority_or_key(active_key_str);
                } EOS_RETHROW_EXCEPTIONS( explained_exception, "Invalid active authority: ${authority}", ("authority", owner_key_str) )
@@ -2622,7 +2626,7 @@ int main( int argc, char** argv ) {
    });
 
    // validate subcommand
-   auto validate = app.add_subcommand("validate", localized("Validate transactions")); 
+   auto validate = app.add_subcommand("validate", localized("Validate transactions"));
    validate->require_subcommand();
 
    // validate signatures
@@ -3578,6 +3582,9 @@ int main( int argc, char** argv ) {
    actionsSubcommand->add_option("action", action,
                                  localized("A JSON string or filename defining the action to execute on the contract"), true)->required();
    actionsSubcommand->add_option("data", data, localized("The arguments to the contract"))->required();
+
+   bool print_return_value = false;
+   actionsSubcommand->add_flag("-v,--return_value", print_return_value, localized("Print action's return value"));
 
    add_standard_transaction_options_plus_signing(actionsSubcommand);
    actionsSubcommand->callback([&] {
