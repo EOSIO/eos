fb0b41f22ecbe4b078f531db837eb4b0578addd5
Dmytro Sydorchenko <dmytro.sydorchenko@block.one>
2020-07-16 12:24:46 -0400

    added comment for make_trimmed_string_view

--- plugins/http_plugin/include/eosio/http_plugin/http_plugin.hpp
+++ plugins/http_plugin/include/eosio/http_plugin/http_plugin.hpp
@@ -159,6 +159,10 @@ namespace eosio {
       error_info error;
    };
 
+   /**
+    * @brief Used to trim whitespace from body. 
+    * Returned string_view valid only for lifetime of body
+    */
    inline std::string_view make_trimmed_string_view(const std::string& body) {
       if (body.empty()) {
          return {};
