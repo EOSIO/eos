8acac2734281da227dd6b0fd093b0feb4c11b1e1
Kevin Heifner <heifnerk@objectcomputing.com>
2020-08-03 08:02:49 -0500

    Upgrade to EOSIO/cppkin v1.2.0

--- scripts/eosio_build_ubuntu.sh
+++ scripts/eosio_build_ubuntu.sh
@@ -31,6 +31,6 @@ VERSION_MAJ=$(echo "${VERSION_ID}" | cut -d'.' -f1)
 VERSION_MIN=$(echo "${VERSION_ID}" | cut -d'.' -f2)
 # CPPKIN Installation
 echo "${COLOR_CYAN}[Installing cppkin]${COLOR_NC}"
-curl -LO https://github.com/EOSIO/cppKin/releases/download/v1.1.1/cppkin_1.1.1-ubuntu-18.04_amd64.deb && \
-  apt install -y ./cppkin_1.1.1-ubuntu-18.04_amd64.deb && \
-  rm ./cppkin_1.1.1-ubuntu-18.04_amd64.deb
+curl -LO https://github.com/EOSIO/cppKin/releases/download/v1.2.0/cppkin_1.2.0-1-ubuntu-18.04_amd64.deb && \
+  apt install -y ./cppkin_1.2.0-1-ubuntu-18.04_amd64.deb && \
+  rm ./cppkin_1.2.0-1-ubuntu-18.04_amd64.deb
