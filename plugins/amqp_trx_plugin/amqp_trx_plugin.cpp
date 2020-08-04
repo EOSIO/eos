0280248ef8ce690e452e72e25666895178ed918b
Kevin Heifner <heifnerk@objectcomputing.com>
2020-08-04 07:02:36 -0500

    Revert "Post trx result on thread pool"
    This reverts commit 1baa21b2984e7d1653a80a3cdb9f43a2a2090dd3.
    
    Performance was worse not better.

--- plugins/amqp_trx_plugin/amqp_trx_plugin.cpp
+++ plugins/amqp_trx_plugin/amqp_trx_plugin.cpp
@@ -83,9 +83,6 @@ private:
       app().post( priority::medium_low, [my=shared_from_this(), delivery_tag, trx{std::move(trx)}]() {
          my->chain_plug->accept_transaction( trx,
             [my, delivery_tag, trx](const fc::static_variant<fc::exception_ptr, chain::transaction_trace_ptr>& result) mutable {
-               boost::asio::post( my->chain_plug->chain().get_thread_pool(),
-                                  [my{std::move(my)}, delivery_tag, trx{std::move(trx)}, result=result]() mutable {
-
                my->amqp_trx->ack( delivery_tag );
 
                fc_create_trace(trx_trace, "Transaction");
@@ -117,7 +114,6 @@ private:
 
             } );
          } );
-      } );
    }
 };
 
