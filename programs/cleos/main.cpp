a2e9e9123e80bb545f844dc72e70dbce99103bbd
Maxim Kirov <maxim.kirov@block.one>
2020-08-10 18:47:47 -0400

    added return_value_data_hex check

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -466,6 +466,27 @@ fc::variant push_actions(std::vector<chain::action>&& actions, const std::vector
    return push_transaction(trx, signing_keys);
 }
 
+void print_return_value( const fc::variant& at ) {
+   std::string return_value, return_value_prefix{"return value: "};
+   const auto  & iter_value = at.get_object().find("return_value_data");
+   const auto  & iter_hex   = at.get_object().find("return_value_hex_data");
+
+   if( iter_value != at.get_object().end() ) {
+      return_value = fc::json::to_string(iter_value->value(), fc::time_point::maximum());
+   }
+   else if( iter_hex != at.get_object().end() ) {
+      return_value = iter_hex->value().as_string();
+      return_value_prefix = "return value (hex): ";
+   }
+
+   if( !return_value.empty() ) {
+      if( return_value.size() > 100 ) {
+         return_value = return_value.substr(0, 100) + "...";
+      }
+      cout << "=>" << std::setw(46) << std::right << return_value_prefix << return_value << "\n";
+   }
+}
+
 void print_action( const fc::variant& at ) {
    auto receiver = at["receiver"].as_string();
    const auto& act = at["act"].get_object();
@@ -482,21 +503,7 @@ void print_action( const fc::variant& at ) {
    */
    if( args.size() > 100 ) args = args.substr(0,100) + "...";
    cout << "#" << std::setw(14) << right << receiver << " <= " << std::setw(28) << std::left << (code +"::" + func) << " " << args << "\n";
-
-   std::string return_value, return_value_prefix{"return value: "};
-   const auto  & iter = at.get_object().find("return_value_data");
-   if( iter != at.get_object().end() ) {
-      return_value = fc::json::to_string(iter->value(), fc::time_point::maximum());
-   } else {
-      return_value = at["return_value_hex_data"].as_string();
-      return_value_prefix = "return value (hex): ";
-   }
-   if( !return_value.empty() ) {
-      if( return_value.size() > 100 ) {
-         return_value = return_value.substr(0, 100) + "...";
-      }
-      cout << "=>" << std::setw(46) << std::right << return_value_prefix << return_value << "\n";
-   }
+   print_return_value(at);
    if( console.size() ) {
       std::stringstream ss(console);
       string line;
