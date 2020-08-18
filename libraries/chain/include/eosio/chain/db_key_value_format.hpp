93d30b484a77a5d78ed10ebdd7759c8dbe9a1f75
Brian Johnson <johnsonb@objectcomputing.com>
2020-08-18 17:43:38 -0500

    Needed to change db_key_value_format from class to namespace for centos7.7.

--- libraries/chain/include/eosio/chain/db_key_value_format.hpp
+++ libraries/chain/include/eosio/chain/db_key_value_format.hpp
@@ -7,49 +7,22 @@
 #include <memory>
 #include <stdint.h>
 
-namespace eosio { namespace chain {
-   class db_key_value_format {
-   public:
-      using key256_t = std::array<uint128_t, 2>;
-      static b1::chain_kv::bytes create_primary_key(name scope, name table, uint64_t db_key);
+namespace eosio { namespace chain { namespace db_key_value_format {
+   using key256_t = std::array<uint128_t, 2>;
+   b1::chain_kv::bytes create_primary_key(name scope, name table, uint64_t db_key);
 
-      static void get_primary_key(const b1::chain_kv::bytes& composite_key, name& scope, name& table, uint64_t& db_key);
+   void get_primary_key(const b1::chain_kv::bytes& composite_key, name& scope, name& table, uint64_t& db_key);
 
-      template<typename Key>
-      static b1::chain_kv::bytes create_secondary_key(name scope, name table, const Key& db_key, uint64_t primary_key) {
-         b1::chain_kv::bytes composite_key;
-         prepare_composite_key<Key>(composite_key, scope, table, sizeof(Key));
-         b1::chain_kv::append_key(composite_key, db_key);
-         b1::chain_kv::append_key(composite_key, primary_key);
-         return composite_key;
-      }
+   enum class key_type : char {
+      primary = 0,
+      sec_i64 = 1,
+      sec_i128 = 2,
+      sec_i256 = 3,
+      sec_double = 4,
+      sec_long_double = 5
+   };
 
-      template<typename Key>
-      static bool get_secondary_key(const b1::chain_kv::bytes& composite_key, name& scope, name& table, Key& db_key, uint64_t& primary_key) {
-         b1::chain_kv::bytes::const_iterator composite_loc;
-         key_type kt = key_type::sec_i64;
-         std::tie(scope, table, composite_loc, kt) = extract_from_composite_key(composite_key.cbegin(), composite_key.cend());
-         constexpr static auto expected_kt = determine_sec_type<Key>::kt;
-         if (kt != expected_kt) {
-            return false;
-         }
-         EOS_ASSERT(b1::chain_kv::extract_key(composite_loc, composite_key.cend(), db_key), bad_composite_key_exception,
-                    "DB intrinsic key-value store composite key is malformed, it is supposed to have an underlying: " +
-                    to_string(kt));
-         EOS_ASSERT(b1::chain_kv::extract_key(composite_loc, composite_key.cend(), primary_key), bad_composite_key_exception,
-                    "DB intrinsic key-value store composite key is malformed, it is supposed to have a trailing primary key");
-         return true;
-      }
-
-      enum class key_type : char {
-         primary = 0,
-         sec_i64 = 1,
-         sec_i128 = 2,
-         sec_i256 = 3,
-         sec_double = 4,
-         sec_long_double = 5
-      };
-   private:
+   namespace detail {
 
       template<typename Key>
       struct determine_sec_type {
@@ -70,10 +43,10 @@ namespace eosio { namespace chain {
       template<>
       struct determine_sec_type<float128_t> { static constexpr key_type kt = key_type::sec_long_double; };
 
-      static std::string to_string(const key_type& kt);
+      std::string to_string(const key_type& kt);
 
       template<typename Key, key_type kt = determine_sec_type<Key>::kt>
-      static void prepare_composite_key(b1::chain_kv::bytes& key_storage, name scope, name table, std::size_t key_size) {
+      void prepare_composite_key(b1::chain_kv::bytes& key_storage, name scope, name table, std::size_t key_size) {
          constexpr static auto scope_size = sizeof(scope);
          constexpr static auto table_size = sizeof(table);
          constexpr static auto type_size = sizeof(key_type);
@@ -86,8 +59,34 @@ namespace eosio { namespace chain {
 
       using intermittent_decomposed_values = std::tuple<name, name, b1::chain_kv::bytes::const_iterator, key_type>;
 
-      static intermittent_decomposed_values extract_from_composite_key(b1::chain_kv::bytes::const_iterator begin, b1::chain_kv::bytes::const_iterator key_end);
+      intermittent_decomposed_values extract_from_composite_key(b1::chain_kv::bytes::const_iterator begin, b1::chain_kv::bytes::const_iterator key_end);
 
-   };
+   }
 
-}} // ns eosio::chain
+   template<typename Key>
+   b1::chain_kv::bytes create_secondary_key(name scope, name table, const Key& db_key, uint64_t primary_key) {
+      b1::chain_kv::bytes composite_key;
+      detail::prepare_composite_key<Key>(composite_key, scope, table, sizeof(Key));
+      b1::chain_kv::append_key(composite_key, db_key);
+      b1::chain_kv::append_key(composite_key, primary_key);
+      return composite_key;
+   }
+
+   template<typename Key>
+   bool get_secondary_key(const b1::chain_kv::bytes& composite_key, name& scope, name& table, Key& db_key, uint64_t& primary_key) {
+      b1::chain_kv::bytes::const_iterator composite_loc;
+      key_type kt = key_type::sec_i64;
+      std::tie(scope, table, composite_loc, kt) = detail::extract_from_composite_key(composite_key.cbegin(), composite_key.cend());
+      constexpr static auto expected_kt = detail::determine_sec_type<Key>::kt;
+      if (kt != expected_kt) {
+         return false;
+      }
+      EOS_ASSERT(b1::chain_kv::extract_key(composite_loc, composite_key.cend(), db_key), bad_composite_key_exception,
+                 "DB intrinsic key-value store composite key is malformed, it is supposed to have an underlying: " +
+                 detail::to_string(kt));
+      EOS_ASSERT(b1::chain_kv::extract_key(composite_loc, composite_key.cend(), primary_key), bad_composite_key_exception,
+                 "DB intrinsic key-value store composite key is malformed, it is supposed to have a trailing primary key");
+      return true;
+   }
+
+}}} // ns eosio::chain::db_key_value_format
