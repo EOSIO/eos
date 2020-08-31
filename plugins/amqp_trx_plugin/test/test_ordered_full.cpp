c7b09e1e43825446e6e49ac2ce333e8aef731403
Kevin Heifner <heifnerk@objectcomputing.com>
2020-08-31 11:55:35 -0500

    Integration test with producer_plugin including abort block

--- plugins/amqp_trx_plugin/test/test_ordered_full.cpp
+++ plugins/amqp_trx_plugin/test/test_ordered_full.cpp
@@ -54,18 +54,39 @@ auto make_unique_trx( const chain_id_type& chain_id, fc::time_point expire = fc:
    return std::make_shared<packed_transaction>( std::move(trx), true, packed_transaction::compression_type::none);
 }
 
-bool verify_equal( const std::deque<packed_transaction_ptr>& trxs, const std::deque<packed_transaction_ptr>& applied_trxs) {
-   for( size_t i = 0; i < trxs.size() && i < applied_trxs.size(); ++i ) {
-      if( trxs[i]->id() != applied_trxs[i]->id() ) {
-         elog( "[${i}]: ${lhs} != ${rhs}", ("i", i)
-               ("lhs", trxs[i]->get_transaction().actions.at(0).name)
-               ("rhs", applied_trxs[i]->get_transaction().actions.at(0).name) );
-         elog( "[${i}]: ${lhs} != ${rhs}", ("i", i)
+// verify all trxs are in blocks in order
+bool verify_equal( const std::deque<packed_transaction_ptr>& trxs, const std::deque<block_state_ptr>& all_blocks) {
+   size_t num_trxs = 0;
+   for( size_t i = 0, j = 0, k = 0; i < trxs.size() && j < all_blocks.size(); ++i ) {
+
+      auto next_trx = [&]() -> const transaction& {
+         while( j < all_blocks.size() ) {
+            const auto& bs = all_blocks[j];
+            if( k < bs->block->transactions.size() ) {
+               const auto& trx = bs->block->transactions[k].trx.get<packed_transaction>().get_transaction();
+               ++k;
+               return trx;
+            } else {
+               k = 0;
+               ++j;
+            }
+         }
+         BOOST_REQUIRE_MESSAGE( false, "Hit end of all_blocks" );
+         return all_blocks.at(j)->block->transactions.at(0).trx.get<packed_transaction>().get_transaction(); // avoid warning
+      };
+
+      const auto& trx = next_trx();
+
+      if( trxs[i]->id() != trx.id() ) {
+         elog( "[${i}],[${j},${k}]: ${lhs} != ${rhs}", ("i", i)("j", j)("k", k)
                ("lhs", trxs[i]->get_transaction().actions.at(0).data_as<testit>().id)
-               ("rhs", applied_trxs[i]->get_transaction().actions.at(0).data_as<testit>().id) );
+               ("rhs", trx.actions.at(0).data_as<testit>().id) );
          return false;
+      } else {
+         ++num_trxs;
       }
    }
+   BOOST_CHECK_EQUAL( num_trxs, trxs.size() );
    return true;
 }
 
@@ -74,6 +95,9 @@ bool verify_equal( const std::deque<packed_transaction_ptr>& trxs, const std::de
 
 BOOST_AUTO_TEST_SUITE(ordered_trxs_full)
 
+// Integration test of fifo_trx_processing_queue and producer_plugin
+// Test verifies that transactions are processed in the order they are submitted to the fifo_trx_processing_queue
+// even when blocks are aborted.
 BOOST_AUTO_TEST_CASE(order) {
    boost::filesystem::path temp = boost::filesystem::temp_directory_path() / boost::filesystem::unique_path();
 
@@ -82,7 +106,8 @@ BOOST_AUTO_TEST_CASE(order) {
       std::future<std::tuple<producer_plugin*, chain_plugin*>> plugin_fut = plugin_promise.get_future();
       std::thread app_thread( [&]() {
          std::vector<const char*> argv =
-               {"test", "--data-dir", temp.c_str(), "--config-dir", temp.c_str(), "-p", "eosio", "-e" };
+               {"test", "--data-dir", temp.c_str(), "--config-dir", temp.c_str(),
+                "-p", "eosio", "-e", "--max-transaction-time", "500" };
          appbase::app().initialize<chain_plugin, producer_plugin>( argv.size(), (char**) &argv[0] );
          appbase::app().startup();
          plugin_promise.set_value(
@@ -93,17 +118,6 @@ BOOST_AUTO_TEST_CASE(order) {
       auto[prod_plug, chain_plug] = plugin_fut.get();
       auto chain_id = chain_plug->get_chain_id();
 
-      std::deque<chain::packed_transaction_ptr> applied_trxs;
-      auto cc = chain_plug->chain().applied_transaction.connect(
-            [&](std::tuple<const transaction_trace_ptr&, const packed_transaction_ptr&> x) {
-         auto& t = std::get<1>(x);
-         if( t->get_transaction().actions.at(0).name != eosio::name("onblock") ) {
-            if( !std::get<0>(x)->except ) {
-               applied_trxs.push_back( t );
-            }
-         }
-      } );
-
       named_thread_pool thread_pool( "test", 5 );
 
       auto queue =
@@ -113,46 +127,74 @@ BOOST_AUTO_TEST_CASE(order) {
                                                                           thread_pool.get_executor(),
                                                                           prod_plug,
                                                                           10 );
+
+      std::deque<block_state_ptr> all_blocks;
+      std::promise<void> empty_blocks_promise;
+      std::future<void> empty_blocks_fut = empty_blocks_promise.get_future();
+      std::atomic<size_t> num_aborts = 0;
+      auto ab = chain_plug->chain().accepted_block.connect( [&](const block_state_ptr& bsp) {
+         static int num_empty = std::numeric_limits<int>::max();
+         all_blocks.push_back( bsp );
+         if( bsp->block->transactions.empty() ) {
+            --num_empty;
+            if( num_empty == 0 ) empty_blocks_promise.set_value();
+         } else { // we want a couple of empty blocks after we have some non-empty blocks
+            num_empty = 2;
+         }
+         queue->on_block_stop();
+      } );
+      auto ba = chain_plug->chain().block_abort.connect( [&]( uint32_t bn ) {
+         ++num_aborts;
+         queue->on_block_stop();
+      } );
+      auto bs = chain_plug->chain().block_start.connect( [&]( uint32_t bn ) {
+         queue->on_block_start();
+      } );
+
       queue->run();
-      queue->on_block_start();
 
       std::deque<packed_transaction_ptr> trxs;
       std::atomic<size_t> next_calls = 0;
       std::atomic<bool> next_trx_match = true;
       const size_t num_pushes = 4242;
-      for( size_t i = 0; i < num_pushes; ++i ) {
+      for( size_t i = 1; i <= num_pushes; ++i ) {
          auto ptrx = make_unique_trx( chain_id );
          trxs.push_back( ptrx );
          queue->push( ptrx,
                       [ptrx, &next_calls, &next_trx_match](
                             const fc::static_variant<fc::exception_ptr, chain::transaction_trace_ptr>& result ) {
-                         if( result.get<chain::transaction_trace_ptr>()->id != ptrx->id() ) {
+                         if( result.contains<fc::exception_ptr>() || result.get<chain::transaction_trace_ptr>()->id != ptrx->id() ) {
                             next_trx_match = false;
                          }
                          ++next_calls;
                       } );
-    // add handling of abort_block for expected results
-//         if( i % (num_pushes / 7) == 0 ) {
-//            app().post(priority::high, [](){
-//               app().find_plugin<producer_plugin>()->get_integrity_hash();
-//            });
-//         }
+         if( i % (num_pushes/3) == 0 ) {
+            // need to sleep or a fast machine might put all trxs in one block
+            usleep( config::block_interval_us / 2 );
+         }
+         if( i % 500 == 0 && num_aborts <= 3 ) {
+            // get_integrity_hash aborts block and places aborted trxs into unapplied_transaction_queue
+            // verifying that aborting block does not change order of transactions when re-applied
+            app().post(priority::high, [](){
+               app().find_plugin<producer_plugin>()->get_integrity_hash();
+            });
+         }
       }
 
-      while( !queue->empty() ) {
-         usleep( 100 );
-      }
+      empty_blocks_fut.wait_for(std::chrono::seconds(10));
 
       queue->stop();
 
+      BOOST_CHECK( all_blocks.size() > 3 ); // should have a few blocks otherwise test is running too fast
+      BOOST_CHECK( num_aborts > 3 ); // should have a few abort_blocks or maybe get_integrity_hash has changed
       BOOST_CHECK_EQUAL( num_pushes, next_calls );
       BOOST_CHECK( next_trx_match.load() );
 
       appbase::app().quit();
-      queue.reset();
       app_thread.join();
+      queue.reset();
 
-      BOOST_REQUIRE( verify_equal(trxs, applied_trxs ) );
+      BOOST_REQUIRE( verify_equal(trxs, all_blocks ) );
 
    } catch ( ... ) {
       bfs::remove_all( temp );
