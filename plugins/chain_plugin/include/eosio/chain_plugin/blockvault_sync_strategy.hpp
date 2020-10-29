7147f9a4f1a4be54cde873fb5c28b80ba8ba51a3
Chris Gundlach <cgundlach@naughtydawgllc.com>
2020-10-29 15:31:03 -0500

    moved over code from blockvaultd implementation/updated interface

--- plugins/chain_plugin/include/eosio/chain_plugin/blockvault_sync_strategy.hpp
+++ plugins/chain_plugin/include/eosio/chain_plugin/blockvault_sync_strategy.hpp
@@ -18,36 +18,46 @@ namespace eosio {
             virtual void append_proposed_block(watermark_t watermark, uint32_t lib, std::string_view block_content) = 0;
             virtual void append_external_block(watermark_t watermark, std::string_view block_content)               = 0;
             virtual void propose_snapshot(watermark_t watermark, const char* snapshot_filename)                     = 0;
-            virtual void sync(std::optional<uint32_t> previous_block_id, sync_callback& callback)                          = 0;
+            virtual void sync(std::string_view previous_block_id, sync_callback& callback)                          = 0;
         };
 
         template<typename BP>
         struct blockvault_sync_strategy : sync_callback {
-            blockvault_sync_strategy(block_vault_interface& service_provider, BP& blockchain_provider) :
-                _service_provider(service_provider), _blockchain_provider(blockchain_provider) {
+            blockvault_sync_strategy(block_vault_interface& blockvault, BP& blockchain_provider) :
+                _blockvault(blockvault), _blockchain_provider(blockchain_provider) {
 
             }
 
-            blockvault_sync_strategy(block_vault_interface& service_provider, BP& blockchain_provider, uint32_t block_id) :
-                    _service_provider(service_provider), _blockchain_provider(blockchain_provider), _previous_block_id(block_id) {
-
+            blockvault_sync_strategy(block_vault_interface& blockvault, BP& blockchain_provider, chain::block_id_type block_id) :
+                _blockvault(blockvault), _blockchain_provider(blockchain_provider), _previous_block_id(block_id) {
             }
 
             void do_sync() {
-                _service_provider.sync(_previous_block_id, *this);
+                std::string previous_block_id;
+                if (_previous_block_id.has_value()) {
+                    previous_block_id = _previous_block_id->str();
+                }
+                _blockvault.sync(previous_block_id, *this);
             }
 
             void on_snapshot(watermark_t watermark, std::string_view snapshot_filename) override {
-
+                auto infile = std::ifstream(snapshot_filename, (std::ios::in | std::ios::binary));
+                auto reader = std::make_shared<chain::istream_snapshot_reader>(infile);
+                _blockchain_provider.startup_from_snapshot(reader);
+                infile.close();
             }
 
             void on_block(watermark_t watermark, std::string_view block) override {
+                chain::signed_block_ptr b = std::make_shared<chain::signed_block>();
+                fc::datastream<const char *> ds(block.cbegin(), block.size());
+                fc::raw::unpack(ds, b);
 
+                _blockchain_provider.accept_block(b, b->calculate_id());
             }
 
         private:
-            std::optional<uint32_t> _previous_block_id;
-            block_vault_interface& _service_provider;
+            std::optional<chain::block_id_type> _previous_block_id;
+            block_vault_interface& _blockvault;
             BP& _blockchain_provider;
         };
 
--- plugins/chain_plugin/test/test_blockvault_sync_strategy.cpp
+++ plugins/chain_plugin/test/test_blockvault_sync_strategy.cpp
@@ -15,13 +15,45 @@ using namespace eosio;
 using namespace eosio::chain;
 using namespace eosio::testing;
 using namespace eosio::chain_apis;
+using namespace eosio::blockvault;
 
+class mock_blockvault_t : public block_vault_interface {
+    virtual void append_proposed_block(watermark_t watermark, uint32_t lib, std::string_view block_content){}
+    virtual void append_external_block(watermark_t watermark, std::string_view block_content){}
+    virtual void propose_snapshot(watermark_t watermark, const char* snapshot_filename){}
+    virtual void sync(std::string_view previous_block_id, sync_callback& callback){
+
+    }
+
+
+};
+
+class mock_chain_t {
+public:
+    void startup_from_snapshot(std::shared_ptr<chain::istream_snapshot_reader>) {}
+    bool accept_block( const chain::signed_block_ptr& block, const chain::block_id_type& id ){
+        _block = block;
+        _id = id;
+        return _accept_block_rc;
+    }
+
+    bool _accept_block_rc;
+    signed_block_ptr _block;
+    block_id_type _id;
+
+};
 
 BOOST_AUTO_TEST_SUITE(blockvault_sync_strategy_tests)
 
 BOOST_FIXTURE_TEST_CASE(newaccount_test, TESTER) { try {
 
-	//BOOST_TEST_REQUIRE(find_account_name(results, tester_account) == true);
+    mock_chain_t chain;
+    mock_blockvault_t bv;
+
+    blockvault_sync_strategy<mock_chain_t> uut(bv, chain);
+    uut.do_sync();
+
+	//BOOST_TEST_REQUIRE( == true);
 
 } FC_LOG_AND_RETHROW() }
 
