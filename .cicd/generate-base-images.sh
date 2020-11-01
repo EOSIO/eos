7618af4e4dff5d1ec830e7b39df85968ba913526
Zach Butler <zach.butler@block.one>
2020-10-31 20:42:27 -0400

    Use vendor-neutral manifest query

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -4,11 +4,15 @@ set -eo pipefail
 . $HELPERS_DIR/file-hash.sh $CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile
 # look for Docker image
 echo "+++ :mag_right: Looking for $FULL_TAG"
+DOCKER_CLI_EXPERIMENTAL='enabled'
 ORG_REPO=$(echo $FULL_TAG | cut -d: -f1)
 TAG=$(echo $FULL_TAG | cut -d: -f2)
-EXISTS=$(curl -s -H "Authorization: Bearer $(curl -sSL "https://auth.docker.io/token?service=registry.docker.io&scope=repository:${ORG_REPO}:pull" | jq --raw-output .token)" "https://registry.hub.docker.com/v2/${ORG_REPO}/manifests/$TAG")
+MANIFEST_COMMAND="docker manifest inspect '$ORG_REPO:$TAG'"
+echo "$ $MANIFEST_COMMAND"
+eval $MANIFEST_COMMAND
+EXISTS="$?"
 # build, if neccessary
-if [[ $EXISTS =~ '404 page not found' || $EXISTS =~ 'manifest unknown' || $FORCE_BASE_IMAGE == true ]]; then # if we cannot pull the image, we build and push it first
+if [[ "$EXISTS" != '0' || "$FORCE_BASE_IMAGE" == 'true' ]]; then # if we cannot pull the image, we build and push it first
     DOCKER_BUILD_COMMAND="docker build --no-cache -t '$FULL_TAG' -f '$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile' ."
     echo "$ $DOCKER_BUILD_COMMAND"
     eval $DOCKER_BUILD_COMMAND
