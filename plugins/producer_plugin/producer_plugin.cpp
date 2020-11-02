7eb18dc8dbaf7a04aaddab1c00b092f56e6af560
Kevin Heifner <heifnerk@objectcomputing.com>
2020-11-02 09:02:43 -0600

    Fix typo

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -2077,7 +2077,7 @@ void producer_plugin_impl::produce_block() {
 
 void producer_plugin::log_failed_transaction(const transaction_id_type& trx_id, const char* reason) const {
    fc_dlog(_trx_failed_trace_log, "[TRX_TRACE] Speculative execution is REJECTING tx: ${txid} : ${why}",
-           ("trid", trx_id)("why", reason));
+           ("txid", trx_id)("why", reason));
 }
 
 } // namespace eosio
