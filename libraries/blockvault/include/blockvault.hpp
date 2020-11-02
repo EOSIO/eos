75ac471479dbd13919baabe63727fb0cc125099f
Huang-Ming Huang <huangh@objectcomputing.com>
2020-11-02 11:13:05 -0600

    update interface

--- libraries/blockvault/include/blockvault.hpp
+++ libraries/blockvault/include/blockvault.hpp
@@ -1,23 +1,24 @@
 #pragma once
 #include <stdint.h>
 #include <string_view>
+#include <eosio/chain/block_timestamp.hpp>
 
 namespace eosio {
 namespace blockvault {
-using watermark_t = uint32_t;
+using watermark_t = std::pair<uint32_t, eosio::chain::block_timestamp_type>;
 
 class sync_callback {
  public:
-   virtual void on_snapshot(watermark_t watermark, std::string_view snapshot_filename) = 0;
-   virtual void on_block(watermark_t watermark, std::string_view block)                = 0;
+   virtual void on_snapshot(std::string_view snapshot_filename) = 0;
+   virtual void on_block(std::string_view block)                = 0;
 };
 
 class block_vault_interface {
  public:
-   virtual void append_proposed_block(watermark_t watermark, uint32_t lib, std::string_view block_content) = 0;
-   virtual void append_external_block(watermark_t watermark, std::string_view block_content)               = 0;
-   virtual void propose_snapshot(watermark_t watermark, const char* snapshot_filename)                     = 0;
-   virtual void sync(std::string_view previous_block_id, sync_callback& callback)                          = 0;
+   virtual bool append_proposed_block(watermark_t watermark, uint32_t lib, std::string_view block_content) = 0;
+   virtual bool append_external_block(uint32_t block_num, uint32_t lib, std::string_view block_content)    = 0;
+   virtual bool propose_snapshot(watermark_t watermark, const char* snapshot_filename)                     = 0;
+   virtual void sync(std::string_view block_id, sync_callback& callback)                          = 0;
 };
 
 } // namespace blockvault
