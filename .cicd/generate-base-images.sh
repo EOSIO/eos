ba7aa583c71a432e4b27ba62ffe14692e755e327
Scott Arnette <scott.a.arnette@gmail.com>
2020-11-04 16:30:41 -0500

    Add support for multiple registries while trying not to break supporting only dockerhub.

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -2,30 +2,33 @@
 set -eo pipefail
 . ./.cicd/helpers/general.sh
 . $HELPERS_DIR/file-hash.sh $CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile
-# look for Docker image
-echo "+++ :mag_right: Looking for $FULL_TAG"
-export DOCKER_CLI_EXPERIMENTAL='enabled'
-export ORG_REPO=$(echo $FULL_TAG | cut -d: -f1)
-export TAG=$(echo $FULL_TAG | cut -d: -f2)
-export MANIFEST_COMMAND="docker manifest inspect '$ORG_REPO:$TAG'"
-set +e # manifest query can return a non-zero exit status
-echo "$ $MANIFEST_COMMAND"
-eval $MANIFEST_COMMAND
-EXISTS="$?"
-set -eo pipefail
-# build, if neccessary
-if [[ "$EXISTS" != '0' || "$FORCE_BASE_IMAGE" == 'true' ]]; then # if we cannot pull the image, we build and push it first
-    export DOCKER_BUILD_COMMAND="docker build --no-cache -t '$FULL_TAG' -f '$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile' ."
-    echo "$ $DOCKER_BUILD_COMMAND"
-    eval $DOCKER_BUILD_COMMAND
-    if [[ $FORCE_BASE_IMAGE != true ]]; then
-        export DOCKER_PUSH_COMMAND="docker push '$FULL_TAG'"
-        echo "$ $DOCKER_PUSH_COMMAND"
-        eval $DOCKER_PUSH_COMMAND
-    else
-        echo "Base image creation successful. Not pushing...".
-        exit 0
+
+echo "+++ :mag_right: Looking for $HASHED_IMAGE_TAG"
+for REGISTRY in "${CI_REGISTRIES[@]}"; do
+  if [[ ! -z $REGISTRY ]]; then
+    if [[ ! $(docker manifest inspect "$REGISTRY:$HASHED_IMAGE_TAG") ]]; then
+      EXISTS='false'
     fi
+  fi
+done
+
+# build, if neccessary
+if [[ "$EXISTS" == 'false' || "$FORCE_BASE_IMAGE" == 'true' ]]; then # if we cannot pull the image, we build and push it first
+  export DOCKER_BUILD_COMMAND="docker build --no-cache -t 'eosio/ci:$HASHED_IMAGE_TAG' -f '$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile' ."
+  echo "$ $DOCKER_BUILD_COMMAND"
+  eval $DOCKER_BUILD_COMMAND
+  if [[ $FORCE_BASE_IMAGE != true ]]; then
+    for REGISTRY in "${CI_REGISTRIES[@]}"; do
+      if [[ ! -z $REGISTRY ]]; then
+        docker tag eosio/ci:$HASHED_IMAGE_TAG $REGISTRY:$HASHED_IMAGE_TAG
+        docker push $REGISTRY:$HASHED_IMAGE_TAG
+        docker rmi $REGISTRY:$HASHED_IMAGE_TAG
+      fi
+    done
+  else
+    echo "Base image creation successful. Not pushing...".
+    exit 0
+  fi
 else
-    echo "$FULL_TAG already exists."
+  echo "$FULL_TAG already exists."
 fi
