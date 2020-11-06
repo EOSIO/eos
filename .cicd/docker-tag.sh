1a70672252d9feba5b290c828df566f7f0d3e8f7
Zach Butler <zach.butler@block.one>
2020-11-05 23:17:03 -0500

    docker-tag.sh does NOT use the CI registry, it only uses the Contracts registry

--- .cicd/docker-tag.sh
+++ .cicd/docker-tag.sh
@@ -5,17 +5,13 @@ echo '+++ :evergreen_tree: Configuring Environment'
 PREFIX='base-ubuntu-18.04'
 SANITIZED_BRANCH=$(echo "$BUILDKITE_BRANCH" | sed 's.^/..' | sed 's/[:/]/_/g')
 SANITIZED_TAG=$(echo "$BUILDKITE_TAG" | sed 's.^/..' | tr '/' '_')
-echo '$ echo ${#CI_REGISTRIES[*]} # array length'
-echo ${#CI_REGISTRIES[*]}
-echo '$ echo ${CI_REGISTRIES[*]} # array'
-echo ${CI_REGISTRIES[*]}
 echo '$ echo ${#CONTRACT_REGISTRIES[*]} # array length'
 echo ${#CONTRACT_REGISTRIES[*]}
 echo '$ echo ${CONTRACT_REGISTRIES[*]} # array'
 echo ${CONTRACT_REGISTRIES[*]}
 # pull
 echo '+++ :arrow_down: Pulling Container(s)'
-for REGISTRY in ${CI_REGISTRIES[*]}; do
+for REGISTRY in ${CONTRACT_REGISTRIES[*]}; do
     if [[ ! -z "$REGISTRY" ]]; then
         echo "Pulling from '$REGISTRY'."
         IMAGE="$REGISTRY:$PREFIX-$BUILDKITE_COMMIT-$PLATFORM_TYPE"
@@ -26,15 +22,15 @@ for REGISTRY in ${CI_REGISTRIES[*]}; do
 done
 # tag
 echo '+++ :label: Tagging Container(s)'
-for INDEX in ${!CI_REGISTRIES[*]}; do
-    if [[ ! -z "${CI_REGISTRIES[$INDEX]}" ]]; then
-        echo "Tagging for index $INDEX."
-        IMAGE="${CI_REGISTRIES[$INDEX]}:$PREFIX-$BUILDKITE_COMMIT-$PLATFORM_TYPE"
-        DOCKER_TAG_COMMAND="docker tag '$IMAGE' '${CONTRACT_REGISTRIES[$INDEX]}:$PREFIX-$SANITIZED_BRANCH'"
+for REGISTRY in ${CONTRACT_REGISTRIES[*]}; do
+    if [[ ! -z "$REGISTRY" ]]; then
+        echo "Tagging for registry $REGISTRY."
+        IMAGE="$REGISTRY:$PREFIX-$BUILDKITE_COMMIT-$PLATFORM_TYPE"
+        DOCKER_TAG_COMMAND="docker tag '$IMAGE' '$REGISTRY:$PREFIX-$SANITIZED_BRANCH'"
         echo "$ $DOCKER_TAG_COMMAND"
         eval $DOCKER_TAG_COMMAND
         if [[ ! -z "$BUILDKITE_TAG" && "$SANITIZED_BRANCH" != "$SANITIZED_TAG" ]]; then
-            DOCKER_TAG_COMMAND="docker tag '$IMAGE' '${CONTRACT_REGISTRIES[$INDEX]}:$PREFIX-$SANITIZED_TAG'"
+            DOCKER_TAG_COMMAND="docker tag '$IMAGE' '$REGISTRY:$PREFIX-$SANITIZED_TAG'"
             echo "$ $DOCKER_TAG_COMMAND"
             eval $DOCKER_TAG_COMMAND
         fi
@@ -57,18 +53,18 @@ for REGISTRY in ${CONTRACT_REGISTRIES[*]}; do
 done
 # cleanup
 echo '+++ :put_litter_in_its_place: Cleaning Up'
-for INDEX in ${!CI_REGISTRIES[*]}; do
-    if [[ ! -z "${CI_REGISTRIES[$INDEX]}" ]]; then
-        echo "Cleaning up from index $INDEX."
-        DOCKER_RMI_COMMAND="docker rmi '${CONTRACT_REGISTRIES[$INDEX]}:$PREFIX-$SANITIZED_BRANCH'"
+for REGISTRY in ${CONTRACT_REGISTRIES[*]}; do
+    if [[ ! -z "$REGISTRY" ]]; then
+        echo "Cleaning up from $REGISTRY."
+        DOCKER_RMI_COMMAND="docker rmi '$REGISTRY:$PREFIX-$SANITIZED_BRANCH'"
         echo "$ $DOCKER_RMI_COMMAND"
         eval $DOCKER_RMI_COMMAND
         if [[ ! -z "$BUILDKITE_TAG" && "$SANITIZED_BRANCH" != "$SANITIZED_TAG" ]]; then
-            DOCKER_RMI_COMMAND="docker rmi '${CONTRACT_REGISTRIES[$INDEX]}:$PREFIX-$SANITIZED_TAG'"
+            DOCKER_RMI_COMMAND="docker rmi '$REGISTRY:$PREFIX-$SANITIZED_TAG'"
             echo "$ $DOCKER_RMI_COMMAND"
             eval $DOCKER_RMI_COMMAND
         fi
-        DOCKER_RMI_COMMAND="docker rmi '${CI_REGISTRIES[$INDEX]}:$PREFIX-$BUILDKITE_COMMIT-$PLATFORM_TYPE'"
+        DOCKER_RMI_COMMAND="docker rmi '$REGISTRY:$PREFIX-$BUILDKITE_COMMIT-$PLATFORM_TYPE'"
         echo "$ $DOCKER_RMI_COMMAND"
         eval $DOCKER_RMI_COMMAND
     fi
