7ebd956eb623c3b5bcf458b6a2ff8fae1fc88276
Zach Butler <zach.butler@block.one>
2020-11-05 19:56:07 -0500

    Spacing (conform to repo/POSIX standards)

--- .cicd/generate-base-images.sh
+++ .cicd/generate-base-images.sh
@@ -4,40 +4,40 @@ set -eo pipefail
 . $HELPERS_DIR/file-hash.sh $CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile
 
 echo "+++ :mag_right: Looking for $HASHED_IMAGE_TAG"
-for REGISTRY in "${CI_REGISTRIES[@]}"; do
-  if [[ ! -z $REGISTRY ]]; then
-    if [[ ! $(docker manifest inspect "$REGISTRY:$HASHED_IMAGE_TAG") ]]; then
-      EXISTS='false'
+for REGISTRY in "${CI_REGISTRIES[*]}"; do
+    if [[ ! -z "$REGISTRY" ]]; then
+        if [[ ! $(docker manifest inspect "$REGISTRY:$HASHED_IMAGE_TAG") ]]; then
+            EXISTS='false'
+        fi
     fi
-  fi
 done
 
 # build, if neccessary
 if [[ "$EXISTS" == 'false' || "$FORCE_BASE_IMAGE" == 'true' ]]; then # if we cannot pull the image, we build and push it first
-  export DOCKER_BUILD_COMMAND="docker build --no-cache -t 'ci:$HASHED_IMAGE_TAG' -f '$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile' ."
-  echo "$ $DOCKER_BUILD_COMMAND"
-  eval $DOCKER_BUILD_COMMAND
-  if [[ $FORCE_BASE_IMAGE != true ]]; then
-    for REGISTRY in "${CI_REGISTRIES[@]}"; do
-      if [[ ! -z $REGISTRY ]]; then
-        DOCKER_TAG_COMMAND="docker tag ci:$HASHED_IMAGE_TAG $REGISTRY:$HASHED_IMAGE_TAG"
-        DOCKER_PUSH_COMMAND="docker push $REGISTRY:$HASHED_IMAGE_TAG"
-        DOCKER_RMI_COMMAND="docker rmi $REGISTRY:$HASHED_IMAGE_TAG"
-        echo "$ $DOCKER_TAG_COMMAND"
-        echo "$ $DOCKER_PUSH_COMMAND"
+    export DOCKER_BUILD_COMMAND="docker build --no-cache -t 'ci:$HASHED_IMAGE_TAG' -f '$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile' ."
+    echo "$ $DOCKER_BUILD_COMMAND"
+    eval $DOCKER_BUILD_COMMAND
+    if [[ "$FORCE_BASE_IMAGE" != 'true' ]]; then
+        for REGISTRY in "${CI_REGISTRIES[*]}"; do
+            if [[ ! -z "$REGISTRY" ]]; then
+                DOCKER_TAG_COMMAND="docker tag ci:$HASHED_IMAGE_TAG $REGISTRY:$HASHED_IMAGE_TAG"
+                DOCKER_PUSH_COMMAND="docker push $REGISTRY:$HASHED_IMAGE_TAG"
+                DOCKER_RMI_COMMAND="docker rmi $REGISTRY:$HASHED_IMAGE_TAG"
+                echo "$ $DOCKER_TAG_COMMAND"
+                echo "$ $DOCKER_PUSH_COMMAND"
+                echo "$ $DOCKER_RMI_COMMAND"
+                eval $DOCKER_TAG_COMMAND
+                eval $DOCKER_PUSH_COMMAND
+                eval $DOCKER_RMI_COMMAND
+            fi
+        done
+        DOCKER_RMI_COMMAND="docker rmi ci:$HASHED_IMAGE_TAG"
         echo "$ $DOCKER_RMI_COMMAND"
-        eval $DOCKER_TAG_COMMAND
-        eval $DOCKER_PUSH_COMMAND
         eval $DOCKER_RMI_COMMAND
-      fi
-    done
-    DOCKER_RMI_COMMAND="docker rmi ci:$HASHED_IMAGE_TAG"
-    echo "$ $DOCKER_RMI_COMMAND"
-    eval $DOCKER_RMI_COMMAND
-  else
-    echo "Base image creation successful. Not pushing...".
-    exit 0
-  fi
+    else
+        echo "Base image creation successful. Not pushing...".
+        exit 0
+    fi
 else
-  echo "$FULL_TAG already exists."
+    echo "$FULL_TAG already exists."
 fi
