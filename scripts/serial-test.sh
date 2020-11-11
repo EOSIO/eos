651bf868212d3261b4a8ff843a36a0feef8cb9cb
Zach Butler <zach.butler@block.one>
2020-11-10 19:54:45 -0500

    Fix shell string quoting issues

--- scripts/serial-test.sh
+++ scripts/serial-test.sh
@@ -3,23 +3,23 @@ set -eo pipefail
 # variables
 echo "+++ $([[ "$BUILDKITE" == 'true' ]] && echo ':evergreen_tree: ')Configuring Environment"
 GIT_ROOT="$(dirname $BASH_SOURCE[0])/.."
-[[ -z "$TEST" ]] && export TEST=$1
+[[ -z "$TEST" ]] && export TEST="$1"
 if [[ "$(uname)" == 'Linux' ]]; then
     . /etc/os-release
     if [[ "$ID" == 'centos' ]]; then
         [[ -f /opt/rh/rh-python36/enable ]] && source /opt/rh/rh-python36/enable
     fi
-    cd $GIT_ROOT && npm install
+    cd "$GIT_ROOT" && npm install
 else
     npm install
 fi
-cd $GIT_ROOT/build
+cd "$GIT_ROOT/build"
 # tests
 if [[ -z "$TEST" ]]; then # run all serial tests
     # count tests
     echo "+++ $([[ "$BUILDKITE" == 'true' ]] && echo ':microscope: ')Running Non-Parallelizable Tests"
-    TEST_COUNT=$(ctest -N -L nonparallelizable_tests | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
-    if [[ $TEST_COUNT > 0 ]]; then
+    TEST_COUNT=$(ctest -N -L 'nonparallelizable_tests' | grep -i 'Total Tests: ' | cut -d ':' -f '2' | awk '{print $1}')
+    if [[ "$TEST_COUNT" > '0' ]]; then
         echo "$TEST_COUNT tests found."
         # run tests
         set +e # defer ctest error handling to end
@@ -36,7 +36,7 @@ else # run specific serial test
     # ensure test exists
     echo "+++ $([[ "$BUILDKITE" == 'true' ]] && echo ':microscope: ')Running $TEST"
     TEST_COUNT=$(ctest -N -R ^$TEST$ | grep -i 'Total Tests: ' | cut -d ':' -f 2 | awk '{print $1}')
-    if [[ $TEST_COUNT > 0 ]]; then
+    if [[ "$TEST_COUNT" > '0' ]]; then
         echo "$TEST found."
         # run tests
         set +e # defer ctest error handling to end
