068d81f5e69327eefa172c40fed24167964ea337
Zach Butler <zach.butler@block.one>
2020-11-10 19:24:22 -0500

    Print even MORE commands before running them to make this BASH spaghetti easier to debug

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -6,10 +6,13 @@ set -eo pipefail
 if [[ $(uname) == 'Darwin' ]]; then # macOS
     set +e # defer error handling to end
     [[ "$CI" == 'true' ]] && source ~/.bash_profile
-    "./$1" ${@: 2}
+    TEST_COMMAND="\"./$1\" ${@: 2}"
+    echo "$ $TEST_COMMAND"
+    eval $TEST_COMMAND
     EXIT_STATUS=$?
 else # Linux
-    COMMANDS="\"$MOUNTED_DIR/$1\" ${@: 2}"
+    TEST_COMMAND="\"$MOUNTED_DIR/$1\" ${@: 2}"
+    COMMANDS="echo \"$ $TEST_COMMAND\" && $TEST_COMMAND"
     . "$HELPERS_DIR/file-hash.sh" "$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile"
     DOCKER_RUN_COMMAND="docker run --rm --init -v \"\$(pwd):$MOUNTED_DIR\" $(buildkite-intrinsics) -e JOBS -e BUILDKITE_API_KEY '$FULL_TAG' bash -c '$COMMANDS'"
     set +e # defer error handling to end
