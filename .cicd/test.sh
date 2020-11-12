a6590e5464a7dca481e7132c5f25c0ab6cac1ff1
Zach Butler <zach.butler@block.one>
2020-11-12 01:51:38 -0500

    Escape sequences

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -12,7 +12,7 @@ if [[ $(uname) == 'Darwin' ]]; then # macOS
     EXIT_STATUS=$?
 else # Linux
     echo '--- :docker: Selecting Container'
-    TEST_COMMAND="'\\\"'$MOUNTED_DIR/$1'\\\"' ${@: 2}"
+    TEST_COMMAND="'\"'$MOUNTED_DIR/$1'\"' ${@: 2}"
     COMMANDS="echo \"$ $TEST_COMMAND\" && eval $TEST_COMMAND"
     . "$HELPERS_DIR/file-hash.sh" "$CICD_DIR/platforms/$PLATFORM_TYPE/$IMAGE_TAG.dockerfile"
     DOCKER_RUN_COMMAND="docker run --rm --init -v \"\$(pwd):$MOUNTED_DIR\" $(buildkite-intrinsics) -e JOBS -e BUILDKITE_API_KEY '$FULL_TAG' bash -c '$COMMANDS'"
