2e5b16d861e6172731a31f37e488740c81eb1997
Zach Butler <zach.butler@block.one>
2020-11-12 23:01:59 -0500

    Support spaces in paths & other strings

--- scripts/generate_deb.sh
+++ scripts/generate_deb.sh
@@ -1,39 +1,39 @@
 #!/bin/bash
 set -eo pipefail
 
-PREFIX="usr"
-SPREFIX=${PREFIX}
+PREFIX='usr'
+SPREFIX="${PREFIX}"
 SUBPREFIX="opt/${PROJECT}/${VERSION}"
 SSUBPREFIX="opt\/${PROJECT}\/${VERSION}"
 RELEASE="${VERSION_SUFFIX}"
 
 # default release to "1" if there is no suffix
-if [[ -z $RELEASE ]]; then
-    RELEASE="1"
+if [[ -z "$RELEASE" ]]; then
+    RELEASE='1'
 fi
 
 NAME="${PROJECT}_${VERSION_NO_SUFFIX}-${RELEASE}_amd64"
 
-if [[ -f /etc/upstream-release/lsb-release ]]; then
-    source /etc/upstream-release/lsb-release
-elif [[ -f /etc/lsb-release ]]; then
-    source /etc/lsb-release
+if [[ -f '/etc/upstream-release/lsb-release' ]]; then
+    source '/etc/upstream-release/lsb-release'
+elif [[ -f '/etc/lsb-release' ]]; then
+    source '/etc/lsb-release'
 else
-    echo "Unrecognized Debian derivative.  Not generating .deb file."
+    echo 'Unrecognized Debian derivative. Not generating .deb file.'
     exit 1
 fi
 
-if [ ${DISTRIB_RELEASE} = "16.04" ]; then
-    RELEASE_SPECIFIC_DEPS="libssl1.0.0"
-elif [ ${DISTRIB_RELEASE} = "18.04" ]; then
-    RELEASE_SPECIFIC_DEPS="libssl1.1"
+if [[ "${DISTRIB_RELEASE}" == '16.04' ]]; then
+    RELEASE_SPECIFIC_DEPS='libssl1.0.0'
+elif [[ "${DISTRIB_RELEASE}" == '18.04' ]]; then
+    RELEASE_SPECIFIC_DEPS='libssl1.1'
 else
-    echo "Unrecognized Ubuntu version.  Update generate_deb.sh.  Not generating .deb file."
+    echo 'Unrecognized Ubuntu version. Update generate_deb.sh. Not generating .deb file.'
     exit 2
 fi
 
-mkdir -p ${PROJECT}/DEBIAN
-chmod 0755 ${PROJECT}/DEBIAN
+mkdir -p "${PROJECT}/DEBIAN"
+chmod 0755 "${PROJECT}/DEBIAN"
 echo "Package: ${PROJECT}
 Version: ${VERSION_NO_SUFFIX}-${RELEASE}
 Section: devel
@@ -42,17 +42,17 @@ Depends: libc6, libgcc1, ${RELEASE_SPECIFIC_DEPS}, libstdc++6, libtinfo5, zlib1g
 Architecture: amd64
 Homepage: ${URL}
 Maintainer: ${EMAIL}
-Description: ${DESC}" &> ${PROJECT}/DEBIAN/control
-cat ${PROJECT}/DEBIAN/control
+Description: ${DESC}" &> "${PROJECT}/DEBIAN/control"
+cat "${PROJECT}/DEBIAN/control"
 
 export PREFIX
 export SUBPREFIX
 export SPREFIX
 export SSUBPREFIX
 
-. ./generate_tarball.sh ${NAME}
+. ./generate_tarball.sh "${NAME}"
 echo "Unpacking tarball: ${NAME}.tar.gz..."
-tar -xzvf ${NAME}.tar.gz -C ${PROJECT}
-$(type -P fakeroot) dpkg-deb --build ${PROJECT}
-mv ${PROJECT}.deb ${NAME}.deb
-rm -r ${PROJECT}
+tar -xzvf "${NAME}.tar.gz" -C "${PROJECT}"
+$(type -P fakeroot) dpkg-deb --build "${PROJECT}"
+mv "${PROJECT}.deb" "${NAME}.deb"
+rm -r "${PROJECT}"
--- scripts/generate_tarball.sh
+++ scripts/generate_tarball.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 set -eo pipefail
 
-NAME=$1
-EOS_PREFIX=${PREFIX}/${SUBPREFIX}
-mkdir -p ${PREFIX}/bin/
-mkdir -p ${EOS_PREFIX}/bin
-mkdir -p ${EOS_PREFIX}/licenses/eosio
+NAME="$1"
+EOS_PREFIX="${PREFIX}/${SUBPREFIX}"
+mkdir -p "${PREFIX}/bin/"
+mkdir -p "${EOS_PREFIX}/bin"
+mkdir -p "${EOS_PREFIX}/licenses/eosio"
 
 # install binaries 
-cp -R ${BUILD_DIR}/bin/* ${EOS_PREFIX}/bin
+cp -R "${BUILD_DIR}"/bin/* ${EOS_PREFIX}/bin
 
 # install licenses
-cp -R ${BUILD_DIR}/licenses/eosio/* ${EOS_PREFIX}/licenses
+cp -R "${BUILD_DIR}"/licenses/eosio/* ${EOS_PREFIX}/licenses
 
 for f in $(ls "${BUILD_DIR}/bin/"); do
-    bn=$(basename $f)
-    ln -sf ../${SUBPREFIX}/bin/$bn ${PREFIX}/bin/$bn
+    bn=$(basename "$f")
+    ln -sf ../"${SUBPREFIX}/bin/$bn" "${PREFIX}/bin/$bn"
 done
 echo "Generating Tarball $NAME.tar.gz..."
-tar -cvzf $NAME.tar.gz ./${PREFIX}/*
-rm -r ${PREFIX}
+tar -cvzf "$NAME.tar.gz" ./"${PREFIX}"/*
+rm -r "${PREFIX}"
