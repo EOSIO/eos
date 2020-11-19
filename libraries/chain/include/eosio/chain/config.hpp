094d9ecd2d895875bbb07a41d8070d3a3d971d4b
Victor Camacho <victor.camacho@block.one>
2020-11-19 13:13:40 -0500

    address comments on the PR

--- libraries/chain/include/eosio/chain/config.hpp
+++ libraries/chain/include/eosio/chain/config.hpp
@@ -91,7 +91,7 @@ const static uint32_t   default_max_action_return_value_size         = 256;
 const static uint16_t   default_persistent_storage_num_threads       = 1;
 const static int        default_persistent_storage_max_num_files     = -1;
 const static uint64_t   default_persistent_storage_write_buffer_size = 128 * 1024 * 1024;
-const static uint64_t   default_persistent_storage_bytes_per_sync    = 1048576;
+const static uint64_t   default_persistent_storage_bytes_per_sync    = 1 * 1024 * 1024;
 
 static_assert(MAX_SIZE_OF_BYTE_ARRAYS == 20*1024*1024, "Changing MAX_SIZE_OF_BYTE_ARRAYS breaks consensus. Make sure this is expected");
 
