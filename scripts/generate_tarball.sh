1348530d497571824203466d721cfd30a027e304
Lin Huang <lin.huang@block.one>
2020-11-30 16:48:41 -0500

    eosio-bios and eosio-boot contracts support for KV inside eosio

--- scripts/generate_tarball.sh
+++ scripts/generate_tarball.sh
@@ -6,12 +6,19 @@ EOS_PREFIX="${PREFIX}/${SUBPREFIX}"
 mkdir -p "${PREFIX}/bin/"
 mkdir -p "${EOS_PREFIX}/bin"
 mkdir -p "${EOS_PREFIX}/licenses/eosio"
+mkdir -p "${EOS_PREFIX}/etc/eosio/contracts"
 
 # install binaries 
 cp -R "${BUILD_DIR}"/bin/* ${EOS_PREFIX}/bin
 
 # install licenses
 cp -R "${BUILD_DIR}"/licenses/eosio/* ${EOS_PREFIX}/licenses
+
+# install bios and boot contracts
+cp -R "${BUILD_DIR}"/contracts/eosio.bios/eosio.abi ${EOS_PREFIX}/etc/eosio/contracts/eosio.bios.abi
+cp -R "${BUILD_DIR}"/contracts/eosio.bios/eosio.bios ${EOS_PREFIX}/etc/eosio/contracts/eosio.bios.wasm
+cp -R "${BUILD_DIR}"/contracts/eosio.boot/eosio.abi ${EOS_PREFIX}/etc/eosio/contracts/eosio.boot.abi
+cp -R "${BUILD_DIR}"/contracts/eosio.boot/eosio.boot ${EOS_PREFIX}/etc/eosio/contracts/eosio.boot.wasm
 
 for f in $(ls "${BUILD_DIR}/bin/"); do
     bn=$(basename "$f")
