b46229a7c691c1b090f0265860d06e79f9fe95a0
Huang-Ming Huang <huangh@objectcomputing.com>
2020-11-30 11:22:16 -0600

    tweak the priority of using docker and ci

--- scripts/postgres_control.sh
+++ scripts/postgres_control.sh
@@ -4,48 +4,27 @@
 #
 # This script controls the starting-up/stopping a PostgresSQL server or executing an SQL statement based on the following conditions
 #
-# 1. The program `docker` can be executed in current environment and can be executed without sudo: the script would use docker to start/stop PostgreSQL 
-#     or execute SQL stamtement against the started server.
-# 2. The program `pg_ctlcluster` or `pg_ctl` can be executed in current environment and the 'CI' environment variable is 'true': the script would use 
+# 1. The program `pg_ctlcluster` or `pg_ctl` can be executed in current environment and the 'CI' environment variable is 'true': the script would use 
 #    'pg_ctlcluster' or `pg_ctl` to start/stop the PostgreSQL server. It is intended to be used by CI docker container only.
+# 2. The program `docker` can be executed in current environment and can be executed without sudo: the script would use docker to start/stop PostgreSQL 
+#     or execute SQL stamtement against the started server.
 # 3. The program 'psql' can be executed in current environment and the environment variables PGHOST, PGPORT, PGPASSWORD, PGUSER are properly
 #    defined to access an running PostgreSQL server. 
 #
 #
 # Usage:
 #
-#   # start the PostgreSQL server when not existed and execute the "Drop TABLE mytable" on the PostgreSQL server when necessary
+#   # start the PostgreSQL server if unavailable and execute the "Drop TABLE mytable" on the PostgreSQL server in case 1 or 3
 #   $ postgres_control.sh start "Drop TABLE mytable"
 # 
-#   # start the PostgreSQL server in case 1 or 2; in cause 3, do nothing. 
+#   # stop the PostgreSQL server in case 1 or 2; in cause 3, do nothing. 
 #   $ postgres_control.sh stop
 #
 #   # execute SQL in a running PostgreSQL server started by this script
 #   $ postgres_control.sh exec "SELECT (*) FROM table;"
 # 
 ########################################################################################################################################################
-
-if [ ! -z `which docker 2>/dev/null` ]; then
-   ### use docker
-   case "$1" in 
-   start)
-      if [ -z `docker ps --filter name=test_postgres -q` ]; then
-         docker run --rm -p 127.0.0.1:5432:5432 --name test_postgres -e POSTGRES_PASSWORD=password -d postgres
-         until docker run --rm --link test_postgres:pg postgres pg_isready -U postgres -h pg; do sleep 1; done
-      elif [ ! -z "$2" ]; then
-         docker run --rm --link test_postgres:pg -e PGPASSWORD=password postgres psql -U postgres -h pg -q -c "$2"
-      fi
-      ;;
-   stop)
-      docker rm -f test_postgres
-      ;;
-   exec)
-      docker run --rm --link test_postgres:pg -e PGPASSWORD=password postgres psql -U postgres -h pg -q -c "$2"
-      ;;
-   status)
-      echo true 
-   esac
-elif [ "$CI" = "true" ]; then
+if [ "$CI" = "true" ]; then
    PG_CTL=`which pg_ctl 2>/dev/null`
    if [ -z $PG_CTL ]; then 
      if [ -f "$PostgreSQL_ROOT/bin/pg_ctl" ]; then
@@ -95,6 +74,26 @@ elif [ "$CI" = "true" ]; then
          esac
       fi
    fi
+elif [ ! -z `which docker 2>/dev/null` ]; then
+   ### use docker
+   case "$1" in 
+   start)
+      if [ -z `docker ps --filter name=test_postgres -q` ]; then
+         docker run --rm -p 127.0.0.1:5432:5432 --name test_postgres -e POSTGRES_PASSWORD=password -d postgres
+         until docker run --rm --link test_postgres:pg postgres pg_isready -U postgres -h pg; do sleep 1; done
+      elif [ ! -z "$2" ]; then
+         docker run --rm --link test_postgres:pg -e PGPASSWORD=password postgres psql -U postgres -h pg -q -c "$2"
+      fi
+      ;;
+   stop)
+      docker rm -f test_postgres
+      ;;
+   exec)
+      docker run --rm --link test_postgres:pg -e PGPASSWORD=password postgres psql -U postgres -h pg -q -c "$2"
+      ;;
+   status)
+      echo true 
+   esac
 elif [ ! -z "$PGHOST" ] && [ -n `which psql` ]; then
    ## user supplied postgres server
    case "$1" in 
