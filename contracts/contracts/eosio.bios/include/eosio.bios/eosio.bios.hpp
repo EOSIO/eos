30d3761c5a005b37c9a3040b0009323a3324c8ab
larryk85 <larrykittinger@gmail.com>
2020-12-03 12:58:59 -0500

    update bios to have 'temporary' setpparams action

--- contracts/contracts/eosio.bios/include/eosio.bios/eosio.bios.hpp
+++ contracts/contracts/eosio.bios/include/eosio.bios/eosio.bios.hpp
@@ -68,7 +68,7 @@ namespace eosiobios {
 
    /**
     * The `eosio.bios` is the first sample of system contract provided by `block.one` through the EOSIO platform. It is a minimalist system contract because it only supplies the actions that are absolutely critical to bootstrap a chain and nothing more. This allows for a chain agnostic approach to bootstrapping a chain.
-    * 
+    *
     * Just like in the `eosio.system` sample contract implementation, there are a few actions which are not implemented at the contract level (`newaccount`, `updateauth`, `deleteauth`, `linkauth`, `unlinkauth`, `canceldelay`, `onerror`, `setabi`, `setcode`), they are just declared in the contract so they will show in the contract's ABI and users will be able to push those actions to the chain via the account holding the `eosio.system` contract, but the implementation is at the EOSIO core level. They are referred to as EOSIO native actions.
     */
    class [[eosio::contract("eosio.bios")]] bios : public eosio::contract {
@@ -227,6 +227,14 @@ namespace eosiobios {
           */
          [[eosio::action]]
          void setparams( const eosio::blockchain_parameters& params );
+
+         /**
+          * Set params action, sets the blockchain parameters. By tuning these parameters, various degrees of customization can be achieved.
+          *
+          * @param params - New blockchain parameters to set
+          */
+         [[eosio::action]]
+         void setpparams( const std::vector<char>& bytes);
 
          /**
           * Set KV params action, sets the KV  parameters. By tuning these parameters, various degrees of customization can be achieved.
