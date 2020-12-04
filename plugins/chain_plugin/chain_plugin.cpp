82afed204a8da71c62ba1e36b1b04d4d02d8e146
Timothy Banks <timothy.banks@block.one>
2020-12-04 14:12:55 -0500

    Delete rocksdb files when starting replay

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -483,14 +483,6 @@ void clear_directory_contents( const fc::path& p ) {
    }
 }
 
-void clear_chainbase_files( const fc::path& p ) {
-   if( !fc::is_directory( p ) )
-      return;
-
-   fc::remove( p / "shared_memory.bin" );
-   fc::remove( p / "shared_memory.meta" );
-}
-
 std::optional<builtin_protocol_feature> read_builtin_protocol_feature( const fc::path& p  ) {
    try {
       return fc::json::from_file<builtin_protocol_feature>( p );
@@ -975,7 +967,7 @@ void chain_plugin::plugin_initialize(const variables_map& options) {
          ilog( "Replay requested: deleting state database" );
          if( options.at( "truncate-at-block" ).as<uint32_t>() > 0 )
             wlog( "The --truncate-at-block option does not work for a regular replay of the blockchain." );
-         clear_chainbase_files( my->chain_config->state_dir );
+         eosio::chain::combined_database::destroy( my->chain_config->state_dir );
          if( options.at( "fix-reversible-blocks" ).as<bool>()) {
             if( !recover_reversible_blocks( my->chain_config->blog.log_dir / config::reversible_blocks_dir_name,
                                             my->chain_config->reversible_cache_size )) {
