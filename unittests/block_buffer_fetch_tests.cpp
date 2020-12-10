c28ad278f2749fd95f66a30e6d18c12e4703559e
Jingjun Zhao <jingjun.zhao@block.one>
2020-12-09 22:56:46 -0500

    Update unit tests

--- unittests/block_buffer_fetch_tests.cpp
+++ unittests/block_buffer_fetch_tests.cpp
@@ -23,6 +23,25 @@ namespace bfs = boost::filesystem;
 
 BOOST_AUTO_TEST_SUITE(block_buffer_fetch_tests)
 
+    void fetch_block_buffer(TESTER& chain, uint32_t block_num, bool return_signed_block){
+        std::shared_ptr<std::vector<char>> pBuffer;
+
+        pBuffer = chain.control->fetch_block_buffer_by_number(block_num, return_signed_block);
+        if (!pBuffer) return;
+
+        //unpack
+        fc::datastream<const char*> ds((*pBuffer).data(), (*pBuffer).size());
+        if (return_signed_block){
+            signed_block sb;
+            sb.unpack(ds, packed_transaction::cf_compression_type::none);
+            BOOST_CHECK(sb.block_num() == block_num);
+        }else{
+            signed_block_v0 sb_v0;
+            fc::raw::unpack(ds, sb_v0);
+            BOOST_CHECK(sb_v0.block_num() == block_num);
+        }
+    }
+
     BOOST_AUTO_TEST_CASE(fetch_signed_block_test) {
         try{
             namespace bfs = boost::filesystem;
@@ -57,36 +76,55 @@ BOOST_AUTO_TEST_SUITE(block_buffer_fetch_tests)
             BOOST_CHECK(bfs::exists( blocks_dir / "blocks-121-140.log" ));
             BOOST_CHECK(bfs::exists( blocks_dir / "blocks-121-140.index" ));
 
-            std::shared_ptr<std::vector<char>> pBuffer;
-            pBuffer = chain.control->fetch_block_buffer_by_number(60,true);
-            //unpack
-            fc::datastream<const char*> ds((*pBuffer).data(), (*pBuffer).size());
-            signed_block sb;
-            sb.unpack(ds, packed_transaction::cf_compression_type::none);
-
-            BOOST_CHECK( sb.block_num() == 60);
-
-            //...
-
+            //fetch a block that is in a catalog file
+            fetch_block_buffer(chain, 60, true);
+            fetch_block_buffer(chain, 60, false);
+            //fetch a block that is in the blocks.log file
+            fetch_block_buffer(chain, 145, true);
+            fetch_block_buffer(chain, 145, false);
+            //fetch a block that doesn't exist locally
+            fetch_block_buffer(chain, 200, true);
+            fetch_block_buffer(chain, 200, false);
         }FC_LOG_AND_RETHROW()
 
     }
 
+    struct blocklog_version_set {
+        blocklog_version_set(uint32_t ver) { block_log::set_version(ver); };
+        ~blocklog_version_set() { block_log::set_version(block_log::max_supported_version); };
+    };
+
     BOOST_AUTO_TEST_CASE(fetch_signed_block_v0_test) {
         try{
-            TESTER test;
-            std::vector<signed_block_ptr> blocks;
+            namespace bfs = boost::filesystem;
+            fc::temp_directory temp_dir;
 
-            blocks.push_back(test.produce_block());
-            blocks.push_back(test.produce_block());
-            blocks.push_back(test.produce_block());
+            // set block log version less than block_log::max_supported_version
+            blocklog_version_set set_version(1);
 
-            //convert to signed_block_v0
-            signed_block_ptr sbp = blocks.back();
-            auto signed_block_v0 = sbp->to_signed_block_v0();
+            TESTER chain(
+                    temp_dir,
+                    [](controller::config& config) {
+                        config.blog.archive_dir        = "archive";
+                        config.blog.stride             = 20;
+                        config.blog.max_retained_files = 5;
+                    },
+                    true);
 
+            //produce less than stride size (20) blocks to avoid block-log-split that sets log version as block_log::max_supported_version
+            chain.produce_blocks(10);
 
-            //...
+            auto blocks_dir = chain.get_config().blog.log_dir;
+            BOOST_CHECK(bfs::exists( blocks_dir / "blocks.log" ));
+            BOOST_CHECK(bfs::exists( blocks_dir / "blocks.index" ));
+
+            //fetch a block that is in blocks.log file
+            fetch_block_buffer(chain, 5, true);
+            fetch_block_buffer(chain, 5, false);
+
+            //fetch a block that doesn't exist locally
+            fetch_block_buffer(chain, 20, true);
+            fetch_block_buffer(chain, 20, false);
 
         }FC_LOG_AND_RETHROW()
     }
