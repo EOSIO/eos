7abf870d94af077a3cc2aa9244519d8face231ee
Edgar Gonzalez <martting0@hotmail.com>
2020-12-10 13:27:19 -0600

    debugging error from build

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -643,7 +643,7 @@ cat <<EOF
     timeout: "${TIMEOUT:-5}"
     skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}
 
-  - label: ":docker: :ubuntu: Docker - Build 18.04 Docker Image"
+  - label: ":docker::ubuntu: Docker - Build 18.04 Docker Image"
     command:"./.cicd/create-docker-from-binary.sh"
     agents:
       queue: "$BUILDKITE_BUILD_AGENT_QUEUE"
