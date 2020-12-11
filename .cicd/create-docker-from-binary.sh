3f7ef0737d2da2864da3c4d15ed22559c6ce554f
Edgar Gonzalez <martting0@hotmail.com>
2020-12-10 18:38:40 -0600

    Enviroment vars changed

--- .cicd/create-docker-from-binary.sh
+++ .cicd/create-docker-from-binary.sh
@@ -16,19 +16,16 @@ echo ":docker::build: Build image"
 
 set -e
 
-echo "Building $IMAGE:$SOURCE_COMMIT"
-docker build -t "$IMAGE:$SOURCE_COMMIT" -f "./docker/Dockerfile" "./docker"
+echo "Building...."
+docker build -t "EOS_18.04_image:$BUILDKITE_COMMIT" -f "./docker/Dockerfile" "./docker"
 echo ":done: Done"
 
 # do docker push
 
-echo "Pushing $IMAGE:$SOURCE_COMMIT"
-docker push "$IMAGE:$SOURCE_COMMIT"
+echo "Pushing Image..."
+docker push "EOS_18.04_image:$BUILDKITE_COMMIT"
 echo ":done: Done"
 
-echo "Cleaning up $IMAGE:$SOURCE_COMMIT"
-docker rmi "$IMAGE:$SOURCE_COMMIT" --force
+echo "Cleaning up EOS_18.04_image:$BUILDKITE_COMMIT"
+docker rmi "EOS_18.04_image:$BUILDKITE_COMMIT" --force
 echo ":done: clean"
-
-buildkite-agent annotate ":docker: $VERSION_NAME - Built and pushed image $IMAGE:$SOURCE_COMMIT" --style success --context "eosio-$VERSION_NAME"
-echo ":done: Annotation done"
