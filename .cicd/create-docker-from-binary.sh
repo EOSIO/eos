da35204585bd924a21c5c30b57db39f6b65a2153
Edgar Gonzalez <martting0@hotmail.com>
2020-12-17 20:13:17 -0600

    push to ECR test Zach

--- .cicd/create-docker-from-binary.sh
+++ .cicd/create-docker-from-binary.sh
@@ -20,7 +20,7 @@ IMAGE="$MIRROR_REGISTRY:eosio-ubuntu-18.04-$BUILDKITE_COMMIT-bin"
 
 echo ":docker: Building Image...."
 #docker build -t "${MIRROR_REGISTRY}/eosio_18.04-bin:${BUILDKITE_COMMIT}" -f "./docker/Dockerfile" .
-docker build -t $IMAGE -f ./docker/dockerfile .
+docker build -t $IMAGE -f ./docker/Dockerfile .
 echo "Build Done!!"
 
 #echo "Tag Images to be pushed...."
