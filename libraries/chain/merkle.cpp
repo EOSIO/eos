e7cdf5bf26d45ce5d80c112d133f470fce3d80d2
P.M <60963077+goodengineer@users.noreply.github.com>
2020-12-31 10:56:53 +0200

    Update merkle.cpp
    Evaluation proposal for execution computing power saving:
    
    In function digest_type merkle(...), ids.size() 4 calls, reduced to 3 calls by introducing service variable size_t sz.
    
    Rows count in a few functions reduced.

--- libraries/chain/merkle.cpp
+++ libraries/chain/merkle.cpp
@@ -13,14 +13,12 @@ namespace eosio { namespace chain {
 
 digest_type make_canonical_left(const digest_type& val) {
    digest_type canonical_l = val;
-   canonical_l._hash[0] &= 0xFFFFFFFFFFFFFF7FULL;
-   return canonical_l;
+   return (canonical_l._hash[0] &= 0xFFFFFFFFFFFFFF7FULL);
 }
 
 digest_type make_canonical_right(const digest_type& val) {
    digest_type canonical_r = val;
-   canonical_r._hash[0] |= 0x0000000000000080ULL;
-   return canonical_r;
+   return (canonical_r._hash[0] |= 0x0000000000000080ULL);
 }
 
 bool is_canonical_left(const digest_type& val) {
@@ -34,18 +32,17 @@ bool is_canonical_right(const digest_type& val) {
 
 digest_type merkle(vector<digest_type> ids) {
    if( 0 == ids.size() ) { return digest_type(); }
-
-   while( ids.size() > 1 ) {
-      if( ids.size() % 2 )
+   size_t sz = ids.size(),i;
+   while( sz > 1 ) {
+      if( sz % 2 )
          ids.push_back(ids.back());
-
-      for (size_t i = 0; i < ids.size() / 2; i++) {
+      sz = ids.size();
+      for (i = 0; i < sz / 2; i++) {
          ids[i] = digest_type::hash(make_canonical_pair(ids[2 * i], ids[(2 * i) + 1]));
       }
-
-      ids.resize(ids.size() / 2);
+      ids.resize(sz/2);
+      sz = ids.size();
    }
-
    return ids.front();
 }
 
