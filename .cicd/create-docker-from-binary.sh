146478fd83d548c9c2aae4f04f9f6fa5809834d6
Edgar Gonzalez <martting0@hotmail.com>
2021-01-04 21:25:00 -0600

    syntax issue in tag step

--- .cicd/create-docker-from-binary.sh
+++ .cicd/create-docker-from-binary.sh
@@ -22,22 +22,22 @@ eval $DOCKER_BUILD_GEN
 
 EOSIO_REGS=("$EOSIO_REGISTRY" "$MIRROR_REGISTRY" "$DOCKERHUB_REGISTRY")
 for REG in ${EOSIO_REGS[@]}; do
-    DOCKER_TAG_COMMIT="docker tag eos_image REG:$BUILDKITE_COMMIT"
-    DOCKER_TAG_BRANCH="docker tag eos_image REG:$SANITIZED_BRANCH"
-    echo "$ Tagging Images: \n$DOCKER_TAG_COMMIT \n$DOCKER_TAG_BRANCH"
+    DOCKER_TAG_COMMIT="docker tag eos_image $REG:$BUILDKITE_COMMIT"
+    DOCKER_TAG_BRANCH="docker tag eos_image $REG:$SANITIZED_BRANCH"
+    echo -e "$ Tagging Images: \n$DOCKER_TAG_COMMIT \n$DOCKER_TAG_BRANCH"
     eval $DOCKER_TAG_COMMIT $DOCKER_TAG_BRANCH
-    DOCKER_PUSH_COMMIT="docker push REG:$BUILDKITE_COMMIT"
-    DOCKER_PUSH_BRANCH="docker push REG:$SANITIZED_BRANCH"
-    echo "$ Pushing Images: \n$DOCKER_PUSH_COMMIT \n$DOCKER_PUSH_BRANCH"
+    DOCKER_PUSH_COMMIT="docker push $REG:$BUILDKITE_COMMIT"
+    DOCKER_PUSH_BRANCH="docker push $REG:$SANITIZED_BRANCH"
+    echo -e "$ Pushing Images: \n$DOCKER_PUSH_COMMIT \n$DOCKER_PUSH_BRANCH"
     eval $DOCKER_PUSH_COMMIT $DOCKER_PUSH_BRANCH
-    CLEAN_IMAGE_COMMIT="docker rmi REG:$BUILDKITE_COMMIT"
-    CLEAN_IMAGE_BRANCH="docker rmi REG:$SANITIZED_BRANCH"
-    echo "Cleaning Up: \n$CLEAN_IMAGE_COMMIT \n$CLEAN_IMAGE_BRANCH$"
+    CLEAN_IMAGE_COMMIT="docker rmi $REG:$BUILDKITE_COMMIT"
+    CLEAN_IMAGE_BRANCH="docker rmi $REG:$SANITIZED_BRANCH"
+    echo -e "Cleaning Up: \n$CLEAN_IMAGE_COMMIT \n$CLEAN_IMAGE_BRANCH$"
     eval $CLEAN_IMAGE_COMMIT $CLEAN_IMAGE_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
-        DOCKER_TAG="docker tag eos_image REG:$BUILDKITE_TAG"
-        DOCKER_REM="docker rmi REG:$BUILDKITE_TAG"
-        echo "$ \n Tagging Image: \n$DOCKER_TAG \n Cleaning Up: \n$DOCKER_REM"
+        DOCKER_TAG="docker tag eos_image $REG:$BUILDKITE_TAG"
+        DOCKER_REM="docker rmi $REG:$BUILDKITE_TAG"
+        echo -e "$ \n Tagging Image: \n$DOCKER_TAG \n Cleaning Up: \n$DOCKER_REM"
         eval $DOCKER_TAG $DOCKER_REM
     fi
 done
