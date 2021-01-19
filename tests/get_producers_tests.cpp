0765d2d31e1122be8a20f850bec0c686540ce016
Brian Johnson <johnsonb@objectcomputing.com>
2021-01-19 18:57:21 +0000

    Added another test to verify get_producers logic for retrieving from the system contract's producers table

--- tests/get_producers_tests.cpp
+++ tests/get_producers_tests.cpp
@@ -22,7 +22,7 @@
 #include <utility>
 
 #include <eosio/testing/backing_store_tester_macros.hpp>
-//#include <eosio/testing/eosio_system_tester.hpp>
+#include <eosio/testing/eosio_system_tester.hpp>
 #include <fc/log/log_message.hpp>
 #include <fc/log/logger.hpp>
 
@@ -31,6 +31,7 @@ BOOST_AUTO_TEST_SUITE(get_producers_tests)
 using namespace eosio::testing;
 using backing_store_ts = boost::mpl::list< TESTER, ROCKSDB_TESTER >;
 
+// this test verifies the exception case of get_producer, where it is populated by the active schedule of producers
 BOOST_AUTO_TEST_CASE_TEMPLATE( get_producers, TESTER_T, backing_store_ts) { try {
    TESTER_T chain;
 
@@ -40,7 +41,12 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( get_producers, TESTER_T, backing_store_ts) { try 
    auto results = plugin.get_producers(params);
    BOOST_REQUIRE_EQUAL(results.more, "");
    BOOST_REQUIRE_EQUAL(results.rows.size(), 1);
-   BOOST_CHECK_EQUAL(results.rows[0]["owner"].as_string(), "eosio");
+   const auto& row = results.rows[0].get_object();
+   BOOST_REQUIRE(row.contains("owner"));
+   BOOST_REQUIRE_EQUAL(row["owner"].as_string(), "eosio");
+   // check for producer_authority, since it is only set when the producer schedule is used
+   BOOST_REQUIRE(row.contains("producer_authority"));
+
 
    chain.produce_blocks(2);
 
@@ -54,8 +60,32 @@ BOOST_AUTO_TEST_CASE_TEMPLATE( get_producers, TESTER_T, backing_store_ts) { try 
    auto owners = std::vector<std::string>{"dan", "sam", "pam"};
    auto it     = owners.begin();
    for (const auto& elem : results.rows) {
-      BOOST_CHECK_EQUAL(elem["owner"].as_string(), *it++);
+      BOOST_REQUIRE_EQUAL(elem["owner"].as_string(), *it++);
    }
-} FC_LOG_AND_RETHROW() } /// get_table_next_key_test
+} FC_LOG_AND_RETHROW() }
+
+using backing_store_system_ts = boost::mpl::list< eosio_system::eosio_system_tester<TESTER>, eosio_system::eosio_system_tester<ROCKSDB_TESTER> >;
+
+// this test verifies the normal case of get_producer, where the contents of the system contract's producers table is used
+BOOST_AUTO_TEST_CASE_TEMPLATE( get_producers_from_table, TESTER_T, backing_store_system_ts) { try {
+   TESTER_T chain;
+
+   // ensure that enough voting is occurring so that producer1111 is elected as the producer
+   chain.cross_15_percent_threshold();
+
+   eosio::chain_apis::read_only plugin(*(chain.control), {}, fc::microseconds::maximum());
+   eosio::chain_apis::read_only::get_producers_params params = { .json = true, .lower_bound = "", .limit = 21 };
+
+   auto results = plugin.get_producers(params);
+   BOOST_REQUIRE_EQUAL(results.more, "");
+   BOOST_REQUIRE_EQUAL(results.rows.size(), 1);
+   const auto& row = results.rows[0].get_object();
+   BOOST_REQUIRE(row.contains("owner"));
+   BOOST_REQUIRE_EQUAL(row["owner"].as_string(), "producer1111");
+   // check for producer_authority not present, since it is only set when the producer schedule is used, this verifies producers table was used
+   BOOST_REQUIRE(!row.contains("producer_authority"));
+
+} FC_LOG_AND_RETHROW() }
+
 
 BOOST_AUTO_TEST_SUITE_END()
