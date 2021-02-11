fbb0f30a97ac4c66f3aee6f0b8907e18ed8e4a54
Scott Arnette <scott.arnette@block.one>
2021-02-11 10:06:35 -0500

    Fix postgres init in Big Sur platform steps.

--- .cicd/platforms/pinned/macos-11-pinned.sh
+++ .cicd/platforms/pinned/macos-11-pinned.sh
@@ -25,4 +25,5 @@ sudo rm -rf boost_1_72_0.tar.bz2 boost_1_72_0
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
 # initialize postgres configuration files
+sudo rm -rf /usr/local/var/postgres
 initdb --locale=C -E UTF-8 /usr/local/var/postgres
--- .cicd/platforms/unpinned/macos-11-unpinned.sh
+++ .cicd/platforms/unpinned/macos-11-unpinned.sh
@@ -7,4 +7,5 @@ brew install git cmake python libtool libusb graphviz automake wget gmp pkgconfi
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
 # initialize postgres configuration files
+sudo rm -rf /usr/local/var/postgres
 initdb --locale=C -E UTF-8 /usr/local/var/postgres
