22ff93fa8bcc7f7920de799b0ef3b23fc5fdee56
Scott Arnette <scott.arnette@block.one>
2021-02-11 10:13:49 -0500

    Fix postgres init issue. Unblocks MacOS base image rebuilds.

--- .cicd/platforms/unpinned/macos-10.14-unpinned.sh
+++ .cicd/platforms/unpinned/macos-10.14-unpinned.sh
@@ -5,5 +5,6 @@ brew update
 brew install git cmake python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl@1.1 jq boost libpq libpqxx postgres || :
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
-# initialize postgres configuration files	
-initdb --locale=C -E UTF-8 /usr/local/var/postgres 
+# initialize postgres configuration files
+sudo rm -rf /usr/local/var/postgres
+initdb --locale=C -E UTF-8 /usr/local/var/postgres
