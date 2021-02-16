2ce1771e18c6f9eefac67d353ab97fde3d25cc9c
Henrik Hautakoski <henrik@eossweden.org>
2021-02-16 19:02:28 +0100

    programs/cleos/httpc.hpp: Adding "get_get_accounts_by_authorizers_func"

--- programs/cleos/httpc.hpp
+++ programs/cleos/httpc.hpp
@@ -88,6 +88,7 @@ namespace eosio { namespace client { namespace http {
    const string get_block_info_func = chain_func_base + "/get_block_info";
    const string get_block_header_state_func = chain_func_base + "/get_block_header_state";
    const string get_account_func = chain_func_base + "/get_account";
+   const string get_get_accounts_by_authorizers_func = chain_func_base  + "/get_accounts_by_authorizers";
    const string get_table_func = chain_func_base + "/get_table_rows";
    const string get_kv_table_func = chain_func_base + "/get_kv_table_rows";
    const string get_table_by_scope_func = chain_func_base + "/get_table_by_scope";
