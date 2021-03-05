ef5233637d17acc1f1d674cbee4851f51ada1d6b
Rusty Fleming <russell.fleming-ext@block.one>
2021-03-05 15:25:20 -0600

    EPE-672: Feedback changes
    1) Removed unused code
    2) Clarified comments
    3) Disabled cmake processing until a test is added

--- plugins/net_plugin/test/main.cpp
+++ plugins/net_plugin/test/main.cpp
@@ -1,10 +1,5 @@
-#include <cstdlib>
-#include <iostream>
 #include <boost/test/included/unit_test.hpp>
-#include <fc/log/logger.hpp>
 #include <eosio/chain/exceptions.hpp>
-
-//extern uint32_t EOS_TESTING_GENESIS_TIMESTAMP;
 
 void translate_fc_exception(const fc::exception &e) {
    std::cerr << "\033[33m" <<  e.to_detail_string() << "\033[0m" << std::endl;
@@ -12,8 +7,8 @@ void translate_fc_exception(const fc::exception &e) {
 }
 
 boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[]) {
-   // Turn off blockchain logging if no --verbose parameter is not added
-   // To have verbose enabled, call "tests/chain_test -- --verbose"
+   // Turn off logging if --verbose parameter not provided
+   // To enable, add --verbose to the command line
    bool is_verbose = false;
    std::string verbose_arg = "--verbose";
    for (int i = 0; i < argc; i++) {
@@ -26,16 +21,5 @@ boost::unit_test::test_suite* init_unit_test_suite(int argc, char* argv[]) {
 
    // Register fc::exception translator
    boost::unit_test::unit_test_monitor.register_exception_translator<fc::exception>(&translate_fc_exception);
-
-   std::srand(time(NULL));
-   std::cout << "Random number generator seeded to " << time(NULL) << std::endl;
-   /*
-   const char* genesis_timestamp_str = getenv("EOS_TESTING_GENESIS_TIMESTAMP");
-   if( genesis_timestamp_str != nullptr )
-   {
-      EOS_TESTING_GENESIS_TIMESTAMP = std::stoul( genesis_timestamp_str );
-   }
-   std::cout << "EOS_TESTING_GENESIS_TIMESTAMP is " << EOS_TESTING_GENESIS_TIMESTAMP << std::endl;
-   */
    return nullptr;
 }
