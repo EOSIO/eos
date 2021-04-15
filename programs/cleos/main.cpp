20608dcbb11ccfdce4644e46487d1bf2a71654b6
Keke Li <softprofe@gmail.com>
2021-04-14 19:11:12 -0700

    Add cleos support for cpu subjective billing

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -2304,9 +2304,9 @@ void get_account( const string& accountName, const string& coresym, bool json_fo
          }
       }
 
-
       std::cout << std::fixed << setprecision(3);
       std::cout << indent << std::left << std::setw(11) << "used:"      << std::right << std::setw(18) << to_pretty_time( res.cpu_limit.used ) << "\n";
+      if(res.subjective_cpu_bill > 0) std::cout << indent << std::left << std::setw(11) << "subjective used:"  << std::right << std::setw(18) << to_pretty_time( res.subjective_cpu_bill)  << "\n";
       std::cout << indent << std::left << std::setw(11) << "available:" << std::right << std::setw(18) << to_pretty_time( res.cpu_limit.available ) << "\n";
       std::cout << indent << std::left << std::setw(11) << "limit:"     << std::right << std::setw(18) << to_pretty_time( res.cpu_limit.max ) << "\n";
       std::cout << std::endl;
