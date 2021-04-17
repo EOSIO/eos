3c0a2445c54991f071ac70cac1f20ca83d46879d
Brian Johnson <johnsonb@objectcomputing.com>
2021-04-17 00:36:15 -0500

    Fixing more errors.

--- tests/privacy_simple_network.py
+++ tests/privacy_simple_network.py
@@ -113,7 +113,7 @@ try:
     def security_group(addNodeNums=[], removeNodeNums=[]):
         def createAction(nodeNums):
             return None if len(nodeNums) == 0 else \
-                "[[{}]]".format(','.join(['"[]"'.format(Node.participantName(nodeNum)) for nodeNum in nodeNums]))
+                "[[{}]]".format(','.join(['"{}"'.format(Node.participantName(nodeNum)) for nodeNum in nodeNums]))
 
         addAction = createAction(addNodeNums)
         removeAction = createAction(removeNodeNums)
