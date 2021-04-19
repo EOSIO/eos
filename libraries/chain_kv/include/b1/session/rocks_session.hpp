7a26b255c093a7ee90619c50a002ea7afda433f1
Santhosh Kumaraswamy <kumaraswamy.santhosh@gmail.com>
2021-04-18 20:40:11 -0400

    Exploring RocksDB read/write options

--- libraries/chain_kv/include/b1/session/rocks_session.hpp
+++ libraries/chain_kv/include/b1/session/rocks_session.hpp
@@ -196,8 +196,8 @@ inline session<rocksdb_t>::session(std::shared_ptr<rocksdb::DB> db, size_t max_i
       m_iterator_read_options{ [&]() {
          auto read_options                                 = rocksdb::ReadOptions{};
          read_options.verify_checksums                     = false;
-         read_options.fill_cache                           = false;
-         read_options.background_purge_on_iterator_cleanup = true;
+//         read_options.fill_cache                           = false;
+//         read_options.background_purge_on_iterator_cleanup = true;
          return read_options;
       }() },
       m_iterators{ [&]() {
