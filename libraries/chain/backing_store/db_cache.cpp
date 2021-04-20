23fff97ca81081cbeedb565bfc3ebd337c0fd1d9
Lin Huang <lin.huang@block.one>
2021-04-20 18:27:03 -0400

    Clean up commented out code

--- libraries/chain/backing_store/db_cache.cpp
+++ libraries/chain/backing_store/db_cache.cpp
@@ -2,58 +2,35 @@
 
 namespace eosio { namespace chain { namespace backing_store {
 
-   //std::map<bytes, bytes> db_cache_store;
-   ////std::unordered_map<eosio::session::shared_bytes, bytes> db_cache_store;
    static std::unordered_map<eosio::session::shared_bytes, eosio::session::shared_bytes> db_cache_store;
 
-   ////void db_cache_insert(const eosio::session::shared_bytes& key, const char* data, std::size_t size) {
-    void db_cache_upsert(const eosio::session::shared_bytes& key, const eosio::session::shared_bytes& value) {
-      //bytes k(key.size());
-      //std::memcpy(k.data(), key.data(), key.size());
-
-      ////bytes b(size);
-      ////std::memcpy(b.data(), data, size);
-      //db_cache_store[k] = b;
-      ////db_cache_store[key] = b;
+   void db_cache_upsert(const eosio::session::shared_bytes& key, const eosio::session::shared_bytes& value) {
       db_cache_store[key] = value;
    }
 
-   ////std::optional<bytes> db_cache_find(const eosio::session::shared_bytes& key) {
-    std::optional<eosio::session::shared_bytes> db_cache_find(const eosio::session::shared_bytes& key) {
-	   /*
-      bytes k(key.size());
-      std::memcpy(k.data(), key.data(), key.size());
-
-      auto it = db_cache_store.find(k);
-      */
+   std::optional<eosio::session::shared_bytes> db_cache_find(const eosio::session::shared_bytes& key) {
       auto it = db_cache_store.find(key);
       if ( it != std::end(db_cache_store) ) {
          return it->second;
       } else {
-	 return {};
+	       return {};
       }
    }
 
-    bool db_cache_contains(const eosio::session::shared_bytes& key) {
+   bool db_cache_contains(const eosio::session::shared_bytes& key) {
       auto it = db_cache_store.find(key);
       if ( it != std::end(db_cache_store) ) {
          return true;
       } else {
-	 return false;
+	       return false;
       }
    }
 
-    void db_cache_remove(const eosio::session::shared_bytes& key) {
-	   /*
-      bytes k(key.size());
-      std::memcpy(k.data(), key.data(), key.size()); 
-      db_cache_store.erase(k);
-      */
+   void db_cache_remove(const eosio::session::shared_bytes& key) {
       db_cache_store.erase(key);
    }
 
-    void db_cache_clear() {
-	   //ilog("====db_cache_store.size: ${s}", ("s", db_cache_store.size()));
+   void db_cache_clear() {
       db_cache_store.clear();
    }
 }}} // namespace eosio::chain::backing_store
