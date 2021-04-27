0e2819889b579f19013628a6e76db129df62102b
Victor Camacho <victor.camacho@block.one>
2021-04-27 14:04:31 -0400

    Change Logic on Producer Plugin to change the account that is going to be the authorizer of the transaction, if there is data on the resource_payer transaction extension

--- plugins/producer_plugin/producer_plugin.cpp
+++ plugins/producer_plugin/producer_plugin.cpp
@@ -466,7 +466,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                app().post( priority::low, [self, future{std::move(future)}, persist_until_expired, next{std::move( next )}, trx{std::move(trx)}, return_failure_trace]() mutable {
                   auto exception_handler = [self, &next, trx{std::move(trx)}](fc::exception_ptr ex) {
                      fc_dlog(_trx_failed_trace_log, "[TRX_TRACE] Speculative execution is REJECTING tx: ${txid}, auth: ${a} : ${why} ",
-                            ("txid", trx->id())("a",trx->get_transaction().authorizer())("why",ex->what()));
+                            ("txid", trx->id())("a",trx->get_transaction().resource_payer())("why",ex->what()));
                      next(ex);
 
                      if (_trx_trace_failure_log.is_enabled(fc::log_level::debug)) {
@@ -508,7 +508,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                         ("block_num", chain.head_block_num() + 1)
                         ("prod", get_pending_block_producer())
                         ("txid", trx->id())
-                        ("a", trx->packed_trx()->get_transaction().authorizer())
+                        ("a", trx->packed_trx()->get_transaction().resource_payer())
                         ("why",std::get<fc::exception_ptr>(response)->what()));
 
                   if (_trx_trace_failure_log.is_enabled(fc::log_level::debug)) {
@@ -521,7 +521,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                } else {
                   fc_dlog(_trx_failed_trace_log, "[TRX_TRACE] Speculative execution is REJECTING tx: ${txid}, auth: ${a} : ${why} ",
                           ("txid", trx->id())
-                          ("a", trx->packed_trx()->get_transaction().authorizer())
+                          ("a", trx->packed_trx()->get_transaction().resource_payer())
                           ("why",std::get<fc::exception_ptr>(response)->what()));
 
                   if (_trx_trace_failure_log.is_enabled(fc::log_level::debug)) {
@@ -538,7 +538,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                             ("block_num", chain.head_block_num() + 1)
                             ("prod", get_pending_block_producer())
                             ("txid", trx->id())
-                            ("a", trx->packed_trx()->get_transaction().authorizer()));
+                            ("a", trx->packed_trx()->get_transaction().resource_payer()));
 
                     if (_trx_trace_success_log.is_enabled(fc::log_level::debug)) {
                         auto entire_trace = chain_plug->get_entire_trx_trace(std::get<transaction_trace_ptr>(response));
@@ -552,7 +552,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                     fc_dlog(_trx_successful_trace_log,
                             "[TRX_TRACE] Speculative execution is ACCEPTING tx: ${txid}, auth: ${a}",
                             ("txid", trx->id())
-                            ("a", trx->packed_trx()->get_transaction().authorizer()));
+                            ("a", trx->packed_trx()->get_transaction().resource_payer()));
 
                     if (_trx_trace_success_log.is_enabled(fc::log_level::debug)) {
                         auto entire_trace = chain_plug->get_entire_trx_trace(std::get<transaction_trace_ptr>(response));
@@ -601,13 +601,13 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                                               || ( persist_until_expired && _disable_subjective_api_billing )
                                               || ( !persist_until_expired && _disable_subjective_p2p_billing );
 
-            auto authorizer = trx->packed_trx()->get_transaction().authorizer();
+            auto resource_payer = trx->packed_trx()->get_transaction().resource_payer();
             uint32_t sub_bill = 0;
             if( !disable_subjective_billing )
-               sub_bill = _subjective_billing.get_subjective_bill( authorizer, fc::time_point::now() );
+               sub_bill = _subjective_billing.get_subjective_bill( resource_payer, fc::time_point::now() );
 
             auto trace = chain.push_transaction( trx, deadline, trx->billed_cpu_time_us, false, sub_bill );
-            fc_dlog( _trx_failed_trace_log, "Subjective bill for ${a}: ${b} elapsed ${t}us", ("a",authorizer)("b",sub_bill)("t",trace->elapsed));
+            fc_dlog( _trx_failed_trace_log, "Subjective bill for ${a}: ${b} elapsed ${t}us", ("a",resource_payer)("b",sub_bill)("t",trace->elapsed));
             if( trace->except ) {
                if( exception_is_exhausted( *trace->except, deadline_is_subjective )) {
                   _unapplied_transactions.add_incoming( trx, persist_until_expired, next );
@@ -638,7 +638,7 @@ class producer_plugin_impl : public std::enable_shared_from_this<producer_plugin
                   _unapplied_transactions.add_persisted( trx );
                } else {
                   // if db_read_mode SPECULATIVE then trx is in the pending block and not immediately reverted
-                  _subjective_billing.subjective_bill( trx->id(), expire, authorizer, trace->elapsed,
+                  _subjective_billing.subjective_bill( trx->id(), expire, resource_payer, trace->elapsed,
                                                        chain.get_read_mode() == chain::db_read_mode::SPECULATIVE );
                }
                send_response( trace );
@@ -1940,15 +1940,15 @@ bool producer_plugin_impl::process_unapplied_trxs( const fc::time_point& deadlin
             auto start = fc::time_point::now();
             auto trx_deadline = start + fc::milliseconds( _max_transaction_time_ms );
 
-            auto authorizer = trx->packed_trx()->get_transaction().authorizer();
-            if( account_fails.failure_limit( authorizer ) ) {
+            auto resource_payer = trx->packed_trx()->get_transaction().resource_payer();
+            if( account_fails.failure_limit( resource_payer ) ) {
                ++num_failed;
                itr = _unapplied_transactions.erase( itr );
                continue;
             }
 
             auto prev_billed_cpu_time_us = trx->billed_cpu_time_us;
-            if( !_subjective_billing.is_disabled() && prev_billed_cpu_time_us > 0 && !rl.is_unlimited_cpu( authorizer )) {
+            if( !_subjective_billing.is_disabled() && prev_billed_cpu_time_us > 0 && !rl.is_unlimited_cpu( resource_payer )) {
                auto prev_billed_plus100 = prev_billed_cpu_time_us + EOS_PERCENT( prev_billed_cpu_time_us, 100 * config::percent_1 );
                auto trx_dl = start + fc::microseconds( prev_billed_plus100 );
                if( trx_dl < trx_deadline ) trx_deadline = trx_dl;
@@ -1963,7 +1963,7 @@ bool producer_plugin_impl::process_unapplied_trxs( const fc::time_point& deadlin
             const uint32_t sub_bill = 0;
 
             auto trace = chain.push_transaction( trx, trx_deadline, prev_billed_cpu_time_us, false, sub_bill );
-            fc_dlog( _trx_failed_trace_log, "Subjective unapplied bill for ${a}: ${b} prev ${t}us", ("a",authorizer)("b",prev_billed_cpu_time_us)("t",trace->elapsed));
+            fc_dlog( _trx_failed_trace_log, "Subjective unapplied bill for ${a}: ${b} prev ${t}us", ("a",resource_payer)("b",prev_billed_cpu_time_us)("t",trace->elapsed));
             if( trace->except ) {
                if( exception_is_exhausted( *trace->except, deadline_is_subjective ) ) {
                   if( block_is_exhausted() ) {
@@ -1973,15 +1973,15 @@ bool producer_plugin_impl::process_unapplied_trxs( const fc::time_point& deadlin
                   }
                   // don't erase, subjective failure so try again next time
                } else {
-                  fc_dlog( _trx_failed_trace_log, "Subjective unapplied bill for failed ${a}: ${b} prev ${t}us", ("a",authorizer)("b",prev_billed_cpu_time_us)("t",trace->elapsed));
+                  fc_dlog( _trx_failed_trace_log, "Subjective unapplied bill for failed ${a}: ${b} prev ${t}us", ("a",resource_payer)("b",prev_billed_cpu_time_us)("t",trace->elapsed));
                   auto failure_code = trace->except->code();
                   if( failure_code != tx_duplicate::code_value ) {
                      // this failed our configured maximum transaction time, we don't want to replay it
                      fc_dlog( _log, "Failed ${c} trx, prev billed: ${p}us, ran: ${r}us, id: ${id}",
                               ("c", trace->except->code())("p", prev_billed_cpu_time_us)
                               ("r", fc::time_point::now() - start)("id", trx->id()) );
-                     account_fails.add( authorizer, failure_code );
-                     _subjective_billing.subjective_bill_failure( authorizer, trace->elapsed, fc::time_point::now() );
+                     account_fails.add( resource_payer, failure_code );
+                     _subjective_billing.subjective_bill_failure( resource_payer, trace->elapsed, fc::time_point::now() );
                   }
                   ++num_failed;
                   if( itr->next ) itr->next( trace );
@@ -1989,9 +1989,9 @@ bool producer_plugin_impl::process_unapplied_trxs( const fc::time_point& deadlin
                   continue;
                }
             } else {
-               fc_dlog( _trx_successful_trace_log, "Subjective unapplied bill for success ${a}: ${b} prev ${t}us", ("a",authorizer)("b",prev_billed_cpu_time_us)("t",trace->elapsed));
+               fc_dlog( _trx_successful_trace_log, "Subjective unapplied bill for success ${a}: ${b} prev ${t}us", ("a",resource_payer)("b",prev_billed_cpu_time_us)("t",trace->elapsed));
                // if db_read_mode SPECULATIVE then trx is in the pending block and not immediately reverted
-               _subjective_billing.subjective_bill( trx->id(), trx->packed_trx()->expiration(), authorizer, trace->elapsed,
+               _subjective_billing.subjective_bill( trx->id(), trx->packed_trx()->expiration(), resource_payer, trace->elapsed,
                                                     chain.get_read_mode() == chain::db_read_mode::SPECULATIVE );
                ++num_applied;
                if( itr->trx_type != trx_enum_type::persisted ) {
