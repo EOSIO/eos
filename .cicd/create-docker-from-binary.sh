c63de1c7fb2d0a9b09f537ed96a1d93d415eb1bf
Zach Butler <zach.butler@block.one>
2021-05-14 18:44:08 -0400

    More useful print statements

--- .cicd/create-docker-from-binary.sh
+++ .cicd/create-docker-from-binary.sh
@@ -2,7 +2,6 @@
 echo '--- :evergreen_tree: Configuring Environment'
 set -euo pipefail
 buildkite-agent artifact download '*.deb' --step ':ubuntu: Ubuntu 18.04 - Package Builder' .
-echo ":done: download successful"
 SANITIZED_BRANCH="$(sanitize "$BUILDKITE_BRANCH")"
 echo "Branch '$BUILDKITE_BRANCH' sanitized as '$SANITIZED_BRANCH'."
 SANITIZED_TAG="$(sanitize "$BUILDKITE_TAG")"
@@ -20,12 +19,13 @@ EOSIO_REGS=("$EOSIO_REGISTRY" "$DOCKERHUB_REGISTRY")
 for REG in ${EOSIO_REGS[@]}; do
     DOCKER_TAG_COMMIT="docker tag eosio_image:$BUILD_TAG $REG:$BUILDKITE_COMMIT"
     DOCKER_TAG_BRANCH="docker tag eosio_image:$BUILD_TAG $REG:$SANITIZED_BRANCH"
-    echo -e "$ Tagging Images: \n$DOCKER_TAG_COMMIT \n$DOCKER_TAG_BRANCH"
+    echo "$ $DOCKER_TAG_COMMIT"
     eval $DOCKER_TAG_COMMIT 
+    echo "$ $DOCKER_TAG_BRANCH"
     eval $DOCKER_TAG_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
         DOCKER_TAG="docker tag eosio_image:$BUILD_TAG $REG:$SANITIZED_TAG"
-        echo -e "$ \n Tagging Image: \n$DOCKER_TAG"
+        echo "$ $DOCKER_TAG"
         eval $DOCKER_TAG
     fi
 done
@@ -34,11 +34,13 @@ echo '--- :arrow_up: Push Container'
 for REG in ${EOSIO_REGS[@]}; do
     DOCKER_PUSH_COMMIT="docker push $REG:$BUILDKITE_COMMIT"
     DOCKER_PUSH_BRANCH="docker push $REG:$SANITIZED_BRANCH"
-    echo -e "$ Pushing Images: \n$DOCKER_PUSH_COMMIT \n$DOCKER_PUSH_BRANCH"
+    echo "$ $DOCKER_PUSH_COMMIT"
     eval $DOCKER_PUSH_COMMIT 
+    echo "$ $DOCKER_PUSH_BRANCH"
     eval $DOCKER_PUSH_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
         DOCKER_PUSH_TAG="docker push $REG:$SANITIZED_TAG"
+        echo "$ $DOCKER_PUSH_TAG"
         eval $DOCKER_PUSH_TAG
     fi
 done
@@ -47,15 +49,16 @@ echo '--- :put_litter_in_its_place: Cleanup'
 for REG in ${EOSIO_REGS[@]}; do
     CLEAN_IMAGE_COMMIT="docker rmi $REG:$BUILDKITE_COMMIT || :"
     CLEAN_IMAGE_BRANCH="docker rmi $REG:$SANITIZED_BRANCH || :"
-    echo -e "Cleaning Up: \n$CLEAN_IMAGE_COMMIT \n$CLEAN_IMAGE_BRANCH$"
+    echo "$ $CLEAN_IMAGE_COMMIT"
     eval $CLEAN_IMAGE_COMMIT 
+    echo "$ $CLEAN_IMAGE_BRANCH"
     eval $CLEAN_IMAGE_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
         DOCKER_REM="docker rmi $REG:$SANITIZED_TAG || :"
-        echo -e "$ Cleaning Up: \n$DOCKER_REM"
+        echo "$ $DOCKER_REM"
         eval $DOCKER_REM
     fi
 done
 DOCKER_GEN="docker rmi eosio_image:$BUILD_TAG || :"
-echo "Clean up base image"
+echo "$ $DOCKER_GEN"
 eval $DOCKER_GEN
