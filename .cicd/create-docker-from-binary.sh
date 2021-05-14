70a1abe371c6ff76a88385ce0f75f83d331458cc
Zach Butler <zach.butler@block.one>
2021-05-14 18:50:45 -0400

    String quoting

--- .cicd/create-docker-from-binary.sh
+++ .cicd/create-docker-from-binary.sh
@@ -8,7 +8,7 @@ SANITIZED_TAG="$(sanitize "$BUILDKITE_TAG")"
 [[ -z "$SANITIZED_TAG" ]] || echo "Branch '$BUILDKITE_TAG' sanitized as '$SANITIZED_TAG'."
 # docker build
 echo "+++ :docker: Build Docker Container"
-DOCKERHUB_REGISTRY="docker.io/eosio/eosio"
+DOCKERHUB_REGISTRY='docker.io/eosio/eosio'
 IMAGE="${DOCKERHUB_REGISTRY}:${BUILDKITE_COMMIT:-latest}"
 DOCKER_BUILD_GEN="docker build -t '$IMAGE' -f ./docker/dockerfile ."
 echo "$ $DOCKER_BUILD_GEN"
@@ -17,14 +17,14 @@ eval $DOCKER_BUILD_GEN
 echo '--- :label: Tag Container'
 EOSIO_REGS=("$EOSIO_REGISTRY" "$DOCKERHUB_REGISTRY")
 for REG in ${EOSIO_REGS[@]}; do
-    DOCKER_TAG_COMMIT="docker tag '$IMAGE' $REG:$BUILDKITE_COMMIT"
-    DOCKER_TAG_BRANCH="docker tag '$IMAGE' $REG:$SANITIZED_BRANCH"
+    DOCKER_TAG_COMMIT="docker tag '$IMAGE' '$REG:$BUILDKITE_COMMIT'"
+    DOCKER_TAG_BRANCH="docker tag '$IMAGE' '$REG:$SANITIZED_BRANCH'"
     echo "$ $DOCKER_TAG_COMMIT"
     eval $DOCKER_TAG_COMMIT 
     echo "$ $DOCKER_TAG_BRANCH"
     eval $DOCKER_TAG_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
-        DOCKER_TAG="docker tag '$IMAGE' $REG:$SANITIZED_TAG"
+        DOCKER_TAG="docker tag '$IMAGE' '$REG:$SANITIZED_TAG'"
         echo "$ $DOCKER_TAG"
         eval $DOCKER_TAG
     fi
@@ -32,14 +32,14 @@ done
 # docker push
 echo '--- :arrow_up: Push Container'
 for REG in ${EOSIO_REGS[@]}; do
-    DOCKER_PUSH_COMMIT="docker push $REG:$BUILDKITE_COMMIT"
-    DOCKER_PUSH_BRANCH="docker push $REG:$SANITIZED_BRANCH"
+    DOCKER_PUSH_COMMIT="docker push '$REG:$BUILDKITE_COMMIT'"
+    DOCKER_PUSH_BRANCH="docker push '$REG:$SANITIZED_BRANCH'"
     echo "$ $DOCKER_PUSH_COMMIT"
     eval $DOCKER_PUSH_COMMIT 
     echo "$ $DOCKER_PUSH_BRANCH"
     eval $DOCKER_PUSH_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
-        DOCKER_PUSH_TAG="docker push $REG:$SANITIZED_TAG"
+        DOCKER_PUSH_TAG="docker push '$REG:$SANITIZED_TAG'"
         echo "$ $DOCKER_PUSH_TAG"
         eval $DOCKER_PUSH_TAG
     fi
@@ -47,14 +47,14 @@ done
 # docker rmi
 echo '--- :put_litter_in_its_place: Cleanup'
 for REG in ${EOSIO_REGS[@]}; do
-    CLEAN_IMAGE_COMMIT="docker rmi $REG:$BUILDKITE_COMMIT || :"
-    CLEAN_IMAGE_BRANCH="docker rmi $REG:$SANITIZED_BRANCH || :"
+    CLEAN_IMAGE_COMMIT="docker rmi '$REG:$BUILDKITE_COMMIT' || :"
+    CLEAN_IMAGE_BRANCH="docker rmi '$REG:$SANITIZED_BRANCH' || :"
     echo "$ $CLEAN_IMAGE_COMMIT"
     eval $CLEAN_IMAGE_COMMIT 
     echo "$ $CLEAN_IMAGE_BRANCH"
     eval $CLEAN_IMAGE_BRANCH
     if [[ ! -z "$SANITIZED_TAG" ]]; then
-        DOCKER_REM="docker rmi $REG:$SANITIZED_TAG || :"
+        DOCKER_REM="docker rmi '$REG:$SANITIZED_TAG' || :"
         echo "$ $DOCKER_REM"
         eval $DOCKER_REM
     fi
