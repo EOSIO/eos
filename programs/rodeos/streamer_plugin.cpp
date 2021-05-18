44ef5382ef5e593194638289583b8d104b7af528
Huang-Ming Huang <huangh@objectcomputing.com>
2021-05-18 15:10:01 -0500

    comment span in streamer_plugin

--- programs/rodeos/streamer_plugin.cpp
+++ programs/rodeos/streamer_plugin.cpp
@@ -20,9 +20,9 @@ using namespace std::literals;
 struct streamer_plugin_impl : public streamer_t {
 
    void start_block(uint32_t block_num) override {
-      auto blk_trace = fc_create_trace("Block");
-      auto blk_span = fc_create_span(blk_trace, "Filter-Start");
-      fc_add_tag( blk_span, "block_num", block_num );
+      // auto blk_trace = fc_create_trace("Block");
+      // auto blk_span = fc_create_span(blk_trace, "Filter-Start");
+      // fc_add_tag( blk_span, "block_num", block_num );
 
       for (const auto& stream : streams) {
          stream->start_block(block_num);
@@ -45,9 +45,9 @@ struct streamer_plugin_impl : public streamer_t {
    }
 
    void stop_block(uint32_t block_num) override {
-      auto blk_trace = fc_create_trace("Block");
-      auto blk_span = fc_create_span(blk_trace, "Filter-Stop");
-      fc_add_tag( blk_span, "block_num", block_num );
+      // auto blk_trace = fc_create_trace("Block");
+      // auto blk_span = fc_create_span(blk_trace, "Filter-Stop");
+      // fc_add_tag( blk_span, "block_num", block_num );
 
       for (const auto& stream : streams) {
          stream->stop_block(block_num);
