d02a7eab8cca69ee4106408506a30f404d1c1e3b
Jonathan Giszczak <jonrg@hypercubepc.com>
2021-05-20 13:56:43 -0500

    Rename get_contract_query to push_ro_transaction.

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -3183,7 +3183,7 @@ read_only::get_transaction_id_result read_only::get_transaction_id( const read_o
    return params.id();
 }
 
-void read_only::get_contract_query(const read_only::get_contract_query_params& params, chain::plugin_interface::next_function<read_only::get_contract_query_results> next) const {
+void read_only::push_ro_transaction(const read_only::push_ro_transaction_params& params, chain::plugin_interface::next_function<read_only::push_ro_transaction_results> next) const {
    try {
       packed_transaction_v0 input_trx_v0;
       auto resolver = make_resolver(db, abi_serializer::create_yield_function( abi_serializer_max_time ));
@@ -3240,8 +3240,8 @@ void read_only::get_contract_query(const read_only::get_contract_query_params& p
                      pending_transactions.push_back(std::get<packed_transaction>(receipt.trx).id());
                   }
                }
-               next(read_only::get_contract_query_results{db.head_block_num(), db.head_block_id(), db.last_irreversible_block_num(), db.last_irreversible_block_id(),
-                                                          accnt_metadata_obj.code_hash, pending_transactions, output});
+               next(read_only::push_ro_transaction_results{db.head_block_num(), db.head_block_id(), db.last_irreversible_block_num(), db.last_irreversible_block_id(),
+                                                           accnt_metadata_obj.code_hash, pending_transactions, output});
             } CATCH_AND_CALL(next);
          }
       });
