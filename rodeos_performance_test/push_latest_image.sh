a266f3d865ed9174f749724b79e45e3c4e1dbe94
Huang-Ming Huang <huangh@objectcomputing.com>
2021-05-28 07:50:08 -0500

    fix swarm deployment

--- rodeos_performance_test/push_latest_image.sh
+++ rodeos_performance_test/push_latest_image.sh
@@ -4,5 +4,4 @@ MANAGER_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
 IMAGE_ID=$(docker images eos-boxed:latest -q)
 docker tag eos-boxed:latest $MANAGER_IP:5443/eos-boxed:${IMAGE_ID}
 docker push $MANAGER_IP:5443/eos-boxed:${IMAGE_ID}
-export EOS_IMAGE=$MANAGER_IP:5443/eos-boxed:${IMAGE_ID}
 
