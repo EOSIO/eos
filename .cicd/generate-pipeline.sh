05f73a262b5dd2fdff8c7fe50faf2e3b2c905415
William Blevins <william.blevins@block.one>
2021-06-22 14:37:27 -0400

    Include MacOS 10.15 skip flag. Do not allow partial brew update.

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -701,7 +701,7 @@ cat <<EOF
     agents:
       queue: "automation-basic-builder-fleet"
     timeout: "${TIMEOUT:-5}"
-    skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}
+    skip: ${SKIP_PACKAGE_BUILDER}${SKIP_MAC}${SKIP_MACOS_10_14}${SKIP_MACOS_10_15}
 
   - label: ":docker: :ubuntu: Docker - Build 18.04 Docker Image"
     command:  "./.cicd/create-docker-from-binary.sh"
