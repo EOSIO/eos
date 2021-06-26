42d9c65e5c2e31a96313f1dac18ccc92e2e53a75
Zach Butler <zach.butler@block.one>
2021-06-25 21:47:09 -0400

    Include test metrics step in test stability runs

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -530,7 +530,7 @@ EOF
     fi
 fi
 # pipeline tail
-[[ -z "$TEST" ]] && cat <<EOF
+cat <<EOF
   - wait:
     continue_on_failure: true
 
@@ -545,6 +545,8 @@ fi
     timeout: ${TIMEOUT:-10}
     soft_fail: true
 
+EOF
+[[ -z "$TEST" ]] && cat <<EOF
   - wait
 
     # packaging
