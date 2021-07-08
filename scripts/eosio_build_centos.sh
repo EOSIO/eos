d1540bdc07af5be675b6cb78a222bd5e73a008d7
Jonathan Giszczak <jonrg@hypercubepc.com>
2021-07-08 17:38:57 -0500

    Add CentOS 8 specific dependencies and don't force clang build.
    Use boost 1.69.

--- scripts/eosio_build_centos.sh
+++ scripts/eosio_build_centos.sh
@@ -39,14 +39,31 @@ if [[ "$(echo ${VERSION} | sed 's/ .*//g')" == 8 ]]; then
         install-package https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
         group-install-package 'Development Tools'
         install-package openssl-devel
+        # Repo necessary for rh-python3, devtoolset-8 and llvm-toolset-7.0
+        ensure-scl
+        # GCC8 for Centos / Needed for CMAKE install even if we're pinning
+        ensure-devtoolset
+        if [[ -d /opt/rh/devtoolset-8 ]]; then
+	        echo "${COLOR_CYAN}[Enabling Centos devtoolset-8 (so we can use GCC 8)]${COLOR_NC}"
+	        execute-always source /opt/rh/devtoolset-8/enable
+	        echo " - ${COLOR_GREEN}Centos devtoolset-8 successfully enabled!${COLOR_NC}"
+        fi
+
+        # Ensure packages exist
+        ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos8_deps"
+        export PYTHON3PATH="/opt/rh/rh-python36"
+        if $DRYRUN || [ -d $PYTHON3PATH ]; then
+	        echo "${COLOR_CYAN}[Enabling python36]${COLOR_NC}"
+	        execute source $PYTHON3PATH/enable
+	        echo " ${COLOR_GREEN}- Python36 successfully enabled!${COLOR_NC}"
+	        echo ""
+        fi
 fi
 
 # Handle clang/compiler
 ensure-compiler
 # CMAKE Installation
 ensure-cmake
-# CLANG Installation
-build-clang
 # LLVM Installation
 ensure-llvm
 # BOOST Installation
