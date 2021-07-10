90ec7b9c251acaef1fb1ae53d5d638f3e1f22f4c
Jonathan Giszczak <jonrg@hypercubepc.com>
2021-07-09 21:18:58 -0500

    Update CentOS 8 dependencies.  Fix boost build, all platforms.
    Set up postgresql in CentOS 8 pinned docker build using CentOS 8 idiom.

--- scripts/eosio_build_centos.sh
+++ scripts/eosio_build_centos.sh
@@ -13,32 +13,6 @@ echo "Disk space available: ${DISK_AVAIL}G"
 echo ""
 
 if [[ "$(echo ${VERSION} | sed 's/ .*//g')" == 7 ]]; then
-# Repo necessary for rh-python3, devtoolset-8 and llvm-toolset-7.0
-ensure-scl
-# GCC8 for Centos / Needed for CMAKE install even if we're pinning
-ensure-devtoolset
-if [[ -d /opt/rh/devtoolset-8 ]]; then
-	echo "${COLOR_CYAN}[Enabling Centos devtoolset-8 (so we can use GCC 8)]${COLOR_NC}"
-	execute-always source /opt/rh/devtoolset-8/enable
-	echo " - ${COLOR_GREEN}Centos devtoolset-8 successfully enabled!${COLOR_NC}"
-fi
-
-# Ensure packages exist
-ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos7_deps"
-export PYTHON3PATH="/opt/rh/rh-python36"
-if $DRYRUN || [ -d $PYTHON3PATH ]; then
-	echo "${COLOR_CYAN}[Enabling python36]${COLOR_NC}"
-	execute source $PYTHON3PATH/enable
-	echo " ${COLOR_GREEN}- Python36 successfully enabled!${COLOR_NC}"
-	echo ""
-fi
-fi
-
-if [[ "$(echo ${VERSION} | sed 's/ .*//g')" == 8 ]]; then
-        echo "Install Development Tools ..."
-        install-package https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-        group-install-package 'Development Tools'
-        install-package openssl-devel
         # Repo necessary for rh-python3, devtoolset-8 and llvm-toolset-7.0
         ensure-scl
         # GCC8 for Centos / Needed for CMAKE install even if we're pinning
@@ -48,6 +22,23 @@ if [[ "$(echo ${VERSION} | sed 's/ .*//g')" == 8 ]]; then
 	        execute-always source /opt/rh/devtoolset-8/enable
 	        echo " - ${COLOR_GREEN}Centos devtoolset-8 successfully enabled!${COLOR_NC}"
         fi
+
+        # Ensure packages exist
+        ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos7_deps"
+        export PYTHON3PATH="/opt/rh/rh-python36"
+        if $DRYRUN || [ -d $PYTHON3PATH ]; then
+	        echo "${COLOR_CYAN}[Enabling python36]${COLOR_NC}"
+	        execute source $PYTHON3PATH/enable
+	        echo " ${COLOR_GREEN}- Python36 successfully enabled!${COLOR_NC}"
+	        echo ""
+        fi
+fi
+
+if [[ "$(echo ${VERSION} | sed 's/ .*//g')" == 8 ]]; then
+        echo "Install Development Tools ..."
+        install-package https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+        group-install-package 'Development Tools'
+        install-package openssl-devel
 
         # Ensure packages exist
         ensure-yum-packages "${REPO_ROOT}/scripts/eosio_build_centos8_deps"
