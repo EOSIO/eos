eddf0c01ccc76072434b8189831f444cf2915352
Huang-Ming Huang <huangh@objectcomputing.com>
2021-07-14 18:22:28 -0500

    fix ports

--- vcpkg-ports/yubihsm/portfile.cmake
+++ vcpkg-ports/yubihsm/portfile.cmake
@@ -5,18 +5,12 @@ if(VCPKG_TARGET_IS_LINUX)
     message("${PORT} currently requires the libusb from the system package manager:\n\nThese can be installed via apt-get install libusb-1.0-0-dev on Ubuntu systems\n or via yum install libusbx-devel on CentOS systems.")
 endif()
 
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/Yubico/yubihsm-shell/archive/${YUBIHSM_VERSION}.tar.gz"
-    FILENAME "yubihsm-shell-src.tar.gz"
-    SHA512 0224a64145a39ace015d56e008804d7a179711915d389be6e3c4b0a0a14f2d3c95d0aceaf0b779e70fc124606b50a3d5886f8fcd11427499c140b504495f70c6
-)
-
-vcpkg_extract_source_archive_ex(
+vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${ARCHIVE}
-    REF ${YUBIHSM_VERSION}
-    PATCHES
-      yubihsm.patch
+    REPO Yubico/yubihsm-shell
+    REF 2.2.0
+    SHA512 e4a77fbf920a826ce3df5552ef94e329bca3e1e26bdb5cb0b80e30fabbd0cdc8c338d11cae3d00ec3b00d7b4688f9fad8ded64d42bfcf09df393a7a75a5ec551
+    PATCHES yubihsm.patch
 )
 
 vcpkg_configure_cmake(
