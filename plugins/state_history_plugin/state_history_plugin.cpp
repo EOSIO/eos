09583648e79bb8fe0153341b2b3df24f8c25d21c
Kevin Heifner <heifnerk@objectcomputing.com>
2021-07-19 08:32:10 -0500

    Try a sync write in strand env

--- plugins/state_history_plugin/state_history_plugin.cpp
+++ plugins/state_history_plugin/state_history_plugin.cpp
@@ -226,17 +226,22 @@ struct state_history_plugin_impl : std::enable_shared_from_this<state_history_pl
 
          auto span = fc_create_span_from_token(i.token, "send");
          fc_add_tag(span, "buffer_size", i.data->size());
-         fc_ilog(_log, "async_write, block_num: ${b}", ("b", i.block_num));
+//         fc_ilog(_log, "async_write, block_num: ${b}", ("b", i.block_num));
+         fc_ilog(_log, "write, block_num: ${b}", ("b", i.block_num));
          sending = true;
-         this->derived_session().socket_stream->async_write(boost::asio::buffer(*i.data),
-               boost::asio::bind_executor( work_strand,
-                 [ptr=i.data, self = derived_session().shared_from_this(), block_num=i.block_num]( boost::system::error_code ec, std::size_t w ) {
-                    fc_ilog(_log, "async_write complete, block_num: ${b}", ("b", block_num));
-                    self->sending = false;
-                    self->callback(ec, "async_write", [self]() { self->send_next(); });
-               }) );
+         this->derived_session().socket_stream->write(boost::asio::buffer(*i.data));
+//         this->derived_session().socket_stream->async_write(boost::asio::buffer(*i.data),
+//               boost::asio::bind_executor( work_strand,
+//                 [ptr=i.data, self = derived_session().shared_from_this(), block_num=i.block_num]( boost::system::error_code ec, std::size_t w ) {
+//                    fc_ilog(_log, "async_write complete, block_num: ${b}", ("b", block_num));
+//                    self->sending = false;
+//                    self->callback(ec, "async_write", [self]() { self->send_next(); });
+//               }) );
 
+         fc_ilog(_log, "write complete, block_num: ${b}", ("b", i.block_num));
+         sending = false;
          send_queue.pop();
+         callback(boost::system::error_code{}, "", [&]() { send_next(); });
       }
 
       // called from main thread
