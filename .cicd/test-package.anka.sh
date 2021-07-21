11f0e9e7a588faf359738827a93f9e9577d02fdd
William Blevins <william.blevins@block.one>
2021-07-21 17:32:38 -0400

    Remove unneeded check for prebuilt images.

--- .cicd/test-package.anka.sh
+++ .cicd/test-package.anka.sh
@@ -5,11 +5,6 @@ set -euo pipefail
 
 echo '--- :anka: Pretest Setup'
 
-if [[ ! $(brew --version 2>/dev/null) ]]; then
-   perform 'echo | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"'
-   perform 'brew analytics off'
-fi
-
 if [[ ! $(python3 --version 2>/dev/null) ]]; then
    perform 'brew update'
    perform 'brew install python3'
