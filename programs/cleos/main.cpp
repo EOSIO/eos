9ad8099250f5b367ab6c2933bfd3336b86264db5
Jonathan Giszczak <jonrg@hypercubepc.com>
2021-08-11 17:27:32 -0500

    Add fallback to v1 send API when possible.

--- programs/cleos/main.cpp
+++ programs/cleos/main.cpp
@@ -484,11 +484,15 @@ fc::variant push_transaction( signed_transaction& trx, const std::vector<public_
                } else {
                   return call(send_txn_func_v2, args);
                }
-            } catch( chain::missing_chain_api_plugin_exception& ) {
-               std::cerr << "New RPC /v2/chain/send_transaction or send_ro_transaction may not be supported." << std::endl
-                        << "Add flag --use-old-send-rpc or --use-old-rpc to use old RPC send_transaction or " << std::endl
-                        << "push_transaction instead or submit your transaction to a different node." << std::endl;
-               throw;
+            }
+            catch (chain::missing_chain_api_plugin_exception &) {
+               if (tx_read_only || tx_rtn_failure_trace) {
+                  std::cerr << "New RPC /v2/chain/send_transaction or send_ro_transaction may not be supported." << std::endl
+                           << "Add flag --use-old-send-rpc or --use-old-rpc to use old RPC send_transaction or " << std::endl
+                           << "push_transaction instead or submit your transaction to a different node." << std::endl;
+                  throw;
+               }
+               return call(send_txn_func, pt_v0);  // With compatible options, silently fall back to v1 API
             }
          }
       }
