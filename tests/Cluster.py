7d2fcbf8f3c943b0c36d0fe50ed06f679fc700dc
Kevin Heifner <heifnerk@objectcomputing.com>
2021-08-13 12:48:04 -0500

    Update tests to create queue via curl

--- tests/Cluster.py
+++ tests/Cluster.py
@@ -617,6 +617,12 @@ class Cluster(object):
         return ret
 
     @staticmethod
+    def createAMQPQueue(queueName):
+        cmd="curl -u guest:guest -H \"content-type:application/json\" -XPUT http://127.0.0.1:15672/api/queues/%2F/%s -d'{\"durable\":true}' " % \
+            (queueName)
+        return Utils.runCmdReturnStr(cmd)
+
+    @staticmethod
     def getClientVersion(verbose=False):
         """Returns client version (string)"""
         p = re.compile(r'^Build version:\s(\w+)\n$')
