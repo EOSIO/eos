a2182ecc030f0466950e584c3a5c86c71a2098fc
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-09-16 15:55:15 -0400

    Update generate-pipeline.sh
    Added new tests to pipeline script

--- .cicd/generate-pipeline.sh
+++ .cicd/generate-pipeline.sh
@@ -347,7 +347,9 @@ EOF
                  [[ "$TEST_NAME" != 'rodeos_idle_restart_producer_eos-vm-oc_test' ]] && \
                  [[ "$TEST_NAME" != 'rodeos_idle_restart_rodeos_eos-vm-oc_test' ]] && \
                  [[ "$TEST_NAME" != 'rodeos_idle_restart_ship_eos-vm-oc_test' ]] && \
-                 [[ "$TEST_NAME" != 'rodeos_idle_restart_rodeos_producer_eos-vm-oc_test' ]] 
+                 [[ "$TEST_NAME" != 'rodeos_idle_restart_rodeos_producer_eos-vm-oc_test' ]] && \
+                 [[ "$TEST_NAME" != 'rodeos_idle_multi_ship_kill_clean_restart_eos_vm_oc' ]] && \
+                 [[ "$TEST_NAME" != 'rodeos_idle_multi_ship_kill_restart_eos_vm_oc' ]]
                  then
                 cat <<EOF
   - label: "$(echo "$PLATFORM_JSON" | jq -r .ICON) $(echo "$PLATFORM_JSON" | jq -r .PLATFORM_NAME_FULL) - $TEST_NAME"
