5cb5a08139367e13458f55c9724081361becb5d7
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-10-04 13:58:39 -0400

    Update rodeos_wasmQL_http_timeout.py

--- tests/rodeos_wasmQL_http_timeout.py
+++ tests/rodeos_wasmQL_http_timeout.py
@@ -6,18 +6,8 @@ from rodeos_utils import RodeosCluster
 from TestHelper import AppArgs
 import time
 import os
-
-try: 
-    import requests
-except ImportError:
-    os.system('pip install requests')
-    import requests
-
-try: 
-    import requests_unixsocket
-except ImportError:
-    os.system('pip install requests_unixsocket')
-    import requests_unixsocket
+import requests
+import requests_unixsocket
 
 ###############################################################
 # rodeos_wasmQL http time out test
