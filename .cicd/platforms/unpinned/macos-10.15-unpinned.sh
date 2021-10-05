69feae9ad616ac5040a57a8e79fed1d1ab8b7745
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-10-05 15:01:40 -0400

    Update macos-10.15-unpinned.sh

--- .cicd/platforms/unpinned/macos-10.15-unpinned.sh
+++ .cicd/platforms/unpinned/macos-10.15-unpinned.sh
@@ -3,10 +3,12 @@ set -eo pipefail
 VERSION=1
 export SDKROOT="$(xcrun --sdk macosx --show-sdk-path)"
 brew update
-brew install git cmake python brew-pip libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl jq boost libpq libpqxx postgres rabbitmq || :
+brew install git cmake python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl jq boost libpq libpqxx postgres rabbitmq || :
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
-# install request and requests_requests_unixsocket for rodeos http timeout test
+# install pip, request and requests_requests_unixsocket for rodeos http timeout test
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
 pip install requests && pip install requests_unixsocket
 # add sbin to path from rabbitmq-server
 echo "export PATH=$PATH:/usr/local/sbin" >> ~/.bash_profile
