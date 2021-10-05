75600b61b0c364e2f7a770eae6ec324857779b3d
Farhad Shahabi <farhad.shahabi@colorado.edu>
2021-10-05 15:57:31 -0400

    Update macos-10.15-unpinned.sh

--- .cicd/platforms/unpinned/macos-10.15-unpinned.sh
+++ .cicd/platforms/unpinned/macos-10.15-unpinned.sh
@@ -6,10 +6,8 @@ brew update
 brew install git cmake python libtool libusb graphviz automake wget gmp pkgconfig doxygen openssl jq boost libpq libpqxx postgres rabbitmq || :
 # install nvm for ship_test
 cd ~ && brew install nvm && mkdir -p ~/.nvm && echo "export NVM_DIR=$HOME/.nvm" >> ~/.bash_profile && echo 'source $(brew --prefix nvm)/nvm.sh' >> ~/.bash_profile && cat ~/.bash_profile && source ~/.bash_profile && echo $NVM_DIR && nvm install --lts=dubnium
-# install pip, request and requests_requests_unixsocket for rodeos http timeout test
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python get-pip.py
-pip install requests && pip install requests_unixsocket
+# install request and requests_requests_unixsocket for rodeos http timeout test
+pip3 install requests && pip3 install requests_unixsocket
 # add sbin to path from rabbitmq-server
 echo "export PATH=$PATH:/usr/local/sbin" >> ~/.bash_profile
 # initialize postgres configuration files
