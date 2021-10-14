aac3ea389a885e305ea122289c352bddfdf3ec8b
Patrick Raphael <patrick.raphael@block.one>
2021-10-14 13:51:50 -0400

    blockchain_audit_tool: add ability to compare to reference, rework arg parsing, detect new accounts added, warn if info could not all be retrieved within single block

--- scripts/blockchain_audit_tool.py
+++ scripts/blockchain_audit_tool.py
@@ -3,18 +3,24 @@
 import http.client
 import sys
 import json
+import re
 
 USAGE = "\
-Usage: (1) blockchain_audit_tool.py [<rpc_endpoint> [<page_size> [<scope_limit> [<table_row_limit>]]]]\n\
-       (2) blockchain_audit_tool.py --help"
+Usage: blockchain_audit_tool.py [OPTIONS] [<rpc_endpoint>]\n"
 
 HELP_INFO = "\
     Retrieve info from EOSIO blockchain.\n\
     <rpc_endpoint> RPC endpoint URI of nodeos with chain_api_plugin enabled. Defaults to 'localhost:8888'\n\
-      <page_size>  Integer > 0. The 'limit' field in RPC queries for accounts and.  Default 1024\n\
-    <scope_limit>  Integer > 0. The 'limit' field when calling /v1/chain/get_table_by_scope.  Defualts 10 \n\
-<table_row_limit>  Integer > 0. The 'limit' field when calling /v1/chain/get_table_rows and.  Defaults 10 \n\
-    Outputs to stdout in a human readable format, and write to JSON file blockcahin_audit_data.json:\n\
+    OPTIONS:\n\
+                  --help  Print help info and exit\n\
+       --comp <filepath>  Do not query nodeos.  Instead use <filepath> as basis for comparison combined with '--ref' option\n\
+           -o <filepath>  Output to filepath instead of default 'blockchain_audit_data.json'\n\
+       --page_size <num>  Integer > 0, default 1024. The 'limit' field in RPC queries for accounts and.  Default 1024\n\
+        --ref <filepath>  Use <filepath> to comparefor non-transient data to use comparison\n\
+     --scope-limit <num>  Integer > 0, default 10. The 'limit' field when calling /v1/chain/get_table_by_scope. \n\
+ --table-row-limit <num>  Integer > 0, default 10. The 'limit' field when calling /v1/chain/get_table_rows and. \n\
+    Outputs to stdout in a human readable format, and write to JSON file specified by '-o' which defaults to:\n\
+         'blockchain_audit_data.json'\n\
     Status and error info is written to stderr.\n\
     Information currently includes:\n\
         Full server info\n\
@@ -24,6 +30,99 @@ HELP_INFO = "\
         Activated protocol features\n\
         Preview of MI and KV tables on each account\n\
         Deferred transactions"
+
+class ArgumentParseError(Exception):
+    def __init__(self, msg):
+        Exception()
+        self.msg = msg
+
+# parse argument option
+# assumes argv[i] starts with '-'
+# options map is an map of the argument to a 2-tuple of the option name
+# and default parameter value
+# if default value is not None, it will attempt to parse the parameter
+# either with an '-option=value' ro an '-option value' semantic
+# if default value is None, the param value will be 'True' (bool type)
+# to indicate parameter is present
+# otherwise parameter values will remain strings (no type conversion is attempted)
+# returns nexted tuple ((option, value), i)
+def parseArgOption(argv, i, optionsMap):
+    arg = argv[i]
+
+    nArg = len(argv)
+    if arg in optionsMap:
+        e = optionsMap[arg]
+        option = e[0]
+        if e[1] is None:  # no parameter, assume boolean
+            return (option, True), i+1
+        elif isinstance(e[1], tuple):
+            reqArgs = len(e[1])
+            i += 1
+            if i + reqArgs > nArg:
+                raise ArgumentParseError(f"option '{arg}' expected {reqArgs} parameters")
+            return (option, tuple(argv[i:i+reqArgs])), i+reqArgs
+        else:
+            i += 1
+            if i >= nArg:
+                raise ArgumentParseError(f"option '{arg}' expected parameter")
+            a = argv[i]
+            # for integer arguments, attempt conversion
+            if isinstance(e[1], int):
+                try:
+                    a = int(a)
+                except:
+                    raise ArgumentParseError(f"option '{arg}' expected integer argument, got '{arg[i]}'")
+            return (option, a), i+1
+    else:
+        r = re.split("=", arg)
+        if r[0] not in optionsMap:
+            raise ArgumentParseError(f"unkonwn option '{r[0]}'")
+        if len(r) > 2:
+            raise ArgumentParseError(f"option '{arg}' multiple equal signs")
+        e = optionsMap[r[0]]
+        a = r[1]
+        # for integer arguments, attempt conversion
+        if isinstance(e[1], int):
+            try:
+                a = int(a)
+            except:
+                raise ArgumentParseError(f"option '{r[0]}' expected integer argument, got '{r[1]}'")
+        return (e[0], a), i+1
+
+
+# parse arguments given command line argv, map of option arguments to option names and default values
+# min/max other arguments
+# returns 2-tuple, (optionMapOut, otherArgsList)
+#   optionMapOut is a map of the option name to the value
+#   otherArgsList is list of non-pption arguments
+# throw ArgumentParseError if parse fails for any reason
+def parseArgs(argv, optionsMapIn, otherArgsMin, otherArgsMax):
+    nArg = len(argv)
+    i = 1
+    optionsMapOut = dict()
+    # populate options with default values
+    for k, v in optionsMapIn.items():
+        optionsMapOut[v[0]] = v[1]
+        if v[1] is None:
+            optionsMapOut[v[0]] = False
+
+    otherArgsList = []
+    while i < nArg:
+        arg = argv[i]
+        if arg.startswith("-"):
+            option, i = parseArgOption(argv, i, optionsMapIn)
+            optionsMapOut[option[0]] = option[1]
+        else:
+            otherArgsList.append(arg)
+            i += 1
+
+    nOtherArgs = len(otherArgsList)
+    if nOtherArgs > otherArgsMax:
+        raise ArgumentParseError(f"too many non-option arguments supplied, got {nOtherArgs}, max is {otherArgsMax}")
+    if nOtherArgs < otherArgsMin:
+        raise ArgumentParseError(f"not enough non-option arguments supplied, got {nOtherArgs}, min is {otherArgsMin}")
+
+    return optionsMapOut, otherArgsList
 
 
 def getJSONResp(conn, rpc, req_body="", exitOnError=True):
@@ -42,55 +141,7 @@ def getJSONResp(conn, rpc, req_body="", exitOnError=True):
     return json_data
 
 
-if __name__ == "__main__":
-    rpc_endpt = "127.0.0.1:8888"
-    nArg = len(sys.argv)
-    if nArg > 5:
-        print(f"ERROR At most four arguments expected, got {nArg}")
-        print(USAGE)
-        exit(1)
-    if nArg > 1:
-        arg = sys.argv[1]
-        if arg.startswith('-'):
-            if arg == '--help':
-                print(USAGE)
-                print("\n" + HELP_INFO)
-                exit(0)
-            else:
-                print(f"ERORR: Unknown option '{arg}'")
-                print(USAGE)
-                exit(1)
-
-        rpc_endpt = arg
-
-    page_size = 1024
-    if len(sys.argv) > 2:
-        page_size = int(sys.argv[2])
-        if page_size < 1:
-            print("ERROR: page_size must be > 0")
-            print(USAGE)
-            exit(1)
-    scope_limit = 10
-    if len(sys.argv) > 3:
-        scope_limit = int(sys.argv[3])
-        if scope_limit < 1:
-            print("ERROR: scope_limit must be > 0")
-            print(USAGE)
-            exit(1)
-    table_row_limit = 10
-    if len(sys.argv) > 4:
-        table_row_limit = int(sys.argv[4])
-        if table_row_limit < 1:
-            print("ERROR: table_row_limit must be > 0")
-            print(USAGE)
-            exit(1)
-
-    # get the server info
-    conn = http.client.HTTPConnection(rpc_endpt)
-    server_info_begin = getJSONResp(conn, "/v1/chain/get_info")
-
-    # get all accounts on the chain
-    limit = page_size
+def getAllAccounts(limit):
     moreAccounts = True
     all_accts = []
     req_body = '{"limit":' + str(limit) + '}'
@@ -107,8 +158,173 @@ if __name__ == "__main__":
         if moreAccounts:
             nextAcct = accts['more']
             req_body = '{"limit":' + str(limit) + f', "lower_bound":"{nextAcct}"' + '}'
+    return all_accts
 
-    if len(all_accts) == 0:
+
+def compareAccountNames(ref, cmp):
+    mismatch = []
+    for nm in ref:
+        if nm not in cmp:
+            mismatch.append((nm, '(none)'))
+    for nm in cmp:
+        if nm not in ref:
+            mismatch.append(('(none)', nm))
+    return mismatch
+
+
+def compareAccountsField(ref, cmp, fieldName, subFieldName=None):
+    mismatches = []
+    for nm, ref_acct in ref.items():
+        if nm in cmp:
+            ref_v = ref_acct[fieldName]
+            if subFieldName is not None:
+                ref_v = ref_v[subFieldName]
+            cmp_v = cmp[nm][fieldName]
+            if subFieldName is not None:
+                cmp_v = cmp_v[subFieldName]
+            if ref_v != cmp_v:
+                mismatches.append((nm, ref_v, cmp_v))
+    return mismatches
+
+
+def compareServerInfo(ref, cmp):
+    fields = ('server_version', 'chain_id', 'head_block_producer', 'virtual_block_cpu_limit',
+              'virtual_block_net_limit', 'block_cpu_limit', 'block_net_limit', 'server_version_string',
+              'server_full_version_string')
+    mismatches = []
+    for f in fields:
+        if ref[f] != cmp[f]:
+            mismatches.append((f, ref[f], cmp[f]))
+    return mismatches
+
+
+def compareProtocolFeatures(ref, cmp):
+    fieldNames = ('feature_digest', 'activation_ordinal', 'description_digest', 'dependencies',
+                  'protocol_feature_type', 'specification')
+    mismatches = []
+    for digest, ref_feat in ref.items():
+        if digest in cmp:
+            cmp_feat = cmp[digest]
+            for f in fieldNames:
+                if ref_feat[f] != cmp_feat[f]:
+                    mismatches.append((f"digest '{digest}' field '{f}'", ref_feat[f], cmp_feat[f]))
+        else:
+            mismatches.append(('digest', digest, '(none)'))
+    return mismatches
+
+
+def compareDeferredTrx(ref, cmp):
+    mismatches = []
+    if ref != cmp:
+        mismatches.append("transactions", ref, cmp)
+    return mismatches
+
+
+def compareProduceSchedules(ref, cmp):
+    mismatches = []
+    for s in ('active', 'pending', 'proposed'):
+        if ref[s] != cmp[s]:
+            mismatches.append(s, ref[s], cmp[s])
+    return mismatches
+
+
+def compareRefData(refData, cmpData):
+    # build dictionary of account names to ease in comparison
+    refAccts = dict()
+    for a in refData['accounts']:
+        refAccts[a['name']] = a
+    cmpAccts = dict()
+    for a in cmpData['accounts']:
+        cmpAccts[a['name']] = a
+
+    # build dictionary of feature digest to protocol feature
+    refProtFeats = dict()
+    for feat in refData['activated_protocol_features']:
+        refProtFeats[feat['feature_digest']] = feat
+    cmpProtFeats = dict()
+    for feat in cmpData['activated_protocol_features']:
+        cmpProtFeats[feat['feature_digest']] = feat
+
+    mismatches = dict()
+    mismatches['account_names'] = compareAccountNames(refAccts, cmpAccts)
+    mismatches['accounts_privilege'] = compareAccountsField(refAccts, cmpAccts, 'metadata', 'privileged')
+    mismatches['accounts_hash'] = compareAccountsField(refAccts, cmpAccts, 'code_hash')
+    mismatches['accounts_scopes'] = compareAccountsField(refAccts, cmpAccts, 'scopes')
+    mismatches['accounts_tables'] = compareAccountsField(refAccts, cmpAccts, 'tables')
+    mismatches['accounts_kv_tables'] = compareAccountsField(refAccts, cmpAccts, 'kv_tables')
+    mismatches['accounts_permissions'] = compareAccountsField(refAccts, cmpAccts, 'metadata', 'permissions')
+    mismatches['producer_schedule'] = compareProduceSchedules(refData['producer_schedule'], cmpData['producer_schedule'])        
+    mismatches['deferred_trx'] = compareDeferredTrx(refData['deferred_transactions'], cmpData['deferred_transactions'])
+    mismatches['server_info'] = compareServerInfo(refData['server_info_begin'], cmpData['server_info_begin'])
+    mismatches['protocol_features'] = compareProtocolFeatures(refProtFeats, cmpProtFeats)
+
+    isSame = True
+    for k, v in mismatches.items():
+        if len(v) > 0:
+            isSame = False
+            print(f"ERROR: mismatch in '{k}'", file=sys.stderr)
+            for i in v:
+                print(f"  ref: {i[0]}  comp: {i[1]}", file=sys.stderr)
+    if isSame:
+        print("Reference comparison SUCCESS", file=sys.stderr)
+    else:
+        print("Reference comparison FAILURE - see errors above.", file=sys.stderr)
+    return isSame
+
+if __name__ == "__main__":
+    rpc_endpt = "127.0.0.1:8888"
+    optionsMap = {
+               "--comp" : ("comp-filepath", ""),
+               "--help" : ("help", None),
+                   "-o" : ("output-filepath", "blockchain_audit_data.json"),
+          "--page-size" : ("page-size", 1024), 
+                "--ref" : ("reference-filepath", ""),
+        "--scope-limit" : ("scope-limit", 10),
+    "--table-row-limit" : ("table-row-limit", 10)          
+                }
+
+    # parse options
+    optionsMap, otherArgLst = parseArgs(sys.argv, optionsMap, 0, 1)
+    if optionsMap['help']:
+        print(USAGE)
+        print(HELP_INFO)
+        exit(0)
+
+    if len(otherArgLst) > 0:
+        rpc_endpt = otherArgLst[0]
+
+    page_size = optionsMap['page-size']
+    scope_limit = optionsMap['scope-limit']
+    table_row_limit = optionsMap['table-row-limit']
+    output_filepath = optionsMap['output-filepath']
+    ref_filepath = optionsMap['reference-filepath']
+    comp_filepath = optionsMap['comp-filepath']
+
+    if comp_filepath != "":
+        if ref_filepath == "":
+            print("'--comp 'option must be used in combination with '--ref' option")
+            exit(1)
+        fComp = open(comp_filepath, "r")
+        s = fComp.read()
+        cmpData = json.loads(s)
+        fRef = open(ref_filepath, "r")
+        s = fRef.read()
+        refData = json.loads(s)
+        compSuccess = compareRefData(refData, cmpData)
+        if compSuccess:
+            exit(0)
+        exit(1)
+
+    # establish connection to nodeos
+    conn = http.client.HTTPConnection(rpc_endpt)
+
+    # get the server info
+    server_info_begin = getJSONResp(conn, "/v1/chain/get_info")
+
+    # get all accounts on the chain
+    all_accts = getAllAccounts(page_size)
+    numAccounts = len(all_accts)
+    if numAccounts == 0:
         print("Error, no accounts returned from get_all_accounts!", file=sys.stderr)
         exit(1)
 
@@ -164,6 +380,7 @@ if __name__ == "__main__":
         # get abi so we can determine kv_tables
         req_body = '{' + f'"account_name": "{nm}"' + '}'
         abi = getJSONResp(conn, "/v1/chain/get_abi", req_body)
+        a['kv_tables'] = dict()
         if 'abi' in abi:
             kv_tables = abi['abi']['kv_tables']
             for tbl_name, tbl_def in kv_tables.items():
@@ -205,6 +422,17 @@ if __name__ == "__main__":
 
     data['server_info_begin'] = server_info_begin
     data['server_info_end'] = server_info_end
+
+    # get all accounts again
+    all_accts_end = getAllAccounts(page_size)
+    numAccounts_end = len(all_accts_end)
+    if numAccounts != numAccounts_end:
+        msg = f"ERROR: Accounts added during audit. begin= {numAccounts} end= {numAccounts_end}"
+        print(msg, file=sys.stderr)
+        exit(1)
+    
+    if server_info_begin['head_block_num'] != server_info_end['head_block_num']:
+        print("WARNING: Audit data was collected from different blocks.  Data may be inconssitent.", file=sys.stderr)
 
     # print out results
     try:
@@ -340,5 +568,14 @@ if __name__ == "__main__":
         raise e
 
     # save results as json
-    with open("blockchain_audit_data.json", "wt") as f:
+    with open(output_filepath, "wt") as f:
         f.write(json.dumps(data))
+
+    if ref_filepath != "":
+        with open(ref_filepath, "r") as f:
+            s = f.read()         
+            refData = json.loads(s)
+            compSuccess = compareRefData(refData, data)
+            if compSuccess:
+                exit(0)
+            exit(1)
