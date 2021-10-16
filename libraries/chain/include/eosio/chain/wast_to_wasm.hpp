ef3408c7341574faa91d34b74674194617107cc4
Kevin Heifner <heifnerk@objectcomputing.com>
2021-10-16 08:34:22 -0500

    Remove no longer used wasm_to_wast

--- libraries/chain/include/eosio/chain/wast_to_wasm.hpp
+++ libraries/chain/include/eosio/chain/wast_to_wasm.hpp
@@ -5,7 +5,5 @@
 namespace eosio { namespace chain {
 
 std::vector<uint8_t> wast_to_wasm( const std::string& wast );
-std::string  wasm_to_wast( const std::vector<uint8_t>& wasm, bool strip_names );
-std::string  wasm_to_wast( const uint8_t* data, uint64_t size, bool strip_names, bool check_limits = true );
 
 } } /// eosio::chain
