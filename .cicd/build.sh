070c5c1624e459f4e077f68d15a621cf4a68fa39
Patrick Raphael <patrick.raphael@block.one>
2021-10-29 14:13:09 -0400

    oracle linux 8 add CMake flag back in, build script use single expression

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -34,6 +34,8 @@ else # Linux
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DLLVM_DIR=\"/opt/rh/llvm-toolset-7.0/root/usr/lib64/cmake/llvm\""
     elif [[ "$IMAGE_TAG" == 'ubuntu-18.04-unpinned' ]]; then
         CMAKE_EXTRAS="$CMAKE_EXTRAS -DCMAKE_CXX_COMPILER=\"clang++-7\" -DCMAKE_C_COMPILER=\"clang-7\" -DLLVM_DIR=\"/usr/lib/llvm-7/lib/cmake/llvm\""
+    elif [[ "$IMAGE_TAG" == oracle_linux-8* ]]; then
+-        CMAKE_EXTRAS="$CMAKE_EXTRAS -DPostgreSQL_TYPE_INCLUDE_DIR=/usr/local/include/postgresql"
     fi
     if [[ "$IMAGE_TAG" == centos-7.* ]]; then
         PRE_COMMANDS="$PRE_COMMANDS && source /opt/rh/rh-python36/enable"
