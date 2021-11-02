7e7b555b041c3892c82179da5260d6b9ad3efd6e
Farhad Shahabi <farhad.shahabi@bullish.com>
2021-11-01 23:03:14 -0400

    Update rodeos_wasmQL_http_timeout.py
    New version of test

--- tests/rodeos_wasmQL_http_timeout.py
+++ tests/rodeos_wasmQL_http_timeout.py
@@ -6,6 +6,7 @@ from rodeos_utils import RodeosCluster
 from TestHelper import AppArgs
 import time
 import os
+import logging
 
 try: 
     import requests
@@ -22,20 +23,43 @@ except ImportError:
 ###############################################################
 # rodeos_wasmQL http time out test
 # 
-#   This test verifies timeout for wasmQL http connection through TCP/IP and unix socket works as expected. This test
-#   launches a cluster of 1 SHiP and 2 rodeos instances and verifies that the cluster is stable and rodeos's are receiving blocks.
-#   
-#
+#   This test verifies timeout for wasmQL http connection through TCP/IP and unix socket to rodeos. The scenario in this test
+#   is such that a persistent http connection is made to rodeos with timeout period of 5 second. Then we make 10 queries with a 
+#   delay of 2.5s between each query and the session must be alive as the connection was active within the timeout period. 
+#   Then the connection is kept in idle for the timeout period and rodeos must close out this idle session. Once we make another query
+#   to rodeos, request module must reset the dropped session and throws this action in its logs. Through this log, this test can verify that
+#   rodeos already closed out the idle session and new persistent connection has to be made again.
 ###############################################################
 
 Print=Utils.Print
 
 extraArgs=AppArgs()
 extraArgs.add_bool("--eos-vm-oc-enable", "Use OC for rodeos")
+extraArgs.add_bool("--unix-socket", "Enable unix socket")
 
 args=TestHelper.parse_args({"--dump-error-details","--keep-logs","-v","--leave-running","--clean-run"}, extraArgs)
 enableOC=args.eos_vm_oc_enable
+enableUnixSocket=args.unix_socket
 Utils.Debug=args.v
+
+class LogStream(object): # this class parse log outputs from request module
+    def __init__(self):
+        self.logs = []
+
+    def write(self, str):
+        if len(self.logs) > 0 and 'Resetting dropped connection' in self.logs[-1]: # Once persistence conmection is closed
+            self.logs[-1]+= str                                                    # request throws 'Resetting dropped connection' in logs
+        else:
+            self.logs.append(str)
+
+    def flush(self):
+        pass
+
+    def getlastlog(self):
+        if len(self.logs) > 0:
+            return self.logs[-1]
+        return 'No log record'
+
 
 TestHelper.printSystemInfo("BEGIN")
 testSuccessful=False
@@ -56,34 +80,40 @@ def launch_cluster(num_ships, num_rodeos, unix_socket_option, timeout, eos_vm_oc
             assert cluster.waitRodeosReady(i), "Rodeos failed to stand up for a cluster of {} ship node and {} rodeos node".format(num_ships, num_rodeos)
 
         rodeosId = 0 # making http connection to rodeosId = 0
+        log_stream = LogStream()
+        logging.basicConfig(stream=log_stream, level=logging.DEBUG, format="%(message)s")
 
         if unix_socket_option:
-            s = requests_unixsocket.Session()
+            s = requests_unixsocket.Session()  # Opening persistence connetion
             os.chdir(cluster.rodeosDir[rodeosId])
-            r = s.get('http+unix://rodeos{}.sock/v1/chain/get_info'.format(rodeosId))
+            for i in range(10):                # making 10 wasm queries to rodeos with a half timeout delay netween each query
+                r = s.get('http+unix://rodeos{}.sock/v1/chain/get_info'.format(rodeosId))
+                time.sleep(timeout // 2000)
+                assert r.status_code == 200, "Http request was not successful" # Http request must be successful
+                print(log_stream.getlastlog())
+                assert 'Resetting dropped connection: localhost' not in log_stream.getlastlog() # Connection made with timeout period so connection must be alive
         else:
             s = requests.Session()
-            r = s.get('{}v1/chain/get_info'.format(cluster.wqlEndPoints[rodeosId]))
-        
-        assert r.status_code == 200, "Http request was not successful"
+            for i in range(10):
+                r = s.get('{}v1/chain/get_info'.format(cluster.wqlEndPoints[rodeosId]))
+                time.sleep(timeout // 2000)
+                assert r.status_code == 200, "Http request was not successful"
+                print(log_stream.getlastlog())
+                assert 'Resetting dropped connection: localhost' not in log_stream.getlastlog()
 
-        time.sleep(timeout // 1000) # idle connection for timeout
-
-        try:
-            if unix_socket_option:
-                s.get('http+unix://rodeos{}.sock/v1/chain/get_info'.format(rodeosId))
-            else:
-                s.get('{}v1/chain/get_info'.format(cluster.wqlEndPoints[rodeosId]))
-        except requests.exceptions.RequestException:
-            # connection timeout exception to happen here.
-            pass
-        else:
-            if os.getcwd() != maincwd:
-                os.chdir(maincwd)
-            print('###### Error: WasmQL http connection timeout failed for {} ######'\
-                .format((lambda x: 'unix-socket' if (x==True) else 'TCP')(unix_socket_option)))
-            exit(1)
-            
+        time.sleep(timeout // 1000) # idle connection for timeout period and expect rodeos to close the http session
+                                    # should see a timeout warning in rodeos logs also
+        if unix_socket_option:
+            s.get('http+unix://rodeos{}.sock/v1/chain/get_info'.format(rodeosId)) # making new query 
+            assert r.status_code == 200, "Http request was not successful" # Http request must be successful
+            print(log_stream.getlastlog())
+        else: 
+            s.get('{}v1/chain/get_info'.format(cluster.wqlEndPoints[rodeosId]))
+            assert r.status_code == 200, "Http request was not successful" # Http request must be successful
+            print(log_stream.getlastlog())
+        assert 'Resetting dropped connection' in log_stream.getlastlog() # Rodeos have already closed out previous session due to timeout
+                                                                         # so request module has to open up a new session and throws 
+                                                                         # Resetting dropped connection in its logs 
         if os.getcwd() != maincwd:
             os.chdir(maincwd)
         cluster.setTestSuccessful(True)
@@ -91,10 +121,8 @@ def launch_cluster(num_ships, num_rodeos, unix_socket_option, timeout, eos_vm_oc
 # Test cases: (1 ships, 2 rodeos)
 numSHiPs=[1]
 numRodeos=[2]
-NumTestCase=len(numSHiPs)
-for i in [True, False]: # True means Unix-socket option, False means TCP/IP
-    for j in range(NumTestCase):
-        launch_cluster(numSHiPs[j], numRodeos[j], i, 5000, enableOC)
+
+launch_cluster(numSHiPs[0], numRodeos[0], enableUnixSocket, 5000, enableOC)
 
 testSuccessful=True
 errorCode = 0 if testSuccessful else 1
