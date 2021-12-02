0fd5f32054c4b44746471e4b759876976567f649
Henry Monti <henry.monti@block.one>
2021-12-02 11:19:07 -0500

    Update clang to 12

--- scripts/helpers/eosio.sh
+++ scripts/helpers/eosio.sh
@@ -285,17 +285,17 @@ function build-clang() {
                 export CMAKE_XTRA_FLAGS="-DCOMPILER_RT_INCLUDE_TESTS=0 -DCOMPILER_RT_BUILD_SANITIZERS=0 -DCOMPILER_RT_BUILD_XRAY=0"
             fi
             execute bash -c "cd ${TEMP_DIR} \
-            && rm -rf clang11 \
-            && git clone --single-branch --branch $PINNED_COMPILER_BRANCH https://github.com/llvm/llvm-project clang11 \
-            && cd clang11 \
+            && rm -rf clang12 \
+            && git clone --single-branch --branch $PINNED_COMPILER_BRANCH https://github.com/llvm/llvm-project clang12 \
+            && cd clang12 \
             && mkdir build && cd build \
             && ${CMAKE} -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX='${CLANG_ROOT}' -DLLVM_ENABLE_PROJECTS='lld;polly;clang;clang-tools-extra;libcxx;libcxxabi;libunwind;compiler-rt' -DLLVM_BUILD_LLVM_DYLIB=ON -DLLVM_ENABLE_RTTI=ON -DLLVM_INCLUDE_DOCS=OFF -DLLVM_TARGETS_TO_BUILD=host -DCMAKE_BUILD_TYPE=Release ${CMAKE_XTRA_FLAGS} ../llvm \
             && make -j${JOBS} \
             && make install"
-            echo " - Clang 11 successfully installed @ ${CLANG_ROOT}"
+            echo " - Clang 12 successfully installed @ ${CLANG_ROOT}"
             echo ""
         else
-            echo " - Clang 11 found @ ${CLANG_ROOT}"
+            echo " - Clang 12 found @ ${CLANG_ROOT}"
             echo ""
         fi
         export CXX=$CPP_COMP
