64ae8ebc99afb90e3746c7efd4798410d85ea630
Lin Huang <lin.huang@block.one>
2021-12-03 09:12:23 -0500

    Make total_cpu_weight and total_net_weight optional so that different versions of cleos and nodoes can work

--- plugins/chain_plugin/chain_plugin.cpp
+++ plugins/chain_plugin/chain_plugin.cpp
@@ -1741,8 +1741,6 @@ read_only::get_info_results read_only::get_info(const read_only::get_info_params
       db.head_block_id(),
       db.head_block_time(),
       db.head_block_producer(),
-      rm.get_total_cpu_weight(),
-      rm.get_total_net_weight(),
       rm.get_virtual_block_cpu_limit(),
       rm.get_virtual_block_net_limit(),
       rm.get_block_cpu_limit(),
@@ -1753,7 +1751,9 @@ read_only::get_info_results read_only::get_info(const read_only::get_info_params
       db.fork_db_pending_head_block_num(),
       db.fork_db_pending_head_block_id(),
       app().full_version_string(),
-      db.last_irreversible_block_time()
+      db.last_irreversible_block_time(),
+      rm.get_total_cpu_weight(),
+      rm.get_total_net_weight()
    };
 }
 
