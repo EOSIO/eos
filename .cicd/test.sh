293eb948ac35ba5f7e6518f9ace0c4a094a063b3
eyobedT <91893582+eyobedT@users.noreply.github.com>
2021-12-03 12:16:44 -0500

    Update test.sh
    changing sleep command to fix substitution error on building

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -3,7 +3,6 @@ set -eo pipefail
 echo '+++ :guide_dog: New Job Ensurer'
 echo 'These steps are load testing the new Buildkite job ensurer:'
 printf "\033]1339;url=https://github.com/EOSIO/auto-buildkite-job-runner;content=https://github.com/EOSIO/auto-buildkite-job-runner\a\n"
-export SLEEP_COMMAND="sleep ${$SLEEP:-60}"
-echo "$ $SLEEP_COMMAND"
-eval $SLEEP_COMMAND
+echo 'sleeping for 60s'
+sleep 60
 echo '--- :white_check_mark: Done!'
