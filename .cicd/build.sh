0131f8cf955245831e712e5ada019b424e3fd0c2
Zach Butler <zach.butler@block.one>
2021-12-06 12:40:09 -0500

    Just use the TIMEOUT variable for the sleep duration instead of SLEEP because it has to be set anyways

--- .cicd/build.sh
+++ .cicd/build.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 echo '+++ :ladybug: Buildfleet Debugging'
 echo 'We are hanging Buildkite agents to debug the "Agent Lost" error.'
 printf "See \033]1339;url=https://blockone.atlassian.net/browse/BLU-27876;content=BLU-27876\a for more information.\n"
-SLEEP_COMMAND="sleep ${SLEEP:-60}"
+SLEEP_COMMAND="sleep '$(( ${TIMEOUT:-1} * 60 ))'"
 echo "$ $SLEEP_COMMAND"
 eval $SLEEP_COMMAND
 echo '--- :white_check_mark: Done!'
