31eae11440cd1ed7e483a8d2a72c60798b4f2d64
Farhad Shahabi <farhad.shahabi@bullish.com>
2021-12-15 17:53:25 -0500

    Update macos-11-pinned.sh

--- .cicd/platforms/pinned/macos-11-pinned.sh
+++ .cicd/platforms/pinned/macos-11-pinned.sh
@@ -12,13 +12,14 @@ make -j $(getconf _NPROCESSORS_ONLN)
 sudo make install
 cd ../..
 rm -rf clang10
+# install boost from source
+curl -LO https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.bz2
+tar -xjf boost_1_72_0.tar.bz2
 # set enviroment variables
 export LIBRARY_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib:$LIBRARY_PATH
 export CPLUS_INCLUDE_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include:$CPLUS_INCLUDE_PATH
 export CPLUS_INCLUDE_PATH=/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1:$CPLUS_INCLUDE_PATH
-# install boost from source
-curl -LO https://boostorg.jfrog.io/artifactory/main/release/1.72.0/source/boost_1_72_0.tar.bz2
-tar -xjf boost_1_72_0.tar.bz2
+echo $CPLUS_INCLUDE_PATH
 cd boost_1_72_0
 ./bootstrap.sh --prefix=/usr/local
 sudo -E ./b2 --with-iostreams --with-date_time --with-filesystem --with-system --with-program_options --with-chrono --with-test -q -j$(getconf _NPROCESSORS_ONLN) install
