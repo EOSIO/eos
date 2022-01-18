b09f9c42dcf992c3d42d273223fbeac79892cdf4
Steven Watanabe <steven@clarionos.com>
2022-01-18 12:14:13 -0500

    Use different exception codes for different exceptions

--- libraries/chain/include/eosio/chain/exceptions.hpp
+++ libraries/chain/include/eosio/chain/exceptions.hpp
@@ -548,7 +548,7 @@ namespace eosio { namespace chain {
       FC_DECLARE_DERIVED_EXCEPTION( wasm_config_unknown_version,          contract_exception,
                                     3160015, "Unknown wasm_config version" )
       FC_DECLARE_DERIVED_EXCEPTION( config_parse_error,                   contract_exception,
-                                    3160015, "Parsing config error" )
+                                    3160016, "Parsing config error" )
 
    FC_DECLARE_DERIVED_EXCEPTION( producer_exception,           chain_exception,
                                  3170000, "Producer exception" )
