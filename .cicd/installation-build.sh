045aced6b5c7d58d52575815f9ce4555f32e52a5
Farhad Shahabi <farhad.shahabi@block.one>
2022-01-20 14:31:02 -0500

    Update installation-build.sh

--- .cicd/installation-build.sh
+++ .cicd/installation-build.sh
@@ -19,10 +19,14 @@ eval $BUILD_COMMAND
 echo '+++ :arrow_up: Pushing Container'
 for REGISTRY in "${CONTRACT_REGISTRIES[@]}"; do
     if [[ ! -z "$REGISTRY" ]]; then
-        COMMITS=("$REGISTRY:base-ubuntu-18.04-$BUILDKITE_COMMIT-$PLATFORM_TYPE")
-        # Platform agnostic elements should be unpinned
-        if [[ "$PLATFORM_TYPE" == 'unpinned' ]] ; then
-            COMMITS=(${COMMITS[@]} "$REGISTRY:base-ubuntu-18.04-$BUILDKITE_COMMIT" "$REGISTRY:base-ubuntu-18.04-$SANITIZED_BRANCH-$BUILDKITE_COMMIT")
+        if [[ "$BUILDKITE_PIPELINE_SLUG" == 'eosio-contract-build' ]]; then
+            COMMITS=("$REGISTRY:base-$UBUNTU_VERSION-$BUILDKITE_COMMIT" "$REGISTRY:base-$UBUNTU_VERSION-$BUILDKITE_COMMIT-$PLATFORM_TYPE" "$REGISTRY:base-$UBUNTU_VERSION-$SANITIZED_BRANCH-$BUILDKITE_COMMIT")
+        else
+            COMMITS=("$REGISTRY:base-ubuntu-18.04-$BUILDKITE_COMMIT-$PLATFORM_TYPE")
+            # Platform agnostic elements should be unpinned
+            if [[ "$PLATFORM_TYPE" == 'unpinned' ]] ; then
+                COMMITS=(${COMMITS[@]} "$REGISTRY:base-ubuntu-18.04-$BUILDKITE_COMMIT" "$REGISTRY:base-ubuntu-18.04-$SANITIZED_BRANCH-$BUILDKITE_COMMIT")
+            fi
         fi
         for COMMIT in "${COMMITS[@]}"; do
             COMMIT_COMMAND="docker commit 'ci-contracts-builder-$BUILDKITE_PIPELINE_SLUG-$BUILDKITE_BUILD_NUMBER-$UBUNTU_VERSION' '$COMMIT'"
