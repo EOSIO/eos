6ad064aa0462b60bdd83a7585bcc5e1cf5e5ad8b
Jingjun Zhao <jingjun.zhao@block.one>
2022-01-29 09:16:03 -0500

    Disable HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK

--- .cicd/test-package.run.sh
+++ .cicd/test-package.run.sh
@@ -19,15 +19,11 @@ elif [[ $(brew --version 2>/dev/null) ]]; then # homebrew packaging
     perform 'cp *.rb homebrew-eosio'
     perform "sed -i.bk -e 's/url \".*\"/url \"http:\/\/127.0.0.1:7800\"/' homebrew-eosio/*.rb"
     perform "pushd homebrew-eosio && git add *.rb && git commit -m 'test it!' && popd"
+    perform 'export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=false'
     perform "brew tap eosio/eosio homebrew-eosio"
     perform '{ python3 -m http.server 7800 & } && export HTTP_SERVER_PID=$!'
     perform 'sleep 20s'
     perform 'brew install eosio'
-    ret=$?
-    if [[ "$ret" -ne 0 ]]; then
-      echo "Try installing eosio again"
-      perform 'brew install eosio'
-    fi
     perform 'kill $HTTP_SERVER_PID'
 else
     echo 'ERROR: Package manager not detected!'
