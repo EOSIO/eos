7aeb897ee802f42776664235872569b3dddce712
Zach Butler <zach.butler@block.one>
2022-02-02 14:31:30 -0500

    Fix indentation

--- .cicd/helpers/file-hash.sh
+++ .cicd/helpers/file-hash.sh
@@ -5,6 +5,6 @@ FILE_NAME="$(basename "$1" | awk '{split($0,a,/\.(d|s)/); print a[1] }')"
 export DETERMINED_HASH=$(sha1sum "$1" | awk '{ print $1 }')
 export HASHED_IMAGE_TAG="eos-${FILE_NAME}-${DETERMINED_HASH}"
 if [[ -n "${REGISTRY_BASE:-''}" ]] ; then
-  export FULL_TAG="$REGISTRY_BASE:$HASHED_IMAGE_TAG"
+    export FULL_TAG="$REGISTRY_BASE:$HASHED_IMAGE_TAG"
 fi
 set +u
