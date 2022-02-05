ac221437744ec2b0a35b373955ec71540d2bc029
Jeeyong Um <conr2d@gmail.com>
2022-02-05 12:55:51 +0800

    add description to recover_key_safe error codes

--- libraries/chain/include/eosio/chain/webassembly/error_codes.hpp
+++ libraries/chain/include/eosio/chain/webassembly/error_codes.hpp
@@ -3,13 +3,13 @@
 namespace eosio { namespace chain { namespace webassembly { namespace error_codes {
 
    enum recover_key_safe : int32_t {
-      undefined = -1,
-      none = 0,
-      invalid_message_digest,
-      invalid_signature_format,
-      unactivated_key_type,
-      invalid_signature_data,
-      insufficient_output_buffer,
+      undefined = -1, ///< undefined error
+      none = 0, ///< succeed
+      invalid_message_digest, ///< failed to deserialize a message digest
+      invalid_signature_format, ///< failed to deserialize a signature
+      unactivated_key_type, ///< failed to recover a key using unactivated key algorithm
+      invalid_signature_data, ///< failed to recover a key from the given signature data
+      insufficient_output_buffer, ///< failed to store a recovered key due to insufficient output buffer
    };
 
 }}}} // ns eosio::chain::webassembly::error_codes
