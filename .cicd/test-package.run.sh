bafd72d273587ffaa3d925ef28de338ee5f260c0
Jingjun Zhao <jingjun.zhao@block.one>
2022-02-07 11:31:36 -0500

    Add brew upgrade

--- .cicd/test-package.run.sh
+++ .cicd/test-package.run.sh
@@ -13,13 +13,13 @@ elif [[ $(yum --version 2>/dev/null) ]]; then # RHEL family packaging
     perform 'yum install -y /eos/*.rpm'
 elif [[ $(brew --version 2>/dev/null) ]]; then # homebrew packaging
     perform 'brew update'
+    perform 'brew upgrade'
     perform 'brew --version'
     perform 'mkdir homebrew-eosio'
     perform 'git init homebrew-eosio'
     perform 'cp *.rb homebrew-eosio'
     perform "sed -i.bk -e 's/url \".*\"/url \"http:\/\/127.0.0.1:7800\"/' homebrew-eosio/*.rb"
     perform "pushd homebrew-eosio && git add *.rb && git commit -m 'test it!' && popd"
-    perform 'export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=false'
     perform "brew tap eosio/eosio homebrew-eosio"
     perform '{ python3 -m http.server 7800 & } && export HTTP_SERVER_PID=$!'
     perform 'sleep 20s'
